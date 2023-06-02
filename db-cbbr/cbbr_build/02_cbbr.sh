2f85b68be4c1495d98621b40dd7dbf374e785a9d
Finn van Krieken <finnvankrieken@gmail.com>
2023-06-02 20:21:15 +0000

    first steps moved to common bash functionality

--- db-cbbr/cbbr_build/02_cbbr.sh
+++ db-cbbr/cbbr_build/02_cbbr.sh
@@ -4,18 +4,18 @@ source config.sh
 echo "CBBR Version $VERSION : 02 CBBR"
 # TODO delete tables that are created by this stage
 echo "Create build tables to modify ..."
-run_sql sql/preprocessing.sql
+run_sql_file sql/preprocessing.sql
 
 ## Skipping for dev of initial FY2024 build
 # psql $BUILD_ENGINE -f sql/apply_agency_updates.sql
 
 echo "Normalize agency values ..."
-run_sql sql/normalize_agency.sql
+run_sql_file sql/normalize_agency.sql
 
 echo "Normalize commdist values ..."
-run_sql sql/normalize_commdist.sql
+run_sql_file sql/normalize_commdist.sql
 
 echo "Normalize denominator values ..."
-run_sql sql/normalize_denominator.sql
+run_sql_file sql/normalize_denominator.sql
 
 echo "Done!"
