93d559286e8d319ce5fda168d804500d54432b7b
Finn van Krieken <fvankrieken@planning.nyc.gov>
2023-06-07 19:00:12 +0000

    update old function call

--- bash_utils/config.sh
+++ bash_utils/config.sh
@@ -183,7 +183,7 @@ function shp_export {
 
 
 function fgdb_export {
-    urlparse ${BUILD_ENGINE}
+    parse_connection_string ${BUILD_ENGINE}
     table=${1}
     geomtype=${2}
     name=${3:-${table}}
