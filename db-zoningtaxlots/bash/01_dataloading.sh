4df50c367f2e629d870a27b717eff71485c28b2c
Finn van Krieken <finnvankrieken@gmail.com>
2023-06-08 17:45:57 +0000

    first draft of ztl conversion to shared utils

--- db-zoningtaxlots/bash/01_dataloading.sh
+++ db-zoningtaxlots/bash/01_dataloading.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-set -e
+source ../bash_utils/config.sh
 source bash/config.sh
+set_env ../.env
+set_error_traps
 
-psql $BUILD_ENGINE -v ON_ERROR_STOP=1 -q -c "
+run_sql_command "
   DROP TABLE IF EXISTS versions;
   CREATE TABLE versions ( 
     datasource text, 
@@ -11,24 +13,24 @@ psql $BUILD_ENGINE -v ON_ERROR_STOP=1 -q -c "
 "
 
 # Import Data
-import dcp_commercialoverlay &
-import dcp_limitedheight &
-import dcp_specialpurpose &
-import dcp_specialpurposesubdistricts &
-import dcp_zoningmapamendments &
-import dof_dtm &
-import dcp_zoningdistricts &
-import dcp_zoningmapindex &
+import_recipe dcp_commercialoverlay &
+import_recipe dcp_limitedheight &
+import_recipe dcp_specialpurpose &
+import_recipe dcp_specialpurposesubdistricts &
+import_recipe dcp_zoningmapamendments &
+import_recipe dof_dtm &
+import_recipe dcp_zoningdistricts &
+import_recipe dcp_zoningmapindex &
 wait
 
 rm -rf .library
 
 # Generate source_data_versions table
-psql $BUILD_ENGINE -v ON_ERROR_STOP=1 -1 -c "
+run_sql_command "
   DROP TABLE IF EXISTS source_data_versions;
   SELECT 
     datasource as schema_name, 
     version as v
   INTO source_data_versions
   FROM versions;
-"
+" -1
