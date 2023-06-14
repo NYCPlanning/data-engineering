c8f6b2f191f490460e8a499e6db3bfcd6adf483b
Finn van Krieken <fvankrieken@planning.nyc.gov>
2023-06-14 14:13:42 +0000

    add psql

--- db-pluto/pluto_build/bin/setup.sh
+++ db-pluto/pluto_build/bin/setup.sh
@@ -2,7 +2,7 @@
 
 function setup {
     apt-get update
-    apt-get install -y curl zip
+    apt-get install -y curl zip postgresql
     #wget http://download.osgeo.org/gdal/3.6.4/gdal-3.6.4.tar.gz
     #tar -xvzf gdal-3.6.4.tar.gz && (
     #    cd gdal-3.6.4
