e272d04c5c4f8119c785514ae3a5eeed3eb601a2
Finn van Krieken <fvankrieken@planning.nyc.gov>
2023-06-14 13:24:27 +0000

    add ubuntugis as apt repository to get latest gdal

--- db-pluto/pluto_build/bin/setup.sh
+++ db-pluto/pluto_build/bin/setup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 function setup {
-sudo apt update
-sudo apt install -y curl zip
+    sudo add-apt-repository ppa:ubuntugis/ppa && sudo apt-get update
+    sudo apt-get install -y curl zip
+    sudo apt-get install -y --no-install-recommends gdal-bin
 
-curl -O https://dl.min.io/client/mc/release/linux-amd64/mc
-chmod +x mc
-sudo mv ./mc /usr/bin
-mc config host add spaces $AWS_S3_ENDPOINT $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY --api S3v4
-sudo apt-get -y install --no-install-recommends gdal-bin
-echo $(ogrinfo --version)
+    curl -O https://dl.min.io/client/mc/release/linux-amd64/mc
+    chmod +x mc
+    sudo mv ./mc /usr/bin
+    mc config host add spaces $AWS_S3_ENDPOINT $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY --api S3v4
 }
 register 'setup' 'init' 'install all dependencies' setup
