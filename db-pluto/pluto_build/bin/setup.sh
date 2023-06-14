7f5a44f1aa5176d3ece5a6cdb97f7f44d97c6d6a
Finn van Krieken <fvankrieken@planning.nyc.gov>
2023-06-14 14:05:47 +0000

    use gdal image in job

--- db-pluto/pluto_build/bin/setup.sh
+++ db-pluto/pluto_build/bin/setup.sh
@@ -3,15 +3,15 @@
 function setup {
     sudo apt-get update
     sudo apt-get install -y curl zip
-    wget http://download.osgeo.org/gdal/3.6.4/gdal-3.6.4.tar.gz
-    tar -xvzf gdal-3.6.4.tar.gz && (
-        cd gdal-3.6.4
-
-        ./configure --prefix=/usr 
-        make
-    )
-    rm -rf gdal-3.6.4
-    rm gdal-3.6.4.tar.gz
+    #wget http://download.osgeo.org/gdal/3.6.4/gdal-3.6.4.tar.gz
+    #tar -xvzf gdal-3.6.4.tar.gz && (
+    #    cd gdal-3.6.4
+#
+#        ./configure --prefix=/usr 
+#        make
+#    )
+#    rm -rf gdal-3.6.4
+#    rm gdal-3.6.4.tar.gz
 
     curl -O https://dl.min.io/client/mc/release/linux-amd64/mc
     chmod +x mc
