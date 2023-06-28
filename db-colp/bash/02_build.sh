eea375ea157f2f020280d278c0e5e1ca50f802b2
Finn van Krieken <fvankrieken@planning.nyc.gov>
2023-06-28 16:27:51 +0000

    use base docker image for devcontainer and select github actions

--- db-colp/bash/02_build.sh
+++ db-colp/bash/02_build.sh
@@ -6,14 +6,8 @@ run_sql_file sql/load_modifications.sql
 
 run_sql_file sql/geo_inputs.sql
 
-docker run --rm\
-    --network host\
-    -v `pwd`:/home/colp_build/\
-    -w /home/colp_build\
-    -u $(id -u ${USER}):$(id -g ${USER}) \
-    -e BUILD_ENGINE=${BUILD_ENGINE}\
-    nycplanning/docker-geosupport:latest bash -c "python3 -m python.geocode;
-                                                  python3 -m python.geo_qaqc"
+python3 -m python.geocode
+python3 -m python.geo_qaqc"
 
 run_sql_file sql/_procedures.sql
 run_sql_file sql/clean_parcelname.sql
