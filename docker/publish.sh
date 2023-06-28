4cb7f280eb8098219716777c4d8911a28590ace5
Finn van Krieken <fvankrieken@planning.nyc.gov>
2023-06-28 15:43:09 +0000

    first attempt at docker publish action

---
+++ docker/publish.sh
@@ -0,0 +1,21 @@
+set -e
+
+
+case $1 in
+    base) echo "Publishing '${1}' image" ;;
+    *) 
+        echo "${command} not found" 
+        exit 1;;
+esac
+
+cp $1 Dockerfile
+
+DOCKER_IMAGE_NAME=nycplanning/
+
+echo "$DOCKER_PASSWORD" | docker login -u $DOCKER_USER --password-stdin
+# Build image - Once we reach some sort of MVP, maybe worth starting versioning. For now, just latest
+docker build \
+    --tag $DOCKER_IMAGE_NAME:latest .
+docker push $DOCKER_IMAGE_NAME:latest
+
+rm Dockerfile
