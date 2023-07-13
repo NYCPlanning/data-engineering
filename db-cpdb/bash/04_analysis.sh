a265d1ba4fbcfe09a5e8120792d376096ef6c005
fvankrieken <fvankrieken@planning.nyc.gov>
2023-07-13 19:08:56 +0000

    move analysis sql into sql folder

--- db-cpdb/bash/04_analysis.sh
+++ db-cpdb/bash/04_analysis.sh
@@ -3,22 +3,22 @@ source bash/config.sh
 
 # Summary table by managing and sponsor agency
 echo 'Creating summary tables by managing and sponsor agency'
-run_sql_file analysis/projects_dollars_mapped_categorized_managing.sql
-run_sql_file analysis/projects_dollars_mapped_categorized_sponsor.sql
+run_sql_file sql/analysis/projects_dollars_mapped_categorized_managing.sql
+run_sql_file sql/analysis/projects_dollars_mapped_categorized_sponsor.sql
 
 # Planned commitments by community district
 echo 'Creating reports by community district'
-run_sql_file analysis/projects_by_communitydist.sql
-run_sql_file analysis/projects_by_communitydist_spending.sql
-run_sql_file analysis/projects_by_communitydist_spending_date.sql
+run_sql_file sql/analysis/projects_by_communitydist.sql
+run_sql_file sql/analysis/projects_by_communitydist_spending.sql
+run_sql_file sql/analysis/projects_by_communitydist_spending_date.sql
 
 # # Agency validated geoms summary table
 echo 'Creating agency validated geoms summary table'
-run_sql_file analysis/agency_validated_geoms_summary_table.sql
+run_sql_file sql/analysis/agency_validated_geoms_summary_table.sql
 
 # Geospatial check 
 echo 'Creating geospatial check table'
-run_sql_file analysis/geospatial_check.sql -v ccp_v=$ccp_v 
+run_sql_file sql/analysis/geospatial_check.sql -v ccp_v=$ccp_v 
 
 mkdir -p $(pwd)/output/analysis && (
     cd $(pwd)/output/analysis
