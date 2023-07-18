6fa8c6ae8f48e0dc8ea4237c92572c40b9548194
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2023-07-18 10:15:52 -0400

    Refactor Poetry out of facdb
    Removes Poetry, and other unused cruft, and adds a shared library
    (`dcpy`)

--- db-facilities/facdb/__init__.py
+++ db-facilities/facdb/__init__.py
@@ -1,8 +1,18 @@
-from .geocode.function1B import Function1B
-from .geocode.functionBL import FunctionBL
-from .geocode.functionBN import FunctionBN
-from .geocode.parseAddress import ParseAddress, UseAirportName
-from .utility.executeSQL import ExecuteSQL
-from .utility.export import Export
-from .utility.metadata import dump_metadata
-from .utility.prepare import Prepare
+import os
+from pathlib import Path
+from dotenv import dotenv_values
+
+
+_module_top_path = Path(__file__).resolve().parent
+_config = dotenv_values(_module_top_path.parent / "version.env")
+
+VERSION_PREV = _config["VERSION_PREV"]
+
+SQL_PATH = _module_top_path / "sql"
+BASH_PATH = _module_top_path / "bash"
+CACHE_PATH = _module_top_path.parent / ".cache"
+
+BASE_URL = "https://nyc3.digitaloceanspaces.com/edm-recipes/datasets"
+
+BUILD_ENGINE = os.environ.get("BUILD_ENGINE")
+assert BUILD_ENGINE
