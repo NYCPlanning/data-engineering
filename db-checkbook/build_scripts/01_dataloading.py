dad02c7c6141fefa0d44afa06b3648d24aaa1918
Damon McCullough <damonmcc1391@gmail.com>
2023-07-18 13:33:57 -0400

    autoformat with python package black

--- db-checkbook/build_scripts/01_dataloading.py
+++ db-checkbook/build_scripts/01_dataloading.py
@@ -4,9 +4,12 @@ from dcpy.connectors.s3 import client
 def do_stuff() -> None:
     print("doing stuff")
     s3_client = client()
-    available_buckets = [bucket["Name"] for bucket in s3_client.list_buckets()["Buckets"]]
+    available_buckets = [
+        bucket["Name"] for bucket in s3_client.list_buckets()["Buckets"]
+    ]
     print(f"This S3 client is a {type(s3_client)}")
     print(f"This S3 client has access to buckets: {available_buckets}")
+
 
 if __name__ == "__main__":
     print("started dataloading ...")
