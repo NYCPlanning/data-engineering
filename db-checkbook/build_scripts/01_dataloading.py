19c00b125ec3781d8d3b904fc9b0bef0dd698c62
Dea Bardhoshi <dea.bardhoshi@berkeley.edu>
2023-07-19 12:11:17 -0400

    Adding reading function for private files

--- db-checkbook/build_scripts/01_dataloading.py
+++ db-checkbook/build_scripts/01_dataloading.py
@@ -1,27 +1,32 @@
 from dcpy.connectors import s3
+import json
+from functools import wraps
+from pathlib import Path
+
 import pandas as pd
 
 BASE_BUCKET = 'edm-recipes'
 BASE_URL = "https://edm-recipes.nyc3.cdn.digitaloceanspaces.com"
 
 
-
-def read_s3_edm_recipes_cpdb(version, type_geom, save_file_path):
-    """read EDM data: using S3 connectors
-    example: datasets/dcp_cpdb/2018_adopted_polygons/
-    version: 2018_adopted, 2019_adopted, 2020_adopted, 2021_adopted, 2022_adopted, 2023_executive
-    type_geom: _polygons, _points 
-    """
-    digital_ocean_filepath = f'datasets/dcp_cpdb/{version}{type_geom}/dcp_cpdb.shp'
-    s3.client().download_file(BASE_BUCKET, digital_ocean_filepath, save_file_path)
-    return save_file_path
+import os
+import boto3
 
 
-def read_edm_recipes_nyc_checkbook(version = "latest"):
-    """filepath: datasets/nycoc_checkbook/latest/nycoc_checkbook.csv 
-    """
-    file_name = f'{BASE_URL}/datasets/nycoc_checkbook/{version}/nycoc_checkbook.csv'
-    df = pd.read_csv(file_name, dtype=str, index_col=False)
+
+def read_s3(digital_ocean_filepath, save_file_path):
+
+    #digital_ocean_filepath example "datasets/bpl_libraries/20210208/bpl_libraries.csv"
+
+    s3_client = s3.client()
+    file = s3_client.download_file('edm-recipes', digital_ocean_filepath, save_file_path)
+    return file
+
+# reading in the checkboob daa
+def read_from_csv(name: str) -> pd.DataFrame:
+    df = pd.read_csv(
+        "https://edm-recipes.nyc3.cdn.digitaloceanspaces.com/datasets/test_nypl_libraries/20210122/test_nypl_libraries.csv", dtype=str, index_col=False
+    )
     return df
 
 
