e45e92b4b2262a0693b1f7271b7e061f2428538e
fvankrieken <fvankrieken@planning.nyc.gov>
2023-07-20 14:23:23 +0000

    pull out shared utils, use dcpy utils for s3 interaction in pff pipelines

--- db-factfinder/pipelines/__init__.py
+++ db-factfinder/pipelines/__init__.py
@@ -1,13 +1,18 @@
 import os
+import sys
 from pathlib import Path
 
 from dotenv import load_dotenv
 
-load_dotenv()
-
-API_KEY = os.environ["API_KEY"]
-ROOT_PATH = Path(__file__).parent.parent
-BASE_PATH = ROOT_PATH / ".output"
+PRODUCT_PATH = Path(__file__).parent.parent
+ROOT_PATH = PRODUCT_PATH.parent
+BASE_PATH = PRODUCT_PATH / ".output"
 
 if not os.path.isdir(BASE_PATH):
     os.makedirs(BASE_PATH, exist_ok=True)
+
+load_dotenv(ROOT_PATH / ".env")
+
+sys.path.append(str(ROOT_PATH))
+
+API_KEY = os.environ["API_KEY"]
