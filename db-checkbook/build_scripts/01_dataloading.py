28933452169c0a34aa3b2f40288fbb54fb30f209
Dea Bardhoshi <dea.bardhoshi@berkeley.edu>
2023-07-24 11:37:05 -0400

    changes to 2 data loading functions

--- db-checkbook/build_scripts/01_dataloading.py
+++ db-checkbook/build_scripts/01_dataloading.py
@@ -1,36 +1,28 @@
-#from selectors import EpollSelector
 from dcpy.connectors import s3
-from functools import wraps
-from pathlib import Path
 import pandas as pd
-import geopandas as gpd
 
-base_bucket = 'edm-recipes'
-base_url = "https://edm-recipes.nyc3.cdn.digitaloceanspaces.com"
-
+BASE_BUCKET = 'edm-recipes'
+BASE_URL = "https://edm-recipes.nyc3.cdn.digitaloceanspaces.com"
 
 
-# reading in the data: using S3 connectors
 
-# example: datasets/dcp_cpdb/2018_adopted_polygons/
-# dataset name: datasets/dcp_cpdb
-# version: 2018_adopted, 2019_adopted, 2020_adopted, 2021_adopted, 2022_adopted, 2023_executive
-# type geom: _polygons, _points
+def read_s3_edm_recipes_cpdb(version, type_geom, save_file_path):
+    """read EDM data: using S3 connectors
+    example: datasets/dcp_cpdb/2018_adopted_polygons/
+    version: 2018_adopted, 2019_adopted, 2020_adopted, 2021_adopted, 2022_adopted, 2023_executive
+    type_geom: _polygons, _points 
+    """
+    digital_ocean_filepath = f'datasets/dcp_cpdb/{version}{type_geom}/dcp_cpdb.shp'
+    s3.client().download_file(BASE_BUCKET, digital_ocean_filepath, save_file_path)
+    return save_file_path
 
-def read_s3_edm_recipes_cpdb(dataset_name, version, type_geom, save_file_path):
-    digital_ocean_filepath = f'{dataset_name}/{version}{type_geom}/dcp_cpdb.shp'
-    s3_client = s3.client()
-    file = s3_client.download_file(base_bucket, digital_ocean_filepath, save_file_path)
-    return file
 
-# filepath: datasets/nycoc_checkbook/latest/nycoc_checkbook.csv
-def read_edm_recipes_nyc_checkbook(name = 'nycoc_checkbook.csv', version = "latest"):
-    file_name = f'{base_url}/datasets/nycoc_checkbook/{version}/{name}'
+def read_edm_recipes_nyc_checkbook(version = "latest"):
+    """filepath: datasets/nycoc_checkbook/latest/nycoc_checkbook.csv 
+    """
+    file_name = f'{BASE_URL}/datasets/nycoc_checkbook/{version}/nycoc_checkbook.csv'
     df = pd.read_csv(file_name, dtype=str, index_col=False)
     return df
-
-
-print(read_edm_recipes_nyc_checkbook().head())
 
 
 if __name__ == "__main__":
