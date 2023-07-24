5894a88bc6327c0b1d4e0e7a7dd082dbfd930e9b
Ali Thursland <athursland@gmail.com>
2023-07-24 16:18:17 +0000

    added example for regex str replacement, defined path constant, small changes all around for PR

--- db-checkbook/build_scripts/02_build.py
+++ db-checkbook/build_scripts/02_build.py
@@ -1,26 +1,17 @@
 import pandas as pd
 import geopandas as gpd
-import client
 import re
 import os
-from shapely.geometry import Point # testing
 
-"""
-merge_cpdb_geoms: this function is essentially the same as in my jupyter notebook:
-can someone help me think through how to modify it for using the data 
-from digital ocean? I think I'm a little confused as to how exactly this needs to change
-"""
+PATH_DIR = '../.library/'
 
-def merge_cpdb_geoms(): 
+def _merge_cpdb_geoms() -> gpd.GeoDataFrame: 
     """
-    :param path: raw checkbook nyc data
-    :type data: pandas df
     :return: cleaned checkbook nyc data
     :rtype: pandas df
     """
-    path = '../.library/'
     def extract_year(filename):
-        # TO DO: check for errors
+        # TODO: error checking
         match = re.search(r'^\d{4}', filename)
         if match:
             return int(match.group())
@@ -28,10 +19,9 @@ def merge_cpdb_geoms():
     
     ## stealing this from pluto-enhancements/digital_ocean_utils
     def get_all_filenames_in_folder():
-        # TO DO: check for errors
+        # TODO: error checking
         filenames = set()
-        for filenames in os.listdir(path):
-            filename = object.key.split("/")[-1] # this isn't necessary 
+        for filename in os.listdir(PATH_DIR):
             if filename != "":
                 filenames.add(object.key.split("/")[-1])
         return filenames
@@ -39,42 +29,42 @@ def merge_cpdb_geoms():
     file_list = get_all_filenames_in_folder()
     
     file_list = sorted(file_list, key=lambda x: extract_year(x), reverse=True) # sort by year
-    print(file_list)
-    #file_list = sorted(file_list, key=lambda x: int(re.search(r'\d+$',x).group()), reverse=True) # sort by year
 
     gdf_list = []
     for f in file_list:
-        gdf = gpd.read_file(path + f)
+        gdf = gpd.read_file(PATH_DIR + f)
         gdf_list.append(gdf)
 
     all_cpdb_geoms = pd.concat(gdf_list)
-    # note: currently, we're keeping the latest geometry when there are multiple,
-    # but we should note that more precise logic for choosing a geometry 
-    # should be implemented in the future
+    # NOTE: keeping the latest geometry when there are multiple
     all_cpdb_geoms.drop_duplicates(subset='maprojid', keep='first', inplace=True, ignore_index=True)
     return all_cpdb_geoms
 
-def clean_checkbook(data):
+def _clean_checkbook() -> pd.DataFrame:
     """
-    :param data: raw checkbook nyc data
-    :type data: pandas df
     :return: cleaned checkbook nyc data
     :rtype: pandas df
     """
-    # TO DO: other data cleaning?
-    data = data[data['Check Amount']<99000000] # removing strange seemingly lump sum checks 
+    data = pd.read_csv(PATH_DIR + 'nycoc_checkbook.csv')
+    # NOTE: This data cleaning is NOT complete, and we should investigate other cases where we should omit data
+    data = data[data['Check Amount']<99000000]
     data = data[data['Check Amount']>=0]
+    # taking out white space from capital project for join with cpdb 
+    """
+    remove last three digits and any trailing whitespace from `Capital Project`
+    Example: `Capital Project` = '998CAP2024  005' -> `FMS ID` = '998CAP2024'
+    \s*: matches zero or more whitespace characters
+    d+: matches one or more digits
+    $: assert position at the end of the string
+    """
     data['FMS ID'] = data['Capital Project'].str.replace(r'\s*d+$','') # QA this output because seems this causes an issue with SCA data
     return data
 
-def group_checkbook(data): 
+def _group_checkbook(data: pd.DataFrame) -> pd.DataFrame: 
     """
     :param data: cleaned checkbook nyc data
-    :type data: pandas df
     :return: checkbook nyc data grouped by capital project
-    :rtype: pandas df
     """
-    # is defining a mini-function within the scope of this function fine? in terms of best practices/code organization?  
     def fn_join_vals(x):
         return ';'.join([y for y in list(x) if pd.notna(y)])
 
@@ -96,61 +86,25 @@ def group_checkbook(data):
     df = df_limited_cols.groupby(cols_for_grouping, as_index=False).agg(agg_dict)
     return df
 
-def join_checkbook_geoms(df, cpdb_geoms):
+def _join_checkbook_geoms(df: pd.DataFrame, cpdb_geoms: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
     """
     :param df: Checkbook NYC data collapsed on FMS ID
-    :df type: pandas df
     :param cpdb_geoms: final versions of archived CPDB geometries from every year, and the most recent geometry for the current year
-    :cpdb_geoms type: geopandas df
     :return: CPDB geometries left-joined onto Checkbook NYC data 
-    :rtype: pandas df
     """
     merged = df.merge(cpdb_geoms, how='left', left_on='FMS ID', right_on='maprojid', indicator=True)
-    merged = df.drop('Unnamed: 0', axis=1, inplace=True) # TO DO: figure out how to avoid this column appearing in the first place
+    merged = df.drop('Unnamed: 0', axis=1, inplace=True) # TODO: figure out how to avoid this column appearing in the first place!
     gdf = gpd.GeoDataFrame(merged, geometry='geometry')
     return gdf
 
-# ----  TO DO: category assignment on BC, CP, and high-sensitivity Fixed Asset approach ----
-
+# ----  TODO: category assignment on BC, CP, and high-sensitivity Fixed Asset approach ----
 
 if __name__ == "__main__":
-    # Sample CPDB data (geodataframe)
-    cpdb_data_1 = gpd.GeoDataFrame({
-        'maprojid': [1, 2, 3],
-        'geometry': [Point(0, 0), Point(1, 1), Point(2, 2)]
-    })
-
-    cpdb_data_2 = gpd.GeoDataFrame({
-        'maprojid': [4, 5, 6],
-        'geometry': [Point(0, 0), Point(1, 1), Point(2, 2)]
-    })
-
-    cpdb_data_3 = gpd.GeoDataFrame({
-        'maprojid': [7, 8, 9],
-        'geometry': [Point(0, 0), Point(1, 1), Point(2, 2)]
-    })
-
-    cpdb_data = [cpdb_data_1, cpdb_data_2, cpdb_data_3]
-
-    checkbook_data = pd.DataFrame({
-        'FMS ID': [1, 2, 3],
-        'Contract Purpose': ['Contract A', 'Contract B', 'Contract C'],
-        'Agency': ['Agency X', 'Agency Y', 'Agency Z'],
-        'Budget Code': ['Code 1', 'Code 2', 'Code 3'],
-        'Check Amount': [1000, 2000, 3000]
-    })
 
     print("started build ...")
-    cpdb_geoms = merge_cpdb_geoms(cpdb_data)
-    print(cpdb_geoms)
-
-    cleaned_checkbook = clean_checkbook(checkbook_data)  # Pass the sample checkbook data
-    print(cleaned_checkbook)
-
-    grouped_checkbook = group_checkbook(cleaned_checkbook)  # Pass the cleaned checkbook data
-    print(grouped_checkbook)
-
-    joined_data = join_checkbook_geoms(grouped_checkbook, cpdb_geoms)  # Pass both dataframes
-    print(joined_data)
-
-    ## TO DO: call functions above
+    cpdb_geoms = _merge_cpdb_geoms()
+    cleaned_checkbook = _clean_checkbook() 
+    grouped_checkbook = _group_checkbook(cleaned_checkbook)
+    joined_data = _join_checkbook_geoms(grouped_checkbook, cpdb_geoms)
+    # TODO: call categorization functions
+    # TODO: save outputs
