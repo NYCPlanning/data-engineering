e043a1f5a7cf2888d83779843c78288c7089ddfb
Ali Thursland <athursland@gmail.com>
2023-07-24 13:51:50 +0000

    modified fet_all_filenames_in_folder pulled from pluto-enhancements, added comments

--- db-checkbook/build_scripts/02_build.py
+++ db-checkbook/build_scripts/02_build.py
@@ -3,14 +3,13 @@ import geopandas as gpd
 import client
 import re
 import os
+from shapely.geometry import Point # testing
 
 """
 merge_cpdb_geoms: this function is essentially the same as in my jupyter notebook:
 can someone help me think through how to modify it for using the data 
 from digital ocean? I think I'm a little confused as to how exactly this needs to change
 """
-def get_file_list():
-    return
 
 def merge_cpdb_geoms(): 
     """
@@ -19,22 +18,28 @@ def merge_cpdb_geoms():
     :return: cleaned checkbook nyc data
     :rtype: pandas df
     """
+    path = '../.library/'
     def extract_year(filename):
+        # TO DO: check for errors
         match = re.search(r'^\d{4}', filename)
         if match:
             return int(match.group())
-        return 0
+        return None
     
     ## stealing this from pluto-enhancements/digital_ocean_utils
     def get_all_filenames_in_folder():
+        # TO DO: check for errors
         filenames = set()
-        for filenames in os.listdir(directory_path):
-            filename = object.key.split("/")[-1]
+        for filenames in os.listdir(path):
+            filename = object.key.split("/")[-1] # this isn't necessary 
             if filename != "":
                 filenames.add(object.key.split("/")[-1])
         return filenames
-
-    file_list = sorted(file_list, key=lambda x: int(re.search(r'\d+$',x).group()), reverse=True)
+    
+    file_list = get_all_filenames_in_folder()
+    
+    file_list = sorted(file_list, key=lambda x: extract_year(x), reverse=True) # sort by year
+    file_list = sorted(file_list, key=lambda x: int(re.search(r'\d+$',x).group()), reverse=True) # sort by year
 
     gdf_list = []
     for f in file_list:
@@ -102,12 +107,38 @@ def join_checkbook_geoms(df, cpdb_geoms):
     merged = df.merge(cpdb_geoms, how='left', left_on='FMS ID', right_on='maprojid', indicator=True)
     merged = df.drop('Unnamed: 0', axis=1, inplace=True) # TO DO: figure out how to avoid this column appearing in the first place
     gdf = gpd.GeoDataFrame(merged, geometry='geometry')
-    return
+    return gdf
+
+# ---- # TO DO: category assignment on BC, CP, and high-sensitivity Fixed Asset approach 
 
 
 if __name__ == "__main__":
+    # Sample CPDB data (geodataframe)
+    cpdb_data = gpd.GeoDataFrame({
+        'maprojid': [1, 2, 3],
+        'geometry': [Point(0, 0), Point(1, 1), Point(2, 2)]
+    })
+
+    checkbook_data = pd.DataFrame({
+        'FMS ID': [1, 2, 3],
+        'Contract Purpose': ['Contract A', 'Contract B', 'Contract C'],
+        'Agency': ['Agency X', 'Agency Y', 'Agency Z'],
+        'Budget Code': ['Code 1', 'Code 2', 'Code 3'],
+        'Check Amount': [1000, 2000, 3000]
+    })
+
     print("started build ...")
-    cpdb = merge_cpdb_geoms()
+    cpdb_geoms = merge_cpdb_geoms(cpdb_data)
+    print(cpdb_geoms)
+
+    cleaned_checkbook = clean_checkbook(checkbook_data)  # Pass the sample checkbook data
+    print(cleaned_checkbook)
+
+    grouped_checkbook = group_checkbook(cleaned_checkbook)  # Pass the cleaned checkbook data
+    print(grouped_checkbook)
+
+    joined_data = join_checkbook_geoms(grouped_checkbook, cpdb_geoms)  # Pass both dataframes
+    print(joined_data)
 
     ## TO DO: call functions above
     
