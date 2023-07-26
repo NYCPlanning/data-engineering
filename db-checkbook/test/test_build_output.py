9078808229bf8663b0d99183a9ffd39ac6ecf151
Ali Thursland <alexandra.thursland@duke.edu>
2023-07-26 16:49:21 +0000

    testing build outputs of groupby on checkbook nyc data, trying to discover why the resulting df size is different than expected

--- db-checkbook/test/test_build_output.py
+++ db-checkbook/test/test_build_output.py
@@ -1,6 +1,13 @@
+import os
+import sys
 import pandas as pd
 import geopandas as gpd
-from build_scripts.02_build import run_build
+
+_current_dir = os.path.dirname(os.path.abspath(__file__))
+_parent_dir = os.path.dirname(_current_dir)
+sys.path.insert(0, _parent_dir)
+
+from build_scripts.build import _group_checkbook, _clean_checkbook
 
 def check_df_size(gdf: gpd.GeoDataFrame) -> bool:
     if gdf.shape[0] == 16687:
@@ -12,8 +19,15 @@ def check_join_size(gdf: gpd.GeoDataFrame) -> bool:
         return True
     return False
 
-if __name__ =="__main__":
-    df = run_build()
-    print(check_join_size(df))
-    print(check_df_size(df))
+def test_num_capital_projects() -> bool:
+    print('testing number of capital projects...')
+    df = _clean_checkbook()
 
+def test_checkbook_grouping() -> bool:
+    print('testing checkbook size...')
+    df = _group_checkbook()
+    print(df.shape)
+    assert df.shape[0] == 16687
+
+if __name__ =="__main__":
+    test_checkbook_grouping()
