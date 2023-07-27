40983e3217590c5f47d70ba7e11f154f5e366710
Ali Thursland <35311539+athursland@users.noreply.github.com>
2023-07-27 12:36:51 -0400

    small typo fix in 01_dataloading.py, minor formatting fixes (#90)

--- db-checkbook/build_scripts/01_dataloading.py
+++ db-checkbook/build_scripts/01_dataloading.py
@@ -12,22 +12,18 @@ _curr_file_path = Path(__file__).resolve()
 LIB_DIR = _curr_file_path.parent.parent / '.library'
 load_dotenv(_curr_file_path.parent.parent.parent / '.env') 
 
-
-
-def download_s3_edm_recipes_cpdb():
+def download_s3_edm_recipes_cpdb() -> None:
     """read EDM data: using S3 connectors
     example: datasets/dcp_cpdb/2018_adopted_polygons/
     version: 2017_adopted, 2018_adopted, 2019_adopted, 2020_adopted, 2021_adopted, 2022_adopted, 2023_executive
     type_geom: _polygons, _points 
     """
-
+    prefix = 'datasets/dcp_cpdb/'
     s3_resource = boto3.resource('s3', 
         aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
         aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
         endpoint_url=os.environ["AWS_S3_ENDPOINT"] 
         )
-
-    prefix = 'datasets/dcp_cpdb/'
 
     bucket = s3_resource.Bucket(BASE_BUCKET)     
     for obj in bucket.objects.filter(Prefix = prefix):
@@ -38,23 +34,18 @@ def download_s3_edm_recipes_cpdb():
             bucket.download_file(obj.key, LIB_DIR / key)
     return
 
-
-def read_edm_recipes_nyc_checkbook(version = "latest"):
+def read_edm_recipes_nyc_checkbook(version = "latest") -> None:
     """filepath: datasets/nycoc_checkbook/latest/nycoc_checkbook.csv 
     """
     file_name = f'{BASE_URL}/datasets/nycoc_checkbook/{version}/nycoc_checkbook.csv'
     df = pd.read_csv(file_name, dtype=str, index_col=False)
-    pd.to_csv(LIB_DIR / 'nycoc_checkbook.csv')
+    df.to_csv(LIB_DIR / 'nycoc_checkbook.csv')
     return
 
 def run_dataloading() -> None:
-
     download_s3_edm_recipes_cpdb()
     read_edm_recipes_nyc_checkbook()
-
     return 
-
-
 
 if __name__ == "__main__":
     print("Started dataloading ...")
