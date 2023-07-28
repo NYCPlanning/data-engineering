da37285c7d4786afe330c81d4576538c4d78e9fb
Damon McCullough <damonmcc1391@gmail.com>
2023-07-28 09:09:16 -0400

    update devdb source versions for 23Q2

--- db-developments/bash/01_dataloading.sh
+++ db-developments/bash/01_dataloading.sh
@@ -44,9 +44,9 @@ import_recipe dcp_censusdata_blocks 2020 &
 ## Geocode results shares index with _geo_devdb
 run_sql_command "DROP TABLE IF EXISTS _geo_devdb;"
 
-import_recipe dob_permitissuance $DOB_DATA_DATE &
-import_recipe dob_jobapplications $DOB_DATA_DATE &
-import_recipe dob_geocode_results $DOB_DATA_DATE &
+import_recipe dob_permitissuance &
+import_recipe dob_jobapplications &
+import_recipe dob_geocode_results &
 
 run_sql_file sql/_create.sql
 
