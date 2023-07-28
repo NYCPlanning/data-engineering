f4921a8cf6f47fc6a63ce620f5b81ee2fa28b88f
fvankrieken <fvankrieken@planning.nyc.gov>
2023-07-28 19:12:18 +0000

    use load_dotenv rather than dotenv_values

--- dcpy/__init__.py
+++ dcpy/__init__.py
@@ -1,11 +1,12 @@
+import os
 from pathlib import Path
 
-from dotenv import dotenv_values
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 
 DCPY_ROOT_PATH = Path(__file__).resolve().parent
 
-_config = dotenv_values(DCPY_ROOT_PATH.parent / ".env")
+load_dotenv(DCPY_ROOT_PATH.parent / ".env")
 
-BUILD_ENGINE_RAW = _config["BUILD_ENGINE"]
+BUILD_ENGINE_RAW = os.environ["BUILD_ENGINE"]
 build_engine = create_engine(BUILD_ENGINE_RAW)
