408a880111dfb4f042036adab9dbf656bc860b6b
Ali Thursland <athursland@gmail.com>
2023-08-02 15:56:16 +0000

    updated previous version date formatting in config.sh to use hyphens instead of slashes

--- products/db-zoningtaxlots/bash/config.sh
+++ products/db-zoningtaxlots/bash/config.sh
@@ -11,4 +11,4 @@ set_error_traps
 # Set Date
 DATE=$(date "+%Y-%m-01")
 VERSION=$DATE
-VERSION_PREV=$(date --date="$(date "+%Y/%m/01") - 1 month" "+%Y/%m/01")
+VERSION_PREV=$(date --date="$(date "+%Y-%m-01") - 1 month" "+%Y-%m-01")
