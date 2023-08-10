132704dbfee0562cd01fb5587a45ecae9e463beb
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2023-08-10 15:21:00 -0400

    Add Version Parser (and test cases)
    We'll use this to bump infer the version when we automate Pluto
    minor builds

---
+++ dcpy/utils/versions.py
@@ -0,0 +1,34 @@
+import re
+
+
+def parse(v: str):
+    """Parse a version from our standard format, e.g '23v2.11'."""
+    reg = re.compile(r"(\d{2})v\d.*")
+    if reg.match(v) is not None:
+        year, rst = v.split("v")
+        rst_split = rst.split(".")
+        version = {"year": year}
+        if len(rst_split) > 0:
+            version["major"] = rst_split[0]
+        if len(rst_split) > 1:
+            version["minor"] = rst_split[1]
+        return version
+    else:
+        raise Exception(
+            f"Tried to parse version {v} but it did not match the expected format"
+        )
+
+
+def bump(v: str, bumped_part: str):
+    """Bump a specific part of a version. e.g major, minor or year."""
+    parsed = parse(v)
+    to_bump = int(parsed.get(bumped_part, 0))
+    parsed[bumped_part] = str(to_bump + 1)
+    if bumped_part == "major" and "minor" in parsed:
+        del parsed["minor"]
+    return (
+        parsed["year"]
+        + "v"
+        + parsed.get("major", "")
+        + ("." + parsed.get("minor") if parsed.get("minor") is not None else "")
+    )
