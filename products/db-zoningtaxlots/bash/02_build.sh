8279fa819b14ad6b1e3397454a841004f60913d6
fvankrieken <fvankrieken@planning.nyc.gov>
2023-08-16 20:47:28 +0000

    move first edm_data step to qaqc script, add ztl to centralized build workflow

--- products/db-zoningtaxlots/bash/02_build.sh
+++ products/db-zoningtaxlots/bash/02_build.sh
@@ -23,13 +23,3 @@ run_sql_file sql/correct_duplicatevalues.sql
 run_sql_file sql/correct_zoninggaps.sql
 run_sql_file sql/correct_invalidrecords.sql
 
-
-echo "Archive final output"
-
-pg_dump -d ${BUILD_ENGINE} -t dcp_zoning_taxlot --no-owner --clean | psql ${EDM_DATA}
-psql ${EDM_DATA} -c "
-  CREATE SCHEMA IF NOT EXISTS dcp_zoningtaxlots;
-  ALTER TABLE dcp_zoning_taxlot SET SCHEMA dcp_zoningtaxlots;
-  DROP TABLE IF EXISTS dcp_zoningtaxlots.\"${VERSION_SQL_TABLE}\";
-  ALTER TABLE dcp_zoningtaxlots.dcp_zoning_taxlot RENAME TO \"${VERSION_SQL_TABLE}\";
-"
