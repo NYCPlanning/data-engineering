8c85d0cccf2ccbdc7bb6c52c249dca7023181ff2
fvankrieken <fvankrieken@planning.nyc.gov>
2023-08-21 21:22:43 +0000

    add logic to clean out largest input and intermediate tables

---
+++ products/pluto/pluto_build/sql/clean_database.sql
@@ -0,0 +1,9 @@
+DROP TABLE pluto_pts;
+DROP TABLE pluto_input_cama_dof;
+DROP TABLE pluto_input_cama;
+DROP TABLE pluto_input_geocodes;
+DROP TABLE dof_dtm;
+DROP TABLE pluto_dtm;
+DROP TABLE validdtm;
+DROP TABLE lotzoneper;
+DROP TABLE lotzoneperorder;
