796b1d481117ed4867c8241f6485e0df52b59084
fvankrieken <fvankrieken@planning.nyc.gov>
2023-08-23 15:48:35 +0000

    rework special district assignment

--- products/pluto/pluto_build/sql/zoning_correctdups.sql
+++ products/pluto/pluto_build/sql/zoning_correctdups.sql
@@ -1,15 +1,5 @@
 -- remove duplicate zoning values
+-- this can be deleted if overlay logic is updated to align with zonedist, spdist
 UPDATE pluto
 SET overlay2 = NULL
 WHERE overlay1=overlay2;
-
-UPDATE pluto
-SET spdist2 = NULL
-WHERE spdist1=spdist2;
-UPDATE pluto
-SET spdist3 = NULL
-WHERE spdist1=spdist3;
-
-UPDATE pluto
-SET spdist2 = NULL
-WHERE spdist2=spdist3;
