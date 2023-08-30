df125b5079764aad7f6f98809d5e3f9be7cc218c
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2023-08-30 16:52:07 -0400

    refactor: always drop tables in pluto setup

--- products/pluto/pluto_build/00_setup.sh
+++ products/pluto/pluto_build/00_setup.sh
@@ -2,15 +2,12 @@
 source ./bash/config.sh
 set_error_traps
 
-# DROP all tables
-if [[ $1 == "drop" ]]; then
-    run_sql_command "
-    DO \$\$ DECLARE
-        r RECORD;
-    BEGIN
-        FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public' and tablename !='spatial_ref_sys') LOOP
-            EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
-        END LOOP;
-    END \$\$;
-    "
-fi
+run_sql_command "
+DO \$\$ DECLARE
+    r RECORD;
+BEGIN
+    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public' and tablename !='spatial_ref_sys') LOOP
+        EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
+    END LOOP;
+END \$\$;
+"
