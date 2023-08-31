873d6a28f1fec430aba93043e534b9d277820e9d
fvankrieken <fvankrieken@planning.nyc.gov>
2023-08-31 13:41:35 +0000

    update zoningdistrict to logic from pluto

--- products/zoningtaxlots/sql/parks.sql
+++ products/zoningtaxlots/sql/parks.sql
@@ -1,26 +1,4 @@
 -- processing PARK designations
--- set the zoning district 1 value to PARK
--- where a zoning district is BALL FIELD, PLAYGROUND, or PUBLIC PLACE
-UPDATE dcp_zoning_taxlot a
-SET zoningdistrict1 = 'PARK'
-WHERE zoningdistrict1 = 'BALL FIELD'
-	OR zoningdistrict1 = 'PLAYGROUND'
-	OR zoningdistrict1 = 'PUBLIC PLACE';
-UPDATE dcp_zoning_taxlot a
-SET zoningdistrict2 = 'PARK'
-WHERE zoningdistrict2 = 'BALL FIELD'
-	OR zoningdistrict2 = 'PLAYGROUND'
-	OR zoningdistrict2 = 'PUBLIC PLACE';
-UPDATE dcp_zoning_taxlot a
-SET zoningdistrict3 = 'PARK'
-WHERE zoningdistrict3 = 'BALL FIELD'
-	OR zoningdistrict3 = 'PLAYGROUND'
-	OR zoningdistrict3 = 'PUBLIC PLACE';
-UPDATE dcp_zoning_taxlot a
-SET zoningdistrict4 = 'PARK'
-WHERE zoningdistrict4 = 'BALL FIELD'
-	OR zoningdistrict4 = 'PLAYGROUND'
-	OR zoningdistrict4 = 'PUBLIC PLACE';
 -- where zoningdistrict1 = 'PARK' NULL out all other zoning information
 UPDATE dcp_zoning_taxlot a
 SET zoningdistrict2 = NULL,
@@ -33,5 +11,4 @@ SET zoningdistrict2 = NULL,
 	specialdistrict3 = NULL,
 	limitedheightdistrict = NULL
 WHERE zoningdistrict1 = 'PARK'
-AND (zoningdistrict2 IS NULL
-	OR zoningdistrict2 = 'PARK');
+AND zoningdistrict2 IS NULL;
