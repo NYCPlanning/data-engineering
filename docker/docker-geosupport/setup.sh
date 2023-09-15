f8897e62a4f8d79c1094d805d4beb3003342acd8
fvankrieken <fvankrieken@planning.nyc.gov>
2023-09-15 21:38:14 +0000

    docker-geosupport maintenance

--- docker/docker-geosupport/setup.sh
+++ docker/docker-geosupport/setup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+FILE_NAME=linux_geo${RELEASE}_${MAJOR}_${MINOR}.zip
+echo $FILE_NAME
+curl -O https://s-media.nyc.gov/agencies/dcp/assets/files/zip/data-tools/bytes/$FILE_NAME
+unzip *.zip
+rm *.zip
+
+#!/bin/bash
 if [[ ${PATCH} = 0 ]]; then
     echo "NO UPAD AVAILABLE YET ..."
 else
@@ -8,3 +16,5 @@ else
         unzip -o linux_upad_tpad_${RELEASE}${PATCH}/*.zip -d version-${RELEASE}_${MAJOR}.${MINOR}/fls/ &&
         rm -r linux_upad_tpad_${RELEASE}${PATCH}
 fi
+
+pip install -r requirements.txt -c constraints.txt
