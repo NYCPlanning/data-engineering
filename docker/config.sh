efba9c0b5f9328130aebbc660ba192eae1e61f0d
fvankrieken <fvankrieken@planning.nyc.gov>
2023-09-19 20:19:38 +0000

    rework docker image publication

---
+++ docker/config.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+BUILD_APT_PACKAGES="postgresql-client-15 libpq-dev curl jq wget gdal-bin jq zip unzip git locales"
+DEV_APT_PACKAGES="postgresql-client-15 libpq-dev curl jq wget bash-completion gdal-bin jq zip unzip git locales"
+
+function install_yq {
+    wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq \
+        && chmod +x /usr/bin/yq
+}
+
+function install_mc {
+    curl -O https://dl.min.io/client/mc/release/linux-amd64/mc \
+        && chmod +x mc \
+        && mv ./mc /usr/bin/ 
+}
+
+function install_geosupport {
+    mkdir -p /geocode && (
+        cd /geocode
+        FILE_NAME=linux_geo${RELEASE}_${MAJOR}_${MINOR}.zip
+        echo $FILE_NAME
+        curl -O https://s-media.nyc.gov/agencies/dcp/assets/files/zip/data-tools/bytes/$FILE_NAME
+        unzip *.zip
+        rm *.zip
+
+        #!/bin/bash
+        if [[ ${PATCH} = 0 ]]; then
+            echo "NO UPAD AVAILABLE YET ..."
+        else
+            echo "YES UPAD IS AVAILABLE linux_upad_tpad_${RELEASE}${PATCH}"
+            mkdir linux_upad_tpad_${RELEASE}${PATCH} &&
+                curl -o linux_upad_tpad_${RELEASE}${PATCH}/linux_upad_tpad_${RELEASE}${PATCH}.zip https://s-media.nyc.gov/agencies/dcp/assets/files/zip/data-tools/bytes/linux_upad_tpad_${RELEASE}${PATCH}.zip &&
+                unzip -o linux_upad_tpad_${RELEASE}${PATCH}/*.zip -d version-${RELEASE}_${MAJOR}.${MINOR}/fls/ &&
+                rm -r linux_upad_tpad_${RELEASE}${PATCH}
+        fi
+    )
+}
---
+++ docker/dev/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source config.sh
+set -e
+
+apt-get update && export DEBIAN_FRONTEND=noninteractive \
+    && apt-get -y install $DEV_APT_PACKAGES
+
+install_geosupport
+install_yq
+install_mc
+
+python3 -m pip install -r requirements.txt
