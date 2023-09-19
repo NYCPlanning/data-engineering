a794dd9aa9e383e414f9a7856807ef1c4e84f897
Damon McCullough <damonmcc1391@gmail.com>
2023-09-19 15:20:02 -0400

    refactor and test dcpy
    add PostgresClient class to manage DB connection and most postgres utils
    add git.run_name git.event_name
    refactor recipes to use PostgresClient

---
+++ dcpy/test/utils/test_postgres.py
@@ -0,0 +1,28 @@
+import pytest
+from dcpy.utils import postgres
+
+
+@pytest.mark.parametrize(
+    "test_inputs, expected_uri",
+    [
+        (
+            ("local@local.local://person:pass", "db-1", None),
+            "local@local.local://person:pass/db-1?options=--search_path%3Dpublic",
+        ),
+        (
+            ("local@local.local://person:pass", "db-2", "pr_101"),
+            "local@local.local://person:pass/db-2?options=--search_path%3Dpr_101,public",
+        ),
+        (
+            ("local@local.local://person:pass", "db-2", "pr-dash"),
+            "local@local.local://person:pass/db-2?options=--search_path%3Dpr_dash,public",
+        ),
+    ],
+)
+def test_generate_engine_uri(test_inputs, expected_uri):
+    actual_uri = postgres.generate_engine_uri(
+        server_url=test_inputs[0],
+        database=test_inputs[1],
+        schema=test_inputs[2],
+    )
+    assert actual_uri == expected_uri
