93f79e4df917a8c15087c70b7fa311501f1ec4fb
Alex Richey <alexrichey1@gmail.com>
2023-09-19 14:05:31 -0400

    refactor recipes/publishing to use pydantic + typer (#232)
    * Convert edm.recipes to pydantic
    
    * dcpy recipes: use typer for commands
    
    No logic changed in the commands themselves
    
    * Refactor import_dataset to use DataSet class
    
    * dcpy publishing: use typer for commands
    
    No logic changed in the commands themselves
    
    * dcpy recipes: check recipe version type exists
    
    This is a potential bug, raised by mypy. version_type is optional, so we
    should check it and cast it before passing it to versions.bump
    
    * Remove redundant null-check
    
    * Remove redundant check for dataset version
    
    When `import_dataset` is called, versions should be fully resolved to
    actual versions, not latest.
    
    * use Dataset class in `fetch_sql`
    
    * Add more explicit resolution checks to Recipes
    
    ---------
    
    Co-authored-by: Alex Richey <alexrichey@DCP-APPLE-2180.local>

--- dcpy/connectors/edm/recipes.py
+++ dcpy/connectors/edm/recipes.py
@@ -1,14 +1,13 @@
-import argparse
-import cerberus
 import csv
+from enum import Enum
 import json
 from pathlib import Path
+from pydantic import BaseModel
 import shutil
-import sys
-from typing import Optional
-import yaml
-
 from sqlalchemy import text, update, Table, MetaData
+import typer
+from typing import Optional, List
+import yaml
 
 
 from dcpy import DCPY_ROOT_PATH
@@ -25,34 +24,49 @@ BUCKET = "edm-recipes"
 BASE_URL = f"https://{BUCKET}.nyc3.digitaloceanspaces.com/datasets"
 LIBRARY_DEFAULT_PATH = DCPY_ROOT_PATH.parent / ".library"
 
-RECIPE_FILE_SCHEMA = {
-    "name": {"type": "string", "required": True},
-    "product": {"type": "string", "required": True},
-    "base_recipe": {"type": "string"},
-    "version_type": {"type": "string", "allowed": ["major", "minor"]},
-    "version_strategy": {"type": "string", "allowed": ["bump_latest_release"]},
-    "version": {"type": "string"},
-    "inputs": {
-        "type": "dict",
-        "schema": {
-            "missing_versions_strategy": {
-                "type": "string",
-                "allowed": ["copy_latest_release", "find_latest"],
-            },
-            "datasets": {
-                "type": "list",
-                "schema": {
-                    "type": "dict",
-                    "schema": {
-                        "name": {"type": "string"},
-                        "version": {"type": "string"},
-                        "import_as": {"type": "string"},
-                    },
-                },
-            },
-        },
-    },
-}
+
+class RecipeInputsVersionStrategy(str, Enum):
+    find_latest = "find_latest"
+    copy_latest_release = "copy_latest_release"
+
+
+class VersionStrategy(str, Enum):
+    bump_latest_release = "bump_latest_release"
+
+
+class Dataset(BaseModel, use_enum_values=True):
+    name: str
+    version: Optional[str] = None
+    import_as: Optional[str] = None
+
+    def is_resolved(self):
+        return self.version is not None and self.version != "latest"
+
+
+class RecipeInputs(BaseModel, use_enum_values=True):
+    missing_versions_strategy: Optional[RecipeInputsVersionStrategy] = None
+    datasets: List[Dataset] = []
+
+
+class DatasetVersionType(str, Enum):
+    major = "major"
+    minor = "minor"
+
+
+class Recipe(BaseModel, use_enum_values=True):
+    name: str
+    product: str
+    base_recipe: Optional[str] = None
+    version_type: Optional[DatasetVersionType] = None
+    version_strategy: Optional[VersionStrategy] = None
+    version: Optional[str] = None
+    inputs: RecipeInputs
+
+    def is_resolved(self):
+        return self.version is not None and (
+            len(self.inputs.datasets) == 0
+            or len([x for x in self.inputs.datasets if not x.is_resolved()]) == 0
+        )
 
 
 def get_dataset_sql_path(dataset: str, version: str = "latest"):
@@ -77,62 +91,54 @@ def get_latest_version(name):
     return get_config(name)["dataset"]["version"]
 
 
-def fetch_sql(name: str, version: str, local_library_dir):
+def fetch_sql(ds: Dataset, local_library_dir):
     """Retrieve SQL dump file from edm-recipes. Returns fetched file's path."""
-    target_dir = local_library_dir / "datasets" / name / version
-    target_file_path = target_dir / (name + ".sql")
+    target_dir = local_library_dir / "datasets" / ds.name / ds.version
+    target_file_path = target_dir / (ds.name + ".sql")
     if (target_file_path).exists():
-        print(f"✅ {name}.sql exists in cache")
+        print(f"✅ {ds.name}.sql exists in cache")
     else:
         if not target_dir.exists():
             target_dir.mkdir(parents=True)
-        print(f"🛠 {name}.sql doesn't exists in cache, downloading")
+        print(f"🛠 {ds.name}.sql doesn't exists in cache, downloading")
         s3.download_file(
             bucket=BUCKET,
-            key=f"datasets/{name}/{version}/{name}.sql",
+            key=f"datasets/{ds.name}/{ds.version}/{ds.name}.sql",
             path=target_file_path,
         )
     return target_file_path
 
 
-def import_recipe_dataset(
-    recipe_name: str,
-    *,
-    version="latest",
-    local_library_dir=LIBRARY_DEFAULT_PATH,
-    import_table_name: Optional[str] = None,
-):
+def import_dataset(dataset: Dataset, *, local_library_dir=LIBRARY_DEFAULT_PATH):
     """Import a recipe to local data library folder and build engine."""
-    config = get_config(recipe_name, version)
-    recipe_version = config["dataset"]["version"]
-    sql_script_path = fetch_sql(recipe_name, recipe_version, local_library_dir)
+    if dataset.version == "latest" or dataset.version is None:
+        raise Exception(
+            f"Cannot import a dataset without a resolved version: {dataset}"
+        )
+    sql_script_path = fetch_sql(dataset, local_library_dir)
 
     postgres.execute_file_via_shell(postgres.BUILD_ENGINE_RAW, sql_script_path)
 
     with postgres.build_engine.connect() as con:
         con.execute(
-            text(f"ALTER TABLE {recipe_name} ADD COLUMN data_library_version text;")
+            text(f"ALTER TABLE {dataset.name} ADD COLUMN data_library_version text;")
         )
         con.commit()
 
-        recipes_table = Table(recipe_name, MetaData(), autoload_with=con)
-        con.execute(update(recipes_table).values(data_library_version=version))
+        recipes_table = Table(dataset.name, MetaData(), autoload_with=con)
+        con.execute(update(recipes_table).values(data_library_version=dataset.version))
 
         con.commit()
 
-        if import_table_name is not None:
-            logger.info(f"Renaming table {recipe_name} to {import_table_name}")
+        if dataset.import_as is not None:
+            logger.info(f"Renaming table {dataset.name} to {dataset.import_as}")
             con.execute(
-                text(f"ALTER TABLE {recipe_name} RENAME TO {import_table_name};")
+                text(f"ALTER TABLE {dataset.name} RENAME TO {dataset.import_as};")
             )
             con.commit()
 
 
-def _recipe_datasets(recipe):
-    return recipe.get("inputs", {}).get("datasets", []) or []
-
-
-def plan_recipe(recipe_path: Path) -> dict:
+def plan_recipe(recipe_path: Path) -> Recipe:
     """Plan recipe versions for a product.
 
     Similar to pip freeze, determines recipe versions to use for a build.
@@ -140,147 +146,139 @@ def plan_recipe(recipe_path: Path) -> dict:
     the missing versions strategy will be applied AFTER the recipe inputs are
     merged with the base.
     """
-    recipe = recipe_from_yaml(recipe_path)
+    recipe: Recipe = recipe_from_yaml(recipe_path)
 
-    if "version_strategy" in recipe:
-        strat = recipe["version_strategy"]
-        if strat == "bump_latest_release":
-            prev_version = publishing.get_latest_version(recipe["product"])
-            recipe["version"] = versions.bump(
-                prev_version, bumped_part=recipe["version_type"]
+    # Determine the recipe version
+    if recipe.version is None and recipe.version_strategy is not None:
+        if recipe.version_strategy == VersionStrategy.bump_latest_release:
+            if recipe.version_type is None:
+                raise Exception("Recipe needs a 'version_type' to bump")
+            prev_version = publishing.get_latest_version(recipe.product)
+            recipe.version = versions.bump(
+                prev_version, bumped_part=recipe.version_type
             )
 
     # merge in base recipe inputs
     base_recipe = (
-        recipe_from_yaml(recipe_path.parent / recipe["base_recipe"])
-        if "base_recipe" in recipe
-        else {}
+        recipe_from_yaml(recipe_path.parent / recipe.base_recipe)
+        if recipe.base_recipe is not None
+        else None
     )
 
-    input_dataset_names = {i["name"] for i in _recipe_datasets(recipe)}
-    for ds in _recipe_datasets(base_recipe):
-        if ds["name"] not in input_dataset_names:
-            recipe["inputs"]["datasets"].append(ds)
+    input_dataset_names = {d.name for d in recipe.inputs.datasets}
+    if base_recipe is not None:
+        for base_ds in base_recipe.inputs.datasets:
+            if base_ds.name not in input_dataset_names:
+                recipe.inputs.datasets.append(base_ds)
 
     # Fill in omitted versions
     previous_versions = {}
-    missing_version_strat = recipe["inputs"].get("missing_versions_strategy")
-    if missing_version_strat == "copy_latest_release":
+    if (
+        recipe.inputs.missing_versions_strategy
+        == RecipeInputsVersionStrategy.copy_latest_release
+    ):
         previous_versions = publishing.get_source_data_versions(
-            publishing.PublishKey(recipe["product"], "latest")
+            publishing.PublishKey(recipe.product, "latest")
         ).to_dict()["version"]
 
-    for ds in _recipe_datasets(recipe):
-        if "version" not in ds:
-            if missing_version_strat == "copy_latest_release":
-                ds["version"] = previous_versions[ds["name"]]
+    for ds in recipe.inputs.datasets:
+        if ds.version is None:
+            if (
+                recipe.inputs.missing_versions_strategy
+                == RecipeInputsVersionStrategy.copy_latest_release
+            ):
+                ds.version = previous_versions[ds.name]
             else:
-                ds["version"] = "latest"
+                ds.version = "latest"
 
-        if ds["version"] == "latest":
-            ds["version"] = get_config(ds["name"], "latest")["dataset"]["version"]
+        if ds.version == "latest":
+            ds.version = get_config(ds.name, "latest")["dataset"]["version"]
 
     return recipe
 
 
-def import_recipe_datasets(recipe):
-    """Import all datasets specified in a recipe."""
-    for rec in recipe["inputs"]["datasets"]:
-        import_recipe_dataset(
-            rec["name"], version=rec["version"], import_table_name=rec.get("import_as")
-        )
-
-
-def purge_recipe_cache(local_library_dir=LIBRARY_DEFAULT_PATH):
-    """Delete locally stored recipe files."""
-    shutil.rmtree(local_library_dir)
-
-
-def get_source_data_versions(recipe):
+def get_source_data_versions(recipe: Recipe):
     """Get source data versions table in form of [schema_name, v]."""
     return [["schema_name", "v"]] + [
-        [d["name"], d["version"]] for d in _recipe_datasets(recipe)
+        [d.name, d.version] for d in recipe.inputs.datasets
     ]
 
 
-def recipe_from_yaml(path: Path):
+def recipe_from_yaml(path: Path) -> Recipe:
     """Import a recipe file from yaml, and validate schema."""
-    v = cerberus.Validator(RECIPE_FILE_SCHEMA)
     with open(path, "r", encoding="utf-8") as f:
         s = yaml.safe_load(f)
-    if v.validate(s):
-        return s
+    return Recipe(**s)
+
+
+app = typer.Typer(add_completion=False)
+
+_typer_recipe_file_opt = typer.Option(
+    None, "--recipe-file", "-f", help="Recipe file path"
+)
+
+
+@app.command()
+def plan(
+    recipe_file: Path = _typer_recipe_file_opt,
+    lock_file: Path = typer.Option(
+        None,
+        "-o",
+        "--lock-file",
+        help="Lockfile path",
+    ),
+):
+    logger.info("Planning recipe")
+
+    recipe = plan_recipe(recipe_file)
+    if lock_file is not None:
+        with open(lock_file, "w", encoding="utf-8") as f:
+            logger.info(f"Writing recipe lockfile to {str(lock_file.absolute())}")
+            yaml.dump(recipe.model_dump(), f)
     else:
-        raise Exception(f"Invalid recipe file: {path}. {v.errors}")
+        print(recipe)
+
+
+@app.command("import")
+def import_datasets(planned_recipe_file: Path = _typer_recipe_file_opt):
+    """Import all datasets from a recipe.
+
+    Note, the recipe's datasets must not have unresolved versions, ie either
+    'latest' or None.
+    """
+    logger.info("Importing Recipe Datasets")
+    recipe = recipe_from_yaml(planned_recipe_file)
+    [import_dataset(ds) for ds in recipe.inputs.datasets]
+
+
+@app.command()
+def purge_recipe_cache():
+    """Delete locally stored recipe files."""
+    logger.info(f"Purging local recipes from {LIBRARY_DEFAULT_PATH}")
+    shutil.rmtree(LIBRARY_DEFAULT_PATH)
+
+
+@app.command()
+def write_source_data_versions(recipe_file: Path = _typer_recipe_file_opt):
+    recipe = recipe_from_yaml(recipe_file)
+    source_data_versions_path = recipe_file.parent / "source_data_versions.csv"
+    logger.info(f"Writing source data versions to {source_data_versions_path}")
+
+    sdv = get_source_data_versions(recipe)
+    unresolved_versions = [[k, v] for k, v in sdv if v == "latest"]
+    if len(unresolved_versions) > 0:
+        exception = (
+            "Recipe has unresolved versions! Can't write source "
+            + f"data versions {unresolved_versions}"
+        )
+        logger.error(exception)
+        raise Exception(exception)
+
+    with open(source_data_versions_path, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        for key, value in sdv:
+            writer.writerow([key, value])
 
 
 if __name__ == "__main__":
-    flags_parser = argparse.ArgumentParser()
-    flags_parser.add_argument("cmd")
-    cmd = sys.argv[1]
-
-    if cmd == "plan":
-        logger.info("Planning recipe")
-        flags_parser.add_argument(
-            "-f", "--recipe-file", help="Recipe file path", type=Path
-        )
-        flags_parser.add_argument(
-            "-o",
-            "--lock-file",
-            help="Lockfile path",
-            type=Path,
-            required=False,
-        )
-        flags = flags_parser.parse_args()
-
-        recipe = plan_recipe(Path(flags.recipe_file))
-        if flags.lock_file is not None:
-            with open(flags.lock_file, "w", encoding="utf-8") as f:
-                logger.info(
-                    f"Writing recipe lockfile to {str(flags.lock_file.absolute())}"
-                )
-                yaml.dump(recipe, f)
-        else:
-            print(recipe)
-
-    elif cmd == "import":
-        flags_parser.add_argument(
-            "-f", "--recipe-file", help="Recipe file path", type=Path
-        )
-        flags = flags_parser.parse_args()
-
-        with open(flags.recipe_file, "r", encoding="utf-8") as f:
-            recipe = yaml.safe_load(f)
-        logger.info("Importing Recipes")
-        import_recipe_datasets(recipe)
-
-    elif cmd == "purge-recipe-cache":
-        logger.info(f"Purging local recipes from {LIBRARY_DEFAULT_PATH}")
-        purge_recipe_cache()
-
-    elif cmd == "write-source-data-versions":
-        flags_parser.add_argument(
-            "-f", "--recipe-file", help="Recipe file path", type=Path
-        )
-        flags = flags_parser.parse_args()
-
-        recipe = recipe_from_yaml(flags.recipe_file)
-        source_data_versions_path = (
-            flags.recipe_file.parent / "source_data_versions.csv"
-        )
-        logger.info(f"Writing source data versions to {source_data_versions_path}")
-
-        sdv = get_source_data_versions(recipe)
-        unresolved_versions = [[k, v] for k, v in sdv if v == "latest"]
-        if len(unresolved_versions) > 0:
-            exception = (
-                "Recipe has unresolved versions! Can't write source "
-                + f"data versions {unresolved_versions}"
-            )
-            logger.error(exception)
-            raise Exception(exception)
-
-        with open(source_data_versions_path, "w", newline="") as csvfile:
-            writer = csv.writer(csvfile)
-            for key, value in sdv:
-                writer.writerow([key, value])
+    app()
