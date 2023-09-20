fa9fd060f23e2d02f829f429591b829cb484db7d
Damon McCullough <damonmcc1391@gmail.com>
2023-09-20 10:23:01 -0400

    update TDB use of recipes

--- products/template/build_scripts/load.py
+++ products/template/build_scripts/load.py
@@ -1,21 +1,15 @@
-import os
 from pathlib import Path
-import yaml
 from dcpy.connectors.edm import recipes
-from dcpy.utils.logging import logger
 
 from . import RECIPE_PATH, RECIPE_LOCK_PATH
 
 
 def load_source_data():
-    recipe_lock = recipes.plan_recipe(Path(RECIPE_PATH))
-    with open(RECIPE_LOCK_PATH, "w", encoding="utf-8") as f:
-        logger.info(f"Writing recipe lockfile to {RECIPE_LOCK_PATH}")
-        yaml.dump(recipe_lock, f)
-    with open(RECIPE_LOCK_PATH, "r", encoding="utf-8") as f:
-        recipe_lock = yaml.safe_load(f)
-    logger.info("Importing Recipe")
-    recipes.import_recipe_datasets(recipe_lock)
+    recipes.plan(
+        Path(RECIPE_PATH),
+        Path(RECIPE_LOCK_PATH),
+    )
+    recipes.import_datasets(Path(RECIPE_LOCK_PATH))
 
 
 if __name__ == "__main__":
