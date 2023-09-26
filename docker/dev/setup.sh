73e16f46c6192fc876db3a7dd76d9f245b995598
fvankrieken <fvankrieken@planning.nyc.gov>
2023-09-26 20:13:18 +0000

    add python gdal bindings to dev container, docker setup

--- docker/dev/setup.sh
+++ docker/dev/setup.sh
@@ -3,7 +3,8 @@ source config.sh
 set -e
 
 apt-get update && export DEBIAN_FRONTEND=noninteractive \
-    && apt-get -y install $DEV_APT_PACKAGES
+    && apt-get -y install $DEV_APT_PACKAGES \
+    && apt-get -y install --reinstall build-essential # needed to install gdal. I don't know why, it just is
 
 install_geosupport
 install_yq
