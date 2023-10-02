540abd8a2ff702d184a24740c047f034f987bba4
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2023-10-02 16:06:12 -0400

    Inline @Export

--- products/facilities/facdb/pipelines.py
+++ products/facilities/facdb/pipelines.py
@@ -8,11 +8,10 @@ from .geocode.function1B import Function1B
 from .geocode.functionBL import FunctionBL
 from .geocode.functionBN import FunctionBN
 from .geocode.parseAddress import ParseAddress, UseAirportName
-from .utility.export import Export
+from .utility.export import export
 from .utility.prepare import prepare
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -26,10 +25,9 @@ def bpl_libraries(df: pd.DataFrame = None):
     df["latitude"] = df.position.apply(lambda x: x.split(",")[0].strip())
     df["zipcode"] = df.address.apply(lambda x: x[-6:])
     df["borough"] = "Brooklyn"
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -41,10 +39,9 @@ def nypl_libraries(df: pd.DataFrame = None):
     df = prepare("nypl_libraries")
     df["latitude"] = df.lat
     df["longitude"] = df.lon
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="address_street_name",
     house_number_field="address_building",
@@ -71,10 +68,9 @@ def dca_operatingbusinesses(df: pd.DataFrame = None):
     df = df.loc[((df.license_expiration_date >= today) & (df.industry == "Scrap Metal Processor"))|((df.license_expiration_date >= covid_freeze) & (df.industry != "Scrap Metal Processor")), :]\
         .loc[df.industry.isin(industry), :]
     # fmt:on
-    return df
+    export(df)
 
 
-@Export
 @FunctionBL(bbl_field="bbl")
 @FunctionBN(bin_field="bin")
 @Function1B(
@@ -87,10 +83,9 @@ def dca_operatingbusinesses(df: pd.DataFrame = None):
 def dcla_culturalinstitutions(df: pd.DataFrame = None):
     df = prepare("dcla_culturalinstitutions")
     df["zipcode"] = df.postcode.apply(lambda x: x[:5])
-    return df
+    export(df)
 
 
-@Export
 @FunctionBL(bbl_field="bbl")
 @Function1B(
     street_name_field="sname", house_number_field="hnum", borough_field="borough"
@@ -98,10 +93,9 @@ def dcla_culturalinstitutions(df: pd.DataFrame = None):
 def dcp_colp(df: pd.DataFrame = None):
     df = prepare("dcp_colp")
     df["bbl"] = df.bbl.str.split(pat=".").str[0]
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="street_name",
     house_number_field="address_number",
@@ -112,16 +106,14 @@ def dcp_colp(df: pd.DataFrame = None):
 @FunctionBN(bin_field="bin")
 def dcp_pops(df: pd.DataFrame = None):
     df = prepare("dcp_pops")
-    return df
+    export(df)
 
 
-@Export
 def dcp_sfpsd(df: pd.DataFrame = None):
     df = prepare("dcp_sfpsd")
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="street_name",
     house_number_field="house_number",
@@ -129,10 +121,9 @@ def dcp_sfpsd(df: pd.DataFrame = None):
 )
 def dep_wwtc(df: pd.DataFrame = None):
     df = prepare("dep_wwtc")
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -141,10 +132,9 @@ def dep_wwtc(df: pd.DataFrame = None):
 @ParseAddress(raw_address_field="program_address")
 def dfta_contracts(df: pd.DataFrame = None):
     df = prepare("dfta_contracts")
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -157,10 +147,9 @@ def doe_busroutesgarages(df: pd.DataFrame = None):
     # SCHOOL_YEAR = f"{datetime.date.today().year-1}-{datetime.date.today().year}"
     SCHOOL_YEAR = "2019-2020"  # No records from 2020-2021 school year
     df = df.loc[df.school_year == SCHOOL_YEAR, :]
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -171,10 +160,9 @@ def doe_busroutesgarages(df: pd.DataFrame = None):
 @ParseAddress(raw_address_field="primary_address")
 def doe_lcgms(df: pd.DataFrame = None):
     df = prepare("doe_lcgms")
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -193,10 +181,9 @@ def doe_universalprek(df: pd.DataFrame = None):
             "R": "Staten Island",
         }
     )
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="street",
     house_number_field="building",
@@ -206,10 +193,9 @@ def doe_universalprek(df: pd.DataFrame = None):
 @FunctionBN(bin_field="building_identification_number")
 def dohmh_daycare(df: pd.DataFrame = None):
     df = prepare("dohmh_daycare")
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -219,10 +205,9 @@ def dohmh_daycare(df: pd.DataFrame = None):
 def dot_bridgehouses(df: pd.DataFrame = None):
     df = prepare("dot_bridgehouses")
     df["address"] = df.address.astype(str).replace("n/a", None)
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -233,10 +218,9 @@ def dot_bridgehouses(df: pd.DataFrame = None):
 def dot_ferryterminals(df: pd.DataFrame = None):
     df = prepare("dot_ferryterminals")
     df["bbl"] = df.bbl.fillna(0).astype(float).astype(int)
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -248,16 +232,14 @@ def dot_mannedfacilities(df: pd.DataFrame = None):
     df = prepare("dot_mannedfacilities")
     df["address"] = df.address.astype(str)
     df["bbl"] = df.bbl.fillna(0).astype(float).astype(int)
-    return df
+    export(df)
 
 
-@Export
 def dot_pedplazas(df: pd.DataFrame = None):
     df = prepare("dot_pedplazas")
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -269,10 +251,9 @@ def dot_publicparking(df: pd.DataFrame = None):
     df = prepare("dot_publicparking")
     df["address"] = df.address.astype(str)
     df["bbl"] = df.bbl.fillna(0).astype(float).astype(int)
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -292,10 +273,9 @@ def dpr_parksproperties(df: pd.DataFrame = None):
             "R": "Staten Island",
         }
     )
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -308,10 +288,9 @@ def dpr_parksproperties(df: pd.DataFrame = None):
 def dsny_garages(df: pd.DataFrame = None):
     df = prepare("dsny_garages")
     df["address"] = df.address.astype(str)
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -321,10 +300,9 @@ def dsny_garages(df: pd.DataFrame = None):
 @ParseAddress(raw_address_field="address")
 def dsny_specialwastedrop(df: pd.DataFrame = None):
     df = prepare("dsny_specialwastedrop")
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="street", house_number_field="number", borough_field="borough"
 )
@@ -337,10 +315,9 @@ def dsny_donatenycdirectory(df: pd.DataFrame = None):
         df["categoriesaccepted"].notna()
         & df["categoriesaccepted"].str.contains("Clothing")
     ]
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -353,10 +330,9 @@ def dsny_donatenycdirectory(df: pd.DataFrame = None):
 def dsny_leafdrop(df: pd.DataFrame = None):
     df = prepare("dsny_leafdrop")
     df["bbl"] = df.bbl.fillna(0).astype(float).astype(int)
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -372,10 +348,9 @@ def dsny_fooddrop(df: pd.DataFrame = None):
         return location_str[:-5] if location_str[:-5].isnumeric() else None
 
     df["zip_code"] = df["location"].apply(_loc_to_zipcode)
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="street",
     house_number_field="number",
@@ -387,10 +362,9 @@ def dsny_fooddrop(df: pd.DataFrame = None):
 def dsny_electronicsdrop(df: pd.DataFrame = None):
     df = prepare("dsny_electronicsdrop")
     df["bbl"] = df.bbl.fillna(0).astype(float).astype(int)
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -414,10 +388,9 @@ def dycd_afterschoolprograms(df: pd.DataFrame = None):
     df["spatial"] = df.spatial.apply(lambda x: x.replace("(", "").replace(")", ""))
     df["longitude"] = df.spatial.apply(lambda x: x.split(",")[-1])
     df["latitude"] = df.spatial.apply(lambda x: x.split(",")[0])
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -427,10 +400,9 @@ def dycd_afterschoolprograms(df: pd.DataFrame = None):
 @ParseAddress(raw_address_field="address")
 def fbop_corrections(df: pd.DataFrame = None):
     df = prepare("fbop_corrections")
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -459,10 +431,9 @@ def fdny_firehouses(df: pd.DataFrame = None):
         df_research.facilityname.isin(df.facilityname),
         ["facilityaddress", "borough", "postcode", "wkt"],
     ].values
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -471,10 +442,9 @@ def fdny_firehouses(df: pd.DataFrame = None):
 @ParseAddress(raw_address_field="address")
 def foodbankny_foodbanks(df: pd.DataFrame = None):
     df = prepare("foodbankny_foodbanks")
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -490,10 +460,9 @@ def hhc_hospitals(df: pd.DataFrame = None):
     df["longitude"] = df.spatial.apply(lambda x: x.split(",")[-1])
     df["latitude"] = df.spatial.apply(lambda x: x.split(",")[0])
     df.location_1 = df.location_1.apply(lambda x: x.replace("\n", " "))
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -505,10 +474,9 @@ def hhc_hospitals(df: pd.DataFrame = None):
 @FunctionBN(bin_field="bin")
 def hra_snapcenters(df: pd.DataFrame = None):
     df = prepare("hra_snapcenters")
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -520,10 +488,9 @@ def hra_snapcenters(df: pd.DataFrame = None):
 @FunctionBN(bin_field="bin")
 def hra_jobcenters(df: pd.DataFrame = None):
     df = prepare("hra_jobcenters")
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -535,10 +502,9 @@ def hra_jobcenters(df: pd.DataFrame = None):
 @FunctionBN(bin_field="bin")
 def hra_medicaid(df: pd.DataFrame = None):
     df = prepare("hra_medicaid")
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -556,10 +522,9 @@ def moeo_socialservicesitelocations(df: pd.DataFrame = None):
     df["bin"] = df.bin.fillna(0).astype(float).astype(int)
     df["postcode"] = df.bbl.fillna(0).astype(float).astype(int)
     df["postcode"] = df.postcode.astype(str).apply(lambda x: x[:5])
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="street_name",
     house_number_field="house_number",
@@ -567,10 +532,9 @@ def moeo_socialservicesitelocations(df: pd.DataFrame = None):
 )
 def nycdoc_corrections(df: pd.DataFrame = None):
     df = prepare("nycdoc_corrections")
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -581,10 +545,9 @@ def nycdoc_corrections(df: pd.DataFrame = None):
 @FunctionBN(bin_field="bin")
 def nycha_communitycenters(df: pd.DataFrame = None):
     df = prepare("nycha_communitycenters")
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -594,10 +557,9 @@ def nycha_communitycenters(df: pd.DataFrame = None):
 @ParseAddress(raw_address_field="address")
 def nycha_policeservice(df: pd.DataFrame = None):
     df = prepare("nycha_policeservice")
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -607,17 +569,15 @@ def nycha_policeservice(df: pd.DataFrame = None):
 @ParseAddress(raw_address_field="address")
 def nycourts_courts(df: pd.DataFrame = None):
     df = prepare("nycourts_courts")
-    return df
+    export(df)
 
 
-@Export
 def nysdec_lands(df: pd.DataFrame = None):
     df = prepare("nysdec_lands")
     df = df[df.county.isin(["NEW YORK", "KINGS", "BRONX", "QUEENS", "RICHMOND"])]
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -628,10 +588,9 @@ def nysdec_lands(df: pd.DataFrame = None):
 def nysdec_solidwaste(df: pd.DataFrame = None):
     df = prepare("nysdec_solidwaste")
     df = df[df.county.isin(["New York", "Kings", "Bronx", "Queens", "Richmond"])]
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="street_name",
     house_number_field="house_number",
@@ -642,10 +601,9 @@ def nysdoccs_corrections(df: pd.DataFrame = None):
     df = prepare("nysdoccs_corrections")
     df["zipcode"] = df.zipcode.apply(lambda x: x[:5])
     df = df[df.county.isin(["New York", "Kings", "Bronx", "Queens", "Richmond"])]
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -659,10 +617,9 @@ def nysdoh_healthfacilities(df: pd.DataFrame = None):
         df.facility_county.isin(["New York", "Kings", "Bronx", "Queens", "Richmond"]), :
     ].copy()
     df["zipcode"] = df.facility_zip_code.apply(lambda x: x[:5])
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -673,22 +630,19 @@ def nysdoh_healthfacilities(df: pd.DataFrame = None):
 def nysdoh_nursinghomes(df: pd.DataFrame = None):
     df = prepare("nysdoh_nursinghomes")
     df = df[df.county.isin(["New York", "Kings", "Bronx", "Queens", "Richmond"])]
-    return df
+    export(df)
 
 
-@Export
 def nysed_nonpublicenrollment(df: pd.DataFrame = None):
     df = prepare("nysed_nonpublicenrollment")
-    return df
+    export(df)
 
 
-@Export
 def sca_enrollment_capacity(df: pd.DataFrame = None):
     df = prepare("sca_enrollment_capacity")
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -701,10 +655,9 @@ def nysed_activeinstitutions(df: pd.DataFrame = None):
     df = df[
         df.county_description.isin(["NEW YORK", "KINGS", "BRONX", "QUEENS", "RICHMOND"])
     ]
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -717,10 +670,9 @@ def nysoasas_programs(df: pd.DataFrame = None):
     df = df[
         df.provider_county.isin(["New York", "Kings", "Bronx", "Queens", "Richmond"])
     ]
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -733,10 +685,9 @@ def nysomh_mentalhealth(df: pd.DataFrame = None):
     df = df[
         df.program_county.isin(["New York", "Kings", "Bronx", "Queens", "Richmond"])
     ]
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -747,24 +698,21 @@ def nysomh_mentalhealth(df: pd.DataFrame = None):
 def nysopwdd_providers(df: pd.DataFrame = None):
     df = prepare("nysopwdd_providers")
     df = df[df.county.isin(["BRONX", "NEW YORK", "KINGS", "QUEENS", "RICHMOND"])]
-    return df
+    export(df)
 
 
-@Export
 def nysparks_historicplaces(df: pd.DataFrame = None):
     df = prepare("nysparks_historicplaces")
     df = df[df.county.isin(["Bronx", "New York", "Kings", "Queens", "Richmond"])]
-    return df
+    export(df)
 
 
-@Export
 def nysparks_parks(df: pd.DataFrame = None):
     df = prepare("nysparks_parks")
     df = df[df.county.isin(["Bronx", "New York", "Kings", "Queens", "Richmond"])]
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -776,10 +724,9 @@ def nysparks_parks(df: pd.DataFrame = None):
 @ParseAddress(raw_address_field="address")
 def qpl_libraries(df: pd.DataFrame = None):
     df = prepare("qpl_libraries")
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="street",
     house_number_field="number",
@@ -790,10 +737,9 @@ def qpl_libraries(df: pd.DataFrame = None):
 @FunctionBL(bbl_field="bbl")
 def sbs_workforce1(df: pd.DataFrame = None):
     df = prepare("sbs_workforce1")
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -803,10 +749,9 @@ def sbs_workforce1(df: pd.DataFrame = None):
 def uscourts_courts(df: pd.DataFrame = None):
     df = prepare("uscourts_courts")
     df["zipcode"] = df.buildingzip.apply(lambda x: x[:5])
-    return df
+    export(df)
 
 
-@Export
 @Function1B(
     street_name_field="parsed_sname",
     house_number_field="parsed_hnum",
@@ -829,10 +774,9 @@ def usdot_airports(df: pd.DataFrame = None):
         df.name == "JOHN F KENNEDY INTL", "airport_name"
     ] = "JOHN F KENNEDY INTL AIRPORT"
     df.loc[df.name == "LAGUARDIA", "airport_name"] = "LAGUARDIA AIRPORT"
-    return df
+    export(df)
 
 
-@Export
 def usdot_ports(df: pd.DataFrame = None):
     df = prepare("usdot_ports")
     df = df.loc[
@@ -840,11 +784,10 @@ def usdot_ports(df: pd.DataFrame = None):
         & (df.county_nam.isin(["New York", "Kings", "Bronx", "Queens", "Richmond"])),
         :,
     ]
-    return df
+    export(df)
 
 
-@Export
 def usnps_parks(df: pd.DataFrame = None):
     df = prepare("usnps_parks")
     df = df[df.state == "NY"]
-    return df
+    export(df)
