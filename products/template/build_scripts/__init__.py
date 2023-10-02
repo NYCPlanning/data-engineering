796df6dcac6cde8b2b569b27e16f45572bee9b2f
Damon McCullough <damonmcc1391@gmail.com>
2023-10-02 15:00:02 -0400

    change build tests to reflect expectations

--- products/template/build_scripts/__init__.py
+++ products/template/build_scripts/__init__.py
@@ -2,6 +2,8 @@ import sys
 from pathlib import Path
 from dotenv import load_dotenv
 
+from dcpy.utils import git, postgres
+
 load_dotenv()
 
 _product_path = Path(__file__).resolve().parent.parent
@@ -10,7 +12,16 @@ _proj_root = _product_path.parent.parent
 # Make `dcpy` available
 sys.path.append(str(_proj_root))
 
+PRODUCT_S3_NAME = "db-template"
+
 RECIPE_PATH = _product_path / "recipe.yml"
 RECIPE_LOCK_PATH = _product_path / "recipe.lock.yml"
 
 SQL_QUERY_DIR = _product_path / "sql"
+OUTPUT_DIR = _product_path / "output"
+
+BUILD_NAME = git.run_name()
+
+PG_CLIENT = postgres.PostgresClient(
+    schema=BUILD_NAME,
+)
