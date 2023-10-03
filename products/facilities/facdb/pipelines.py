d3ec508efc151cb0121bf76c3705f52446c28b46
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2023-10-03 15:48:51 -0400

    Remove `None` default argument to pipeline fns

--- products/facilities/facdb/pipelines.py
+++ products/facilities/facdb/pipelines.py
@@ -11,7 +11,7 @@ from .geocode.parseAddress import parse_address, use_airport_name
 from facdb.utility.utils import sanitize_df
 
 
-def bpl_libraries(df: pd.DataFrame = None):
+def bpl_libraries(df: pd.DataFrame):
     df["longitude"] = df.position.apply(lambda x: x.split(",")[1].strip())
     df["latitude"] = df.position.apply(lambda x: x.split(",")[0].strip())
     df["zipcode"] = df.address.apply(lambda x: x[-6:])
@@ -27,7 +27,7 @@ def bpl_libraries(df: pd.DataFrame = None):
     return df
 
 
-def nypl_libraries(df: pd.DataFrame = None):
+def nypl_libraries(df: pd.DataFrame):
     df["latitude"] = df.lat
     df["longitude"] = df.lon
     df = sanitize_df(df)
@@ -41,7 +41,7 @@ def nypl_libraries(df: pd.DataFrame = None):
     return df
 
 
-def dca_operatingbusinesses(df: pd.DataFrame = None):
+def dca_operatingbusinesses(df: pd.DataFrame):
     industry = [
         "Scrap Metal Processor",
         "Parking Lot",
@@ -70,7 +70,7 @@ def dca_operatingbusinesses(df: pd.DataFrame = None):
     return df
 
 
-def dcla_culturalinstitutions(df: pd.DataFrame = None):
+def dcla_culturalinstitutions(df: pd.DataFrame):
     df["zipcode"] = df.postcode.apply(lambda x: x[:5])
     df = sanitize_df(df)
     df = parse_address(df, raw_address_field="address")
@@ -85,7 +85,7 @@ def dcla_culturalinstitutions(df: pd.DataFrame = None):
     return df
 
 
-def dcp_colp(df: pd.DataFrame = None):
+def dcp_colp(df: pd.DataFrame):
     df["bbl"] = df.bbl.str.split(pat=".").str[0]
     df = sanitize_df(df)
     df = Function1B(
@@ -95,7 +95,7 @@ def dcp_colp(df: pd.DataFrame = None):
     return df
 
 
-def dcp_pops(df: pd.DataFrame = None):
+def dcp_pops(df: pd.DataFrame):
     df = sanitize_df(df)
     df = FunctionBN(bin_field="bin").geocode_a_dataframe(df)
     df = FunctionBL(bbl_field="bbl").geocode_a_dataframe(df)
@@ -108,12 +108,12 @@ def dcp_pops(df: pd.DataFrame = None):
     return df
 
 
-def dcp_sfpsd(df: pd.DataFrame = None):
+def dcp_sfpsd(df: pd.DataFrame):
     df = sanitize_df(df)
     return df
 
 
-def dep_wwtc(df: pd.DataFrame = None):
+def dep_wwtc(df: pd.DataFrame):
     df = sanitize_df(df)
     df = Function1B(
         street_name_field="street_name",
@@ -123,7 +123,7 @@ def dep_wwtc(df: pd.DataFrame = None):
     return df
 
 
-def dfta_contracts(df: pd.DataFrame = None):
+def dfta_contracts(df: pd.DataFrame):
     df = sanitize_df(df)
     df = parse_address(df, raw_address_field="program_address")
     df = Function1B(
@@ -134,7 +134,7 @@ def dfta_contracts(df: pd.DataFrame = None):
     return df
 
 
-def doe_busroutesgarages(df: pd.DataFrame = None):
+def doe_busroutesgarages(df: pd.DataFrame):
     # SCHOOL_YEAR = f"{datetime.date.today().year-1}-{datetime.date.today().year}"
     SCHOOL_YEAR = "2019-2020"  # No records from 2020-2021 school year
     df = df.loc[df.school_year == SCHOOL_YEAR, :]
@@ -149,7 +149,7 @@ def doe_busroutesgarages(df: pd.DataFrame = None):
     return df
 
 
-def doe_lcgms(df: pd.DataFrame = None):
+def doe_lcgms(df: pd.DataFrame):
     df = sanitize_df(df)
     df = parse_address(df, raw_address_field="primary_address")
     df = FunctionBL(bbl_field="borough_block_lot").geocode_a_dataframe(df)
@@ -162,7 +162,7 @@ def doe_lcgms(df: pd.DataFrame = None):
     return df
 
 
-def doe_universalprek(df: pd.DataFrame = None):
+def doe_universalprek(df: pd.DataFrame):
     df["boro"] = df.borough.map(
         {
             "M": "Manhattan",
@@ -183,7 +183,7 @@ def doe_universalprek(df: pd.DataFrame = None):
     return df
 
 
-def dohmh_daycare(df: pd.DataFrame = None):
+def dohmh_daycare(df: pd.DataFrame):
     df = sanitize_df(df)
     df = FunctionBN(bin_field="building_identification_number").geocode_a_dataframe(df)
     df = Function1B(
@@ -195,7 +195,7 @@ def dohmh_daycare(df: pd.DataFrame = None):
     return df
 
 
-def dot_bridgehouses(df: pd.DataFrame = None):
+def dot_bridgehouses(df: pd.DataFrame):
     df["address"] = df.address.astype(str).replace("n/a", None)
     df = sanitize_df(df)
     df = parse_address(df, raw_address_field="site")
@@ -207,7 +207,7 @@ def dot_bridgehouses(df: pd.DataFrame = None):
     return df
 
 
-def dot_ferryterminals(df: pd.DataFrame = None):
+def dot_ferryterminals(df: pd.DataFrame):
     df["bbl"] = df.bbl.fillna(0).astype(float).astype(int)
     df = sanitize_df(df)
     df = parse_address(df, raw_address_field="address")
@@ -220,7 +220,7 @@ def dot_ferryterminals(df: pd.DataFrame = None):
     return df
 
 
-def dot_mannedfacilities(df: pd.DataFrame = None):
+def dot_mannedfacilities(df: pd.DataFrame):
     df["address"] = df.address.astype(str)
     df["bbl"] = df.bbl.fillna(0).astype(float).astype(int)
     df = sanitize_df(df)
@@ -234,12 +234,12 @@ def dot_mannedfacilities(df: pd.DataFrame = None):
     return df
 
 
-def dot_pedplazas(df: pd.DataFrame = None):
+def dot_pedplazas(df: pd.DataFrame):
     df = sanitize_df(df)
     return df
 
 
-def dot_publicparking(df: pd.DataFrame = None):
+def dot_publicparking(df: pd.DataFrame):
     df["address"] = df.address.astype(str)
     df["bbl"] = df.bbl.fillna(0).astype(float).astype(int)
     df = sanitize_df(df)
@@ -253,7 +253,7 @@ def dot_publicparking(df: pd.DataFrame = None):
     return df
 
 
-def dpr_parksproperties(df: pd.DataFrame = None):
+def dpr_parksproperties(df: pd.DataFrame):
     df["zipcode"] = df.zipcode.astype(str).apply(lambda x: x[:5])
     df["boro"] = df.borough.map(
         {
@@ -275,7 +275,7 @@ def dpr_parksproperties(df: pd.DataFrame = None):
     return df
 
 
-def dsny_garages(df: pd.DataFrame = None):
+def dsny_garages(df: pd.DataFrame):
     df["address"] = df.address.astype(str)
     df = sanitize_df(df)
     df = FunctionBN(bin_field="bin").geocode_a_dataframe(df)
@@ -290,7 +290,7 @@ def dsny_garages(df: pd.DataFrame = None):
     return df
 
 
-def dsny_specialwastedrop(df: pd.DataFrame = None):
+def dsny_specialwastedrop(df: pd.DataFrame):
     df = sanitize_df(df)
     df = parse_address(df, raw_address_field="address")
     df = Function1B(
@@ -302,7 +302,7 @@ def dsny_specialwastedrop(df: pd.DataFrame = None):
     return df
 
 
-def dsny_donatenycdirectory(df: pd.DataFrame = None):
+def dsny_donatenycdirectory(df: pd.DataFrame):
     df["bbl"] = df.bbl.fillna(0).astype(float).astype(int)
     df = df[
         df["categoriesaccepted"].notna()
@@ -317,7 +317,7 @@ def dsny_donatenycdirectory(df: pd.DataFrame = None):
     return df
 
 
-def dsny_leafdrop(df: pd.DataFrame = None):
+def dsny_leafdrop(df: pd.DataFrame):
     df["bbl"] = df.bbl.fillna(0).astype(float).astype(int)
     df = sanitize_df(df)
     df = FunctionBN(bin_field="bin").geocode_a_dataframe(df)
@@ -332,7 +332,7 @@ def dsny_leafdrop(df: pd.DataFrame = None):
     return df
 
 
-def dsny_fooddrop(df: pd.DataFrame = None):
+def dsny_fooddrop(df: pd.DataFrame):
     def _loc_to_zipcode(location):
         location_str = str(location)
         return location_str[:-5] if location_str[:-5].isnumeric() else None
@@ -349,7 +349,7 @@ def dsny_fooddrop(df: pd.DataFrame = None):
     return df
 
 
-def dsny_electronicsdrop(df: pd.DataFrame = None):
+def dsny_electronicsdrop(df: pd.DataFrame):
     df["bbl"] = df.bbl.fillna(0).astype(float).astype(int)
     df = sanitize_df(df)
     df = FunctionBN(bin_field="bin").geocode_a_dataframe(df)
@@ -363,7 +363,7 @@ def dsny_electronicsdrop(df: pd.DataFrame = None):
     return df
 
 
-def dycd_afterschoolprograms(df: pd.DataFrame = None):
+def dycd_afterschoolprograms(df: pd.DataFrame):
     df["address"] = df.location_1.apply(
         lambda x: str(x).replace("nan", "").split("\n")[0][:-5]
     )
@@ -389,7 +389,7 @@ def dycd_afterschoolprograms(df: pd.DataFrame = None):
     return df
 
 
-def fbop_corrections(df: pd.DataFrame = None):
+def fbop_corrections(df: pd.DataFrame):
     df = sanitize_df(df)
     df = parse_address(df, raw_address_field="address")
     df = Function1B(
@@ -401,7 +401,7 @@ def fbop_corrections(df: pd.DataFrame = None):
     return df
 
 
-def fdny_firehouses(df: pd.DataFrame = None):
+def fdny_firehouses(df: pd.DataFrame):
     df["cleaned_address"] = df["facilityaddress"].map(
         lambda x: re.sub("[^A-Za-z0-9- ]+", "", x)
     )
@@ -432,7 +432,7 @@ def fdny_firehouses(df: pd.DataFrame = None):
     return df
 
 
-def foodbankny_foodbanks(df: pd.DataFrame = None):
+def foodbankny_foodbanks(df: pd.DataFrame):
     df = sanitize_df(df)
     df = parse_address(df, raw_address_field="address")
     df = Function1B(
@@ -443,7 +443,7 @@ def foodbankny_foodbanks(df: pd.DataFrame = None):
     return df
 
 
-def hhc_hospitals(df: pd.DataFrame = None):
+def hhc_hospitals(df: pd.DataFrame):
     df["spatial"] = df.location_1.apply(lambda x: x.split("(")[-1].replace(")", ""))
     df["longitude"] = df.spatial.apply(lambda x: x.split(",")[-1])
     df["latitude"] = df.spatial.apply(lambda x: x.split(",")[0])
@@ -461,7 +461,7 @@ def hhc_hospitals(df: pd.DataFrame = None):
     return df
 
 
-def hra_snapcenters(df: pd.DataFrame = None):
+def hra_snapcenters(df: pd.DataFrame):
     df = sanitize_df(df)
     df = FunctionBN(bin_field="bin").geocode_a_dataframe(df)
     df = FunctionBL(bbl_field="bbl").geocode_a_dataframe(df)
@@ -475,7 +475,7 @@ def hra_snapcenters(df: pd.DataFrame = None):
     return df
 
 
-def hra_jobcenters(df: pd.DataFrame = None):
+def hra_jobcenters(df: pd.DataFrame):
     df = sanitize_df(df)
     df = FunctionBN(bin_field="bin").geocode_a_dataframe(df)
     df = FunctionBL(bbl_field="bbl").geocode_a_dataframe(df)
@@ -489,7 +489,7 @@ def hra_jobcenters(df: pd.DataFrame = None):
     return df
 
 
-def hra_medicaid(df: pd.DataFrame = None):
+def hra_medicaid(df: pd.DataFrame):
     df = sanitize_df(df)
     df = FunctionBN(bin_field="bin").geocode_a_dataframe(df)
     df = FunctionBL(bbl_field="bbl").geocode_a_dataframe(df)
@@ -503,7 +503,7 @@ def hra_medicaid(df: pd.DataFrame = None):
     return df
 
 
-def moeo_socialservicesitelocations(df: pd.DataFrame = None):
+def moeo_socialservicesitelocations(df: pd.DataFrame):
     df["borough"] = df.borough.str.upper()
     df["bbl"] = df.bbl.replace("undefinedundefinedundefined", None)
     df["bbl"] = df.bbl.fillna(0).astype(float).astype(int)
@@ -523,7 +523,7 @@ def moeo_socialservicesitelocations(df: pd.DataFrame = None):
     return df
 
 
-def nycdoc_corrections(df: pd.DataFrame = None):
+def nycdoc_corrections(df: pd.DataFrame):
     df = sanitize_df(df)
     df = Function1B(
         street_name_field="street_name",
@@ -533,7 +533,7 @@ def nycdoc_corrections(df: pd.DataFrame = None):
     return df
 
 
-def nycha_communitycenters(df: pd.DataFrame = None):
+def nycha_communitycenters(df: pd.DataFrame):
     df = sanitize_df(df)
     df = FunctionBN(bin_field="bin").geocode_a_dataframe(df)
     df = FunctionBL(bbl_field="bbl").geocode_a_dataframe(df)
@@ -546,7 +546,7 @@ def nycha_communitycenters(df: pd.DataFrame = None):
     return df
 
 
-def nycha_policeservice(df: pd.DataFrame = None):
+def nycha_policeservice(df: pd.DataFrame):
     df = sanitize_df(df)
     df = parse_address(df, raw_address_field="address")
     df = Function1B(
@@ -558,7 +558,7 @@ def nycha_policeservice(df: pd.DataFrame = None):
     return df
 
 
-def nycourts_courts(df: pd.DataFrame = None):
+def nycourts_courts(df: pd.DataFrame):
     df = sanitize_df(df)
     df = parse_address(df, raw_address_field="address")
     df = Function1B(
@@ -570,13 +570,13 @@ def nycourts_courts(df: pd.DataFrame = None):
     return df
 
 
-def nysdec_lands(df: pd.DataFrame = None):
+def nysdec_lands(df: pd.DataFrame):
     df = df[df.county.isin(["NEW YORK", "KINGS", "BRONX", "QUEENS", "RICHMOND"])]
     df = sanitize_df(df)
     return df
 
 
-def nysdec_solidwaste(df: pd.DataFrame = None):
+def nysdec_solidwaste(df: pd.DataFrame):
     df = df[df.county.isin(["New York", "Kings", "Bronx", "Queens", "Richmond"])]
     df = sanitize_df(df)
     df = parse_address(df, raw_address_field="location_address")
@@ -589,7 +589,7 @@ def nysdec_solidwaste(df: pd.DataFrame = None):
     return df
 
 
-def nysdoccs_corrections(df: pd.DataFrame = None):
+def nysdoccs_corrections(df: pd.DataFrame):
     df["zipcode"] = df.zipcode.apply(lambda x: x[:5])
     df = df[df.county.isin(["New York", "Kings", "Bronx", "Queens", "Richmond"])]
     df = sanitize_df(df)
@@ -602,7 +602,7 @@ def nysdoccs_corrections(df: pd.DataFrame = None):
     return df
 
 
-def nysdoh_healthfacilities(df: pd.DataFrame = None):
+def nysdoh_healthfacilities(df: pd.DataFrame):
     df = df.loc[
         df.facility_county.isin(["New York", "Kings", "Bronx", "Queens", "Richmond"]), :
     ].copy()
@@ -618,7 +618,7 @@ def nysdoh_healthfacilities(df: pd.DataFrame = None):
     return df
 
 
-def nysdoh_nursinghomes(df: pd.DataFrame = None):
+def nysdoh_nursinghomes(df: pd.DataFrame):
     df = df[df.county.isin(["New York", "Kings", "Bronx", "Queens", "Richmond"])]
     df = sanitize_df(df)
     df = parse_address(df, raw_address_field="street_address")
@@ -631,17 +631,17 @@ def nysdoh_nursinghomes(df: pd.DataFrame = None):
     return df
 
 
-def nysed_nonpublicenrollment(df: pd.DataFrame = None):
+def nysed_nonpublicenrollment(df: pd.DataFrame):
     df = sanitize_df(df)
     return df
 
 
-def sca_enrollment_capacity(df: pd.DataFrame = None):
+def sca_enrollment_capacity(df: pd.DataFrame):
     df = sanitize_df(df)
     return df
 
 
-def nysed_activeinstitutions(df: pd.DataFrame = None):
+def nysed_activeinstitutions(df: pd.DataFrame):
     df = df[
         df.county_description.isin(["NEW YORK", "KINGS", "BRONX", "QUEENS", "RICHMOND"])
     ]
@@ -656,7 +656,7 @@ def nysed_activeinstitutions(df: pd.DataFrame = None):
     return df
 
 
-def nysoasas_programs(df: pd.DataFrame = None):
+def nysoasas_programs(df: pd.DataFrame):
     df = df[
         df.provider_county.isin(["New York", "Kings", "Bronx", "Queens", "Richmond"])
     ]
@@ -671,7 +671,7 @@ def nysoasas_programs(df: pd.DataFrame = None):
     return df
 
 
-def nysomh_mentalhealth(df: pd.DataFrame = None):
+def nysomh_mentalhealth(df: pd.DataFrame):
     df = df[
         df.program_county.isin(["New York", "Kings", "Bronx", "Queens", "Richmond"])
     ]
@@ -686,7 +686,7 @@ def nysomh_mentalhealth(df: pd.DataFrame = None):
     return df
 
 
-def nysopwdd_providers(df: pd.DataFrame = None):
+def nysopwdd_providers(df: pd.DataFrame):
     df = df[df.county.isin(["BRONX", "NEW YORK", "KINGS", "QUEENS", "RICHMOND"])]
     df = sanitize_df(df)
     df = parse_address(df, raw_address_field="street_address")
@@ -699,19 +699,19 @@ def nysopwdd_providers(df: pd.DataFrame = None):
     return df
 
 
-def nysparks_historicplaces(df: pd.DataFrame = None):
+def nysparks_historicplaces(df: pd.DataFrame):
     df = df[df.county.isin(["Bronx", "New York", "Kings", "Queens", "Richmond"])]
     df = sanitize_df(df)
     return df
 
 
-def nysparks_parks(df: pd.DataFrame = None):
+def nysparks_parks(df: pd.DataFrame):
     df = df[df.county.isin(["Bronx", "New York", "Kings", "Queens", "Richmond"])]
     df = sanitize_df(df)
     return df
 
 
-def qpl_libraries(df: pd.DataFrame = None):
+def qpl_libraries(df: pd.DataFrame):
     df = sanitize_df(df)
     df = parse_address(df, raw_address_field="address")
     df = FunctionBL(bbl_field="bbl").geocode_a_dataframe(df)
@@ -725,7 +725,7 @@ def qpl_libraries(df: pd.DataFrame = None):
     return df
 
 
-def sbs_workforce1(df: pd.DataFrame = None):
+def sbs_workforce1(df: pd.DataFrame):
     df = sanitize_df(df)
     df = FunctionBL(bbl_field="bbl").geocode_a_dataframe(df)
     df = FunctionBN(bin_field="bin").geocode_a_dataframe(df)
@@ -738,7 +738,7 @@ def sbs_workforce1(df: pd.DataFrame = None):
     return df
 
 
-def uscourts_courts(df: pd.DataFrame = None):
+def uscourts_courts(df: pd.DataFrame):
     df["zipcode"] = df.buildingzip.apply(lambda x: x[:5])
     df = sanitize_df(df)
     df = parse_address(df, raw_address_field="buildingaddress")
@@ -750,7 +750,7 @@ def uscourts_courts(df: pd.DataFrame = None):
     return df
 
 
-def usdot_airports(df: pd.DataFrame = None):
+def usdot_airports(df: pd.DataFrame):
     df = df.loc[
         (df["state_name"] == "NEW YORK")
         & (df.county.isin(["NEW YORK", "KINGS", "BRONX", "QUEENS", "RICHMOND"])),
@@ -775,7 +775,7 @@ def usdot_airports(df: pd.DataFrame = None):
     return df
 
 
-def usdot_ports(df: pd.DataFrame = None):
+def usdot_ports(df: pd.DataFrame):
     df = df.loc[
         (df.state_post == "NY")
         & (df.county_nam.isin(["New York", "Kings", "Bronx", "Queens", "Richmond"])),
@@ -785,7 +785,7 @@ def usdot_ports(df: pd.DataFrame = None):
     return df
 
 
-def usnps_parks(df: pd.DataFrame = None):
+def usnps_parks(df: pd.DataFrame):
     df = df[df.state == "NY"]
     df = sanitize_df(df)
     return df
