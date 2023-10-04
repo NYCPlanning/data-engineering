d8824fecd321b51452a65943adb212da00585bf0
Damon McCullough <damonmcc1391@gmail.com>
2023-10-04 00:22:19 -0400

    delete out of date todos

--- dcpy/test/connectors/test_github.py
+++ dcpy/test/connectors/test_github.py
@@ -3,7 +3,6 @@ from dcpy.connectors.github import get_default_branch, get_branches
 TEST_REPO = "data-engineering"
 
 
-## TODO - these tests don't belong here since this functionality has been moved to dcpy
 def test_get_default_branch():
     branch = get_default_branch(repo=TEST_REPO)
     assert branch == "main"
