9e11f14cd1155f8d1f33c0b71b64e48584a133b1
fvankrieken <fvankrieken@planning.nyc.gov>
2023-10-06 14:17:34 +0000

    fix checkbook upload call

--- products/checkbook/build_scripts/export.py
+++ products/checkbook/build_scripts/export.py
@@ -1,11 +1,11 @@
 from datetime import date
 
 from dcpy.utils import git
-from dcpy.connectors.edm.publishing import upload
+from dcpy.connectors.edm import publishing
 
 from . import OUTPUT_DIR
 
-PUBLISHING_FOLDER = "db-checkbook"
+PRODUCT = "db-checkbook"
 
 
 if __name__ == "__main__":
@@ -13,5 +13,7 @@ if __name__ == "__main__":
     with open("version.txt", "w") as f:
         f.write(str(date.today()))
     build_environment = git.branch()
-    upload(OUTPUT_DIR, PUBLISHING_FOLDER, build_environment, "public-read")
+    publishing.upload(
+        OUTPUT_DIR, publishing.DraftKey(PRODUCT, build_environment), acl="public-read"
+    )
     print("Finished export!")
