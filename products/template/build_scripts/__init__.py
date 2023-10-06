7e07141711711b0ab537f94a5994eba2e0adf050
Damon McCullough <damonmcc1391@gmail.com>
2023-10-06 14:49:53 -0400

    drop project path append

--- products/template/build_scripts/__init__.py
+++ products/template/build_scripts/__init__.py
@@ -7,10 +7,6 @@ from dcpy.utils import git, postgres
 load_dotenv()
 
 _product_path = Path(__file__).resolve().parent.parent
-_proj_root = _product_path.parent.parent
-
-# Make `dcpy` available
-sys.path.append(str(_proj_root))
 
 PRODUCT_S3_NAME = "db-template"
 
