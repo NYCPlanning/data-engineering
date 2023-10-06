c4bf6219eead54f8042e8749face187f32f183ee
Damon McCullough <damonmcc1391@gmail.com>
2023-10-06 15:49:58 -0400

    use builds.load with product name as cli stage

--- dcpy/builds/environment.py
+++
@@ -1,7 +0,0 @@
-from dcpy.utils import postgres
-
-
-def setup(pg_client: postgres.PostgresClient):
-    pg_client.drop_schema()
-    pg_client.create_schema()
-    # TODO delete draft S3 folder
--- dcpy/builds/load.py
+++ dcpy/builds/load.py
@@ -1,13 +1,29 @@
 from pathlib import Path
-from dcpy.utils import postgres
+import typer
+
+from dcpy import REPO_ROOT_PATH
+from dcpy.utils import git, postgres
+from dcpy.utils.logging import logger
 from dcpy.connectors.edm import recipes
 
 
-def load_source_data(
-    pg_client: postgres.PostgresClient,
-    recipe_path: Path,
-):
+def setup_environment(pg_client: postgres.PostgresClient):
+    pg_client.drop_schema()
+    pg_client.create_schema()
+    # TODO delete draft S3 folder
+
+
+def load_source_data(product: str):
+    product_path = REPO_ROOT_PATH / f"products/{product}"
+    build_name = git.run_name()
+    pg_client = postgres.PostgresClient(
+        schema=build_name,
+        database=f"db-{product}",
+    )
+
+    recipe_path = product_path / "recipe.yml"
     recipe_lock_path = recipe_path.parent / "recipe.lock.yml"
+
     recipes.plan(
         recipe_path,
         recipe_lock_path,
@@ -15,9 +31,31 @@ def load_source_data(
     recipe = recipes.recipe_from_yaml(Path(recipe_lock_path))
     recipes.write_source_data_versions(recipe_file=Path(recipe_lock_path))
 
+    setup_environment(pg_client)
+
     [recipes.import_dataset(dataset, pg_client) for dataset in recipe.inputs.datasets]
 
     pg_client.create_table_from_csv(
         "source_data_versions",
         recipe_lock_path.parent / "source_data_versions.csv",
     )
+
+
+app = typer.Typer(add_completion=False)
+
+
+@app.command("load")
+def _cli_wrapper_load(
+    product: str = typer.Option(
+        None,
+        "-p",
+        "--product",
+        help="Name of data product (products subfolder in repo)",
+    ),
+):
+    logger.info(f"Loading {product} source data")
+    load_source_data(product)
+
+
+if __name__ == "__main__":
+    app()
