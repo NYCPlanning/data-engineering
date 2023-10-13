dd94772372e928c2a0f2c99d70796c8399e001ea
fvankrieken <fvankrieken@planning.nyc.gov>
2023-10-13 20:58:15 +0000

    load typecategory patterns into seed table

--- products/cpdb/sql/_create.sql
+++ products/cpdb/sql/_create.sql
@@ -62,3 +62,36 @@ CREATE TABLE dcp_cpdb_agencyverified (
     zipcode character varying
 );
 \COPY dcp_cpdb_agencyverified FROM 'data/dcp_cpdb_agencyverified.csv' DELIMITER ',' CSV HEADER;
+
+DROP TABLE IF EXISTS type_category_patterns CASCADE;
+CREATE TABLE type_category_patterns (
+    pattern character varying,
+    typecategory character varying
+);
+
+CREATE TEMP TABLE staging_patterns (
+    pattern character varying
+);
+
+\COPY staging_patterns FROM 'data/type_patterns_ittvequ.csv' DELIMITER ',' CSV;
+INSERT INTO type_category_patterns (pattern, typecategory)
+SELECT
+    pattern,
+    'ITT, Vehicles, and Equipment' AS typecategory
+FROM staging_patterns;
+DELETE FROM staging_patterns;
+
+\COPY staging_patterns FROM 'data/type_patterns_lump_sum.csv' DELIMITER ',' CSV;
+INSERT INTO type_category_patterns (pattern, typecategory)
+SELECT
+    pattern,
+    'Lump Sum' AS typecategory
+FROM staging_patterns;
+DELETE FROM staging_patterns;
+
+\COPY staging_patterns FROM 'data/type_patterns_fixed_asset.csv' DELIMITER ',' CSV;
+INSERT INTO type_category_patterns (pattern, typecategory)
+SELECT
+    pattern,
+    'Fixed Asset' AS typecategory
+FROM staging_patterns;
