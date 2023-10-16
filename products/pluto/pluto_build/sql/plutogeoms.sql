da2ddc8d6297d9432377da5aa126d39c9aa5f90f
Damon McCullough <damonmcc1391@gmail.com>
2023-10-16 14:54:57 -0400

    use build-specific schema for pluto build

--- products/pluto/pluto_build/sql/plutogeoms.sql
+++ products/pluto/pluto_build/sql/plutogeoms.sql
@@ -1,6 +1,6 @@
 -- add geometry information onto pluto
 -- add a geometry column
-SELECT AddGeometryColumn('public', 'pluto', 'geom', 4326, 'Geometry', 2);
+SELECT AddGeometryColumn(:'build_schema', 'pluto', 'geom', 4326, 'Geometry', 2);
 
 -- join on geometry data
 UPDATE pluto a
