2d984488d10089809b665522625c4aae854ac720
Damon McCullough <damonmcc1391@gmail.com>
2023-10-16 10:23:54 -0400

    rename TDB test data var for clarity

--- products/template/tests/test_utils.py
+++ products/template/tests/test_utils.py
@@ -3,7 +3,7 @@ import geopandas as gpd
 import pandas as pd
 import pytest
 
-from . import TEST_DATA_PATH, TOY_SECRET
+from . import TEST_DATA_DIR, TOY_SECRET
 
 from python.utils import load_data_file, load_geodata_url, load_shapefile
 
@@ -13,14 +13,14 @@ def test_use_of_github_secret():
 
 
 def test_load_data_file_csv():
-    data = load_data_file(filepath=f"{TEST_DATA_PATH}/minimal_data.csv")
+    data = load_data_file(filepath=f"{TEST_DATA_DIR}/minimal_data.csv")
     assert isinstance(data, pd.DataFrame)
     assert len(data) == 3
     assert data["column_b"][2] == "b_2"
 
 
 def test_load_data_file_json():
-    data = load_data_file(filepath=f"{TEST_DATA_PATH}/minimal_data.json")
+    data = load_data_file(filepath=f"{TEST_DATA_DIR}/minimal_data.json")
     assert isinstance(data, pd.DataFrame)
     assert len(data) == 2
     assert data.iloc[1]["key_int"] == 300
@@ -28,11 +28,11 @@ def test_load_data_file_json():
 
 def test_load_data_file_not_implemented():
     with pytest.raises(NotImplementedError):
-        load_data_file(filepath=f"{TEST_DATA_PATH}/data_file.parquet")
+        load_data_file(filepath=f"{TEST_DATA_DIR}/data_file.parquet")
 
 
 def test_load_shapefile():
-    data = load_shapefile(filepath=f"{TEST_DATA_PATH}/Open Streets Locations.zip")
+    data = load_shapefile(filepath=f"{TEST_DATA_DIR}/Open Streets Locations.zip")
     assert isinstance(data, gpd.GeoDataFrame)
     assert len(data) == 325
 
