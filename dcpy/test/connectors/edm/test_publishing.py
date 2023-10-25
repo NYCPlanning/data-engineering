04d150fe53d738f1943241996c53899051849b91
sf-dcp <ofilippova@planning.nyc.gov>
2023-10-25 19:24:25 +0000

    Move conftest.py to test/ directory to make fixtures available to all tests.

--- dcpy/test/connectors/edm/test_publishing.py
+++ dcpy/test/connectors/edm/test_publishing.py
@@ -1,10 +1,27 @@
 import pytest
+import tempfile
 from dcpy.connectors.edm import publishing
 
+
 TEST_PRODUCT_NAME = "test_product"
-TEST_VERSION = "version_001"
+TEST_BUILD = "build_branch"
+TEST_VERSION = "v001"
 
 
-@pytest.fixture
-def get_draft_key(scope="function"):
-    return publishing.DraftKey(product=TEST_PRODUCT_NAME, build=TEST_VERSION)
+@pytest.fixture(scope="module")
+def get_draft_key():
+    return publishing.DraftKey(product=TEST_PRODUCT_NAME, build=TEST_BUILD)
+
+
+@pytest.fixture(scope="module")
+def get_publish_key():
+    return publishing.DraftKey(product=TEST_PRODUCT_NAME, version=TEST_VERSION)
+
+
+@pytest.fixture(scope="module")
+def func():
+    pass
+
+
+def test_upload():
+    pass
