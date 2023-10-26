f6ee53899b36dfc2587f97078916da63e27d106d
sf-dcp <ofilippova@planning.nyc.gov>
2023-10-26 23:08:25 +0000

    Revise test bucket name: Code in publishing.py uses a hard coded bucket name. Changing test bucket name value to be able to test the functionality.

---
+++ dcpy/test/conftest.py
@@ -0,0 +1,23 @@
+import boto3
+import os
+import pytest
+from moto import mock_s3
+from dcpy.utils.s3 import client
+
+
+TEST_BUCKET_NAME = "edm-publishing"
+
+
+@pytest.fixture(scope="module")
+def s3_client():
+    """Mocked S3 client. Note, mock_s3() takes care of updating
+    env variables for fake AWS credentials."""
+    with mock_s3():
+        yield client()
+
+
+@pytest.fixture(scope="module")
+def create_bucket(s3_client):
+    """Creates a test S3 bucket."""
+    s3_client.create_bucket(Bucket=TEST_BUCKET_NAME)
+    yield
