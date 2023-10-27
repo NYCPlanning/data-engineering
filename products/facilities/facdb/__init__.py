42c65b927f0f9fc1d9ab469e9269e43454d4c2ef
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2023-10-27 08:53:20 -0400

    Remove facdby init cruft

--- products/facilities/facdb/__init__.py
+++ products/facilities/facdb/__init__.py
@@ -1,17 +1,11 @@
 import os
-import sys
 from pathlib import Path
-from dotenv import load_dotenv
 
 
 _module_top_path = Path(__file__).resolve().parent
 PRODUCT_PATH = _module_top_path.parent
 _proj_root = PRODUCT_PATH.parent.parent
 
-# Make `dcpy` available
-sys.path.append(str(_proj_root))
-
-load_dotenv(PRODUCT_PATH / "version.env")
 
 SQL_PATH = _module_top_path / "sql"
 BASH_PATH = _module_top_path / "bash"
