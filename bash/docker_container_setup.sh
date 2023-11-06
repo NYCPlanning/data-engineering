544d84e816b0f3a502951973b484b3121a93ddcf
fvankrieken <fvankrieken@planning.nyc.gov>
2023-11-06 20:53:45 +0000

    fix dob_cofos data library script, make less error prone, include dcpy in data library

--- bash/docker_container_setup.sh
+++ bash/docker_container_setup.sh
@@ -3,13 +3,12 @@
 mc config host add spaces $AWS_S3_ENDPOINT $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY --api S3v4
 
 if [[ -z "$CI" ]]; then
-    workspace=/workspace
-else
-    workspace=/__w/data-engineering/data-engineering
-    option="-e"
+    export workspace=/workspace
+    python3 -m pip install -e . -c ./python/constraints.txt
     python3 -m pip install -e data-library -c ./python/constraints.txt
+else
+    export workspace=/__w/data-engineering/data-engineering
+    python3 -m pip install . -c ./python/constraints.txt
 fi 
 
 git config --global --add safe.directory $workspace
-
-python3 -m pip install $option . -c ./python/constraints.txt
