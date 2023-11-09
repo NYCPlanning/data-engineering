30e4390c1118311c5eb30d5802dfa8021c7f48f3
Damon McCullough <damonmcc1391@gmail.com>
2023-11-08 19:10:25 -0500

    oops

--- products/knownprojects/bash/03_aggregate.sh
+++ products/knownprojects/bash/03_aggregate.sh
@@ -11,12 +11,12 @@ run_sql_file aggregate/create_zap_projects.sql
 ## Do SCA aggregation
 echo "Create the longfrom SCA Aggregate Tables..."
 
-# HACK disable to optimize
-# # Preprocess the tables to standardize geometry column name 
-# echo "Preprocess column names to standardize"
-# run_sql_file aggregate/sca/preprocessing.sql
+# Preprocess the tables to standardize geometry column name 
+echo "Preprocess column names to standardize"
+run_sql_file aggregate/sca/preprocessing.sql
 
-# # Aggregate KPDB projects to Elementary School Zones 
+# HACK disable to optimize
+# Aggregate KPDB projects to Elementary School Zones 
 # echo "Build Elementary School Zones Aggregate Table"
 # run_sql_file aggregate/sca/boundaries_es_zone.sql
 
@@ -24,6 +24,7 @@ echo "Create the longfrom SCA Aggregate Tables..."
 # echo "Build School Districts aggregate table"
 # run_sql_file aggregate/sca/boundaries_school_districts.sql
 
+# HACK optimize this query
 # Aggregate KPDB projects to School Subdistrict Zones
 echo "Build School Subdistricts aggregate tables"
 run_sql_file aggregate/sca/boundaries_school_subdistricts.sql
