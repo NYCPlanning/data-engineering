560ee38d401d36f549dab87bafeba93abcc235d8
Damon McCullough <damonmcc1391@gmail.com>
2023-11-09 17:52:19 -0500

    enhance multi-line logging

--- dcpy/utils/logging.py
+++ dcpy/utils/logging.py
@@ -1,6 +1,23 @@
 import logging
 
-logging.basicConfig()
+
+class LoggingHandler(logging.StreamHandler):
+    def __init__(self):
+        super(LoggingHandler, self).__init__()
+
+    def emit(self, record):
+        # Ensure multi-line logs have consistent log formatting
+        messages = record.msg.split("\n")
+        for message in messages:
+            record.msg = message
+            super(LoggingHandler, self).emit(record)
+
+
 logging.root.setLevel(logging.INFO)
 
+handler = LoggingHandler()
+formatter = logging.Formatter(logging.BASIC_FORMAT)
+handler.setFormatter(formatter)
+
 logger = logging.getLogger("dcpy")
+logger.addHandler(handler)
