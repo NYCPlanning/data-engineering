ff34f7a1efc13c5e5ab22e5bfe0d2d99c39bcc0d
Damon McCullough <damonmcc1391@gmail.com>
2023-11-13 11:38:02 -0500

    disable table renaming for now

--- products/knownprojects/python/dedup_projects.py
+++ products/knownprojects/python/dedup_projects.py
@@ -31,8 +31,8 @@ if __name__ == "__main__":
     records_dropped_count = kpdb.shape[0] - kpdb_no_duplicates.shape[0]
     logger.info(f"# of duplicate records removed:\n\t{records_dropped_count}")
 
-    logger.info(f"Saving original data as '{INTERMEDIATE_TABLE_NAME}' ...")
-    pg_client.insert_dataframe(df=kpdb, table_name=INTERMEDIATE_TABLE_NAME)
+    # logger.info(f"Saving original data as '{INTERMEDIATE_TABLE_NAME}' ...")
+    # pg_client.insert_dataframe(df=kpdb, table_name=INTERMEDIATE_TABLE_NAME)
 
-    logger.info(f"Updating final table '{FINAL_TABLE_NAME}' ...")
-    pg_client.insert_dataframe(df=kpdb_no_duplicates, table_name=FINAL_TABLE_NAME)
+    # logger.info(f"Updating final table '{FINAL_TABLE_NAME}' ...")
+    # pg_client.insert_dataframe(df=kpdb_no_duplicates, table_name=FINAL_TABLE_NAME)
