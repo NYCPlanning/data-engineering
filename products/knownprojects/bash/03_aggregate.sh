889e21d92b36739b189cf6f96fc6f27de9836e33
Damon McCullough <damonmcc1391@gmail.com>
2023-11-14 12:27:47 -0500

    better running aggregations in parallel

--- products/knownprojects/bash/03_aggregate.sh
+++ products/knownprojects/bash/03_aggregate.sh
@@ -15,36 +15,40 @@ echo "Create the longfrom SCA Aggregate Tables..."
 echo "Preprocess column names to standardize"
 run_sql_file aggregate/sca/preprocessing.sql
 
-# HACK disable to optimize
-# Aggregate KPDB projects to Elementary School Zones 
-# echo "Build Elementary School Zones Aggregate Table"
-# run_sql_file aggregate/sca/boundaries_es_zone.sql
-
 # HACK optimize these
+# Aggregate KPDB projects to Elementary School Zones 
+echo "Build Elementary School Zones Aggregate Table"
+run_sql_file aggregate/sca/boundaries_es_zone.sql &
+
 # Aggregate KPDB projects to School District Zones
 echo "Build School Districts aggregate table"
 run_sql_file aggregate/sca/boundaries_school_districts.sql &
 
-# Aggregate KPDB projects to School Subdistrict Zones
-echo "Build School Subdistricts aggregate tables"
-run_sql_file aggregate/sca/boundaries_school_subdistricts.sql
-wat
+# HACK disbale due to breaking changes
+# # Aggregate KPDB projects to School Subdistrict Zones
+# echo "Build School Subdistricts aggregate tables"
+# run_sql_file aggregate/sca/boundaries_school_subdistricts.sql
+wait
 
 echo "SCA sggregations are complete"
 
-# HACK disable to optimize
-# ## Do other aggregation
-# echo "Preprocess column names to standardize"
-# run_sql_file aggregate/preprocessing.sql
+# HACK optimize these
+## Do other aggregation
+echo "Create the longfrom Aggregate Tables..."
 
-# # Aggregate KPDB projects to Community Districts
-# echo "Build Community Districts aggregate tables"
-# run_sql_file aggregate/census_tracts.sql
-# run_sql_file aggregate/ntas.sql
-# run_sql_file aggregate/cdtas.sql
-# run_sql_file aggregate/community_districts.sql
+# Preprocess the tables to standardize geometry column name 
+echo "Preprocess column names to standardize"
+run_sql_file aggregate/preprocessing.sql
 
-# echo "Other aggregations are complete"
+# Aggregate KPDB projects to Community Districts
+echo "Build Community Districts aggregate tables"
+run_sql_file aggregate/census_tracts.sql &
+run_sql_file aggregate/ntas.sql &
+run_sql_file aggregate/cdtas.sql &
+run_sql_file aggregate/community_districts.sql &
+wait
+
+echo "Other aggregations are complete"
 
 # run_sql_command "ALTER TABLE _kpdb RENAME COLUMN geometry TO geom;"
 
