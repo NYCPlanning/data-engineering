9f0025ee8ac7b92b34a974f4d2a8b6530958fbb2
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2023-11-14 14:25:09 -0500

    Call build_env_setup.sh in docker_container_setup

--- bash/docker_container_setup.sh
+++ bash/docker_container_setup.sh
@@ -2,6 +2,9 @@
 
 mc config host add spaces $AWS_S3_ENDPOINT $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY --api S3v4
 
+PARENT_DIR=$(dirname "$(readlink -f "$0")")
+source ${PARENT_DIR}/build_env_setup.sh
+
 if [[ -z "$CI" ]]; then
     workspace=/workspace
 else
