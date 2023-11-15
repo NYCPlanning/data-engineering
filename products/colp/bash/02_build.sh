3c07633326585552f75999c14c3f4d1ca3a428f5
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2023-11-15 14:32:49 -0500

    Pass BUILD_SCHEMA to corrections pg function
    The corrections function formerly just looked in the public schema, so
    it was failing to correct anything.

--- products/colp/bash/02_build.sh
+++ products/colp/bash/02_build.sh
@@ -12,8 +12,7 @@ run_sql_file sql/_procedures.sql
 run_sql_file sql/clean_parcelname.sql
 run_sql_file sql/create_colp.sql
 
-
-run_sql_command "CALL apply_correction('_colp', 'modifications');"
+run_sql_command "CALL apply_correction('_colp', '${BUILD_ENGINE_SCHEMA}', 'modifications');"
 
 echo "Generate output tables"
 run_sql_file sql/export_colp.sql
