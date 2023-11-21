48d57ff9763a5ca7b10a0121448c65533bf11b60
fvankrieken <fvankrieken@planning.nyc.gov>
2023-11-21 16:51:44 +0000

    fix paths, dcpy install for qa app docker build

--- apps/qa/bash/docker_build_and_publish.sh
+++ apps/qa/bash/docker_build_and_publish.sh
@@ -7,11 +7,14 @@ ROOT_DIR=$PROJECT_DIR/../..
 source $ROOT_DIR/bash/utils.sh
 set_error_traps
 
-cp -r $ROOT_DIR/dcpy .
-cp -r $ROOT_DIR/python/constraints.txt $PROJECT_DIR
+cp $ROOT_DIR/pyproject.toml $PROJECT_DIR
+cp $ROOT_DIR/python/constraints.txt $PROJECT_DIR
+cp -r $ROOT_DIR/dcpy $PROJECT_DIR
 
 # Build image - Once we reach some sort of MVP, maybe worth starting versioning. For now, just latest
 docker_login
 build_and_publish_docker_image $PROJECT_DIR nycplanning/qa-streamlit latest
 
-rm -rf ./dcpy
+rm $PROJECT_DIR/pyproject.toml
+rm $PROJECT_DIR/constraints.txt
+rm -rf $PROJECT_DIR/dcpy
