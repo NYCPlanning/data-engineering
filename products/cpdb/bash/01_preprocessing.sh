0cfcd5ac7c49262966f76ae7ec534a92d3611f42
fvankrieken <fvankrieken@planning.nyc.gov>
2023-11-28 20:52:20 +0000

    use dcpy recipe/build logic in cpdb

---
+++ products/cpdb/bash/01_preprocessing.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+source bash/config.sh
+
+run_sql_file "sql/_create.sql"
+
+echo "fixing dot_bridges"
+run_sql_command "ALTER TABLE dot_projects_bridges RENAME COLUMN fmsid TO fms_id;"
+python3 python/dot_bridges.py
+
+echo "fixing doitt_buildingfootprints"
+run_sql_command "ALTER TABLE doitt_buildingfootprints RENAME TO doitt_buildingfootprints_source;"
+python3 python/doitt_buildingfootprints.py
+run_sql_command "DROP TABLE IF EXISTS doitt_buildingfootprints_source;"
+
+
+echo "renaming cpdb_capital_spending to capital_spending"
+run_sql_command "DROP TABLE IF EXISTS capital_spending;"
+run_sql_command "ALTER TABLE cpdb_capital_spending RENAME TO capital_spending;"
