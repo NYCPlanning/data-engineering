3f5d1ea3c88a62a55e6374b35a03005b3f2a0687
fvankrieken <fvankrieken@planning.nyc.gov>
2023-11-29 20:29:54 +0000

    delete stale dev docker images

---
+++ docker/delete.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# big thanks to https://devopscell.com/docker/dockerhub/2018/04/09/delete-docker-image-tag-dockerhub.html
+ORGANIZATION="nycplanning"
+IMAGE="$1"
+TAG="$2"
+
+login_data() {
+cat <<EOF
+{
+  "username": "$DOCKER_USER",
+  "password": "$DOCKER_PASSWORD"
+}
+EOF
+}
+
+TOKEN=`curl -s -H "Content-Type: application/json" -X POST -d "$(login_data)" "https://hub.docker.com/v2/users/login/" | jq -r .token`
+
+curl "https://hub.docker.com/v2/repositories/${ORGANIZATION}/${IMAGE}/tags/${TAG}/" \
+-X DELETE \
+-H "Authorization: JWT ${TOKEN}"
