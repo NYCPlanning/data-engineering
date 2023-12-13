76e758837bd9727325b09d6fd025c98beea7c329
fvankrieken <fvankrieken@planning.nyc.gov>
2023-12-13 19:30:25 +0000

    hard-coded ordering for flipped condo lots

--- products/pluto/pluto_build/sql/dedupecondotable.sql
+++ products/pluto/pluto_build/sql/dedupecondotable.sql
@@ -47,14 +47,25 @@ WHERE id IN (
             id,
             ROW_NUMBER() OVER (
                 PARTITION BY condo_base_bbl
-                ORDER BY condo_base_bbl
+                -- TODO This hard-coding is a temporary fix to preserve ordering from 23v3
+                -- 24v1 should have the `condo_billing_bbl IN ...` removed, and order just by condo_billing_bbl
+                ORDER BY condo_billing_bbl IN (
+                    '3002787502',
+                    '3003047501',
+                    '4050667502',
+                    '4050667507',
+                    '4076217502',
+                    '5014957501',
+                    '5024017501',
+                    '5030247501',
+                    '5031737504',
+                    '5070487502',
+                    '5070557501'
+                ) DESC,
+                condo_billing_bbl
             ) AS row_num
         FROM pluto_condo
+        WHERE condo_billing_bbl IS NOT NULL
     ) AS t
     WHERE t.row_num > 1
-)
-AND condo_billing_bbl NOT IN (
-    SELECT DISTINCT billingbbl
-    FROM pluto_rpad_geo
-    WHERE billingbbl IS NOT NULL
 );
