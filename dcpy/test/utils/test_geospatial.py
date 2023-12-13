cf4726deb4a7cc05f02632880a616b78466a5471
Damon McCullough <damonmcc1391@gmail.com>
2023-12-12 23:13:09 -0500

    handle wkt data in `convert_to_geodata`

--- dcpy/test/utils/test_geospatial.py
+++ dcpy/test/utils/test_geospatial.py
@@ -17,6 +17,13 @@ def data_wkb() -> pd.DataFrame:
     )
 
 
+@pytest.fixture(scope="module")
+def data_wkt() -> pd.DataFrame:
+    return pd.read_csv(
+        RESOURCES_DIR / "data_wkt.csv",
+    )
+
+
 def test_convert_to_geodata_wkb(data_wkb):
     geodata = geospatial.convert_to_geodata(
         data=data_wkb,
@@ -26,15 +33,37 @@ def test_convert_to_geodata_wkb(data_wkb):
 
     assert isinstance(geodata, gpd.GeoDataFrame)
     assert geodata.columns.to_list() == ["row_id", "geometry", "geometry_error"]
+    assert isinstance(geodata["geometry"], gpd.GeoSeries)
+    assert geodata.geom_type.to_list() == ["Point", "MultiPolygon", None, None, None]
+    assert geodata.iloc[2:]["geometry_error"].to_list() == [
+        "ParseException: Unexpected EOF parsing WKB",
+        "Expected bytes or string, got float",
+        "Expected bytes or string, got float",
+    ]
+    assert str(geodata.crs) == geospatial.GeometryCRS.wgs_84_deg.value
+
+
+# @pytest.mark.skip
+def test_convert_to_geodata_wkt(data_wkt):
+    geodata = geospatial.convert_to_geodata(
+        data=data_wkt,
+        geometry_format=geospatial.GeometryFormat.wkt,
+        geometry_column="geom",
+    )
+
+    assert isinstance(geodata, gpd.GeoDataFrame)
+    assert geodata.columns.to_list() == ["row_id", "geometry", "geometry_error"]
+    assert isinstance(geodata["geometry"], gpd.GeoSeries)
     assert geodata.geom_type.to_list() == ["Point", "MultiPolygon", None, None, None]
     assert str(geodata.crs) == geospatial.GeometryCRS.wgs_84_deg.value
 
 
-def test_from_wkb_fails(data_wkb):
-    with pytest.raises(
-        shapely.errors.GEOSException, match=r"Unexpected EOF parsing WKB"
-    ):
+def test_geoseries_constructors_fail(data_wkb, data_wkt):
+    with pytest.raises(shapely.errors.GEOSException):
         data_wkb["new_geometry_column"] = gpd.GeoSeries.from_wkb(data_wkb["geom"])
+
+    with pytest.raises(shapely.errors.GEOSException):
+        data_wkb["new_geometry_column"] = gpd.GeoSeries.from_wkt(data_wkt["geom"])
 
 
 def test_projected_crs(data_wkb):
