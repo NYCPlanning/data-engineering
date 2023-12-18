7a5b11aaae62c9a53ee2865f6f1630387d7c40f3
Damon McCullough <damonmcc1391@gmail.com>
2023-12-18 00:20:48 -0500

    expand zap util test

--- products/zap-opendata/tests/test_util.py
+++ products/zap-opendata/tests/test_util.py
@@ -4,6 +4,9 @@ from src.util import timestamp_to_date
 
 def test_timestamp_to_date(test_data_path):
     data = pd.read_csv(f"{test_data_path}/timestamp_data.csv")
-    data_dates = timestamp_to_date(data, date_columns=["date_column_a"])
+    data_dates = timestamp_to_date(
+        data, date_columns=["date_column_a", "date_column_b"]
+    )
 
     assert data_dates["date_column_a"].equals(data_dates["date_column_a_expected"])
+    assert data_dates["date_column_b"].equals(data_dates["date_column_b_expected"])
