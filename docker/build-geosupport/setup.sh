9e621f1a6bebe0117e0cb0dd0795da55ea669fef
fvankrieken <fvankrieken@planning.nyc.gov>
2023-12-29 18:20:39 +0000

    rework build images to support gdal python bindings

--- docker/build-geosupport/setup.sh
+++ docker/build-geosupport/setup.sh
@@ -8,5 +8,6 @@ apt-get update && export DEBIAN_FRONTEND=noninteractive \
 install_geosupport
 install_yq
 install_mc
+install_gdal_python_reqs
 
 python3 -m pip install -r requirements.txt -c constraints.txt
--- docker/dev/setup.sh
+++ docker/dev/setup.sh
@@ -3,11 +3,11 @@ source config.sh
 set -e
 
 apt-get update && export DEBIAN_FRONTEND=noninteractive \
-    && apt-get -y install $DEV_APT_PACKAGES \
-    && apt-get -y install --reinstall build-essential # needed to install gdal. I don't know why, it just is
+    && apt-get -y install $DEV_APT_PACKAGES
 
 install_geosupport
 install_yq
 install_mc
+install_gdal_python_reqs
 
 python3 -m pip install -r requirements.txt
