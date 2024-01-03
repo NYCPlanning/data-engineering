5cfe42977415bb6fdc2591fead1aa6e37cc16468
Damon McCullough <damonmcc1391@gmail.com>
2024-01-03 15:53:18 -0500

    add kpdb recipe

--- products/knownprojects/bash/01_dataloading.sh
+++ products/knownprojects/bash/01_dataloading.sh
@@ -3,21 +3,12 @@ source ../../bash/utils.sh
 set_error_traps
 max_bg_procs 5
 
-if [ -n "${BUILD_ENGINE_SCHEMA}" ]; then
-    echo "Dropping and creating build schema '$BUILD_ENGINE_SCHEMA'"
-    run_sql_command "DROP SCHEMA IF EXISTS ${BUILD_ENGINE_SCHEMA} CASCADE;"
-    # run_sql_command "VACUUM (FULL);"
-    run_sql_command "CREATE SCHEMA ${BUILD_ENGINE_SCHEMA};"
-fi
-
 # Load source data
+python3 -m dcpy.builds.load recipe --recipe-path recipe.yml
+
+# download data/raw and data/corrections
 rm -rf data
 mkdir -p data
-
-create_source_data_table
-
-# download data/raw
-# download data/corrections
 python3 -m python.download
 
 # extract data/raw -> SQL DB
@@ -34,29 +25,6 @@ python3 -m python.extractors dcp_knownprojects
 
 # Load corrections tables
 run_sql_file sql/create_corrections.sql
-
-# Load ZAP tables
-import_recipe dcp_projects
-import_recipe dcp_projectactions
-import_recipe dcp_projectbbls
-import_recipe dcp_dcpprojectteams
-
-# Load other tables
-import_recipe dcp_mappluto_wi
-import_recipe dcp_boroboundaries
-import_recipe dcp_housing
-import_recipe dcp_zoningmapamendments
-
-# Load SCA Geometry Aggregate Tables
-import_recipe doe_eszones
-import_recipe doe_school_subdistricts
-import_recipe dcp_school_districts
-
-# Load geographic boundaries Aggregate Tables
-import_recipe dcp_ct2020_wi
-import_recipe dcp_nta2020
-import_recipe dcp_cdta2020
-import_recipe dcp_cdboundaries_wi
 
 echo
 echo "data loading complate"
