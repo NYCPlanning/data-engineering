99938908ae4d335095a76110ad75b7c9828a875f
fvankrieken <fvankrieken@planning.nyc.gov>
2024-01-05 15:25:32 +0000

    add new 'base' image with manual gdal install

--- docker/build-geosupport/setup.sh
+++ docker/build-geosupport/setup.sh
@@ -2,12 +2,6 @@
 source config.sh
 set -e
 
-apt-get update && export DEBIAN_FRONTEND=noninteractive \
-    && apt-get -y install $BUILD_APT_PACKAGES
-
 install_geosupport
-install_yq
-install_mc
-install_gdal_python_reqs
 
 python3 -m pip install -r requirements.txt -c constraints.txt
