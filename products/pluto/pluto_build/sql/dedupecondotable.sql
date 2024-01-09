740671792cce425a26eedcba6af3751a96ccf2d6
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2024-01-09 15:32:11 -0500

    PLUTO: Remove temporary condo bbl ordering

--- products/pluto/pluto_build/sql/dedupecondotable.sql
+++ products/pluto/pluto_build/sql/dedupecondotable.sql
@@ -47,22 +47,7 @@ WHERE id IN (
             id,
             ROW_NUMBER() OVER (
                 PARTITION BY condo_base_bbl
-                -- TODO This hard-coding is a temporary fix to preserve ordering from 23v3
-                -- 24v1 should have the `condo_billing_bbl IN ...` removed, and order just by condo_billing_bbl
-                ORDER BY condo_billing_bbl IN (
-                    '3002787502',
-                    '3003047501',
-                    '4050667502',
-                    '4050667507',
-                    '4076217502',
-                    '5014957501',
-                    '5024017501',
-                    '5030247501',
-                    '5031737504',
-                    '5070487502',
-                    '5070557501'
-                ) DESC,
-                condo_billing_bbl
+                ORDER BY condo_billing_bbl
             ) AS row_num
         FROM pluto_condo
         WHERE condo_billing_bbl IS NOT NULL
