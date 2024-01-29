896e5ac45b0ac9ee7798699d80f4c435891e04b8
sf-dcp <ofilippova@planning.nyc.gov>
2024-01-29 21:08:55 +0000

    Update code logic for processing doe_universalprek: * Source column names changed. * Values in "type" column also changed.

--- products/facilities/facdb/sql/doe_universalprek.sql
+++ products/facilities/facdb/sql/doe_universalprek.sql
@@ -2,41 +2,41 @@ DROP TABLE IF EXISTS _doe_universalprek;
 SELECT
     uid,
     source,
-    name as facname,
-    parsed_hnum as addressnum,
-    parsed_sname as streetname,
-    address,
-    NULL as city,
-    zip as zipcode,
+    sitename AS facname,
+    parsed_hnum AS addressnum,
+    parsed_sname AS streetname,
+    siteaddress AS address,
+    NULL AS city,
+    state AS zipcode,
     boro,
-    NULL as borocode,
-    NULL as bin,
-    NULL as bbl,
+    NULL AS borocode,
+    NULL AS bin,
+    NULL AS bbl,
     (CASE
-        WHEN type = 'DOE' THEN 'DOE Universal Pre-K'
+        WHEN type = 'DOE' OR type = 'Public School' THEN 'DOE Universal Pre-K'
         WHEN type = 'CHARTER' OR type = 'Charter' THEN 'DOE Universal Pre-K - Charter'
-        WHEN type = 'NYCEEC' THEN 'Early Education Program'
-		WHEN type = 'PKC' THEN 'Pre-K Center'
-	END) as factype,
-    'DOE Universal Pre-Kindergarten' as facsubgrp,
+        WHEN type = 'NYCEEC' OR type = 'CBO' THEN 'Early Education Program'
+        WHEN type = 'PKC' THEN 'Pre-K Center'
+    END) AS factype,
+    'DOE Universal Pre-Kindergarten' AS facsubgrp,
     (CASE
-        WHEN type = 'DOE' THEN 'NYC Department of Education'
-        WHEN type = 'CHARTER' OR type = 'NYCEEC' THEN name
+        WHEN type = 'DOE' OR type = 'Public School' THEN 'NYC Department of Education'
+        WHEN type = 'CHARTER' OR type = 'Charter' OR type = 'NYCEEC' OR type = 'CBO' THEN sitename
         ELSE 'Unknown'
-	END) as opname,
+    END) AS opname,
     (CASE
-        WHEN type = 'DOE' THEN 'NYCDOE'
-        WHEN type = 'CHARTER' THEN 'Charter'
-        WHEN type = 'NYCEEC' THEN 'Non-public'
+        WHEN type = 'DOE' OR type = 'Public School' THEN 'NYCDOE'
+        WHEN type = 'Charter' THEN 'Charter'
+        WHEN type = 'NYCEEC' OR type = 'CBO' THEN 'Non-public'
         ELSE 'Unknown'
-	END) as opabbrev,
-    'NYCDOE' as overabbrev,
-    NULL as capacity,
-    NULL as captype,
-    NULL as wkb_geometry,
+    END) AS opabbrev,
+    'NYCDOE' AS overabbrev,
+    NULL AS capacity,
+    NULL AS captype,
+    NULL AS wkb_geometry,
     geo_1b,
-    NULL as geo_bl,
-    NULL as geo_bn
+    NULL AS geo_bl,
+    NULL AS geo_bn
 INTO _doe_universalprek
 FROM doe_universalprek;
 
