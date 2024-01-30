b6133c12cc9c86295bc5ef315fa54232e3ab5017
fvankrieken <fvankrieken@planning.nyc.gov>
2024-01-30 20:45:28 +0000

    implement poc of source data dashboard

---
+++ apps/qa/src/pages/source_data/helpers.py
@@ -0,0 +1,29 @@
+from typing import Dict
+
+from src import ROOT_PATH
+
+from dcpy.connectors.edm import recipes
+from dcpy.builds import plan
+
+
+def get_source_datasets(product: str) -> list[str]:
+    recipe = plan.recipe_from_yaml(ROOT_PATH / "products" / product / "recipe.yml")
+    return [dataset.name for dataset in recipe.inputs.datasets]
+
+
+def get_dataset_metadata(product: str) -> Dict[str, list[recipes.ArchivalMetadata]]:
+    datasets = get_source_datasets(product)
+    metadata_df = recipes.get_logged_metadata(datasets)
+    metadata: Dict[str, list[recipes.ArchivalMetadata]] = {}
+    for index, row in metadata_df.iterrows():  ## group by
+        m = recipes.ArchivalMetadata(**row.to_dict())
+        dataset = row["name"]
+        if dataset not in metadata:
+            metadata[dataset] = []
+        metadata[dataset].append(m)
+    return metadata
+
+
+def scrape_metadata(product: str):
+    for dataset in get_source_datasets(product):
+        recipes.scrape_metadata(dataset)
---
+++ apps/qa/src/pages/source_data/source_data.py
@@ -0,0 +1,28 @@
+def source_data():
+    import streamlit as st
+    from . import helpers, components
+
+    product = st.sidebar.selectbox(
+        "Select a product",
+        [
+            "cbbr",
+            "colp",
+            "cpdb",
+            "developments",
+            "facilities",
+            "pluto",
+            "zoningtaxlots",
+        ],
+    )
+
+    datasets = helpers.get_dataset_metadata(product)
+
+    months_back = st.sidebar.number_input("Months to display", min_value=1, value=6)
+
+    st.sidebar.button(
+        label="Scrape source metadata",
+        help=f"Scrape spaces for all versions, timestamps of all source datasets of {product}",
+        on_click=lambda: helpers.scrape_metadata(product),
+    )
+
+    components.plot_series(datasets, months_back)
