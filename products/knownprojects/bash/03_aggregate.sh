45b0129834a7f4360d40b93360f49c39942c6cf0
Damon McCullough <damonmcc1391@gmail.com>
2024-01-30 15:06:17 -0500

    move kpdb aggregation scripts into sql folder

--- products/knownprojects/bash/03_aggregate.sh
+++ products/knownprojects/bash/03_aggregate.sh
@@ -6,39 +6,39 @@ echo "Preprocess for all aggregations"
 run_sql_command "ALTER TABLE _kpdb RENAME COLUMN geom TO geometry;"
 
 echo "Create ZAP Project Many BBLs table"
-run_sql_file aggregate/create_zap_projects.sql
+run_sql_file sql/aggregate/create_zap_projects.sql
 
 ## Do SCA aggregation
 echo "Create the longfrom SCA Aggregate Tables..."
 
 # Preprocess the tables to standardize geometry column name 
 echo "Preprocess column names to standardize"
-run_sql_file aggregate/sca/preprocessing.sql
+run_sql_file sql/aggregate/sca/preprocessing.sql
 
 # Aggregate KPDB projects to Elementary School Zones 
 echo "Build Elementary School Zones Aggregate Table"
-run_sql_file aggregate/sca/boundaries_es_zone.sql
+run_sql_file sql/aggregate/sca/boundaries_es_zone.sql
 
 # Aggregate KPDB projects to School District Zones
 echo "Build School Districts aggregate table"
-run_sql_file aggregate/sca/boundaries_school_districts.sql
+run_sql_file sql/aggregate/sca/boundaries_school_districts.sql
 
 # Aggregate KPDB projects to School Subdistrict Zones
 echo "Build School Subdistricts aggregate tables"
-run_sql_file aggregate/sca/boundaries_school_subdistricts.sql
+run_sql_file sql/aggregate/sca/boundaries_school_subdistricts.sql
 
 echo "SCA sggregations are complete"
 
 ## Do other aggregation
 echo "Preprocess column names to standardize"
-run_sql_file aggregate/preprocessing.sql
+run_sql_file sql/aggregate/preprocessing.sql
 
 # Aggregate KPDB projects to Community Districts
 echo "Build Community Districts aggregate tables"
-run_sql_file aggregate/census_tracts.sql
-run_sql_file aggregate/ntas.sql
-run_sql_file aggregate/cdtas.sql
-run_sql_file aggregate/community_districts.sql
+run_sql_file sql/aggregate/census_tracts.sql
+run_sql_file sql/aggregate/ntas.sql
+run_sql_file sql/aggregate/cdtas.sql
+run_sql_file sql/aggregate/community_districts.sql
 
 echo "Other aggregations are complete"
 
