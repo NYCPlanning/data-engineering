6d52e9fed8001dd2fe726d195ad0c226348df12f
Damon McCullough <damonmcc1391@gmail.com>
2024-02-08 15:47:37 -0500

    improve comments and logs in KPDB aggregation bash script

--- products/knownprojects/bash/03_aggregate.sh
+++ products/knownprojects/bash/03_aggregate.sh
@@ -2,39 +2,43 @@
 source ../../bash/utils.sh
 set_error_traps
 
+## Do preprocessing for aggregations
 echo "Create ZAP Project Many BBLs table"
 run_sql_file sql/aggregate/create_zap_projects.sql
 
 echo "Preprocess column names to standardize"
 run_sql_file sql/aggregate/preprocessing.sql
 
-## Do SCA aggregation
-echo "Create the longfrom SCA Aggregate Tables..."
+## Do SCA aggregations
+echo "Create the SCA aggregation tables..."
 
-# Aggregate KPDB projects to Elementary School Zones 
-echo "Build Elementary School Zones Aggregate Table"
+echo "Build Elementary School Zones aggregate table"
 run_sql_file sql/aggregate/sca/boundaries_es_zone.sql
 
-# Aggregate KPDB projects to School District Zones
 echo "Build School Districts aggregate table"
 run_sql_file sql/aggregate/sca/boundaries_school_districts.sql
 
-# Aggregate KPDB projects to School Subdistrict Zones
-echo "Build School Subdistricts aggregate tables"
+echo "Build School Subdistricts aggregate table"
 run_sql_file sql/aggregate/sca/boundaries_school_subdistricts.sql
-
 echo "SCA sggregations are complete"
 
-## Do other aggregation
-# Aggregate KPDB projects to Community Districts
-echo "Build Community Districts aggregate tables"
+## Do general aggregations
+echo "Create the general aggregate tables..."
+
+echo "Build Census Tract aggregate table"
 run_sql_file sql/aggregate/longform_ct_output.sql
+
+echo "Build NTA aggregate table"
 run_sql_file sql/aggregate/longform_nta_output.sql
+
+echo "Build CDTA aggregate table"
 run_sql_file sql/aggregate/longform_cdta_output.sql
+
+echo "Build Community District aggregate table"
 run_sql_file sql/aggregate/longform_cd_output.sql
+echo "General aggregations are complete"
 
-echo "Other aggregations are complete"
-
+## Do postprocessing
 run_sql_command "ALTER TABLE _kpdb RENAME COLUMN geometry TO geom;"
 
 echo "All aggregations are complete"
