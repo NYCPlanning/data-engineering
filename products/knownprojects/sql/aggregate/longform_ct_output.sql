69191fec7d98ea88db9568a72941bef82ba93a02
Damon McCullough <damonmcc1391@gmail.com>
2024-02-13 21:17:06 -0500

    calculate phased units in aggregations
    if a record is split between multiple geographies, the phased units must be calculated for each geography

--- products/knownprojects/sql/aggregate/longform_ct_output.sql
+++ products/knownprojects/sql/aggregate/longform_ct_output.sql
@@ -336,7 +336,13 @@ FROM (
         a.*,
         b.ct_1 AS ct,
         b.proportion_in_ct_1 AS proportion_in_ct,
-        round(a.units_net * b.proportion_in_ct_1) AS units_net_in_ct
+        round(a.units_net * b.proportion_in_ct_1) AS units_net_in_ct,
+        round(b.proportion_in_ct_1 * a.within_5_years::decimal
+        ) AS within_5_years_in_ct,
+        round(b.proportion_in_ct_1 * a.from_5_to_10_years::decimal
+        ) AS from_5_to_10_years_in_ct,
+        round(b.proportion_in_ct_1 * a.after_10_years::decimal
+        ) AS after_10_years_in_ct
     FROM
         -- capitalplanning.kpdb_2021_09_10_nonull a 
         kpdb AS a
