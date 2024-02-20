05c6959129a03b607c302bc39d70ef3e4a191534
fvankrieken <fvankrieken@planning.nyc.gov>
2024-02-20 17:14:41 +0000

    Add edes csv to ceqr_survey build

---
+++ products/ceqr_survey/models/intermediate/int__edesignation_flags.sql
@@ -0,0 +1,24 @@
+WITH dcp_edesignation AS (
+    SELECT * FROM {{ source('ceqr_survey_sources', 'dcp_edesignation_csv') }}
+)
+
+SELECT
+    bbl,
+    'edesignation_hazmat' AS variable,
+    enumber AS id
+FROM dcp_edesignation
+WHERE hazmat_code = '1'
+UNION ALL
+SELECT
+    bbl,
+    'edesignation_air' AS variable,
+    enumber AS id
+FROM dcp_edesignation
+WHERE air_code = '1'
+UNION ALL
+SELECT
+    bbl,
+    'edesignation_noise' AS variable,
+    enumber AS id
+FROM dcp_edesignation
+WHERE noise_code = '1'
