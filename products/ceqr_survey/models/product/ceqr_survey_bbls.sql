86f260ee66bf822aafd431bdc1ae5ab906cb123a
fvankrieken <fvankrieken@planning.nyc.gov>
2024-02-21 19:27:07 +0000

    pivot long ceqr flag table using seed input

--- products/ceqr_survey/models/product/ceqr_survey_bbls.sql
+++ products/ceqr_survey/models/product/ceqr_survey_bbls.sql
@@ -1,16 +1,53 @@
-WITH bbls AS (
-    SELECT * FROM
-        {{ ref('stg__pluto') }}
+{% set sql_statement %} -- noqa: disable=all
+    SELECT * FROM {{ ref('ceqr_variables') }}
+{% endset %}
+
+{% if execute %}
+    {% set ceqr_variables = run_query(sql_statement) %}
+{% endif %}
+
+WITH pluto AS (
+    SELECT * FROM {{ ref('stg__pluto') }}
 ),
 
-ceqr_survery_bbls AS (
+ceqr_flags_long AS (
+    SELECT * FROM {{ ref('ceqr_flags_long') }}
+),
+
+ceqr_flags_ranked AS (
     SELECT
         bbl,
-        0 AS zoning_districts_check,
-        null AS zoning_districts_values,
-        0 AS zoning_coastal_risk_districts_check,
-        null AS zoning_coastal_risk_districts_values
-    FROM bbls
+        variable_type,
+        variable_id,
+        ROW_NUMBER()
+            OVER (PARTITION BY bbl, variable_type ORDER BY distance_from)
+        AS row_number
+    FROM ceqr_flags_long
+),
+
+ceqr_flags_wide AS (
+    SELECT
+        {% for row in ceqr_variables -%}
+            MAX(
+                CASE
+                    WHEN
+                        variable_type = '{{ row["variable_type"] }}'
+                        THEN variable_id
+                END
+            ) AS "{{ row['label'] }}",
+        {% endfor %}
+        bbl::text -- TODO remove when not pulling long flags from seed table
+    FROM ceqr_flags_ranked
+    WHERE row_number = 1
+    GROUP BY bbl
 )
 
-SELECT * FROM ceqr_survery_bbls
+SELECT
+    pluto.bbl,
+    {% for row in ceqr_variables -%}
+        f."{{ row['label'] }}",
+    {% endfor %}
+    pluto.geom
+FROM
+    pluto
+LEFT JOIN ceqr_flags_wide AS f ON pluto.bbl = f.bbl
