8e3765a51c3cef0d53de39a987b6244bcfee63f6
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2024-02-21 15:07:49 -0500

    Add CEQR Survey all_buffers dbt node

---
+++ products/ceqr_survey/models/intermediate/int__all_buffers.sql
@@ -0,0 +1,18 @@
+WITH all_buffers AS (  -- noqa: disable=all
+{{ dbt_utils.union_relations(
+    relations=[
+        ref('int__elevated_railways'),
+        ref('int__nysdec_state_facility_permits'),
+        ref('int__nysdec_title_v_facility_permits'),
+        ref('int__dep_cats_permits'),
+        ref('stg__dcm_arterial_highways'),
+        ref('stg__dcp_air_quality_vent_towers_buffered'),
+        ref('stg__panynj_airports')
+    ],
+    column_override={"raw_geom": "geometry", "buffer": "geometry"}
+) }}
+)
+-- Note: without `column_override`, dbt throws an error trying to cast.
+-- e.g.: `cast("raw_geom" as USER-DEFINED) as "raw...`
+
+SELECT * FROM all_buffers
