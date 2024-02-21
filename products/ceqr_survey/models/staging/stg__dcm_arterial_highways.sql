16a58883310b9ff12e038685b056dde36d2be6ee
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2024-02-21 16:35:00 -0500

    Remove unnecessary quotes from arterial_highways

--- products/ceqr_survey/models/staging/stg__dcm_arterial_highways.sql
+++ products/ceqr_survey/models/staging/stg__dcm_arterial_highways.sql
@@ -3,9 +3,9 @@ WITH arterial_highways_raw AS (
 )
 
 SELECT
-    "name" AS variable_id,
+    name AS variable_id,
     'arterial_highway' AS variable_type,
     wkb_geometry AS raw_geom,
     ST_BUFFER(wkb_geometry, 75) AS buffer
 FROM arterial_highways_raw
-WHERE "source" = 'Appendix H'
+WHERE source = 'Appendix H'
