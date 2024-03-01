a3894a34cc87033739c5c4a595bc03847832a568
Damon McCullough <damonmcc1391@gmail.com>
2024-02-29 21:26:26 -0500

    export all points from source data

--- products/green_fast_track/models/product/cats_permits_points.sql
+++ products/green_fast_track/models/product/cats_permits_points.sql
@@ -1,6 +1,5 @@
 SELECT
     variable_type,
     variable_id,
-    raw_geom
-FROM {{ ref('int__dep_cats_permits') }}
-WHERE ST_GEOMETRYTYPE(raw_geom) = 'ST_Point'
+    permit_geom AS raw_geom
+FROM {{ ref('stg__dep_cats_permits') }}
