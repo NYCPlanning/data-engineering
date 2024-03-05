4f9bf63ca2a5fc24841c23a92697be5fc15a1ecb
Finn van Krieken <fvankrieken@planning.nyc.gov>
2024-03-05 15:52:13 +0000

    add additional templates, separate out yml fields

--- dcpy/extract/models.py
+++ dcpy/extract/models.py
@@ -14,11 +14,18 @@ class ImportDefinition(
 
     name: str
     acl: recipes.ValidAclValues
-    source: Source.WebEndpoint | Source.Socrata
+
+    ## these two fields might merge to "source" or something equivalent at some point
+    ## for now, they are distinct so that they can be worked on separately
+    ## when implemented, "None" should not be valid type
+    archive_raw_metadata: ArchiveRawMetadata.WebEndpoint | ArchiveRawMetadata.Socrata | None
+    transform_to_parquet_metadata: None
+
     ## this is the original library template, included just for reference while we build out our new templates
     library_dataset: DatasetDefinition
 
-    class Source:
+    class ArchiveRawMetadata:
+        # might need to differentiate between file download endpoint and something that returns just json response
         class WebEndpoint(BaseModel):
             url: str
 
