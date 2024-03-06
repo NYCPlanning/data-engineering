5e7dbc0f54c8641bf5834fae9f2adc83b15528c5
Damon McCullough <damonmcc1391@gmail.com>
2024-03-06 10:56:33 -0500

    sort list of flag values by distance and name

--- products/green_fast_track/models/product/green_fast_track_bbls.sql
+++ products/green_fast_track/models/product/green_fast_track_bbls.sql
@@ -19,6 +19,7 @@ flags_ranked AS (
         bbl,
         variable_type,
         variable_id,
+        distance,
         ROW_NUMBER()
             OVER (PARTITION BY bbl, variable_type ORDER BY distance)
         AS row_number
@@ -28,8 +29,12 @@ flags_ranked AS (
 flags_wide AS (
     SELECT
         {% for row in variables -%}
+            /* construct a comma-separated list of values ordered by distance and name */
             ARRAY_TO_STRING(
-                ARRAY_AGG(variable_id) FILTER (WHERE variable_type = '{{ row["variable_type"] }}'), ', '
+                ARRAY_AGG(variable_id ORDER BY distance ASC, variable_id ASC) FILTER (
+                    WHERE variable_type = '{{ row["variable_type"] }}'
+                ),
+                ', '
             ) AS "{{ row['label'] }}",
         {% endfor %}
         bbl
