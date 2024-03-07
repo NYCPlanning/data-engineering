b744647511da60e0b361d2c9c58c71eca4996c15
Damon McCullough <damonmcc1391@gmail.com>
2024-03-07 13:49:00 -0500

    reorder exports

--- products/green_fast_track/bash/export.sh
+++ products/green_fast_track/bash/export.sh
@@ -18,12 +18,20 @@ mkdir -p output && (
     echo "Generate FileGeodatabase ${fgdb_filename}"
     fgdb_export_partial ${fgdb_filename} MULTIPOLYGON green_fast_track_bbls green_fast_track_bbls ${default_srs}
 
-    fgdb_export_partial ${fgdb_filename} POINT sources__cats_permits_points sources__cats_permits_points ${default_srs} -update
-    fgdb_export_partial ${fgdb_filename} MULTIPOLYGON sources__cats_permits_lots sources__cats_permits_lots ${default_srs} -update
-    fgdb_export_partial ${fgdb_filename} MULTIPOLYGON sources__cats_permits_buffers sources__cats_permits_buffers ${default_srs} -update
+    fgdb_export_partial ${fgdb_filename} MULTIPOLYGON sources__airports sources__airports ${default_srs} -update
+
+    fgdb_export_partial ${fgdb_filename} MULTIPOLYGON sources__arterial_highways_buffers sources__arterial_highways_buffers ${default_srs} -update
+
+    fgdb_export_partial ${fgdb_filename} MULTIPOLYGON sources__elevated_railways_buffers sources__elevated_railways_buffers ${default_srs} -update
+    
+    fgdb_export_partial ${fgdb_filename} MULTIPOLYGON sources__vent_towers_buffers sources__vent_towers_buffers ${default_srs} -update
 
     fgdb_export_partial ${fgdb_filename} MULTIPOLYGON sources__industrial_sources_lots sources__industrial_sources_lots ${default_srs} -update
     fgdb_export_partial ${fgdb_filename} MULTIPOLYGON sources__industrial_sources_buffers sources__industrial_sources_buffers ${default_srs} -update
+
+    fgdb_export_partial ${fgdb_filename} POINT sources__cats_permits_points sources__cats_permits_points ${default_srs} -update
+    fgdb_export_partial ${fgdb_filename} MULTIPOLYGON sources__cats_permits_lots sources__cats_permits_lots ${default_srs} -update
+    fgdb_export_partial ${fgdb_filename} MULTIPOLYGON sources__cats_permits_buffers sources__cats_permits_buffers ${default_srs} -update
 
     fgdb_export_partial ${fgdb_filename} POINT sources__state_facility_permits_points sources__state_facility_permits_points ${default_srs} -update
     fgdb_export_partial ${fgdb_filename} MULTIPOLYGON sources__state_facility_permits_lots sources__state_facility_permits_lots ${default_srs} -update
@@ -32,14 +40,6 @@ mkdir -p output && (
     fgdb_export_partial ${fgdb_filename} POINT sources__title_v_permits_points sources__title_v_permits_points ${default_srs} -update
     fgdb_export_partial ${fgdb_filename} MULTIPOLYGON sources__title_v_permits_lots sources__title_v_permits_lots ${default_srs} -update
     fgdb_export_partial ${fgdb_filename} MULTIPOLYGON sources__title_v_permits_buffers sources__title_v_permits_buffers ${default_srs} -update
-
-    fgdb_export_partial ${fgdb_filename} MULTIPOLYGON sources__airports sources__airports ${default_srs} -update
-
-    fgdb_export_partial ${fgdb_filename} MULTIPOLYGON sources__arterial_highways_buffers sources__arterial_highways_buffers ${default_srs} -update
-
-    fgdb_export_partial ${fgdb_filename} MULTIPOLYGON sources__elevated_railways_buffers sources__elevated_railways_buffers ${default_srs} -update
-    
-    fgdb_export_partial ${fgdb_filename} MULTIPOLYGON sources__vent_towers_buffers sources__vent_towers_buffers ${default_srs} -update
     
     fgdb_export_partial ${fgdb_filename} NONE variables variables ${default_srs} -update
     fgdb_export_partial ${fgdb_filename} NONE source_data_versions source_data_versions ${default_srs} -update
