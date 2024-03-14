3e31259c5d1c3be2a8c8abadee691b174e044672
Finn van Krieken <fvankrieken@planning.nyc.gov>
2024-03-14 13:57:56 +0000

    chk

--- products/historical_zoning/models/historical_zoning.sql
+++ products/historical_zoning/models/historical_zoning.sql
@@ -1,19 +1,35 @@
-SELECT
-    zd as zoning_district,
-    null AS change_type,
-    geom,
-    version
-FROM 
-    dcp_zoningdistricts_{{ envvar("first_version") }}
-{% for versions in pairwise_versions -%}
-    UNION ALL
+WITH intersects AS (
     SELECT
-        b.zd as zoning_district,
-        NULL AS change_type, -- macro -> upzoning, downzoning, etc
-        ST_INTERSECTION(a.geom, b.geom) AS geom,
-        b.version
-        FROM 
-        dcp_zoningdistricts_{{ versions[0] }} AS a 
-        INNER JOIN dcp_zoningdistricts_{{ versions[1] }} AS b
-            ON ST_INTERSECTS(a.geom, b.geom) AND a.zd <> b.zd
-{% endfor %}
+        zonedist as zoning_district,
+        null AS change_type,
+        wkb_geometry AS geom,
+        data_library_version AS "version"
+    FROM 
+        main.dcp_zoningdistricts_{{ var("first_version") }}
+    {% for versions in var("versions") -%}
+        UNION ALL
+        SELECT
+            b.zonedist as zoning_district,
+            NULL AS change_type, -- macro -> upzoning, downzoning, etc
+            ST_INTERSECTION(a.wkb_geometry, b.wkb_geometry) AS geom,
+            b.data_library_version AS "version"
+            FROM 
+            main.dcp_zoningdistricts_{{ versions[0] }} AS a 
+            INNER JOIN main.dcp_zoningdistricts_{{ versions[1] }} AS b
+                ON ST_INTERSECTS(a.wkb_geometry, b.wkb_geometry) AND a.zonedist <> b.zonedist
+    {% endfor %}
+),
+dumped AS (
+    SELECT 
+        zoning_district, 
+        change_type,
+        (ST_DUMP(geom)).geom AS geom,
+        "version"
+    FROM intersects WHERE st_geometrytype(geom) = 'ST_Polygon' 
+)
+    SELECT 
+        zoning_district, 
+        change_type,
+        geom,
+        "version" 
+    FROM intersects WHERE st_geometrytype(geom) = 'ST_Polygon' 
