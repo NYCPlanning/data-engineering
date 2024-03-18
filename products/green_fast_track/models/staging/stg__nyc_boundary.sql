9eb948b38f5b73bb43ee6c85f00a268ebbd5c6d4
Finn van Krieken <fvankrieken@planning.nyc.gov>
2024-03-18 17:51:47 +0000

    add macro to clip to nyc boundary

---
+++ products/green_fast_track/models/staging/stg__nyc_boundary.sql
@@ -0,0 +1,20 @@
+{{ config(
+    materialized = 'table',
+    indexes=[
+      {'columns': ['geom'], 'type': 'gist'},
+    ]
+) }}
+
+WITH polygons AS (
+    -- this is unfortunately all necessary
+    -- - union to merge boros. this now has very narrow, line-like holes between boros in parts
+    -- - dump then dumps this multipolygon (with holes represented) to individual polygons, some of them being the "holes"
+    -- - ExteriorRing then draws exterior ring around these, giving us three lines - one for mainland (wi) NYC, one each for ellis and liberty islands
+    -- - MakePolygon converts these lines to polygons
+    SELECT ST_MAKEPOLYGON(ST_EXTERIORRING((ST_DUMP(ST_UNION(wkb_geometry))).geom)) AS geom
+    FROM {{ source('recipe_sources', 'dcp_boroboundaries_wi') }}
+)
+-- another union is done to get a single row with all NYC area in a multipolygon
+-- postgresql is unhappy if this is attempted in the previous query
+SELECT ST_TRANSFORM(ST_UNION(geom), 2263) AS geom
+FROM polygons
