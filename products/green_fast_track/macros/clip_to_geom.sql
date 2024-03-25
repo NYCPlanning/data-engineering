aa048018cb58dc5ac73a2c467ac9bcbf009d216d
fvankrieken <fvankrieken@planning.nyc.gov>
2024-03-25 14:58:55 -0700

    add optional left_columns arg (#712)

--- products/green_fast_track/macros/clip_to_geom.sql
+++ products/green_fast_track/macros/clip_to_geom.sql
@@ -1,7 +1,13 @@
-{% macro clip_to_geom(left, right=ref("stg__nyc_boundary"), left_by="geom", right_by="geom") %}
+{% macro clip_to_geom(left, right=ref("stg__nyc_boundary"), left_by="geom", right_by="geom", left_columns=[]) %}
 
     SELECT
-        {{ dbt_utils.star(from=left, except=[left_by]) }},
+        {% if left_columns == [] %}
+            {{ dbt_utils.star(from=left, except=[left_by]) }},
+        {% else %}
+            {% for column in left_columns %}
+                {{ left }}.{{ column }},
+            {% endfor %}
+        {% endif %}
         ST_INTERSECTION({{ left }}.{{ left_by }}, {{ right }}.{{ right_by }}) AS geom
     FROM {{ left }}
     INNER JOIN {{ right }} ON ST_INTERSECTS({{ left }}.{{ left_by }}, {{ right }}.{{ right_by }})
