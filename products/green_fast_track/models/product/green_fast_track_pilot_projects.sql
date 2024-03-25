7eb1a12b864b0944d45f02e1afaff2be5fdcb26f
Damon McCullough <damonmcc1391@gmail.com>
2024-03-24 21:18:16 -0400

    test expected pilot project values

---
+++ products/green_fast_track/models/product/green_fast_track_pilot_projects.sql
@@ -0,0 +1,17 @@
+WITH gft_bbls AS (
+    SELECT * FROM {{ ref('green_fast_track_bbls') }}
+),
+
+expected_pilot_projects AS (
+    SELECT * FROM {{ ref('expected_green_fast_track_pilot_projects') }}
+),
+
+final AS (
+    SELECT
+        expected_pilot_projects.project_label,
+        gft_bbls.*
+    FROM gft_bbls INNER JOIN expected_pilot_projects
+        ON gft_bbls.bbl = expected_pilot_projects.bbl
+)
+
+SELECT * FROM final
