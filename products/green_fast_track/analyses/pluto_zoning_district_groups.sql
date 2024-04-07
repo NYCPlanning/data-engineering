d380e4cd433c68254afc318a97dfeb68675e5b5d
Damon McCullough <damonmcc1391@gmail.com>
2024-04-07 18:08:12 -0400

    add `pluto_zoning_district_groups` analysis
    analyses SQL queries that are useful but not executed/materialized during a build

---
+++ products/green_fast_track/analyses/pluto_zoning_district_groups.sql
@@ -0,0 +1,28 @@
+WITH pluto AS (
+    SELECT
+        coalesce(zonedist1, 'NULL') AS zonedist1,
+        coalesce(zonedist2, 'NULL') AS zonedist2,
+        coalesce(zonedist3, 'NULL') AS zonedist3,
+        coalesce(zonedist4, 'NULL') AS zonedist4
+    FROM {{ ref('stg__pluto') }}
+),
+
+group_by_rollup AS (
+    SELECT
+        zonedist1,
+        zonedist2,
+        zonedist3,
+        zonedist4,
+        count(*) AS zoning_combo_count
+    FROM pluto
+    GROUP BY ROLLUP (zonedist1, zonedist2, zonedist3, zonedist4) -- noqa: PRS
+)
+
+SELECT
+    coalesce(zonedist1, 'ALL') AS zonedist1,
+    coalesce(zonedist2, 'ALL') AS zonedist2,
+    coalesce(zonedist3, 'ALL') AS zonedist3,
+    coalesce(zonedist4, 'ALL') AS zonedist4,
+    zoning_combo_count
+FROM group_by_rollup
+ORDER BY zonedist1, zonedist2, zonedist3, zonedist4
