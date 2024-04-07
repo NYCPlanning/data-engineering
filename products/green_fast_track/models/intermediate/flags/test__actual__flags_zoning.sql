e44eb293677596bd3b85ab53ecd5a65dfc692be7
Damon McCullough <damonmcc1391@gmail.com>
2024-04-07 17:01:54 -0400

    test `int_flags__zoning` logic
    uses the same approach as the pilot project test

---
+++ products/green_fast_track/models/intermediate/flags/test__actual__flags_zoning.sql
@@ -0,0 +1,40 @@
+{{ config(
+    materialized = 'table'
+) }}
+WITH flags_zoning AS (
+    SELECT * FROM {{ ref('int_flags__zoning') }}
+),
+
+pluto AS (
+    SELECT * FROM {{ ref('stg__pluto') }}
+),
+
+expected_flags_zoning AS (
+    SELECT * FROM {{ ref('test__expected__flags_zoning') }}
+),
+
+flags_zoning_districts AS (
+    SELECT * FROM flags_zoning WHERE variable_type = 'zoning_districts'
+),
+
+flags_with_zonedists AS (
+    SELECT
+        pluto.bbl,
+        pluto.zonedist1,
+        pluto.zonedist2,
+        pluto.zonedist3,
+        pluto.zonedist4,
+        flags_zoning_districts.variable_id AS "Zoning Districts"
+    FROM pluto LEFT JOIN flags_zoning_districts
+        ON pluto.bbl = flags_zoning_districts.bbl
+),
+
+final AS (
+    SELECT
+        expected_flags_zoning.lot_label,
+        flags_with_zonedists.*
+    FROM flags_with_zonedists INNER JOIN expected_flags_zoning
+        ON flags_with_zonedists.bbl = expected_flags_zoning.bbl
+)
+
+SELECT * FROM final ORDER BY lot_label ASC, bbl ASC
