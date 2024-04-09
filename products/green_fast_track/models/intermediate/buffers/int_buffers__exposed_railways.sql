ed890c261c2272a562aca22563827ec36e17e6b8
Finn van Krieken <fvankrieken@planning.nyc.gov>
2024-04-09 19:51:48 +0000

    rename exposed railways

--- products/green_fast_track/models/intermediate/buffers/int_buffers__exposed_railways.sql
+++ products/green_fast_track/models/intermediate/buffers/int_buffers__exposed_railways.sql
@@ -12,7 +12,7 @@ filtered AS (
 )
 
 SELECT
-    'elevated_railways' AS variable_type,
+    'exposed_railways' AS variable_type,
     street AS variable_id,
     geom AS raw_geom,
     ST_BUFFER(geom, 1500) AS buffer
