7d6c48f4b716cd5358bf64e07893dee0129c5ccc
Finn van Krieken <fvankrieken@planning.nyc.gov>
2024-04-10 15:48:28 +0000

    use hexes for spatial join

--- products/green_fast_track/models/intermediate/flags/int_flags__spatial.sql
+++ products/green_fast_track/models/intermediate/flags/int_flags__spatial.sql
@@ -5,7 +5,20 @@
     ]
 ) }}
 
-WITH buffered_flags AS (
+/*
+We have a few monstrous polygons in our buffers, so a tiled approach greatly improves performance
+
+First, a hex grid is generated for NYCs extent. 8000 as a size was roughly optimized for performance
+This creates roughly a 13x14 grid of hexagons covering the city (175 hexagons total)
+
+These hexagons are joined to the buffers table, creating one row per unique buffer and tiled hexagon
+
+This buffer/hexagon table is then joined to pluto lots. hexagons geoms are used as first join criterion, then actual geom
+
+Finally, rows are deduplicated based on bbl, variable_id, and variable_type
+*/
+
+WITH buffers AS (
     SELECT
         variable_type,
         variable_id,
@@ -22,16 +35,39 @@ pluto AS (
     FROM {{ ref('stg__pluto') }}
 ),
 
--- Spatially join pluto with buffered flags. 
--- Calculate distance to raw geometry of the joined flags
-final AS (
+hexes AS (
+    {% set nyc_rel = ref('stg__nyc_boundary') %}
+    SELECT * FROM ST_HEXAGONGRID(
+        8000,
+        ST_SETSRID(ST_ESTIMATEDEXTENT('{{ nyc_rel.schema }}', '{{ nyc_rel.identifier }}', 'geom'), 2263)
+    )
+),
+
+buffers_hexes AS (
     SELECT
+        b.*,
+        hexes.geom
+    FROM buffers AS b
+    LEFT JOIN hexes ON ST_INTERSECTS(b.buffer, hexes.geom)
+),
+
+joined_hexes AS (
+    SELECT DISTINCT ON (p.bbl, b.variable_type, b.variable_id)
         p.bbl,
+        p.bbl_geom,
         b.variable_type,
         b.variable_id,
-        ST_DISTANCE(p.bbl_geom, b.raw_geom) AS distance
-    FROM buffered_flags AS b INNER JOIN pluto AS p
-        ON ST_INTERSECTS(b.buffer, p.bbl_geom)
+        b.raw_geom
+    FROM buffers_hexes AS b INNER JOIN pluto AS p
+        ON ST_INTERSECTS(b.geom, p.bbl_geom) AND ST_INTERSECTS(b.buffer, p.bbl_geom)
 )
 
-SELECT * FROM final
+SELECT
+    bbl,
+    variable_type,
+    variable_id,
+    CASE
+        WHEN variable_type = 'natural_resource_shadow' THEN 0
+        ELSE ST_DISTANCE(bbl_geom, raw_geom)
+    END AS distance
+FROM joined_hexes
