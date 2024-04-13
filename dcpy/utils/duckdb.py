8fbb1be42fc342bdb8d43b0e20180a0928012bd7
Finn van Krieken <fvankrieken@planning.nyc.gov>
2024-04-13 20:18:03 +0000

    add duckdb s3 setup util

---
+++ dcpy/utils/duckdb.py
@@ -0,0 +1,16 @@
+import duckdb
+import os
+
+
+def setup_s3_secret():
+    duckdb.sql("INSTALL httpfs; LOAD httpfs;")
+    duckdb.sql(
+        f"""
+            CREATE SECRET IF NOT EXISTS s3_secret (
+                TYPE S3,
+                KEY_ID '{os.environ["AWS_ACCESS_KEY_ID"]}',
+                SECRET '{os.environ["AWS_SECRET_ACCESS_KEY"]}',
+                ENDPOINT '{os.environ["AWS_S3_ENDPOINT"].split('://')[1]}'
+            );
+        """
+    )
