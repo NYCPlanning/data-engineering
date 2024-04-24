61104286ab4b1b61ca1384034f424297f579ea9f
Damon McCullough <damonmcc1391@gmail.com>
2024-04-24 17:54:25 -0400

    fix gft export bash script

--- products/green_fast_track/bash/export.sh
+++ products/green_fast_track/bash/export.sh
@@ -22,86 +22,86 @@ mkdir -p output && (
     echo "Generate FileGeodatabase ${fgdb_filename}"
     fgdb_export_partial ${fgdb_filename} MULTIPOLYGON green_fast_track_bbls green_fast_track_bbls ${default_srs}
 
-    export sources__airports 
+    export_source sources__airports
 
-    export sources__arterial_highways MULTILINESTRING
-    export sources__arterial_highways_buffers
+    export_source sources__arterial_highways MULTILINESTRING
+    export_source sources__arterial_highways_buffers
 
-    export sources__exposed_railways MULTILINESTRING
-    export sources__exposed_railways_buffers
+    export_source sources__exposed_railways MULTILINESTRING
+    export_source sources__exposed_railways_buffers
     
-    export sources__vent_towers
-    export sources__vent_towers_buffers
+    export_source sources__vent_towers
+    export_source sources__vent_towers_buffers
 
-    export sources__industrial_sources_lots
-    export sources__industrial_sources_buffers
+    export_source sources__industrial_sources_lots
+    export_source sources__industrial_sources_buffers
 
-    export sources__cats_permits_points POINT
-    export sources__cats_permits_lots
-    export sources__cats_permits_buffers
+    export_source sources__cats_permits_points POINT
+    export_source sources__cats_permits_lots
+    export_source sources__cats_permits_buffers
 
-    export sources__state_facility_permits_points POINT
-    export sources__state_facility_permits_lots
-    export sources__state_facility_permits_buffers
+    export_source sources__state_facility_permits_points POINT
+    export_source sources__state_facility_permits_lots
+    export_source sources__state_facility_permits_buffers
 
-    export sources__title_v_permits_points POINT
-    export sources__title_v_permits_lots
-    export sources__title_v_permits_buffers
+    export_source sources__title_v_permits_points POINT
+    export_source sources__title_v_permits_lots
+    export_source sources__title_v_permits_buffers
 
-    export sources__nyc_parks_properties
-    export sources__nyc_parks_properties_buffers
+    export_source sources__nyc_parks_properties
+    export_source sources__nyc_parks_properties_buffers
 
-    export sources__dpr_schoolyard_to_playgrounds_lots
-    export sources__dpr_schoolyard_to_playgrounds_buffers
+    export_source sources__dpr_schoolyard_to_playgrounds_lots
+    export_source sources__dpr_schoolyard_to_playgrounds_buffers
 
-    export sources__nys_parks_properties
-    export sources__nys_parks_properties_buffers
+    export_source sources__nys_parks_properties
+    export_source sources__nys_parks_properties_buffers
 
-    export sources__pops_points POINT
-    export sources__pops_lots
-    export sources__pops_buffers
+    export_source sources__pops_points POINT
+    export_source sources__pops_lots
+    export_source sources__pops_buffers
 
-    export sources__us_parks_properties
-    export sources__us_parks_properties_buffers
+    export_source sources__us_parks_properties
+    export_source sources__us_parks_properties_buffers
 
-    export sources__waterfront_access_pow
-    export sources__waterfront_access_pow_buffers
+    export_source sources__waterfront_access_pow
+    export_source sources__waterfront_access_pow_buffers
 
-    export sources__waterfront_access_wpaa
-    export sources__waterfront_access_wpaa_buffers
+    export_source sources__waterfront_access_wpaa
+    export_source sources__waterfront_access_wpaa_buffers
 
     # Natural Resources
-    export sources__beaches
-    export sources__forever_wild_reserves
-    export sources__national_wetlands
-    export sources__natural_heritage_communities
-    export sources__priority_estuaries
-    export sources__priority_lakes
-    export sources__priority_streams MULTLINESTRING
-    export sources__recognized_ecological_complexes
-    export sources__special_natural_waterfront_areas
-    export sources__state_freshwater_wetlands_checkzones
-    export sources__state_freshwater_wetlands
-    export sources__state_tidal_wetlands
+    export_source sources__beaches
+    export_source sources__forever_wild_reserves
+    export_source sources__national_wetlands
+    export_source sources__natural_heritage_communities
+    export_source sources__priority_estuaries
+    export_source sources__priority_lakes
+    export_source sources__priority_streams MULTLINESTRING
+    export_source sources__recognized_ecological_complexes
+    export_source sources__special_natural_waterfront_areas
+    export_source sources__state_freshwater_wetlands_checkzones
+    export_source sources__state_freshwater_wetlands
+    export_source sources__state_tidal_wetlands
 
-    export sources__natural_resources_buffer
+    export_source sources__natural_resources_buffer
 
     # Historic
-    export sources__nyc_historic_buildings_points POINT
-    export sources__nyc_historic_buildings_lots
-    export sources__nyc_historic_buildings_buffers
-    export sources__nyc_historic_districts
-    export sources__scenic_landmarks
-    export sources__nys_historic_buildings_points POINT
-    export sources__nys_historic_buildings_lots
-    export sources__nys_historic_buildings_buffers
-    export sources__nys_historic_districts
-    export sources__us_historic_places
-    export sources__us_historic_places_buffers
-    export sources__archaeological_areas
+    export_source sources__nyc_historic_buildings_points POINT
+    export_source sources__nyc_historic_buildings_lots
+    export_source sources__nyc_historic_buildings_buffers
+    export_source sources__nyc_historic_districts
+    export_source sources__scenic_landmarks
+    export_source sources__nys_historic_buildings_points POINT
+    export_source sources__nys_historic_buildings_lots
+    export_source sources__nys_historic_buildings_buffers
+    export_source sources__nys_historic_districts
+    export_source sources__us_historic_places
+    export_source sources__us_historic_places_buffers
+    export_source sources__archaeological_areas
 
-    export variables NONE
-    export source_data_versions NONE
+    export_source variables NONE
+    export_source source_data_versions NONE
 
     fgdb_export_cleanup ${fgdb_filename}
 )
