11757fa6afcdefb636a31b5f8eac2d12d8bc5a30
Finn van Krieken <fvankrieken@planning.nyc.gov>
2024-04-29 14:29:56 +0000

    f

--- dcpy/lifecycle/builds/clean_artifacts.py
+++ dcpy/lifecycle/builds/clean_artifacts.py
@@ -46,16 +46,16 @@ def delete_stale_schemas(active_build_names: list[str]):
                 logger.info(f"Keeping tests schema {database}.{build_test_schema}")
 
 
-def delete_stale_image_tags(active_build_names: list[str]):
+def delete_stale_image_tags(active_build_names: list[str]) -> None:
     logger.info(f"Potential active branch build names: {active_build_names}")
     DOCKER_IMAGES = ["dev", "build-base", "build-geosupport"]
     for image in DOCKER_IMAGES:
-        tags = requests.get(
+        tags: list[str] = requests.get(
             f"https://hub.docker.com/v2/repositories/nycplanning/{image}/tags?page_size=1000"
         ).json()["results"]
         dev_tags = [tag for tag in tags if tag["name"].startswith("dev-")]
         for tag in dev_tags:
-            if tag[4:] not in active_build_names:
+            if tag.removeprefix("dev-") not in active_build_names:
                 logger.warning(f"Deleting tag {image}:{tag['name']}")
                 # Should we include this file in dcpy? A little odd to rely on this file existing, but it's going to be a bit hacky regardless
                 # The intonation seems a bit finicky so don't really want to implement in python rather than bash
