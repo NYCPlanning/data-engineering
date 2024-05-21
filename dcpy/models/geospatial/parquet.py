aa9266f7e33309bcf88370e369a771dba46a4168
Finn van Krieken <fvankrieken@planning.nyc.gov>
2024-05-21 18:43:07 +0000

    datamodel codegen proj and geoparquet
    These models are largely generated by datamodel-codegen
    using json schema for geoparquet geo metadata.
    They allow reading in of geoparquet metadata in a
    type-safe manner

---
+++ dcpy/models/geospatial/parquet.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel
+from typing import Any, Literal
+
+from . import projjson as proj
+
+
+class Columns(BaseModel, extra="forbid"):
+    """GeoParquet column metadata as specified by https://geoparquet.org/releases/v1.0.0/schema.json"""
+
+    encoding: Literal["WKB"]
+    geometry_types: set[
+        Literal[
+            "Point",
+            "LineString",
+            "Polygon",
+            "MultiPoint",
+            "MultiLineString",
+            "MultiPolygon",
+            "GeometryCollection",
+        ]
+    ]
+    crs: proj.Model
+    edges: Literal["planar", "spherical"] | None = None
+    orientation: Literal["counterclockwise"] = "counterclockwise"
+    bbox: list[float] | None = None
+    epoch: float | None = None
+
+
+class GeoParquet(BaseModel, extra="forbid"):
+    """GeoParquet metadata as specified by https://geoparquet.org/releases/v1.0.0/schema.json"""
+
+    version: Literal["1.0.0"]
+    primary_column: str
+    columns: dict[str, Columns]
+    creator: dict | None = None
+
+
+class MetaData(BaseModel, extra="forbid"):
+    file_metadata: Any
+    geo_parquet: GeoParquet
