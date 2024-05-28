86fd3ba109f2ef8997852e9b08589d60a89d7c4d
Damon McCullough <damonmcc1391@gmail.com>
2024-05-28 11:40:34 -0400

    export GFT source models

--- products/green_fast_track/bash/export.sh
+++ products/green_fast_track/bash/export.sh
@@ -20,89 +20,66 @@ mkdir -p output && (
     cp ../build_metadata.json .
     
     echo "Generate FileGeodatabase ${fgdb_filename}"
+    echo "FGDB export green_fast_track_bbls ..."
     fgdb_export_partial ${fgdb_filename} MULTIPOLYGON green_fast_track_bbls green_fast_track_bbls ${default_srs}
-
-    # export_source sources__airports
-
-    # export_source sources__arterial_highways MULTILINESTRING
-    # export_source sources__arterial_highways_buffers
-
-    # export_source sources__exposed_railways MULTILINESTRING
-    # export_source sources__exposed_railways_buffers
     
-    # export_source sources__vent_towers
-    # export_source sources__vent_towers_buffers
+    echo "FGDB export Air Quality ..."
+    export_source source__cats_permit_points POINT
+    export_source source__cats_permit_lots
+    export_source source__cats_permit_buffer
 
-    # export_source sources__industrial_sources_lots
-    # export_source sources__industrial_sources_buffers
+    export_source source__industrial_lots
+    export_source source__industrial_lots_buffer
 
-    # export_source sources__cats_permits_points POINT
-    # export_source sources__cats_permits_lots
-    # export_source sources__cats_permits_buffers
+    export_source source__state_facility_points POINT
+    export_source source__state_facility_lots
+    export_source source__state_facility_buffer
 
-    # export_source sources__state_facility_permits_points POINT
-    # export_source sources__state_facility_permits_lots
-    # export_source sources__state_facility_permits_buffers
+    export_source source__title_v_permit_points POINT
+    export_source source__title_v_permit_lots
+    export_source source__title_v_permit_buffer
 
-    # export_source sources__title_v_permits_points POINT
-    # export_source sources__title_v_permits_lots
-    # export_source sources__title_v_permits_buffers
+    export_source source__vent_tower
+    export_source source__vent_tower_buffer
+    
+    echo "FGDB export Noise ..."
+    export_source source__arterial_highway MULTILINESTRING
+    export_source source__arterial_highway_buffer
 
-    # export_source sources__nyc_parks_properties
-    # export_source sources__nyc_parks_properties_buffers
+    export_source source__exposed_railway MULTILINESTRING
+    export_source source__exposed_railway_buffer
 
-    # export_source sources__dpr_schoolyard_to_playgrounds_lots
-    # export_source sources__dpr_schoolyard_to_playgrounds_buffers
+    export_source source__airport_noise
 
-    # export_source sources__nys_parks_properties
-    # export_source sources__nys_parks_properties_buffers
+    echo "FGDB export Natural Resources ..."
+    export_source source__natural_resources_points POINT
+    export_source source__natural_resources_lines MULTILINESTRING
+    export_source source__natural_resources_polys
 
-    # export_source sources__pops_points POINT
-    # export_source sources__pops_lots
-    # export_source sources__pops_buffers
+    export_source source__wetland_checkzone
 
-    # export_source sources__us_parks_properties
-    # export_source sources__us_parks_properties_buffers
+    echo "FGDB export Historic Resources ..."
+    export_source source__archaeological_area
+    export_source source__historic_districts
+    export_source source__historic_resources POINT
 
-    # export_source sources__waterfront_access_pow
-    # export_source sources__waterfront_access_pow_buffers
+    export_source source__historic_resources_adj POINT
+    export_source source__historic_resources_adj_buffer
 
-    # export_source sources__waterfront_access_wpaa
-    # export_source sources__waterfront_access_wpaa_buffers
+    echo "FGDB export Shadows ..."
+    export_source source__shadow_open_spaces_points POINT
+    export_source source__shadow_open_spaces_polys
+    export_source source__shadow_open_spaces_buffer
 
-    # # Natural Resources
-    # export_source sources__beaches
-    # export_source sources__forever_wild_reserves
-    # export_source sources__national_wetlands
-    # export_source sources__natural_heritage_communities
-    # export_source sources__priority_estuaries
-    # export_source sources__priority_lakes
-    # export_source sources__priority_streams MULTILINESTRING
-    # export_source sources__recognized_ecological_complexes POINT
-    # export_source sources__special_natural_waterfront_areas
-    # export_source sources__state_freshwater_wetlands_checkzones
-    # export_source sources__state_freshwater_wetlands
-    # export_source sources__state_tidal_wetlands
+    export_source source__shadow_nat_resources_lines MULTILINESTRING
+    export_source source__shadow_nat_resources_polys
+    export_source source__shadow_nat_resources_buffer
 
-    # export_source sources__natural_resources_buffer
+    export_source source__shadow_hist_resources_points POINT
+    export_source source__shadow_hist_resources_polys
+    export_source source__shadow_hist_resources_buffer
 
-    # # Historic
-    # export_source sources__nyc_historic_buildings_points MULTIPOINT
-    # export_source sources__nyc_historic_buildings_lots
-    # export_source sources__nyc_historic_buildings_buffers
-    # export_source sources__nyc_historic_buildings_buffers_200
-    # export_source sources__nyc_historic_districts
-    # export_source sources__scenic_landmarks
-    # export_source sources__nys_historic_buildings_points POINT
-    # export_source sources__nys_historic_buildings_lots
-    # export_source sources__nys_historic_buildings_buffers
-    # export_source sources__nys_historic_buildings_buffers_200
-    # export_source sources__nys_historic_districts
-    # export_source sources__us_historic_places
-    # export_source sources__us_historic_places_buffers
-    # export_source sources__archaeological_areas
-    # export_source sources__historic_resource_buffer
-
+    echo "FGDB export non-spatial tables ..."
     export_source variables NONE
     export_source source_data_versions NONE
 
