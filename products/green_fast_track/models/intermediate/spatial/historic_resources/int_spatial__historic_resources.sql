643bddeda51492f0712deb0c7306be8391b347c4
Damon McCullough <damonmcc1391@gmail.com>
2024-05-29 23:15:14 -0400

    fix lack of lots in GFT Historic Resources

--- products/green_fast_track/models/intermediate/spatial/historic_resources/int_spatial__historic_resources.sql
+++ products/green_fast_track/models/intermediate/spatial/historic_resources/int_spatial__historic_resources.sql
@@ -1,4 +1,4 @@
-WITH all_natural_resources AS (
+WITH historic_resources AS (
 {{ dbt_utils.union_relations(
     relations=[
         ref('stg__lpc_landmarks'),
@@ -8,14 +8,35 @@ WITH all_natural_resources AS (
     include=["variable_type", "variable_id", "raw_geom"],
     column_override={"raw_geom": "geometry"}
 ) }}
-)
+),
 -- Note: without `column_override`, dbt throws an error trying to cast.
 -- e.g.: `cast("raw_geom" as USER-DEFINED) as "raw...`
 
-SELECT
-    source_relation,
-    'historic_resources' AS flag_id_field_name,
-    variable_type,
-    variable_id,
-    raw_geom
-FROM all_natural_resources
+pluto AS (
+    SELECT geom
+    FROM {{ ref('stg__pluto') }}
+),
+
+historic_resources_with_pluto AS (
+    SELECT
+        h.source_relation,
+        h.variable_type,
+        h.variable_id,
+        h.raw_geom,
+        p.geom AS lot_geom
+    FROM historic_resources AS h
+    LEFT JOIN pluto AS p ON ST_WITHIN(h.raw_geom, p.geom)
+),
+
+final AS (
+    SELECT
+        source_relation,
+        'historic_resources' AS flag_id_field_name,
+        variable_type,
+        variable_id,
+        ST_MULTI(raw_geom) AS raw_geom,
+        ST_MULTI(lot_geom) AS lot_geom
+    FROM historic_resources_with_pluto
+)
+
+SELECT * FROM final
--- products/green_fast_track/models/intermediate/spatial/historic_resources/int_spatial__historic_resources_adj.sql
+++ products/green_fast_track/models/intermediate/spatial/historic_resources/int_spatial__historic_resources_adj.sql
@@ -3,6 +3,7 @@ SELECT
     'historic_resources_adj' AS flag_id_field_name,
     variable_type,
     variable_id,
+    lot_geom,
     raw_geom,
-    ST_BUFFER(raw_geom, 90) AS buffer_geom
+    ST_MULTI(ST_BUFFER(COALESCE(lot_geom, raw_geom), 90)) AS buffer_geom
 FROM {{ ref('int_spatial__historic_resources') }}
