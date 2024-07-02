191dd8e7526d74833d029554882ad0e0f5ecfbb4
Heng Jiang <hengj@andrew.cmu.edu>
2024-07-02 14:35:22 -0400

    add macros for qa queries

---
+++ products/zoningtaxlots/macros/is_diff.sql
@@ -0,0 +1,9 @@
+{% macro is_diff(left_table, right_table, column) %}
+    (
+        {{left_table}}.{{column}} != {{right_table}}.{{column}} 
+        OR 
+        ({{left_table}}.{{column}} IS NULL AND {{right_table}}.{{column}} IS NOT NULL)
+        OR
+        ({{left_table}}.{{column}} IS NOT NULL AND {{right_table}}.{{column}} IS NULL)
+        )
+{% endmacro %}
