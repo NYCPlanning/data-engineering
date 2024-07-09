10b426fd8a5343a617351c93e19c39b874a9f3e6
Heng Jiang <hengj@andrew.cmu.edu>
2024-07-09 16:43:41 -0400

    update ztl export bash and workflow reference

--- products/zoningtaxlots/bash/export.sh
+++ products/zoningtaxlots/bash/export.sh
@@ -3,8 +3,8 @@ source bash/config.sh
 set_error_traps
 
 echo "Archive final output"
-echo "Copy dcp_zoning_taxlot to DB defaultdb ..."
-run_sql_command "CREATE TABLE public.dcp_zoning_taxlot AS SELECT * FROM ${BUILD_ENGINE_SCHEMA}.dcp_zoning_taxlot;"
+echo "Copy zoningtaxlots to DB defaultdb ..."
+run_sql_command "CREATE TABLE public.dcp_zoning_taxlot AS SELECT * FROM ${BUILD_ENGINE_SCHEMA}.int__zoningtaxlots;"
 pg_dump ${BUILD_ENGINE} -t public.dcp_zoning_taxlot --no-owner --clean | psql ${EDM_DATA}
 run_sql_command "DROP TABLE IF EXISTS public.dcp_zoning_taxlot;"
 
@@ -17,12 +17,11 @@ psql ${EDM_DATA} -c "
 "
 
 echo "Run export and qaqc scripts ..."
-run_sql_file sql/export.sql
-psql ${EDM_DATA} --set ON_ERROR_STOP=1 -v VERSION=${VERSION_SQL_TABLE} -f sql/qaqc/frequency.sql
-psql ${EDM_DATA} --set ON_ERROR_STOP=1 -v VERSION=${VERSION_SQL_TABLE} -v VERSION_PREV=${VERSION_PREV_SQL_TABLE} -f sql/qaqc/bbl.sql
-psql ${EDM_DATA} --set ON_ERROR_STOP=1 -v VERSION=${VERSION_SQL_TABLE} -v VERSION_PREV=${VERSION_PREV_SQL_TABLE} -f sql/qaqc/mismatch.sql
-psql ${EDM_DATA} --set ON_ERROR_STOP=1 -v VERSION=${VERSION_SQL_TABLE} -v VERSION_PREV=${VERSION_PREV_SQL_TABLE} -f sql/qaqc/out_bbldiffs.sql | 
-    psql ${BUILD_ENGINE} --set ON_ERROR_STOP=1 -f sql/qaqc/in_bbldiffs.sql
+run_sql_file models/product/zoningtaxlots.sql
+psql ${EDM_DATA} --set ON_ERROR_STOP=1 -v VERSION=${VERSION_SQL_TABLE} -f models/product/qa/qa_freq.sql
+psql ${EDM_DATA} --set ON_ERROR_STOP=1 -v VERSION=${VERSION_SQL_TABLE} -v VERSION_PREV=${VERSION_PREV_SQL_TABLE} -f models/product/qa/qa_bbl.sql
+psql ${EDM_DATA} --set ON_ERROR_STOP=1 -v VERSION=${VERSION_SQL_TABLE} -v VERSION_PREV=${VERSION_PREV_SQL_TABLE} -f models/product/qa/qa_mismatch.sql
+psql ${EDM_DATA} --set ON_ERROR_STOP=1 -v VERSION=${VERSION_SQL_TABLE} -v VERSION_PREV=${VERSION_PREV_SQL_TABLE} -f models/product/qa/qa_bbldiffs.sql | 
 
 ## remove dtm_id column from archive because it isn't a true id but rather one we generate during build
 psql $EDM_DATA -c "ALTER TABLE dcp_zoningtaxlots.\"${VERSION_SQL_TABLE}\" DROP COLUMN dtm_id;"
@@ -35,20 +34,20 @@ rm -rf output && mkdir -p output
     cp ../source_data_versions.csv ./
     cp ../build_metadata.json ./
     
-    csv_export dcp_zoning_taxlot_export zoningtaxlot_db &
+    csv_export zoningtaxlots zoningtaxlot_db &
 
     psql ${EDM_DATA} -c "\copy (
-    SELECT * FROM dcp_zoningtaxlots.qaqc_frequency 
+    SELECT * FROM dcp_zoningtaxlots.qaqc_freq 
     order by version::timestamp
     ) TO STDOUT DELIMITER ',' CSV HEADER;" > qaqc_frequency.csv &
 
     psql ${EDM_DATA} -c "\copy (
-    SELECT * FROM dcp_zoningtaxlots.qaqc_bbl 
+    SELECT * FROM dcp_zoningtaxlots.qa_bbl 
     order by version::timestamp
     ) TO STDOUT DELIMITER ',' CSV HEADER;" > qaqc_bbl.csv &
 
     psql ${EDM_DATA} -c "\copy (
-    SELECT * FROM dcp_zoningtaxlots.qaqc_mismatch 
+    SELECT * FROM dcp_zoningtaxlots.qa_mismatch 
     order by version::timestamp
     ) TO STDOUT DELIMITER ',' CSV HEADER;" > qaqc_mismatch.csv &
 
@@ -60,20 +59,20 @@ rm -rf output && mkdir -p output
         bblprev, zd1prev, zd2prev, zd3prev, zd4prev, 
         co1prev, co2prev, sd1prev, sd2prev, sd3prev, 
         lhdprev, zmnprev, zmcprev
-    FROM qc_bbldiffs
-    ) TO STDOUT DELIMITER ',' CSV HEADER;" > qc_bbldiffs.csv &
+    FROM qa_bbldiffs
+    ) TO STDOUT DELIMITER ',' CSV HEADER;" > qa_bbldiffs.csv &
 
-    shp_export qc_bbldiffs MULTIPOLYGON -t_srs "EPSG:2263"
+    shp_export qa_bbldiffs MULTIPOLYGON -t_srs "EPSG:2263"
 
     echo "${DATE}" > version.txt
     wait
 )
 
 psql -q ${EDM_DATA} -v VERSION=${VERSION_SQL_TABLE} -v VERSION_PREV=${VERSION_PREV_SQL_TABLE} \
-    -f sql/qaqc/versioncomparison.sql > output/qc_versioncomparison.csv
+    -f models/product/qa/qa_vers_comparison.sql > output/qc_versioncomparison.csv
 
 psql -q ${EDM_DATA} -v VERSION=${VERSION_SQL_TABLE} -v VERSION_PREV=${VERSION_PREV_SQL_TABLE} \
-    -f sql/qaqc/null.sql > output/qaqc_null.csv
+    -f models/product/qa/qa_null.sql > output/qaqc_null.csv
 
 echo "Upload outpits ..."
 python3 -m dcpy.connectors.edm.publishing upload -p db-zoningtaxlots -a public-read
