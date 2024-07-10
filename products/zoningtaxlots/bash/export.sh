42cde1dc5f08f2092897e505b5f5a8b086d6dfae
Heng Jiang <hengj@andrew.cmu.edu>
2024-07-10 16:30:42 -0400

    delete config and ztl bash, revise export bash , revise qa table query

--- products/zoningtaxlots/bash/export.sh
+++ products/zoningtaxlots/bash/export.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source bash/config.sh
+source ../../bash/utils.sh
 set_error_traps
 
 echo "Generate csvs and shapefiles ..."
@@ -11,35 +11,18 @@ rm -rf output && mkdir -p output
     cp ../build_metadata.json ./
     
     csv_export zoningtaxlots zoningtaxlot_db &
-
     csv_export qa_freq qaqc_frequency &
-
     csv_export qa_bbl qaqc_bbl &
-
     csv_export qa_mismatch qaqc_mismatch &
-
     csv_export qa_freq qaqc_bbl &
-
     csv_export qa_freq qaqc_bbl &
-
     csv_export qa_vers_comparison qc_versioncomparison &
-
     csv_export qa_null qaqc_null &
-
-    run_sql_command "\copy (
-    SELECT borough_code, tax_block,tax_lot , bblnew ,zd1new , 
-        zd2new ,zd3new , zd4new ,co1new , co2new ,
-        sd1new , sd2new ,sd3new , lhdnew zmnnew , 
-        zmcnew , area, inzonechange , 
-        bblprev, zd1prev, zd2prev, zd3prev, zd4prev, 
-        co1prev, co2prev, sd1prev, sd2prev, sd3prev, 
-        lhdprev, zmnprev, zmcprev
-    FROM ${BUILD_ENGINE_SCHEMA}.qa_bbldiffs
-    ) TO STDOUT DELIMITER ',' CSV HEADER;" > qc_bbldiffs.csv &
+    csv_export qa_bbldiffs qc_bbldiffs &
 
     shp_export qa_bbldiffs MULTIPOLYGON -t_srs "EPSG:2263"
 
-    echo "${DATE}" > version.txt
+    echo "${VERSION}" > version.txt
     wait
 )
 
