50bdd06db70e6e1b77c8291a39093911739ce3ee
Heng Jiang <hengj@andrew.cmu.edu>
2024-07-10 17:26:20 -0400

    drop csv_export renames

--- products/zoningtaxlots/bash/export.sh
+++ products/zoningtaxlots/bash/export.sh
@@ -10,15 +10,15 @@ rm -rf output && mkdir -p output
     cp ../source_data_versions.csv ./
     cp ../build_metadata.json ./
     
-    csv_export zoningtaxlots zoningtaxlot_db &
-    csv_export qaqc_frequency qaqc_frequency &
-    csv_export qaqc_bbl qaqc_bbl &
-    csv_export qaqc_mismatch qaqc_mismatch &
-    csv_export qc_versioncomparison qc_versioncomparison &
-    csv_export qaqc_null qaqc_null &
-    csv_export qc_bbldiffs qc_bbldiffs &
+    csv_export zoningtaxlot_db &
+    csv_export qaqc_frequency &
+    csv_export qaqc_bbl &
+    csv_export qaqc_mismatch &
+    csv_export qc_versioncomparison &
+    csv_export qaqc_null &
+    csv_export qc_bbldiffs &
 
-    shp_export qc_bbldiffs MULTIPOLYGON -f qc_bbldiffs -t_srs "EPSG:2263"
+    shp_export qc_bbldiffs MULTIPOLYGON -t_srs "EPSG:2263"
 
     echo "${VERSION}" > version.txt
     wait
