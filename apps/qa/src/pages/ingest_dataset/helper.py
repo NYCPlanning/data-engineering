798755efbe76e5adb0996d7186814e62f16605c0
Heng Jiang <hengj@andrew.cmu.edu>
2024-07-22 15:46:02 -0400

    black fix

--- apps/qa/src/pages/ingest_dataset/helper.py
+++ apps/qa/src/pages/ingest_dataset/helper.py
@@ -5,8 +5,9 @@ from dcpy.utils import s3
 
 BUCKET = "edm-recipes"
 
+
 def archive_raw_data(dataset_name: str, version: str, uploaded_file, file_name: str):
-    
+
     base_path = Path(dataset_name)
     file_path = base_path / dataset_name
     base_path.mkdir(parents=True, exist_ok=True)
@@ -17,7 +18,9 @@ def archive_raw_data(dataset_name: str, version: str, uploaded_file, file_name: 
     with open(file_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
 
-    st.success(f"Temporary file {uploaded_file.name} saved successfully as {dataset_name}")
+    st.success(
+        f"Temporary file {uploaded_file.name} saved successfully as {dataset_name}"
+    )
 
     s3.upload_file(
         BUCKET,
