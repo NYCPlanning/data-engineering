46315e6906a488652d6f2a98f8143c7e5ea6609b
Heng Jiang <hengj@andrew.cmu.edu>
2024-07-22 10:16:32 -0400

    black linting fix

--- apps/qa/src/pages/ingest_dataset/helper.py
+++ apps/qa/src/pages/ingest_dataset/helper.py
@@ -9,15 +9,16 @@ from dcpy.utils import s3
 import yaml
 from dcpy.library.archive import Archive
 import os
-        
+
 BUCKET = "edm-recipes"
 
-def archive_raw_data (dataset_name: str, version: str, uploaded_file,file_name):
+
+def archive_raw_data(dataset_name: str, version: str, uploaded_file, file_name):
     base_path = Path(dataset_name) / version
     base_path.mkdir(parents=True, exist_ok=True)
-    
+
     file_path = base_path / dataset_name
-    s3_path = Path('inbox')/dataset_name/version/f"{file_name}"
+    s3_path = Path("inbox") / dataset_name / version / f"{file_name}"
     st.success(f"Archive started...")
 
     with open(file_path, "wb") as f:
@@ -29,37 +30,41 @@ def archive_raw_data (dataset_name: str, version: str, uploaded_file,file_name):
 
     recipes._archive_dataset(config, file_path, s3_path)
 
-    st.success(f"Dataset {dataset_name} version {version} has been archived successfully to {s3_path}.")
+    st.success(
+        f"Dataset {dataset_name} version {version} has been archived successfully to {s3_path}."
+    )
 
     try:
         shutil.rmtree(base_path.parent)
         shutil.rmtree(Path("__pycache__"))
         st.success(f"Local temporary files and pycache deleted successfully.")
     except Exception as e:
-        st.error('no deletion needed') 
+        st.error("no deletion needed")
 
     st.success(f"Process completed.")
     return file_path
 
-def edit_tamplate (dataset_name, s3_path):
+
+def edit_tamplate(dataset_name, s3_path):
     st.warning(f"Current working directory: {os.getcwd()}")
-    template_path = Path("../../../../../dcpy/library/templates") / f"{dataset_name}.yml"
-    with open(template_path,"r") as temp:
+    template_path = (
+        Path("../../../../../dcpy/library/templates") / f"{dataset_name}.yml"
+    )
+    with open(template_path, "r") as temp:
         template = yaml.safe_load(temp)
     new_tamplate_path = Path(f"{dataset_name}.yml")
-    s3_path=str(s3_path)
-    
-    template['dataset']['source']['script']['path'] = f"s3://{BUCKET}/{s3_path}"
-    with open(new_tamplate_path,"w") as temp:
-        yaml.safe_dump(template,temp)
+    s3_path = str(s3_path)
+
+    template["dataset"]["source"]["script"]["path"] = f"s3://{BUCKET}/{s3_path}"
+    with open(new_tamplate_path, "w") as temp:
+        yaml.safe_dump(template, temp)
     return new_tamplate_path
+
 
 def library_archive(template_path):
     a = Archive()
-    a(
-        template_path,
-        output_format="csv", push=True, clean=True,
-        latest=True
-    )
+    a(template_path, output_format="csv", push=True, clean=True, latest=True)
     return null
-#### error: bucket not found 
+
+
+#### error: bucket not found
--- apps/qa/src/pages/ingest_dataset/home.py
+++ apps/qa/src/pages/ingest_dataset/home.py
@@ -8,14 +8,13 @@ st.title("Ingest Dataset")
 dataset_name = st.text_input("Dataset Name")
 version = st.text_input("Version")
 uploaded_file = st.file_uploader("Choose a file")
-s3_path = Path('inbox')/dataset_name/version
+s3_path = Path("inbox") / dataset_name / version
 file_name = uploaded_file.name
 
 if st.button("Ingest"):
     if dataset_name and version and uploaded_file:
-        file_path = archive_raw_data(dataset_name, version, uploaded_file,file_name)
-        template_path = edit_tamplate (dataset_name, s3_path)
+        file_path = archive_raw_data(dataset_name, version, uploaded_file, file_name)
+        template_path = edit_tamplate(dataset_name, s3_path)
         library_archive(template_path)
     else:
         st.warning("Please input all fields.")
-
