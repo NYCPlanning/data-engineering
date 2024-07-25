4610eac8c4b7f0c4286fb7a0d083d1fa89291c77
Heng Jiang <hengj@andrew.cmu.edu>
2024-07-25 15:47:45 -0400

    black fix

--- products/metadata/save_html.py
+++ products/metadata/save_html.py
@@ -1,28 +1,32 @@
-from dcpy.models.product.dataset.metadata import Metadata 
+from dcpy.models.product.dataset.metadata import Metadata
 import argparse
 from pathlib import Path
 from jinja2 import Environment, FileSystemLoader
 
+
 def save_html(yaml_file_path, html_template_path, output_html_path):
     html_output = to_html(yaml_file_path, html_template_path)
     output_path = Path(output_html_path)
-    with open(output_path, 'w') as f:
+    with open(output_path, "w") as f:
         f.write(html_output)
-    return output_path 
+    return output_path
+
 
 def to_html(yaml_file_path, html_template_path):
     env = Environment(loader=FileSystemLoader(Path(html_template_path).parent))
     template = env.get_template(Path(html_template_path).name)
     return template.render(metadata=get_metadata(yaml_file_path))
 
+
 def get_metadata(yaml_file_path):
-    metadata = Metadata.from_path(Path(yaml_file_path), template_vars={'var1': 'value1'})
+    metadata = Metadata.from_path(
+        Path(yaml_file_path), template_vars={"var1": "value1"}
+    )
     return metadata
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Convert a YAML file to a HTML file."
-    )
+    parser = argparse.ArgumentParser(description="Convert a YAML file to a HTML file.")
     parser.add_argument(
         "yaml_file_path", type=str, help="The path to the input YAML file"
     )
