1dce0030df37ddba6c4a4f9a258e3377860fb2bb
Heng Jiang <hengj@andrew.cmu.edu>
2024-07-26 12:00:32 -0400

    black fix

--- apps/qa/src/pages/ingest/ingest.py
+++ apps/qa/src/pages/ingest/ingest.py
@@ -2,27 +2,27 @@ def ingest():
     import streamlit as st
     from .helpers import archive_raw_data
     from pathlib import Path
-    import time 
+    import time
 
     st.title("Ingest Dataset")
 
-    if 'running' not in st.session_state:
+    if "running" not in st.session_state:
         st.session_state.running = False
 
-    dataset_name = st.text_input("Dataset Name", disabled = st.session_state.running)
-    version = st.text_input("Version", disabled = st.session_state.running)
-    uploaded_file = st.file_uploader("Choose a file", disabled = st.session_state.running)
+    dataset_name = st.text_input("Dataset Name", disabled=st.session_state.running)
+    version = st.text_input("Version", disabled=st.session_state.running)
+    uploaded_file = st.file_uploader("Choose a file", disabled=st.session_state.running)
     s3_path = Path("inbox") / dataset_name / version
 
-    disabled = st.session_state.get("running",False)
-    ingest_button_pressed = st.button("Ingest", disabled = st.session_state.running)
+    disabled = st.session_state.get("running", False)
+    ingest_button_pressed = st.button("Ingest", disabled=st.session_state.running)
     if ingest_button_pressed == True:
         if dataset_name and version and uploaded_file:
-            st.session_state.running = True 
+            st.session_state.running = True
             st.rerun()
-        else: 
+        else:
             st.warning("Please input all fields.")
-    
+
     if st.session_state.running == True:
         with st.spinner("Ingesting"):
             time.sleep(5)
@@ -30,7 +30,5 @@ def ingest():
             file_path = archive_raw_data(
                 dataset_name, version, uploaded_file, file_name
             )
-        st.session_state.running = False 
+        st.session_state.running = False
         st.rerun()
-
-            
