c210064b10f810b0cc84fea7a7ec1c7920df6b51
Heng Jiang <hengj@andrew.cmu.edu>
2024-07-26 10:18:27 -0400

    added data types to functions

--- dcpy/lifecycle/package/generate_metadata_assets.py
+++ dcpy/lifecycle/package/generate_metadata_assets.py
@@ -1,4 +1,4 @@
-from dcpy.models.product.dataset.metadata import Metadata
+from dcpy.models.product.dataset.metadata import Metadata, SocrataMetada
 import argparse
 from pathlib import Path
 from jinja2 import Environment, FileSystemLoader
@@ -8,7 +8,7 @@ app = typer.Typer()
 
 
 @app.command()
-def generate_metadata_assets(yaml_file_path, html_template_path, output_html_path):
+def generate_metadata_assets(yaml_file_path: str, html_template_path: str, output_html_path: str) -> Path:
     html_output = to_html(yaml_file_path, html_template_path)
     output_path = Path(output_html_path)
     with open(output_path, "w") as f:
@@ -16,13 +16,13 @@ def generate_metadata_assets(yaml_file_path, html_template_path, output_html_pat
     return output_path
 
 
-def to_html(yaml_file_path, html_template_path):
+def to_html(yaml_file_path: str, html_template_path: str) -> str:
     env = Environment(loader=FileSystemLoader(Path(html_template_path).parent))
     template = env.get_template(Path(html_template_path).name)
     return template.render(metadata=get_metadata(yaml_file_path))
 
 
-def get_metadata(yaml_file_path):
+def get_metadata(yaml_file_path: str) -> SocrataMetada:
     metadata = Metadata.from_path(
         Path(yaml_file_path), template_vars={"var1": "value1"}
     )
