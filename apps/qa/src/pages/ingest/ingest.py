268499a54c0bd656db5e3a1daef28c3ac0c363e7
Heng Jiang <hengj@andrew.cmu.edu>
2024-07-26 11:58:12 -0400

    add codes to lock input boxes when running

--- apps/qa/src/pages/ingest/ingest.py
+++ apps/qa/src/pages/ingest/ingest.py
@@ -2,20 +2,35 @@ def ingest():
     import streamlit as st
     from .helpers import archive_raw_data
     from pathlib import Path
+    import time 
 
     st.title("Ingest Dataset")
 
-    dataset_name = st.text_input("Dataset Name")
-    version = st.text_input("Version")
-    uploaded_file = st.file_uploader("Choose a file")
+    if 'running' not in st.session_state:
+        st.session_state.running = False
+
+    dataset_name = st.text_input("Dataset Name", disabled = st.session_state.running)
+    version = st.text_input("Version", disabled = st.session_state.running)
+    uploaded_file = st.file_uploader("Choose a file", disabled = st.session_state.running)
     s3_path = Path("inbox") / dataset_name / version
 
-    ingest_button_pressed = st.button("Ingest")
+    disabled = st.session_state.get("running",False)
+    ingest_button_pressed = st.button("Ingest", disabled = st.session_state.running)
     if ingest_button_pressed == True:
         if dataset_name and version and uploaded_file:
+            st.session_state.running = True 
+            st.rerun()
+        else: 
+            st.warning("Please input all fields.")
+    
+    if st.session_state.running == True:
+        with st.spinner("Ingesting"):
+            time.sleep(5)
             file_name = uploaded_file.name
             file_path = archive_raw_data(
                 dataset_name, version, uploaded_file, file_name
             )
-        else:
-            st.warning("Please input all fields.")
+        st.session_state.running = False 
+        st.rerun()
+
+            
