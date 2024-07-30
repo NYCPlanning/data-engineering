080ef5cb4646e62d137e1c65da1c7d30b910bfd3
Heng Jiang <hengj@andrew.cmu.edu>
2024-07-30 12:10:10 -0400

    revise button evals and session_state contents

--- apps/qa/src/pages/ingest/ingest.py
+++ apps/qa/src/pages/ingest/ingest.py
@@ -7,19 +7,14 @@ def ingest():
     if "ingest" not in st.session_state:
         st.session_state["ingest"] = {
             "running": False,
-            "dataset_name": None,
-            "version": None,
-            "s3_path": None,
-            "uploaded_file": None,
-            "ingest_status": None,
-            "ingest_error_message": None,
+            "ingest_status": None
         }
 
     def run_start():
         if (
-            st.session_state["ingest"]["dataset_name"]
-            and st.session_state["ingest"]["version"]
-            and st.session_state["ingest"]["uploaded_file"]
+            dataset_name
+            and version
+            and uploaded_file
         ):
             st.session_state["ingest"]["running"] = True
         else:
@@ -29,54 +24,50 @@ def ingest():
         with st.spinner("Ingesting"):
             try:
                 file_path = archive_raw_data(
-                    st.session_state["ingest"]["dataset_name"],
-                    st.session_state["ingest"]["version"],
-                    st.session_state["ingest"]["uploaded_file"],
-                    st.session_state["ingest"]["uploaded_file"].name,
+                    dataset_name,
+                    version,
+                    uploaded_file,
+                    uploaded_file.name
                 )
                 st.session_state["ingest"]["ingest_status"] = "success"
             except Exception as e:
                 st.session_state["ingest"]["ingest_status"] = "fail"
-                st.session_state["ingest"]["ingest_error_message"] = e
+                ingest_error_message = e
 
     def run_stop():
         st.session_state["ingest"]["running"] = False
         st.session_state["ingest"]["ingest_status"] = None
-        st.session_state["ingest"]["ingest_error_message"] = None
+
 
     st.title("Ingest Dataset")
-    st.session_state["ingest"]["dataset_name"] = st.text_input(
+
+    dataset_name = st.text_input(
         "Dataset Name",
-        value=st.session_state["ingest"]["dataset_name"],
         disabled=st.session_state["ingest"]["running"],
     )
-    st.session_state["ingest"]["version"] = st.text_input(
+    version = st.text_input(
         "Version",
-        value=st.session_state["ingest"]["version"],
         disabled=st.session_state["ingest"]["running"],
     )
-    st.session_state["ingest"]["uploaded_file"] = st.file_uploader(
-        "Choose a file", disabled=st.session_state["ingest"]["running"]
+    uploaded_file = st.file_uploader(
+        "Choose a file", 
+        disabled=st.session_state["ingest"]["running"]
     )
-    if (
-        st.session_state["ingest"]["dataset_name"]
-        and st.session_state["ingest"]["version"]
-    ):
-        st.session_state["ingest"]["s3_path"] = (
-            Path("inbox")
-            / st.session_state["ingest"]["dataset_name"]
-            / st.session_state["ingest"]["version"]
-        )
-    if (
-        st.button(
-            "Ingest", on_click=run_start, disabled=st.session_state["ingest"]["running"]
-        )
-        and st.session_state["ingest"]["running"] == True
-    ):
+
+    if dataset_name and version:
+        s3_path = (
+                Path("inbox")
+                / dataset_name
+                / version
+            )
+    
+    ingest_button_pressed  = st.button("Ingest", on_click=run_start, disabled=st.session_state["ingest"]["running"])
+    
+    if ingest_button_pressed == True and st.session_state["ingest"]["running"] == True:
         start_ingest()
     if st.session_state["ingest"]["ingest_status"] == "success":
         st.success("Ingest Successful")
         st.button("Restart", on_click=run_stop)
     if st.session_state["ingest"]["ingest_status"] == "fail":
-        st.error(st.session_state["ingest"]["ingest_error_message"])
+        st.error(ingest_error_message)
         st.button("Restart", on_click=run_stop)
