cdd1ffe9f4fee5025f66de357a5152049b17b292
Finn van Krieken <fvankrieken@planning.nyc.gov>
2024-08-12 18:51:15 +0000

    f

--- products/green_fast_track/models/staging/stg__dcp_wrp_special_natural_waterfront_areas.sql
+++ products/green_fast_track/models/staging/stg__dcp_wrp_special_natural_waterfront_areas.sql
@@ -1,6 +1,6 @@
 SELECT
     'snwa' AS variable_type,
     name AS variable_id,
-    shape AS raw_geom,
+    wkb_geometry AS raw_geom,
     NULL AS buffer
 FROM {{ source("recipe_sources", "dcp_wrp_special_natural_waterfront_areas") }}
