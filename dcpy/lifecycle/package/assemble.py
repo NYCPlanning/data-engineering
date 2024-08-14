a4c738b6ef2158c138af22b86dc54b4fbb7d617e
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2024-08-14 11:49:36 -0400

    Rewrite lifecycle.package.assemble (and use v2 md)

--- dcpy/lifecycle/package/assemble.py
+++ dcpy/lifecycle/package/assemble.py
@@ -1,74 +1,99 @@
+import os
 from pathlib import Path
-import typer
-import tempfile
-import urllib.request
 import shutil
+import tempfile
+import typer
 
-import dcpy.models.product.dataset.metadata as models
-from dcpy.utils.logging import logger
 from dcpy.lifecycle import WORKING_DIRECTORIES
-from dcpy.connectors.edm import packaging
+import dcpy.models.product.dataset.metadata_v2 as md
+from dcpy.utils.logging import logger
 
-ASSEMBLY_DIR = WORKING_DIRECTORIES.packaging / ".assembly"
+ASSEMBLY_DIR = WORKING_DIRECTORIES.packaging / "assembly"
 
 
-def dataset_from_bytes(
-    product_metadata: models.Metadata, product: str, version: str, dataset: str
+def make_package_folder(path: Path):
+    logger.info(f"Making package folders at: {path}")
+    path.mkdir(exist_ok=True, parents=True)
+    (path / "attachments").mkdir(exist_ok=True)
+    (path / "dataset_files").mkdir(exist_ok=True)
+    (path / "zip_files").mkdir(exist_ok=True)
+
+
+def _file_id_to_zipped_paths(
+    product_metadata: md.Metadata,
+    package_id: str,
+) -> dict[str, dict[str, Path]]:
+    """Creates a mapping for locations of
+    1. filenames in a zip and
+    2. their location in the package.
+    We'll have to account for paths in the zip at some point."""
+    package = product_metadata.get_package(package_id)
+    mapping = {}
+    for pf in package.contents:
+        f = product_metadata.get_file_and_overrides(pf.id)
+        folder = "attachments" if f.file.is_metadata else "dataset_files"
+        mapping[f.file.id] = {
+            "package_path": Path(folder) / f.file.filename,
+            "zipped_path": Path(pf.filename or f.file.filename),
+        }
+    return mapping
+
+
+def package(
+    output_path: Path,
+    package_path: Path,
+    package_id: str,
+    product_metadata: md.Metadata,
 ):
-    out_dir = ASSEMBLY_DIR / product / version / dataset
-    out_dir.mkdir(exist_ok=True, parents=True)
-    (out_dir / "attachments").mkdir(exist_ok=True)
-    (out_dir / "dataset_files").mkdir(exist_ok=True)
-    (out_dir / "zip_files").mkdir(exist_ok=True)
+    package = product_metadata.get_package(package_id)
+    file_ids_to_paths = _file_id_to_zipped_paths(product_metadata, package_id)
+    logger.info(f"Packaging files: {file_ids_to_paths}")
+    pass  # TODO: One day, the rest
 
-    product_metadata.write_source_metadata_to_file(Path(out_dir / "metadata.yml"))
 
-    package_files_by_id = product_metadata.package.files_by_id()
-    asset_types_by_file_id = product_metadata.package.asset_types_by_file_id()
+def unzip_into_package(
+    zip_path: Path,
+    package_path: Path,
+    package_id: str,
+    product_metadata: md.Metadata,
+):
+    """Unpackages a zipped file into our package format.
 
-    for bytes_dest in [d for d in product_metadata.destinations if d.type == "bytes"]:
-        for dest_file in bytes_dest.files:
-            f = package_files_by_id[dest_file.id]
-            asset_type = asset_types_by_file_id[dest_file.id]
-            file_path = Path(out_dir) / asset_type / f.filename
-            logger.info(f"downloading{f.filename} to {file_path}")
-            urllib.request.urlretrieve(dest_file.url, file_path)
+    Rationale: A product like Pluto will often zip assets (a csv, a readme) into a zip.
+    This allows us to reverse that process, to construct packages straight from what's
+    zipped up on Bytes.
+    """
+    package = product_metadata.get_package(package_id)
+    file_ids_to_paths = _file_id_to_zipped_paths(product_metadata, package_id)
+    logger.info(f"Unpackaging files: {file_ids_to_paths}")
 
-            if type(f) == models.ZipFile:
-                logger.info(f"Unzipping: {f.id} to retrieve contents")
-                with tempfile.TemporaryDirectory() as temp_unpacked_dir:
-                    shutil.unpack_archive(file_path, temp_unpacked_dir)
-                    for contained_file in f.contains:
-                        cf = package_files_by_id[contained_file.id]
-                        asset_type = asset_types_by_file_id[contained_file.id]
-                        logger.info(f"Extracting zipped file: {cf.id}")
-                        shutil.copy2(
-                            Path(temp_unpacked_dir) / contained_file.filename,  # type: ignore
-                            out_dir / asset_type / cf.filename,
-                        )
+    make_package_folder(package_path)
+
+    with tempfile.TemporaryDirectory() as temp_unpacked_dir:
+        unpacked_dir = Path(temp_unpacked_dir) / zip_path.stem
+        logger.info(f"Unpacking zip at: {zip_path}, to: {unpacked_dir}")
+        shutil.unpack_archive(zip_path, unpacked_dir)
+        assert (
+            unpacked_dir.exists()
+        ), f"Expected {unpacked_dir} to exist. Found {os.listdir(temp_unpacked_dir)}"
+        logger.info(f"Files in unzipped dir: {os.listdir(unpacked_dir)}")
+
+        if zip_path.stem in os.listdir(unpacked_dir):
+            # If the file was zipped in a certain way, it may contain an intermediate folder.
+            unpacked_dir = unpacked_dir / zip_path.stem
+
+        for contained_file in package.contents:
+            paths = file_ids_to_paths[contained_file.id]
+            logger.info(
+                f"Extracting zipped file: {contained_file.id}. Path mappings: {paths}"
+            )
+            copy_from_path = unpacked_dir / str(paths["zipped_path"])
+            copy_to_path = package_path / paths["package_path"]
+            logger.info(f"Copying {copy_from_path} to {copy_to_path}")
+            shutil.copy2(
+                copy_from_path,
+                copy_to_path,
+            )
 
 
 app = typer.Typer()
-
-
-@app.command("dataset_from_bytes")
-def _dataset_from_bytes_cli(
-    metadata_path: Path, product: str, version: str, dataset: str
-):
-    md = models.Metadata.from_path(
-        metadata_path,
-        template_vars={"version": version},
-    )
-    dataset_from_bytes(md, product, version, dataset)
-
-
-@app.command("product_from_bytes")
-def _product_from_bytes_cli(product_metadata_path: Path, product: str, version: str):
-    for folder in [
-        p for p in product_metadata_path.iterdir() if not p.name.startswith(".")
-    ]:
-        md = models.Metadata.from_path(
-            folder / "metadata.yml",
-            template_vars={"version": version},
-        )
-        dataset_from_bytes(md, product, version, dataset=folder.name)
