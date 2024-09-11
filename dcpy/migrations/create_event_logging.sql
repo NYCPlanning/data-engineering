39d1e1e6f7e3c4ce1b87ae785e02b53c775739c0
sf-dcp <ofilippova@planning.nyc.gov>
2024-09-11 12:02:13 -0400

    POST REVIEW: add SQL file with DDL query for `event_logging` table

---
+++ dcpy/migrations/create_event_logging.sql
@@ -0,0 +1,19 @@
+/*
+This SQL file contains queries for creating the event_logging table.
+The table is used to track the lifecycle of data products, 
+from building to publishing, and is part of the de-qaqc database.
+*/
+
+CREATE TABLE product_data.event_logging (
+    product VARCHAR(50) NOT NULL,
+    version VARCHAR(20) NOT NULL,
+    event VARCHAR(30) NOT NULL,
+    path TEXT NOT NULL,
+    old_path TEXT,
+    runner_type VARCHAR,
+    runner VARCHAR,
+    timestamp TIMESTAMP NOT NULL,
+    custom_fields JSONB
+);
+
+CREATE INDEX idx_product_version_path ON product_data.event_logging (product, version, path);
