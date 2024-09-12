0e87b9cbebe43b76ea633a080b3c32a38cb0a06f
Finn van Krieken <fvankrieken@planning.nyc.gov>
2024-09-12 12:34:25 -0400

    appease ruff for in-progress test

--- dcpy/test/lifecycle/package/test_parse_shapefile_to_md.py
+++ dcpy/test/lifecycle/package/test_parse_shapefile_to_md.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+# from pathlib import Path
 import yaml
 
 from dcpy.lifecycle.package import shapefiles
@@ -6,14 +6,15 @@ from dcpy.models.product.dataset import metadata_v2 as dsmd
 from . import RESOURCES_PATH
 
 
+# TODO remove noqas
 def test_parsing_shapefile_md():
     shape_path = RESOURCES_PATH / "shapefile_with_md" / "sample_shapefile_metadata.xml"
     md_path = RESOURCES_PATH / "shapefile_with_md" / "sample_shapefile_metadata.yml"
 
     md = None
     with open(md_path, "r", encoding="utf-8") as raw:
-        md = dsmd.Metadata.model_construct(**yaml.safe_load(raw.read()))
-    parsed_md = shapefiles.parse_shapefile_metadata(file_path=shape_path)
+        md = dsmd.Metadata.model_construct(**yaml.safe_load(raw.read()))  # noqa: F841
+    parsed_md = shapefiles.parse_shapefile_metadata(file_path=shape_path)  # noqa: F841
 
     # TODO: post-ruffing. This test is failing for mysterious reasons
     # assert type(parsed_md.attributes) is dsmd.DatasetAttributes
