1c36ebb1ec09d3d947167bcb0db8310fc437bcd3
Finn van Krieken <fvankrieken@planning.nyc.gov>
2024-09-13 09:59:20 -0400

    read csvs simply

--- dcpy/library/script/dob_now_permits.py
+++ dcpy/library/script/dob_now_permits.py
@@ -1,30 +1,17 @@
 import pandas as pd
 
+from dcpy.utils import s3
 from .scriptor import ScriptorInterface
 from . import df_to_tempfile
-from dotenv import load_dotenv
-import boto3
-import os
-import io
-
-# Load environmental variables
-load_dotenv()
 
 
 class Scriptor(ScriptorInterface):
     def ingest(self) -> pd.DataFrame:
-        client = boto3.client(
-            "s3",
-            aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
-            aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
-            endpoint_url=os.environ["AWS_S3_ENDPOINT"],
+        data = s3.get_file_as_stream(
+            "edm-private",
+            f"dob_now/dob_now_permits/DOB_Now_Permit_Filing_Data_for_DCP_{self.version}.csv",
         )
-        obj = client.get_object(
-            Bucket="edm-private",
-            Key=f"dob_now/dob_now_permits/DOB_Now_Permit_Filing_Data_for_DCP_{self.version}.csv",
-        )
-        data = obj["Body"].read()
-        df = pd.read_csv(io.BytesIO(data), encoding="cp1252", sep="|")
+        df = pd.read_csv(data, encoding="cp1252", sep="\t")
         return df
 
     def runner(self) -> str:
