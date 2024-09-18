443ad2277538378ccd91fd01261788a37a801e88
Damon McCullough <damonmcc1391@gmail.com>
2024-09-18 12:32:45 -0400

    combine all data dictionary generation tests

--- dcpy/test/lifecycle/package/conftest.py
+++ dcpy/test/lifecycle/package/conftest.py
@@ -2,9 +2,14 @@ from pathlib import Path
 import pytest
 import shutil
 
-RESOURCES_PATH = Path(__file__).parent.resolve() / "resources"
+from dcpy.test.conftest import RESOURCES
 
-TEST_PACKAGE_PATH = RESOURCES_PATH / "test_package"
+TEST_ASSEMBLED_PACKAGE_AND_METADATA_PATH = (
+    RESOURCES / "product_metadata" / "assembled_package_and_metadata"
+)
+
+PACKAGE_RESOURCES_PATH = Path(__file__).parent.resolve() / "resources"
+TEST_PACKAGE_PATH = PACKAGE_RESOURCES_PATH / "test_package"
 
 TEST_METADATA_YAML_PATH = TEST_PACKAGE_PATH / "metadata.yml"
 
--- dcpy/test/lifecycle/package/test_generate_data_dictionary.py
+++ dcpy/test/lifecycle/package/test_generate_data_dictionary.py
@@ -1,15 +1,21 @@
 import pytest
 from unittest import TestCase
-from dcpy.test.lifecycle.package.conftest import TEST_METADATA_YAML_PATH, TEMP_DATA_PATH
+from dcpy.test.lifecycle.package.conftest import (
+    TEST_ASSEMBLED_PACKAGE_AND_METADATA_PATH,
+    TEST_METADATA_YAML_PATH,
+    TEMP_DATA_PATH,
+)
 
-from dcpy.lifecycle.package import generate_metadata_assets
+from dcpy.lifecycle.package import generate_metadata_assets, oti_xlsx
 
 
 @pytest.mark.usefixtures("file_setup_teardown")
 class TestDataDictionary(TestCase):
+    package_path = TEST_ASSEMBLED_PACKAGE_AND_METADATA_PATH
     yaml_file_path = TEST_METADATA_YAML_PATH
     output_html_path = TEMP_DATA_PATH / "metadata.html"
     output_pdf_path = TEMP_DATA_PATH / "metadata.pdf"
+    output_xlsx_path = TEMP_DATA_PATH / "my_data_dictionary.xlsx"
 
     def test_generate_html_from_yaml(self):
         html_path = generate_metadata_assets.generate_html_from_yaml(
@@ -30,3 +36,10 @@ class TestDataDictionary(TestCase):
             output_pdf_path=self.output_pdf_path,
         )
         assert pdf_path.exists()
+
+    def test_generate_xslx(self):
+        oti_xlsx.write_oti_xlsx(
+            metadata_path=self.package_path / "metadata.yml",
+            output_path=self.output_xlsx_path,
+        )
+        assert self.output_xlsx_path.exists()
