408f03ba06cb00e480f1c5c4940719542d5dbc72
Damon McCullough <damonmcc1391@gmail.com>
2024-09-18 12:02:10 -0400

    use `TestCase` class for data dictionary tests

--- dcpy/test/lifecycle/package/test_generate_data_dictionary.py
+++ dcpy/test/lifecycle/package/test_generate_data_dictionary.py
@@ -1,31 +1,32 @@
 import pytest
+from unittest import TestCase
 from dcpy.test.lifecycle.package.conftest import TEST_METADATA_YAML_PATH, TEMP_DATA_PATH
 
 from dcpy.lifecycle.package import generate_metadata_assets
 
-TEST_METADATA_HTML_PATH = TEMP_DATA_PATH / "metadata.html"
-TEST_METADATA_PDF_PATH = TEMP_DATA_PATH / "metadata.pdf"
-
 
 @pytest.mark.usefixtures("file_setup_teardown")
-def test_generate_html_from_yaml():
-    html_path = generate_metadata_assets.generate_html_from_yaml(
-        yaml_file_path=TEST_METADATA_YAML_PATH,
-        output_html_path=TEST_METADATA_HTML_PATH,
-        html_template_path=generate_metadata_assets.DEFAULT_DATA_DICTIONARY_TEMPLATE_PATH,
-    )
-    assert html_path.exists()
+class TestDataDictionary(TestCase):
+    yaml_file_path = TEST_METADATA_YAML_PATH
+    output_html_path = TEMP_DATA_PATH / "metadata.html"
+    output_pdf_path = TEMP_DATA_PATH / "metadata.pdf"
 
+    def test_generate_html_from_yaml(self):
+        html_path = generate_metadata_assets.generate_html_from_yaml(
+            yaml_file_path=self.yaml_file_path,
+            output_html_path=self.output_html_path,
+            html_template_path=generate_metadata_assets.DEFAULT_DATA_DICTIONARY_TEMPLATE_PATH,
+        )
+        assert html_path.exists()
 
-@pytest.mark.usefixtures("file_setup_teardown")
-def test_generate_pdf_from_html():
-    html_path = generate_metadata_assets.generate_html_from_yaml(
-        yaml_file_path=TEST_METADATA_YAML_PATH,
-        output_html_path=TEST_METADATA_HTML_PATH,
-        html_template_path=generate_metadata_assets.DEFAULT_DATA_DICTIONARY_TEMPLATE_PATH,
-    )
-    pdf_path = generate_metadata_assets.generate_pdf_from_html(
-        output_html_path=html_path,
-        output_pdf_path=TEST_METADATA_PDF_PATH,
-    )
-    assert pdf_path.exists()
+    def test_generate_pdf_from_html(self):
+        html_path = generate_metadata_assets.generate_html_from_yaml(
+            yaml_file_path=self.yaml_file_path,
+            output_html_path=self.output_html_path,
+            html_template_path=generate_metadata_assets.DEFAULT_DATA_DICTIONARY_TEMPLATE_PATH,
+        )
+        pdf_path = generate_metadata_assets.generate_pdf_from_html(
+            output_html_path=html_path,
+            output_pdf_path=self.output_pdf_path,
+        )
+        assert pdf_path.exists()
