69fe8179898039b266d689fde0e992d4008d4548
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2024-09-27 15:24:46 -0400

    Add validation script

--- dcpy/lifecycle/scripts/_cli.py
+++ dcpy/lifecycle/scripts/_cli.py
@@ -1,6 +1,8 @@
 import typer
 from .package_and_distribute import app as package_dist_app
+from .product_metadata import app as product_metadata_app
 
 app = typer.Typer()
 
 app.add_typer(package_dist_app, name="package_and_dist")
+app.add_typer(product_metadata_app, name="product_metadata")
