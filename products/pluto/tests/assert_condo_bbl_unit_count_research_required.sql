c17438f723d10718cf94bf086c2a302258d182b6
sf-dcp <ofilippova@planning.nyc.gov>
2024-09-26 23:41:58 -0400

    Refactor singular test to use intermediate tables: * These intermediate tables can also be used to populate reports to share with DOF

--- products/pluto/tests/assert_condo_bbl_unit_count_research_required.sql
+++ products/pluto/tests/assert_condo_bbl_unit_count_research_required.sql
@@ -8,15 +8,18 @@
 				The remaining logic cross-checks these BBL "offenders" with the PLUTO corrections file and the Developments database (aka DevDB) to narrow down the records needing manual review.
 			''',
             'next_steps': '''
-				Manually research failing records. If confirmed, update the corrections file with the correct unit counts and share a report with Amanda to send to DOF.
+				1. Manually research failing records. 
+                2. If confirmed, update the corrections file with the correct total and res unit number; 
+                not confirmed bbls need to be added to the `ignored_bbls_for_unit_count_test` seed file.
+                3. Re-run PLUTO build. If this check passes, share reports from `models/qaqc/reports` with Amanda to send them to DOF.
 			'''
         }
     ) 
 }}
 
--- Find condo records where multiple unit BBLs have coop_apts > 1
+-- Find records where multiple unit BBLs have coop_apts > 1
 WITH condo_prime_bbls AS (
-    SELECT DISTINCT primebbl
+    SELECT primebbl
     FROM {{ ref('pluto_rpad_geo') }}
     WHERE coop_apts > 1
     GROUP BY primebbl
@@ -35,64 +38,23 @@ primebbl_offenders AS (
     GROUP BY primebbl
 ),
 
-historical_unit_corrections AS (
-    SELECT
-        bbl,
-        old_value::numeric
-    FROM {{ source("recipe_sources", "pluto_input_research") }}
-    WHERE field = 'unitsres' AND substring(bbl, 7, 2) = '75'
-),
-
-current_unit_corrections AS (
-    SELECT l.* FROM historical_unit_corrections AS l
-    INNER JOIN primebbl_offenders AS r
-        ON l.bbl = r.primebbl AND l.old_value = r.coop_apts
-),
-
+-- Identify bbl offenders that are not actively corrected
 uncorrected_primebbl_offenders AS (
     SELECT *
     FROM primebbl_offenders
-    WHERE primebbl NOT IN (SELECT bbl FROM current_unit_corrections)
+    WHERE primebbl NOT IN (
+        SELECT bbl
+        FROM {{ ref('qaqc_int__active_condo_bbl_unitsres_corrections') }}
+    )
 ),
 
--- Filter DevDB for uncorrected offenders
-devdb_uncorrected_offenders AS (
-    SELECT
-        *,
-        row_number() OVER (PARTITION BY bbl, bin ORDER BY date_complete DESC) AS order_num,
-        CASE
-            WHEN classa_prop IS null AND job_status = '5. Completed Construction' THEN 0
-            ELSE classa_prop
-        END AS classa_prop_modified
-    FROM {{ ref("stg__dcp_developments") }}
-    WHERE
-        bbl IN
-        (SELECT DISTINCT primebbl FROM uncorrected_primebbl_offenders)
-        AND job_status IN ('5. Completed Construction', '4. Partially Completed Construction')
-),
-
-devdb_uncorrected_offender_grouped_by_latest_bin AS (
-    SELECT
-        bbl,
-        bin,
-        units_co,
-        classa_prop_modified AS classa_prop,
-        job_type,
-        job_status,
-        date_complete
-    FROM devdb_uncorrected_offenders
-    WHERE order_num = 1
-),
-
-devdb_uncorrected_offender_grouped_by_bbl AS (
-    SELECT
-        bbl,
-        sum(units_co) AS units_co,
-        sum(classa_prop) AS classa_prop,
-        count(*) AS count_bins
-    FROM devdb_uncorrected_offender_grouped_by_latest_bin
-    WHERE job_type <> 'Demolition' -- Exclude demolished buildings as property taxes may still be paid, aligning with the DOF data
-    GROUP BY bbl
+not_ignored_primebbls AS (
+    SELECT *
+    FROM uncorrected_primebbl_offenders
+    WHERE primebbl NOT IN (
+        SELECT bbl::decimal::bigint::text
+        FROM {{ ref('ignored_bbls_for_unit_count_test') }}
+    )
 ),
 
 -- Join DevDB with uncorrected offenders
@@ -104,13 +66,13 @@ offenders_joined_with_devdb AS (
         r.count_bins,
         l.coop_apts = r.classa_prop AS dof_matches_devdb_units,
         r.classa_prop - l.coop_apts AS diff
-    FROM uncorrected_primebbl_offenders AS l
-    LEFT JOIN devdb_uncorrected_offender_grouped_by_bbl AS r
+    FROM not_ignored_primebbls AS l
+    LEFT JOIN {{ ref('qaqc_int__devdb_bbl_units_summary') }} AS r
         ON l.primebbl = r.bbl
-    ORDER BY dof_matches_devdb_units
 )
 
 -- Final selection of records where the unit difference exceeds threshold
 SELECT *
 FROM offenders_joined_with_devdb
 WHERE diff >= 50 OR diff <= -50	-- 50 is an arbitrary threshold
+ORDER BY dof_matches_devdb_units
