2e5baea200eb5434c18dd7b5d37994006c7f9358
Damon McCullough <damonmcc1391@gmail.com>
2024-10-07 11:33:35 -0400

    use db path in duckdb utils

--- dcpy/utils/duckdb.py
+++ dcpy/utils/duckdb.py
@@ -1,16 +1,18 @@
 import duckdb
+from pathlib import Path
 import os
 
 
-def setup_s3_secret():
-    duckdb.sql("INSTALL httpfs; LOAD httpfs;")
-    duckdb.sql(
-        f"""
-            CREATE SECRET IF NOT EXISTS s3_secret (
-                TYPE S3,
-                KEY_ID '{os.environ["AWS_ACCESS_KEY_ID"]}',
-                SECRET '{os.environ["AWS_SECRET_ACCESS_KEY"]}',
-                ENDPOINT '{os.environ["AWS_S3_ENDPOINT"].split('://')[1]}'
-            );
-        """
-    )
+def setup_s3_secret(db_path: Path):
+    with duckdb.connect(str(db_path)) as connection:
+        connection.sql("INSTALL httpfs; LOAD httpfs;")
+        connection.sql(
+            f"""
+                CREATE SECRET IF NOT EXISTS s3_secret (
+                    TYPE S3,
+                    KEY_ID '{os.environ["AWS_ACCESS_KEY_ID"]}',
+                    SECRET '{os.environ["AWS_SECRET_ACCESS_KEY"]}',
+                    ENDPOINT '{os.environ["AWS_S3_ENDPOINT"].split('://')[1]}'
+                );
+            """
+        )
