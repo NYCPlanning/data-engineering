6fd2e2f7cc219fae8840c48cac70865e19d82d19
sf-dcp <ofilippova@planning.nyc.gov>
2024-10-10 23:27:05 -0400

    Add test for PLUTO minor (same row count between current and previous PLUTO versions)

---
+++ products/pluto/tests/assert_bbl_count_is_the_same.sql
@@ -0,0 +1,34 @@
+{{ 
+    config(
+        tags = ['de_check', 'minor'], 
+        meta = {
+            'description': '''
+				This test checks that final PLUTO table has same row count as previous version PLUTO. 
+                This test is expected to pass for PLUTO Minor versions only.
+			''',
+            'next_steps': '''
+				Investigate if a correct recipe file was used. If so, review stats for input datasets. 
+                This needs to be addressed prior to promoting PLUTO for GIS review.
+			'''
+        }
+    ) 
+}}
+
+WITH current_pluto AS (
+    SELECT bbl::bigint, 'current' AS source
+    FROM {{ source('build_sources', 'export_pluto') }}
+),
+previous_pluto AS (
+    SELECT bbl::decimal::bigint, 'previous' AS source
+    FROM {{ source('build_sources', 'previous_pluto') }}
+)
+
+-- query to detect differences and isolate records absent in one of the tables
+SELECT COALESCE(current_pluto.bbl, previous_pluto.bbl) AS bbl,
+       COALESCE(current_pluto.source, previous_pluto.source) AS source
+FROM current_pluto
+FULL OUTER JOIN previous_pluto
+    ON current_pluto.bbl = previous_pluto.bbl
+WHERE current_pluto.bbl IS NULL
+   OR previous_pluto.bbl IS NULL
+ORDER BY source
