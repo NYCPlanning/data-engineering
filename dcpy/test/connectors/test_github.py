071a82b55bdd298fd47542b10099327d466e3599
Damon McCullough <damonmcc1391@gmail.com>
2024-12-09 17:18:52 -0500

    add `download_repo` to `connectors.github`

--- dcpy/test/connectors/test_github.py
+++ dcpy/test/connectors/test_github.py
@@ -1,4 +1,4 @@
-from dcpy.connectors.github import get_default_branch, get_branches
+from dcpy.connectors.github import download_repo, get_default_branch, get_branches
 
 TEST_REPO = "data-engineering"
 
@@ -6,6 +6,15 @@ TEST_REPO = "data-engineering"
 def test_get_default_branch():
     branch = get_default_branch(repo=TEST_REPO)
     assert branch == "main"
+
+
+def test_download_repo(create_temp_filesystem):
+    repo_path = download_repo(
+        repo=TEST_REPO,
+        output_directory=create_temp_filesystem,
+        branch="main",
+    )
+    assert repo_path.exists()
 
 
 def test_get_branches():
