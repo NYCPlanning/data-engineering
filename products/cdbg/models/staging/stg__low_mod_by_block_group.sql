74659d70a159a610bed20aaf7e88c1866ff45b8a
Damon McCullough <damonmcc1391@gmail.com>
2024-12-31 14:01:39 -0500

    add columns to staging table

--- products/cdbg/models/staging/stg__low_mod_by_block_group.sql
+++ products/cdbg/models/staging/stg__low_mod_by_block_group.sql
@@ -8,5 +8,7 @@ SELECT
     "BORO" as boro,
     "TRACT" as tract,
     "BLKGRP" as block_group,
+    REPLACE("LOWMODUNIV", ',', '')::numeric as total_population,
+    REPLACE("LOWMOD", ',', '')::numeric as lowmod_population,
     RTRIM("LOWMOD_PCT", '%')::numeric AS lowmod_pct
 FROM {{ source("recipe_sources", "hud_lowmodincomebyblockgroup") }}
