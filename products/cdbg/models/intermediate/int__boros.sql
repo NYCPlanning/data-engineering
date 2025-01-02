48b9a5e1886d8327ea143e3d170c1be8800a30af
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-01-02 12:36:26 -0500

    add boros to cdbg

---
+++ products/cdbg/models/intermediate/int__boros.sql
@@ -0,0 +1,24 @@
+WITH tracts AS (
+    SELECT * FROM {{ ref("int__tracts") }}
+),
+
+boros AS (
+    SELECT
+        borough_name,
+        sum(total_floor_area) AS total_floor_area,
+        sum(residential_floor_area) AS residential_floor_area,
+        sum(total_population) AS total_population,
+        sum(low_mod_income_population) AS low_mod_income_population
+    FROM tracts
+    GROUP BY borough_name
+),
+
+boro_calculation AS (
+    SELECT
+        *,
+        (residential_floor_area / total_floor_area) * 100 AS residential_floor_area_percentage,
+        (low_mod_income_population / total_population) * 100 AS low_mod_income_population_percentage
+    FROM boros
+)
+
+SELECT * FROM boro_calculation
