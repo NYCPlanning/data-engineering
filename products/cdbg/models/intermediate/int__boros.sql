7e0d43562ec2a4a98920f336f46dac303f72d343
Damon McCullough <damonmcc1391@gmail.com>
2025-01-06 15:53:54 -0500

    fix eligibility logic for boroughs

--- products/cdbg/models/intermediate/int__boros.sql
+++ products/cdbg/models/intermediate/int__boros.sql
@@ -9,6 +9,7 @@ boros AS (
         sum(total_floor_area) AS total_floor_area,
         sum(residential_floor_area) AS residential_floor_area,
         sum(total_population) AS total_population,
+        sum(potential_lowmod_population) AS potential_lowmod_population,
         sum(low_mod_income_population) AS low_mod_income_population
     FROM tracts
     GROUP BY
@@ -20,7 +21,7 @@ boro_calculation AS (
     SELECT
         *,
         (residential_floor_area / total_floor_area) * 100 AS residential_floor_area_percentage,
-        (low_mod_income_population / total_population) * 100 AS low_mod_income_population_percentage
+        (low_mod_income_population / potential_lowmod_population) * 100 AS low_mod_income_population_percentage
     FROM boros
 )
 
