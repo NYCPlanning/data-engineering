9ecfbbc3fb7fba0e4527682b76cada2d2417c92c
Damon McCullough <damonmcc1391@gmail.com>
2025-01-07 16:47:34 -0500

    determine tract eligibility earlier
    to avoid using a product model to get eligible tracts in new intermediate models

--- products/cdbg/models/product/cdbg_tracts.sql
+++ products/cdbg/models/product/cdbg_tracts.sql
@@ -1,30 +1,13 @@
-WITH tracts AS (
-    SELECT * FROM {{ ref("int__tracts") }}
-),
-
-eligibility_calculation AS (
-    SELECT
-        geoid,
-        borough_name,
-        borough_code,
-        round(total_floor_area::numeric)::integer AS total_floor_area,
-        round(residential_floor_area::numeric)::integer AS residential_floor_area,
-        round(residential_floor_area_percentage::numeric, 2) AS residential_floor_area_percentage,
-        low_mod_income_population::integer,
-        potential_lowmod_population::integer,
-        round(low_mod_income_population_percentage::numeric, 2) AS low_mod_income_population_percentage,
-        low_mod_income_population_percentage >= 51 AND residential_floor_area_percentage >= 50 AS eligibility_flag
-    FROM tracts
-),
-
-eligibility AS (
-    SELECT
-        *,
-        CASE
-            WHEN eligibility_flag THEN 'CD Eligible'
-            ELSE 'Ineligible'
-        END AS eligibility
-    FROM eligibility_calculation
-)
-
-SELECT * FROM eligibility
+SELECT
+    geoid,
+    borough_name,
+    borough_code,
+    round(total_floor_area::numeric)::integer AS total_floor_area,
+    round(residential_floor_area::numeric)::integer AS residential_floor_area,
+    round(residential_floor_area_percentage::numeric, 2) AS residential_floor_area_percentage,
+    low_mod_income_population::integer,
+    potential_lowmod_population::integer,
+    round(low_mod_income_population_percentage::numeric, 2) AS low_mod_income_population_percentage,
+    eligibility_flag,
+    eligibility
+FROM {{ ref("int__tracts") }}
