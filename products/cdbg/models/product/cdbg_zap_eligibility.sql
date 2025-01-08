deefc7817308e6f12ef5b5845217e30b769d6065
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-01-08 11:13:38 -0500

    enforce product contract

--- products/cdbg/models/product/cdbg_zap_eligibility.sql
+++ products/cdbg/models/product/cdbg_zap_eligibility.sql
@@ -13,10 +13,10 @@ SELECT
     zap.project_id,
     zap.bbls AS all_zap_bbls,
     ARRAY_TO_STRING(e.bbls, ', ') AS distinct_bbls,
-    COALESCE(e.entirely_within_quarter_mile_of_eligible_tract, FALSE) AS entirely_within_quarter_mile_of_eligible_tract,
-    COALESCE(e.entirely_within_half_mile_of_eligible_tract, FALSE) AS entirely_within_half_mile_of_eligible_tract,
-    COALESCE(e.partially_within_quarter_mile_of_eligible_tract, FALSE) AS partially_within_quarter_mile_of_eligible_tract,
-    COALESCE(e.partially_within_half_mile_of_eligible_tract, FALSE) AS partially_within_half_mile_of_eligible_tract
+    {{ bool_to_str("e.entirely_within_quarter_mile_of_eligible_tract") }} AS entirely_within_quarter_mile_of_eligible_tract,
+    {{ bool_to_str("e.entirely_within_half_mile_of_eligible_tract") }} AS entirely_within_half_mile_of_eligible_tract,
+    {{ bool_to_str("e.partially_within_quarter_mile_of_eligible_tract") }} AS partially_within_quarter_mile_of_eligible_tract,
+    {{ bool_to_str("e.partially_within_half_mile_of_eligible_tract") }} AS partially_within_half_mile_of_eligible_tract
 FROM zap_projects AS zap
 LEFT JOIN eligibility AS e
     ON zap.project_id = e.project_id
