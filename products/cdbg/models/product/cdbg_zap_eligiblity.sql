601693b07821cade040bd9c6dd49c19fded7200f
Damon McCullough <damonmcc1391@gmail.com>
2025-01-07 23:18:46 -0500

    fix logic for zap projects entirely within tract buffers

--- products/cdbg/models/product/cdbg_zap_eligiblity.sql
+++ products/cdbg/models/product/cdbg_zap_eligiblity.sql
@@ -1,12 +1,16 @@
 WITH lots_eligibility AS (
-    SELECT * FROM {{ ref('int__zap_lots_eligiblity') }}
+    SELECT * FROM {{ ref("int__zap_lots_eligiblity") }}
+),
+
+projects_eligibility AS (
+    SELECT * FROM {{ ref("int__zap_project_eligiblity") }}
 ),
 
 zap_projects AS (
     SELECT * FROM {{ ref("stg__zap_projects") }}
 ),
 
-project_arrays AS (
+project_lot_arrays AS (
     SELECT
         project_id,
         ARRAY_TO_STRING(ARRAY_AGG(
@@ -19,52 +23,63 @@ project_arrays AS (
     GROUP BY project_id
 ),
 
-project_eligiblity_flags AS (
+project_lot_eligiblity_flags AS (
     SELECT
         project_id,
         bbls,
-        true = ALL(within_quarter_mile_array) AS all_lots_quarter_mile,
-        true = ALL(within_half_mile_array) AS all_lots_half_mile,
         true = ANY(within_quarter_mile_array) AS a_lot_quarter_mile,
         true = ANY(within_half_mile_array) AS a_lot_half_mile
-    FROM project_arrays
+    FROM project_lot_arrays
 ),
 
-project_eligiblity AS (
+eligiblity_flags AS (
+    SELECT
+        project_lot_eligiblity_flags.project_id,
+        project_lot_eligiblity_flags.bbls,
+        projects_eligibility.contained_quarter_mile AS all_lots_quarter_mile,
+        projects_eligibility.contained_half_mile AS all_lots_half_mile,
+        project_lot_eligiblity_flags.a_lot_quarter_mile,
+        project_lot_eligiblity_flags.a_lot_half_mile
+    FROM project_lot_eligiblity_flags
+    LEFT JOIN projects_eligibility
+        ON project_lot_eligiblity_flags.project_id = projects_eligibility.project_id
+),
+
+eligiblity AS (
     SELECT
         project_id,
         bbls,
         CASE
             WHEN all_lots_quarter_mile THEN 'Yes'
             ELSE 'No'
-        END AS all_lots_within_quarter_mile_of_eligible_tract,
+        END AS entirely_within_quarter_mile_of_eligible_tract,
         CASE
             WHEN all_lots_half_mile THEN 'Yes'
             ELSE 'No'
-        END AS all_lots_within_half_mile_of_eligible_tract,
+        END AS entirely_within_half_mile_of_eligible_tract,
         CASE
             WHEN a_lot_quarter_mile THEN 'Yes'
             ELSE 'No'
-        END AS a_lot_within_quarter_mile_of_eligible_tract,
+        END AS partialy_within_quarter_mile_of_eligible_tract,
         CASE
             WHEN a_lot_half_mile THEN 'Yes'
             ELSE 'No'
-        END AS a_lot_within_half_mile_of_eligible_tract
-    FROM project_eligiblity_flags
+        END AS partialy_within_half_mile_of_eligible_tract
+    FROM eligiblity_flags
 ),
 
 original_order AS (
     SELECT
         zap_projects.project_id,
-        project_eligiblity.bbls AS all_zap_bbls,
-        project_eligiblity.bbls AS distinct_bbls,
-        project_eligiblity.all_lots_within_quarter_mile_of_eligible_tract,
-        project_eligiblity.all_lots_within_half_mile_of_eligible_tract,
-        project_eligiblity.a_lot_within_quarter_mile_of_eligible_tract,
-        project_eligiblity.a_lot_within_half_mile_of_eligible_tract
+        zap_projects.bbls AS all_zap_bbls,
+        eligiblity.bbls AS distinct_bbls,
+        eligiblity.entirely_within_quarter_mile_of_eligible_tract,
+        eligiblity.entirely_within_half_mile_of_eligible_tract,
+        eligiblity.partialy_within_quarter_mile_of_eligible_tract,
+        eligiblity.partialy_within_half_mile_of_eligible_tract
     FROM zap_projects
-    LEFT JOIN project_eligiblity
-        ON zap_projects.project_id = project_eligiblity.project_id
+    LEFT JOIN eligiblity
+        ON zap_projects.project_id = eligiblity.project_id
 )
 
 SELECT * FROM original_order
