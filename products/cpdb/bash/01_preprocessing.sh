680fe1b92c068f0cca4c976ca455becc7d44fbbb
ofilippova@planning.nyc.gov <ofilippova@planning.nyc.gov>
2025-01-23 18:09:07 -0500

    Fix processing of `doitt_buildingfootprints`: - Keep raw dataset version - Do processing in database instead of pandas

--- products/cpdb/bash/01_preprocessing.sh
+++ products/cpdb/bash/01_preprocessing.sh
@@ -7,7 +7,11 @@ echo "fixing dot_bridges"
 run_sql_command "ALTER TABLE dot_projects_bridges RENAME COLUMN fmsid TO fms_id;"
 python3 python/dot_bridges.py
 
-echo "fixing doitt_buildingfootprints"
-run_sql_command "ALTER TABLE doitt_buildingfootprints RENAME TO doitt_buildingfootprints_source;"
-python3 python/doitt_buildingfootprints.py
-run_sql_command "DROP TABLE IF EXISTS doitt_buildingfootprints_source;"
+echo "fixing doitt_buildingfootprints_source and saving result into doitt_buildingfootprints"
+run_sql_command "
+    DROP TABLE IF EXISTS doitt_buildingfootprints;
+    CREATE TABLE doitt_buildingfootprints AS 
+    SELECT * 
+    FROM doitt_buildingfootprints_source 
+    WHERE bin is NOT NULL
+"
