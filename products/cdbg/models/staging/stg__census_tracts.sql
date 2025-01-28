34295f679689d5cad945d2de7372a93ce14598e5
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-01-28 15:11:46 -0500

    add nta to fgdb output

--- products/cdbg/models/staging/stg__census_tracts.sql
+++ products/cdbg/models/staging/stg__census_tracts.sql
@@ -2,6 +2,8 @@ SELECT
     geoid,
     borocode::integer AS borough_code,
     boroname AS borough_name,
+    ntaname,
+    nta2020,
     boroct2020 AS bct2020_source,
     borocode::integer || ct2020 as bct2020,
     ct2020,
