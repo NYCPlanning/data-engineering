5078b8073a3f2781f10c37dfd751ce13d79186bf
sf-dcp <ofilippova@planning.nyc.gov>
2025-01-31 16:35:31 -0500

    POST REVIEW: remove pre-processing of `doitt_buildingfootprints`

--- products/cpdb/bash/01_preprocessing.sh
+++ products/cpdb/bash/01_preprocessing.sh
@@ -6,12 +6,3 @@ run_sql_file "sql/_create.sql"
 echo "fixing dot_bridges"
 run_sql_command "ALTER TABLE dot_projects_bridges RENAME COLUMN fmsid TO fms_id;"
 python3 python/dot_bridges.py
-
-echo "fixing doitt_buildingfootprints_source and saving result into doitt_buildingfootprints"
-run_sql_command "
-    DROP TABLE IF EXISTS doitt_buildingfootprints;
-    CREATE TABLE doitt_buildingfootprints AS 
-    SELECT * 
-    FROM doitt_buildingfootprints_source 
-    WHERE bin is NOT NULL
-"
