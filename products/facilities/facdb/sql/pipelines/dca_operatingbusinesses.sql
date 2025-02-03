a8bd4ddf34f89c1965bff691e7741606da54fdd7
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-02-03 14:56:33 -0500

    rename dca_operatingbusinesses geom

--- products/facilities/facdb/sql/pipelines/dca_operatingbusinesses.sql
+++ products/facilities/facdb/sql/pipelines/dca_operatingbusinesses.sql
@@ -34,7 +34,7 @@ SELECT
     'NYCDCA' AS overabbrev,
     NULL AS capacity,
     NULL AS captype,
-    wkt::geometry AS wkb_geometry,
+    wkb_geometry::geometry,
     geo_1b,
     geo_bl,
     geo_bn
