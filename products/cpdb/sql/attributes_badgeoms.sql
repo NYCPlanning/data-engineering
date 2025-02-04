69d68820fe040a5e14c6e0c858a0f6c7f3898cc5
Damon McCullough <damonmcc1391@gmail.com>
2025-02-04 15:53:09 -0500

    fix points with inverted longitude values
    all points in NYC are around latitude -73.9 so assume any points with a positive value should be inverted

--- products/cpdb/sql/attributes_badgeoms.sql
+++ products/cpdb/sql/attributes_badgeoms.sql
@@ -30,3 +30,54 @@ WHERE
         )
     )
     AND geom IS NOT NULL;
+
+-- Fix projects outside of NYC
+-- For now, only fixes point projects with inverted longtiude values
+-- All points in NYC are around latitude -73.9
+DROP TABLE IF EXISTS cpdb_dcpattributes_not_in_nyc_fixed;
+
+WITH point_projects_not_entirely_in_nyc AS (
+    SELECT
+        maprojid,
+        geom
+    FROM cpdb_dcpattributes
+    WHERE
+        ST_GEOMETRYTYPE(geom) = 'ST_MultiPoint'
+        AND ST_XMAX(geom) > 0
+),
+
+project_points AS (
+    SELECT
+        maprojid,
+        (ST_DUMP(geom)).geom AS single_point
+    FROM point_projects_not_entirely_in_nyc
+),
+
+fixed_points AS (
+    SELECT
+        maprojid,
+        CASE
+            WHEN ST_X(single_point) > 0
+                THEN ST_SETSRID(ST_MAKEPOINT(-ST_X(single_point), ST_Y(single_point)), 4326)
+            ELSE single_point
+        END AS single_point_fixed
+    FROM project_points
+),
+
+projects AS (
+    SELECT
+        maprojid,
+        ST_MULTI(ST_UNION(single_point_fixed)) AS geom
+    FROM fixed_points
+    GROUP BY maprojid
+)
+
+SELECT *
+INTO cpdb_dcpattributes_not_in_nyc_fixed
+FROM projects;
+
+UPDATE cpdb_dcpattributes
+SET geom = cpdb_dcpattributes_not_in_nyc_fixed.geom
+FROM cpdb_dcpattributes_not_in_nyc_fixed
+WHERE
+    cpdb_dcpattributes.maprojid = cpdb_dcpattributes_not_in_nyc_fixed.maprojid;
