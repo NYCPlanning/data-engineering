fa0db19778eed6679f7f3f8fb6881f3b4dacc0cc
Damon McCullough <damonmcc1391@gmail.com>
2025-02-06 18:18:44 -0500

    remove special characters from column names

--- products/cdbg/models/product/bytes/cdbg_tracts_bytes_csv.sql
+++ products/cdbg/models/product/bytes/cdbg_tracts_bytes_csv.sql
@@ -7,7 +7,7 @@ SELECT
     residential_floor_area_percentage AS "Res_pct",
     potential_lowmod_population AS "TotalPop",
     low_mod_income_population AS "LowMod_Population",
-    low_mod_income_population_percentage AS "% of Low-/ Moderate-Income Persons",
+    low_mod_income_population_percentage AS "Percent Low and Moderate Income Persons",
     eligibility AS "Eligibility",
     bct2020 AS "CT_text"
 FROM {{ ref("cdbg_tracts") }}
--- products/cdbg/models/product/excel/cdbg_tracts_excel.sql
+++ products/cdbg/models/product/excel/cdbg_tracts_excel.sql
@@ -3,12 +3,12 @@ SELECT
     borough_code AS "Borough Code",
     bct2020 AS "BoroCT",
     ct2020 AS "Census Tract Long",
-    ctlabel AS "Census Tract (with decimal)",
+    ctlabel AS "Census Tract with decimal",
     total_floor_area AS "Total Floor Area",
     residential_floor_area AS "Total Residential Floor Area",
-    residential_floor_area_percentage AS "% of Floor Area Residential",
+    residential_floor_area_percentage AS "Percent Floor Area Residential",
     potential_lowmod_population AS "Total Population",
-    low_mod_income_population AS "Number of Low-/ Moderate-Income Persons",
-    low_mod_income_population_percentage AS "% of Low-/ Moderate-Income Persons",
+    low_mod_income_population AS "Number of Low and Moderate Income Persons",
+    low_mod_income_population_percentage AS "Percent Low and Moderate Income Persons",
     eligibility AS "Eligibility"
 FROM {{ ref("cdbg_tracts") }}
