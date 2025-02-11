c9878aaa3e567bb145bd253620c29b324e7964c7
sf-dcp <144725249+sf-dcp@users.noreply.github.com>
2025-02-11 12:00:55 -0500

    Fix CPDB nightly build (#1405)
    * Fix join for `dcp_cpdb_agencyverified` table:
    The bin column in `dcp_cpdb_agencyverified` has invalid numeric values like "#REF!", "2610 / 336", and "3004301 (Additional BIN: 3829437 )". During the join, the table is filtered for decimal and integer values.
    Added casting to decimal because decimal string like "123.0" cannot be casted to integer directly
    
    * Remove pre-processing of `doitt_buildingfootprints`
    
    * Clean bin null records for `doitt_buildingfootprints`
    
    * POST REVIEW: do not update geom column if doitt_buildingfootprints.geom is null

--- products/cpdb/bash/01_preprocessing.sh
+++ products/cpdb/bash/01_preprocessing.sh
@@ -6,8 +6,3 @@ run_sql_file "sql/_create.sql"
 echo "fixing dot_bridges"
 run_sql_command "ALTER TABLE dot_projects_bridges RENAME COLUMN fmsid TO fms_id;"
 python3 python/dot_bridges.py
-
-echo "fixing doitt_buildingfootprints"
-run_sql_command "ALTER TABLE doitt_buildingfootprints RENAME TO doitt_buildingfootprints_source;"
-python3 python/doitt_buildingfootprints.py
-run_sql_command "DROP TABLE IF EXISTS doitt_buildingfootprints_source;"
