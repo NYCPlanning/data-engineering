0c41dd609158504301eab363c8ab1cf5855e0d04
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-02-13 10:27:32 -0800

    sqlfluff pluto dbt models/tests

--- products/pluto/models/staging/stg__dcp_developments.sql
+++ products/pluto/models/staging/stg__dcp_developments.sql
@@ -1,5 +1,12 @@
 WITH final AS (
-    SELECT bbl::decimal::bigint::text, bin, units_co::numeric, classa_prop::numeric, job_type, job_status, date_complete::date
+    SELECT
+        bbl::decimal::bigint::text,
+        bin,
+        units_co::numeric,
+        classa_prop::numeric,
+        job_type,
+        job_status,
+        date_complete::date
     FROM {{ source("recipe_sources", "dcp_developments") }}
 )
-SELECT * from final
+SELECT * FROM final
