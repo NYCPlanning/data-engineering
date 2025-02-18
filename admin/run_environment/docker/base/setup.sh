88482534736597d9d43d84b139d0e3be3e43dcfe
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-02-18 11:06:19 -0800

    update gdal
    - update gdal to 3.11.3
    - parameterize script slightly
    - don't build my forked gdal
    - update python package version

--- admin/run_environment/docker/base/setup.sh
+++ admin/run_environment/docker/base/setup.sh
@@ -3,6 +3,8 @@
 source config.sh
 set -e
 
+gdal_version=3.11.3
+gdal_short_version="$(echo "$gdal_version" | tr -d ".")"
 COMMON_APT_PACKAGES="curl zip unzip git wget ca-certificates lsb-release build-essential sudo postgresql-client-15 libpq-dev jq locales pandoc weasyprint"
 
 apt-get update && export DEBIAN_FRONTEND=noninteractive \
@@ -29,9 +31,9 @@ function install_gdal {
 
     echo "installing gdal from source" && (
         cd ~
-        wget https://github.com/fvankrieken/gdal/archive/refs/heads/3.8-revert-null-date-behavior.zip
-        unzip "3.8-revert-null-date-behavior.zip"
-        cd gdal-3.8-revert-null-date-behavior
+        wget download.osgeo.org/gdal/${gdal_version}/gdal${gdal_short_version}.zip
+        unzip gdal${gdal_short_version}.zip
+        cd gdal-${gdal_version}
         mkdir build
         cd build
         sudo cmake -DPROJ_INCLUDE_DIR=/usr/include ..
@@ -39,8 +41,8 @@ function install_gdal {
         sudo cmake --build . --target install
 
         cd ~
-        rm -rf gdal-3.8-revert-null-date-behavior
-        rm "3.8-revert-null-date-behavior.zip"
+        rm -rf gdal-${gdal_version}
+        rm gdal${gdal_short_version}.zip
     )
     echo "gdal installed successfully"
 }
