ffa5c3b6d8c7fbbfd1322f93545dd5a8ed7afd40
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-03-04 15:48:45 -0500

    only perform spatial joins once, remove unnecessary nested clauses

--- products/cpdb/sql/attributes_maprojid_firecompanies.sql
+++ products/cpdb/sql/attributes_maprojid_firecompanies.sql
@@ -1,48 +1,11 @@
 -- create maprojid --> fire companies relational table
 DROP TABLE IF EXISTS attributes_maprojid_firecompanies;
 -- spatial join
-CREATE TABLE attributes_maprojid_firecompanies AS (
-    SELECT a.*
-    FROM (
-        SELECT
-            a.maprojid AS feature_id,
-            'fireconum'::text AS admin_boundary_type,
-            b.fireconum::text AS admin_boundary_id
-        FROM cpdb_dcpattributes AS a,
-            dcp_firecompanies AS b
-        WHERE ST_INTERSECTS(a.geom, b.wkb_geometry)
-    ) AS a
-    UNION ALL
-    SELECT b.*
-    FROM (
-        SELECT
-            a.maprojid AS feature_id,
-            'firecotype'::text AS admin_boundary_type,
-            b.firecotype::text AS admin_boundary_id
-        FROM cpdb_dcpattributes AS a,
-            dcp_firecompanies AS b
-        WHERE ST_INTERSECTS(a.geom, b.wkb_geometry)
-    ) AS b
-    UNION ALL
-    SELECT c.*
-    FROM (
-        SELECT
-            a.maprojid AS feature_id,
-            'firebn'::text AS admin_boundary_type,
-            b.firebn::text AS admin_boundary_id
-        FROM cpdb_dcpattributes AS a,
-            dcp_firecompanies AS b
-        WHERE ST_INTERSECTS(a.geom, b.wkb_geometry)
-    ) AS c
-    UNION ALL
-    SELECT d.*
-    FROM (
-        SELECT
-            a.maprojid AS feature_id,
-            'firediv'::text AS admin_boundary_type,
-            b.firediv::text AS admin_boundary_id
-        FROM cpdb_dcpattributes AS a,
-            dcp_firecompanies AS b
-        WHERE ST_INTERSECTS(a.geom, b.wkb_geometry)
-    ) AS d
-);
+CREATE TABLE attributes_maprojid_firecompanies AS
+SELECT
+    maprojid AS feature_id,
+    unnest(ARRAY['fireconum'::text, 'firecotype'::text, 'firebn'::text, 'firediv'::text]) AS admin_boundary_type,
+    unnest(ARRAY[fireconum::text, firecotype::text, firebn::text, firediv::text]) AS admin_boundary_id
+FROM cpdb_dcpattributes AS a,
+    dcp_firecompanies AS b
+WHERE st_intersects(a.geom, b.wkb_geometry);
--- products/cpdb/sql/attributes_maprojid_parkid.sql
+++ products/cpdb/sql/attributes_maprojid_parkid.sql
@@ -9,28 +9,24 @@ SET wkb_geometry = ST_MAKEVALID(wkb_geometry);
 UPDATE cpdb_dcpattributes
 SET geom = ST_MAKEVALID(geom);
 
-CREATE TABLE attributes_maprojid_parkid AS (
-    SELECT *
-    FROM (
-        SELECT
-            REPLACE(fmsid, ' ', '') AS maprojid,
-            park_id AS parkid
-        FROM dpr_capitalprojects_fms_parkid
-        UNION
-        SELECT
-            maprojid,
-            REGEXP_REPLACE(
-                REGEXP_MATCHES(description, '[BMQRX][0-9][0-9][0-9]')::text, '[^0-9a-zA-Z]+', '', 'g'
-            )::text AS parkid
-        FROM cpdb_dcpattributes
-        WHERE magencyacro = 'DPR'
-        UNION
-        SELECT
-            a.maprojid,
-            b.gispropnum AS parkid
-        FROM cpdb_dcpattributes AS a,
-            dpr_parksproperties AS b
-        WHERE ST_WITHIN(a.geom, b.wkb_geometry) AND ST_ISVALID(a.geom)
-    ) AS a
-    ORDER BY maprojid
-);
+CREATE TABLE attributes_maprojid_parkid AS
+SELECT
+    REPLACE(fmsid, ' ', '') AS maprojid,
+    park_id AS parkid
+FROM dpr_capitalprojects_fms_parkid
+UNION
+SELECT
+    maprojid,
+    REGEXP_REPLACE(
+        REGEXP_MATCHES(description, '[BMQRX][0-9][0-9][0-9]')::text, '[^0-9a-zA-Z]+', '', 'g'
+    )::text AS parkid
+FROM cpdb_dcpattributes
+WHERE magencyacro = 'DPR'
+UNION
+SELECT
+    a.maprojid,
+    b.gispropnum AS parkid
+FROM cpdb_dcpattributes AS a,
+    dpr_parksproperties AS b
+WHERE ST_WITHIN(a.geom, b.wkb_geometry) AND ST_ISVALID(a.geom)
+ORDER BY maprojid;
