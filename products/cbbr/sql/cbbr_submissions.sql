2b4c1cb6b32d59705469afe977a26469e094d496
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-03-11 09:35:51 -0700

    update for new source data schema

--- products/cbbr/sql/cbbr_submissions.sql
+++ products/cbbr/sql/cbbr_submissions.sql
@@ -3,30 +3,30 @@ DROP TABLE IF EXISTS _cbbr_submissions;
 
 CREATE TABLE _cbbr_submissions AS
 SELECT
-    unique_id,
+    dcpuniqid AS unique_id,
     trkno AS tracking_code,
     borough,
     RIGHT(cb_label, 2) AS cd,
     cb_label,
     type AS type_br,
     priority,
-    need,
+    need, -- missing
     request,
     REPLACE(reason, E'\n', ' ') AS explanation,
     location,
     loc_type AS type,
-    supported_by_1 AS supporters_1,
-    supported_by_2 AS supporters_2,
-    project_id_1,
-    project_id_2,
-    project_id_3,
-    budget_line_1,
-    budget_line_2,
-    budget_line_3,
+    lowsgrp1 AS supporters_1,
+    lowsgrp2 AS supporters_2,
+    capis1 AS project_id_1, -- all null
+    capis2 AS project_id_2,
+    capis3 AS project_id_3,
+    budline1 AS budget_line_1, -- all null
+    budline2 AS budget_line_2,
+    budline3 AS budget_line_3,
     agency_acronym,
     agency,
-    agy_response_category AS agency_category_response,
-    agy_response AS agency_response,
-    REPLACE(additional_comment, E'\n', ' ') AS additional_comment,
+    agyrspcat AS agency_category_response, -- all null
+    agency_response,
+    REPLACE(explanation, E'\n', ' ') AS additional_comment,
     NULL AS parent_tracking_code
-FROM cbbr_submissions;
+FROM omb_cbbr_agency_responses
