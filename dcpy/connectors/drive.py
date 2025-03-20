9bdd530c1bd6255c2de6a500a8923b37f5fe1d8d
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-03-20 19:42:35 -0400

    add local file connector

---
+++ dcpy/connectors/drive.py
@@ -0,0 +1,39 @@
+import shutil
+from pathlib import Path
+from pydantic import BaseModel
+
+from dcpy.connectors.registry import StorageConnector
+
+
+class Connector(BaseModel, StorageConnector):
+    conn_type: str = "drive"
+    path: Path | None = None
+
+    def _path(self, key: str, conf: dict | None = None) -> Path:
+        if self.path:
+            return self.path / key
+        elif conf and "drive_path" in conf:
+            return conf["drive_path"] / key
+        else:
+            return Path(key)
+
+    def push(self, key: str, push_conf: dict | None = {}) -> dict:
+        assert push_conf and "filepath" in push_conf, (
+            "filepath must be provided in push_conf"
+        )
+        path = self._path(key, push_conf)
+
+        if path.exists() and not push_conf.get("overwrite"):
+            raise Exception(f"'{path}' already exists.")
+        shutil.copy(push_conf["filepath"], path / key)
+
+        return {"path": path}
+
+    def pull(
+        self,
+        key: str,
+        destination_path: Path,
+        pull_conf: dict | None = None,
+    ) -> dict:
+        shutil.copy(self._path(key, pull_conf), destination_path)
+        return {"path": destination_path}
