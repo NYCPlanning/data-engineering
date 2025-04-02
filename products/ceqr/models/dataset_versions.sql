d5b911e140a2b47443acd111360ce981b95aa975
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-04-02 17:20:20 -0400

    f

--- products/ceqr/models/dataset_versions.sql
+++ products/ceqr/models/dataset_versions.sql
@@ -8,6 +8,7 @@ recipe_versions AS (
         data_hub_datasets.dataset_name,
         data_hub_datasets.availability_type,
         data_hub_datasets.file_type,
+        data_hub_datasets.build_source,
         data_hub_datasets.geometry_type,
         COALESCE(source_data_versions.v, '{{ var('build_version') }}') AS version
     FROM data_hub_datasets
