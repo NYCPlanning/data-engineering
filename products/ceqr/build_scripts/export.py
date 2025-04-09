89fbaf1819d1f3d6e2544ec8c55c059e5e25485b
Damon McCullough <damonmcc1391@gmail.com>
2025-04-08 21:24:31 -0400

    use seed files to list all datasets and generate markdown

--- products/ceqr/build_scripts/export.py
+++ products/ceqr/build_scripts/export.py
@@ -8,7 +8,7 @@ import pandas as pd
 from dcpy.utils import postgres
 from dcpy.utils.logging import logger
 
-from .constants import BASH_UTILS_PATH, DATA_DIR, OUTPUT_DIR
+from .constants import BASH_UTILS_PATH, DATASETS_DIR, DATA_DIR, OUTPUT_DIR
 
 
 def _call_bash_function(function_name, *args) -> None:
@@ -43,20 +43,31 @@ def _export_dataset_from_db(
     shutil.move(file_name, file_path)
 
 
-def _export_dataset(row: pd.Series) -> None:
+def _export_dataset(row: pd.Series, pg_client: postgres.PostgresClient):
+    if row["availability_type"] == "webpage":
+        return
+
     if row["file_type"] == "excel":
         file_name = f"{row['dataset_id']}.xlsx"
         logger.info(
             f"For dataset {row['dataset_name']}, copying excel file {file_name}"
         )
-        shutil.copy(DATA_DIR / file_name, OUTPUT_DIR / file_name)
-    else:
-        _export_dataset_from_db(
-            dataset_name=row["dataset_name"],
-            dataset_id=row["dataset_id"],
-            file_type=row["file_type"],
-            geometry_type=row["geometry_type"],
+        shutil.copy(DATASETS_DIR / file_name, OUTPUT_DIR / file_name)
+        return
+
+    table_names = pg_client.get_schema_tables()
+    if row["dataset_id"] not in table_names:
+        logger.warning(
+            f"Dataset '{row['dataset_name']}' ({row['dataset_id']}) is a TODO"
         )
+        return
+
+    _export_dataset_from_db(
+        dataset_name=row["dataset_name"],
+        dataset_id=row["dataset_id"],
+        file_type=row["file_type"],
+        geometry_type=row["geometry_type"],
+    )
 
 
 def export():
@@ -64,22 +75,23 @@ def export():
         shutil.rmtree(OUTPUT_DIR)
     OUTPUT_DIR.mkdir(parents=True)
 
+    pg_client = postgres.PostgresClient()
+
     # copy build metadata
     shutil.copy("build_metadata.json", OUTPUT_DIR / "build_metadata.json")
 
-    # read dataset versions table to a dataframe
-    pg_client = postgres.PostgresClient()
-    dataset_versions = pg_client.read_table_df("dataset_versions")
-
     # export dataset versions table
+    dataset_versions = pg_client.read_table_df("dataset_versions")
     dataset_versions_export_path = (OUTPUT_DIR / "dataset_versions").with_suffix(".csv")
     logger.info(f"Exporting dataset_versions to {dataset_versions_export_path}")
     dataset_versions.to_csv(dataset_versions_export_path, index=False)
 
-    # for each dataset, export the table as the specified file format
-    for _, row in dataset_versions.iterrows():
-        if row["availability_type"] == "download":
-            _export_dataset(row)
+    # export dataset files
+    dataset_versions.apply(lambda row: _export_dataset(row, pg_client), axis=1)
+
+    # export markdown files
+    shutil.copy(DATA_DIR / "datasets_table.md", OUTPUT_DIR / "datasets_table.md")
+    shutil.copy(DATA_DIR / "chapters_tables.md", OUTPUT_DIR / "chapters_tables.md")
 
 
 if __name__ == "__main__":
