a4d86fc0efd2b4a7c7344dc0e30a33ffb7ccf782
Damon McCullough <damonmcc1391@gmail.com>
2025-04-08 21:24:31 -0400

    use seed files to list all datasets and generate markdown

--- products/ceqr/models/dataset_versions.sql
+++ products/ceqr/models/dataset_versions.sql
@@ -1,17 +1,33 @@
 WITH source_data_versions AS (SELECT * FROM {{ source("recipe_sources", "source_data_versions") }}),
 
-data_hub_datasets AS (SELECT * FROM {{ ref("data_hub_datasets") }}),
+datasets AS (SELECT * FROM {{ ref("datasets") }}),
+
+-- preserve the order of rows in the seed file
+datasets_indexed AS (
+    SELECT
+        ROW_NUMBER() OVER (
+            ORDER BY (SELECT NULL)
+        ) AS row_number,
+        *
+    FROM datasets
+),
 
 recipe_versions AS (
     SELECT
-        data_hub_datasets.dataset_id,
-        data_hub_datasets.dataset_name,
-        data_hub_datasets.availability_type,
-        data_hub_datasets.file_type,
-        data_hub_datasets.geometry_type,
-        COALESCE(source_data_versions.v, '{{ var('build_version') }}') AS version
-    FROM data_hub_datasets
-    LEFT JOIN source_data_versions ON data_hub_datasets.dataset_id = source_data_versions.schema_name
+        datasets_indexed.dataset_name,
+        datasets_indexed.dataset_id,
+        (CASE
+            WHEN datasets_indexed.availability_type = 'webpage'
+                THEN NULL
+            ELSE COALESCE(source_data_versions.v, '{{ var('build_version') }}')
+        END) AS version,
+        datasets_indexed.availability_type,
+        datasets_indexed.file_type,
+        datasets_indexed.geometry_type,
+        datasets_indexed.source_url
+    FROM datasets_indexed
+    LEFT JOIN source_data_versions ON datasets_indexed.dataset_id = source_data_versions.schema_name
+    ORDER BY row_number ASC
 )
 
 SELECT * FROM recipe_versions
