0eaaa2ee0668c6d2c9d6f9aa01f3462efbf18e5d
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-04-21 17:12:36 -0400

    rename lion_dat to indicate separate fields

--- products/lion/models/product/dat/manhattan_lion_dat.sql
+++ products/lion/models/product/dat/manhattan_lion_dat.sql
@@ -1,10 +1,3 @@
-WITH manhattan AS (
-    SELECT *
-    FROM {{ ref('lion_dat') }}
-    WHERE
-        "Borough" = '1'
-)
-
--- create a single column in order to export a text file without a delimeter or header
--- NOTE the row length may vary by borough until field lengths are tested in lion_dat
-SELECT rtrim(ltrim(replace(manhattan::text, ',', ''), '('), ')') AS dat_column FROM manhattan
+SELECT *
+FROM {{ ref('lion_dat') }}
+WHERE "Borough" = '1'
