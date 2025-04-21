80fa61a94de8f2a54f00237a77862a902f7d6831
Damon McCullough <damonmcc1391@gmail.com>
2025-04-21 17:38:24 -0400

    allow large features in geojson

--- dcpy/__init__.py
+++ dcpy/__init__.py
@@ -1,3 +1,6 @@
 from pydantic import BaseModel
+import pyogrio
 
 BaseModel.model_config["coerce_numbers_to_str"] = True
+# disable the limit on the size of features in GeoJSON files
+pyogrio.set_gdal_config_options({"OGR_GEOJSON_MAX_OBJ_SIZE": 0})
