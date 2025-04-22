af8618e5315d011d45e44164325a2951cc3866cf
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-04-22 16:41:59 -0400

    filter centerline rows

--- products/lion/models/staging/stg__centerline.sql
+++ products/lion/models/staging/stg__centerline.sql
@@ -9,3 +9,6 @@ SELECT
     {{ dbt_utils.star(from=source("recipe_sources", "dcp_cscl_centerline"), except=['geom']) }},
     st_linemerge(geom) AS geom -- TODO - any reason to not do this here?
 FROM {{ source("recipe_sources", "dcp_cscl_centerline") }}
+WHERE 
+    NOT (rwjurisdiction = '3' AND status = '2')
+    AND rw <> 8
