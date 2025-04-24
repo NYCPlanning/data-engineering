c944a8ab1b6f0d47189421fc95f203b7d6ad21a8
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-04-24 13:06:37 -0700

    typo in centerline filter

--- products/lion/models/staging/stg__centerline.sql
+++ products/lion/models/staging/stg__centerline.sql
@@ -10,5 +10,5 @@ SELECT
     st_linemerge(geom) AS geom -- TODO - any reason to not do this here?
 FROM {{ source("recipe_sources", "dcp_cscl_centerline") }}
 WHERE
-    NOT (rwjurisdiction = '3' AND status = '2')
+    NOT (rwjurisdiction = '3' AND status <> '2')
     AND rw_type <> 8
