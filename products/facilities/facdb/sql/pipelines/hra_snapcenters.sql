eb4549813b9f0ce2c6425940b1ae4cdc549a6e02
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-04-28 08:54:01 -0700

    update ingest column name

--- products/facilities/facdb/sql/pipelines/hra_snapcenters.sql
+++ products/facilities/facdb/sql/pipelines/hra_snapcenters.sql
@@ -20,7 +20,7 @@ SELECT
     'NYCHRA' AS overabbrev,
     NULL AS capacity,
     NULL AS captype,
-    wkt::geometry AS wkb_geometry,
+    geom AS wkb_geometry,
     geo_1b,
     geo_bl,
     geo_bn
