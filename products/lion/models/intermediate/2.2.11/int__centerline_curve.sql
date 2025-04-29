1064af5d8f489f435c42c68099c4e5651f1aa4f3
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-04-28 18:41:09 -0700

    store more intermediate data in int__centerline_curve

--- products/lion/models/intermediate/2.2.11/int__centerline_curve.sql
+++ products/lion/models/intermediate/2.2.11/int__centerline_curve.sql
@@ -6,24 +6,36 @@
 ) }}
 
 WITH curves AS (
-    SELECT * FROM {{ ref("stg__centerline") }} 
+    SELECT * FROM {{ ref("stg__centerline") }}
     WHERE curve <> 'S'
+),
+points AS (
+    SELECT
+        segmentid,
+        curve AS curve_flag,
+        st_startpoint(geom) AS start_point,
+        st_endpoint(geom) AS end_point,
+        -- if n = 3, we need the second point
+        -- if n = 4, we will still take the second. With int rounding, 5/2 = 2
+        st_pointn(geom, (st_numpoints(geom) + 1) / 2) AS mid_point,
+        geom
+    FROM curves
 ),
 center_calculated AS (
     SELECT
         segmentid,
-        curve AS curve_flag,
+        curve_flag,
+        start_point,
+        end_point,
+        mid_point,
+        geom,
         CASE
-            WHEN curve IN ('L', 'R') THEN
-                find_circle(
-                    st_startpoint(geom),
-                    st_endpoint(geom),
-                    st_pointn(geom, st_numpoints(geom)/2)
-                )
+            WHEN curve_flag IN ('L', 'R') THEN
+                find_circle(start_point, mid_point, end_point)
         END AS center_of_curvature
-    FROM curves
+    FROM points
 )
-SELECT 
+SELECT
     *,
     st_x(center_of_curvature) AS center_of_curvature_x,
     st_y(center_of_curvature) AS center_of_curvature_y
