967db7c4a092936b68d1b26e36f8303d25d8c8f7
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2025-04-30 14:21:04 -0400

    Clean the cruft
    e.g. Inline single-use fns

--- products/edde/ingest/make_cache_fn.py
+++ products/edde/ingest/make_cache_fn.py
@@ -1,18 +1,6 @@
 """"""
 
 
-def make_PUMS_cache_fn(
-    year: int, variable_types=None, limited_PUMA=False, include_rw=False
-):
-    fn = f"PUMS_{'_'.join(variable_types)}"
-    fn = f"{fn}_{year}"
-    if limited_PUMA:
-        fn += "_limitedPUMA"
-    if not include_rw:
-        fn += "_noRepWeights"
-    return f"data/{fn}.pkl"
-
-
 def make_HVS_cache_fn(year: 2017, human_readable=True, output_type=".pkl"):
     rv = f"data/HVS_data_{year}"
     if human_readable:
