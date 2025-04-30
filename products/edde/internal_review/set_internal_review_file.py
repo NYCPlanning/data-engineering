8af2bdf66d0f586a5197068879b5d470dc61e84e
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2025-04-30 16:11:47 -0400

    Fix internal review pathing (potential?) bug
    I think we should probably actually delete this functionality.
    In the meantime...

--- products/edde/internal_review/set_internal_review_file.py
+++ products/edde/internal_review/set_internal_review_file.py
@@ -1,11 +1,12 @@
-"""Write one or more .csv files to be pushed to github for internal review"""
-
 from typing import List, Tuple
 import pandas as pd
+from pathlib import Path
 
 
 def set_internal_review_files(data: List[Tuple[pd.DataFrame, str, str]], category):
     """Save list of dataframes as csv."""
     for df, name, geography in data:
         print(f"Writing {name} to internal review folder")
-        df.to_csv(f"internal_review/{category}/{geography}/{name}")
+        dir = Path("internal_review") / category / geography
+        dir.mkdir(exist_ok=True, parents=True)
+        df.to_csv(dir / name)
