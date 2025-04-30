05fefe157eea3f5c72aa0513e3c0bba2e5760314
sf-dcp <ofilippova@planning.nyc.gov>
2025-04-30 17:24:24 -0400

    Add `face_code` field to the intermediate table

--- products/lion/models/intermediate/2.2.1/int__centerline_streetcode_and_facecode.sql
+++ products/lion/models/intermediate/2.2.1/int__centerline_streetcode_and_facecode.sql
@@ -20,9 +20,24 @@ all_lgc_pivoted AS (
         MAX(CASE WHEN lgc_rank = 8 THEN lgc END) AS lgc8,
         MAX(CASE WHEN lgc_rank = 9 THEN lgc END) AS lgc9,
         MAX(board_of_elections_lgc_pointer) AS board_of_elections_lgc_pointer,
-        MAX(CASE WHEN lgc_rank = 1 THEN b5sc END) AS b5sc
+        MAX(CASE WHEN lgc_rank = 1 THEN b5sc END) AS b5sc,
+        MAX(CASE WHEN lgc_rank = 1 THEN b7sc END) AS preferred_b7sc
     FROM {{ ref("int__all_local_group_code_ranked") }}
     GROUP BY segmentid
+),
+principal_streetnames AS (
+    SELECT
+        b7sc,
+        facecode
+    FROM {{ source("recipe_sources", "dcp_cscl_streetname") }}
+    WHERE principal_flag = 'Y'
+),
+principal_features AS (
+    SELECT
+        b7sc,
+        facecode
+    FROM {{ source("recipe_sources", "dcp_cscl_featurename") }}
+    WHERE principal_flag = 'Y'
 )
 
 SELECT
@@ -37,7 +52,10 @@ SELECT
     all_lgc_pivoted.lgc8,
     all_lgc_pivoted.lgc9,
     all_lgc_pivoted.board_of_elections_lgc_pointer,
-    right(all_lgc_pivoted.b5sc, 5) as five_digit_street_code
+    right(all_lgc_pivoted.b5sc, 5) as five_digit_street_code,
+    COALESCE(principal_streetnames.facecode, principal_features.facecode) AS face_code
 FROM
     centerline 
     LEFT JOIN all_lgc_pivoted ON centerline.segmentid = all_lgc_pivoted.segmentid
+    LEFT JOIN principal_streetnames ON all_lgc_pivoted.preferred_b7sc = principal_streetnames.b7sc
+    LEFT JOIN principal_features ON all_lgc_pivoted.preferred_b7sc = principal_features.b7sc
