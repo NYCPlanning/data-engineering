da2e3eb03700529141ce1a5e7e5e337bf25b682c
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2025-04-30 15:00:05 -0400

    CHANGES:  ho_sec.dhs_shelter
    Use DHS Shelter recipe instead of loading on-the-fly.

--- products/edde/aggregate/housing_security/DHS_shelter.py
+++ products/edde/aggregate/housing_security/DHS_shelter.py
@@ -2,28 +2,43 @@
 but no CD. Something to watch out for when testing"""
 
 from aggregate.load_aggregated import initialize_dataframe_geo_index
-from ingest.housing_security.DHS_shelter import load_DHS_shelter
 from internal_review.set_internal_review_file import set_internal_review_files
 from utils.CD_helpers import community_district_to_PUMA
 from utils.PUMA_helpers import borough_name_mapper
+from ingest import ingestion_helpers
+
+DATASET_NAME = "dhs_shelterd_indiv_by_comm_dist"
 
 
 def DHS_shelter(geography, write_to_internal_review=False):
     final = initialize_dataframe_geo_index(geography)
-    for year in [2020, 2022]:
+
+    years = ["2020", "2022"]
+    for year in years:
         single_year = DHS_shelter_single_year(geography, year)
         final = final.merge(single_year, left_index=True, right_index=True)
+
     if write_to_internal_review:
         set_internal_review_files(
-            [(final, f"DHS_shelter_{year}.csv", geography)],
+            [(final, f"DHS_shelter_{years[-1]}.csv", geography)],
             "housing_security",
         )
     return final
 
 
-def DHS_shelter_single_year(geography, year, write_to_internal_review=False):
+def DHS_shelter_single_year(geography: str, year: str, write_to_internal_review=False):
     """Main accessor"""
-    source_data = load_DHS_shelter(year)
+
+    raw_source_data = ingestion_helpers.load_data(name=DATASET_NAME)
+
+    # Previously the edde DHS ingest script would filter down in the api calls.
+    # The new ingested version contains all reports, so we must filter here
+    report_year_filter = f"06/30/{year}"
+
+    source_data = raw_source_data[
+        raw_source_data["report_date"] == report_year_filter
+    ].copy()
+
     source_data["citywide"] = "citywide"
     source_data["borough"] = source_data["borough"].map(borough_name_mapper)
     source_data["CD_code"] = source_data["borough"] + source_data[
