22eb2c93ee707c9360dbfbb5dc5b8d2a299d3f15
fvankrieken <fvankrieken@planning.nyc.gov>
2025-05-02 12:19:12 -0400

    lion - sedat logic (#1648)

---
+++ products/lion/models/intermediate/2.2.4/int__split_election_district.sql
@@ -0,0 +1,28 @@
+{{ config(
+    materialized = 'table',
+    indexes=[
+      {'columns': ['segmentid', 'boroughcode']},
+    ]
+) }}
+WITH sedat AS (
+    SELECT * FROM {{ ref("stg__sedat") }}
+),
+agg AS (
+    SELECT
+        segmentid,
+        lionkey,
+        left(lionkey, 1) AS boroughcode,
+        array_agg(side) AS sedat_sides
+    FROM sedat
+    GROUP BY segmentid, lionkey
+)
+SELECT
+    *,
+    CASE
+        -- staging table has validation that only valid values are '1' and '2'
+        -- cases are then both, all 1, all 2, or none
+        WHEN ARRAY['1', '2'] <@ sedat_sides THEN 'B'
+        WHEN '1' = any(sedat_sides) THEN 'L'
+        WHEN '2' = any(sedat_sides) THEN 'R'
+    END AS split_election_district_flag
+FROM agg
