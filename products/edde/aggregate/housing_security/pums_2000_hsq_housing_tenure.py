35ee31eb9cbb4427a8bf83c3dc618a0aa761efe3
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2025-05-06 15:30:57 -0400

    REFACTOR: ho_sec.pums_2000_hsq

--- products/edde/aggregate/housing_security/pums_2000_hsq_housing_tenure.py
+++ products/edde/aggregate/housing_security/pums_2000_hsq_housing_tenure.py
@@ -9,21 +9,14 @@ from internal_review.set_internal_review_file import set_internal_review_files
 from aggregate.aggregation_helpers import order_aggregated_columns
 
 ## Mapper specific to this script
-housing_tenure_name_mapper = {
+_housing_tenure_name_mapper = {
     "occhu": "units_occupied_2000",
     "oocc": "units_occupied_owner_2000",
     "rocc": "units_occupied_renter_2000",
 }
 
 
-def filter_to_hsq_indicators(df):
-    """Filter to columns that are only housing tenure related that should be included in the housing
-    Security + quality 2000 output"""
-    df = df.filter(regex="Occ|OOcc|ROcc")
-    return df
-
-
-def rename_cols(df):
+def _rename_cols(df):
     cols = map(str.lower, df.columns)
     # Replace dcp pop race codes with dcp DE established codes
     for code, race in race_suffix_mapper.items():
@@ -31,22 +24,49 @@ def rename_cols(df):
     # Replace dcp pop stat suffix code with dcp DE codes
     cols = [map_stat_suffix(col, "count", False) for col in cols]
     # replace data point names
-    for code, name in housing_tenure_name_mapper.items():
+    for code, name in _housing_tenure_name_mapper.items():
         cols = [col.replace(code, name) for col in cols]
 
     df.columns = cols
     return df
 
 
+def _order_pums_2000_hsq(final: pd.DataFrame):
+    indicators_denom: list[tuple] = [
+        (
+            "units",
+            "units_occupied",
+        )
+    ]
+    categories = {
+        "units": [
+            "units_occupied_owner_2000",
+            "units_occupied_renter_2000",
+            "units_occupied_2000",
+        ],
+        "race": dcp_pop_races,
+    }
+    final = order_aggregated_columns(
+        df=final,
+        indicators_denom=indicators_denom,
+        categories=categories,
+        household=False,
+        exclude_denom=True,
+        demographics_category=False,
+    )
+    return final
+
+
 def pums_2000_hsq_housing_tenure(geography: str, write_to_internal_review=False):
-    """Main accessor for this indicator"""
     assert geography in ["puma", "borough", "citywide"]
 
     source_data = load_2000_census_pums_all_data()
 
-    df = filter_to_hsq_indicators(source_data)
+    """Filter to columns that are only housing tenure related that should be included in the housing
+    Security + quality 2000 output"""
+    df = source_data.filter(regex="Occ|OOcc|ROcc")
 
-    final = rename_cols(df)
+    final = _rename_cols(df)
 
     if geography == "citywide":
         final = (
@@ -72,33 +92,6 @@ def pums_2000_hsq_housing_tenure(geography: str, write_to_internal_review=False)
             "housing_security",
         )
 
-    final = order_pums_2000_hsq(final)
+    final = _order_pums_2000_hsq(final)
 
-    return final
-
-
-def order_pums_2000_hsq(final: pd.DataFrame):
-    """Quick function written up against deadline, can definitely be refactored"""
-    indicators_denom: list[tuple] = [
-        (
-            "units",
-            "units_occupied",
-        )
-    ]
-    categories = {
-        "units": [
-            "units_occupied_owner_2000",
-            "units_occupied_renter_2000",
-            "units_occupied_2000",
-        ],
-        "race": dcp_pop_races,
-    }
-    final = order_aggregated_columns(
-        df=final,
-        indicators_denom=indicators_denom,
-        categories=categories,
-        household=False,
-        exclude_denom=True,
-        demographics_category=False,
-    )
     return final
