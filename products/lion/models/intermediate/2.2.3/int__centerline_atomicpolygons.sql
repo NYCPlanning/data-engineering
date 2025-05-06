4cdcce51b884a728d2867823d26597c6c4a03f3f
sf-dcp <144725249+sf-dcp@users.noreply.github.com>
2025-05-06 14:03:50 -0400

    Lion: `2.2.6` & `2.2.8` (#1655)
    * Add centerline & ap boro columns to 2.2.3 intermediate ap table
    
    * Create skeleton of the `int__centerline_segment_locational_status` table
    
    * logic for `2.2.8` in inter table: "Boro Boundary Idicator" field
    
    * logic for `2.2.6` in inter table (part I):  "Segment Location Status" field
    
    * logic for `2.2.6` in inter table (part II)
    
    * Update final `lion` table with the newly added columns
    
    * POST REVIEW: Update GHA for lion and add a test

--- products/lion/models/intermediate/2.2.3/int__centerline_atomicpolygons.sql
+++ products/lion/models/intermediate/2.2.3/int__centerline_atomicpolygons.sql
@@ -10,7 +10,9 @@ WITH centerline_offsets AS (
 
 SELECT
     co.segmentid,
+    co.boroughcode AS centerline_segment_borocode,
     left_poly.atomicid AS left_atomicid,
+    left_poly.borough AS left_borocode,
     left_poly.censustract_2000 AS left_2000_census_tract,
     left(left_poly.censustract_2000, 4)::INT AS left_2000_census_tract_basic,
     nullif(right(left_poly.censustract_2000, 2), '00')::INT AS left_2000_census_tract_suffix,
@@ -30,6 +32,7 @@ SELECT
     left_poly.electdist AS left_election_district,
     left_poly.schooldist AS left_school_district,
     right_poly.atomicid AS right_atomicid,
+    right_poly.borough AS right_borocode,
     right_poly.censustract_2000 AS right_2000_census_tract,
     left(right_poly.censustract_2000, 4)::INT AS right_2000_census_tract_basic,
     nullif(right(right_poly.censustract_2000, 2), '00')::INT AS right_2000_census_tract_suffix,
---
+++ products/lion/models/intermediate/2.2.6/int__centerline_segment_locational_status.sql
@@ -0,0 +1,112 @@
+{{ config(
+    materialized = 'table',
+    indexes=[
+      {'columns': ['segmentid']},
+    ]
+) }}
+WITH atomicpolygons AS (
+    SELECT
+        segmentid,
+        centerline_segment_borocode AS centerline_borocode,
+        left_atomicid,
+        left_borocode,
+        left_2020_census_tract,
+        right_atomicid,
+        right_borocode,
+        right_2020_census_tract
+    FROM {{ ref("int__centerline_atomicpolygons") }}
+),
+segments_with_neighbors AS (
+    SELECT * FROM {{ ref("int__centerline_segment_neighbors") }}
+),
+segments_with_orphan_node AS (
+    SELECT
+        segmentid,
+        MAX(node_missing_neighbor) AS segment_missing_neighbor
+    FROM segments_with_neighbors
+    GROUP BY segmentid
+),
+
+-- All CTEs below divide the atomicpolygons into different categories. 
+-- When unioned together, they should have 1-1 match with atomicpolygons
+left_city_boundary_aps AS (
+    SELECT
+        *,
+        'L' AS borough_boundary_indicator,
+        NULL::boolean AS is_ap_boro_boundary_error,
+        '9' AS segment_locational_status
+    FROM atomicpolygons
+    WHERE left_atomicid IS NULL AND right_atomicid IS NOT NULL
+
+),
+right_city_boundary_aps AS (
+    SELECT
+        *,
+        'R' AS borough_boundary_indicator,
+        NULL::boolean AS is_ap_boro_boundary_error,
+        '9' AS segment_locational_status
+    FROM atomicpolygons
+    WHERE left_atomicid IS NOT NULL AND right_atomicid IS NULL
+),
+same_ap AS (
+    SELECT
+        ap.*,
+        NULL AS borough_boundary_indicator,
+        centerline_borocode <> left_borocode AS is_ap_boro_boundary_error,
+        CASE
+            WHEN o.segment_missing_neighbor = 1 THEN 'I'
+            WHEN o.segment_missing_neighbor = 0 THEN 'H'
+        END AS segment_locational_status
+    FROM atomicpolygons AS ap
+    LEFT JOIN segments_with_orphan_node AS o
+        ON ap.segmentid = o.segmentid
+    WHERE ap.left_atomicid = ap.right_atomicid
+),
+different_aps_different_boros AS (
+    SELECT
+        *,
+        CASE
+            WHEN centerline_borocode = right_borocode THEN 'L'
+            WHEN centerline_borocode = left_borocode THEN 'R'
+        END AS borough_boundary_indicator,
+        centerline_borocode <> left_borocode AND centerline_borocode <> right_borocode AS is_ap_boro_boundary_error,
+        CASE
+            WHEN centerline_borocode = right_borocode THEN left_borocode::char(1)
+            WHEN centerline_borocode = left_borocode THEN right_borocode::char(1)
+        END AS segment_locational_status
+    FROM atomicpolygons
+    WHERE left_atomicid <> right_atomicid AND left_borocode <> right_borocode   -- borocode alone should be sufficient but who knows...
+),
+different_aps_same_boro AS (
+    SELECT
+        *,
+        NULL AS borough_boundary_indicator,
+        centerline_borocode <> left_borocode AS is_ap_boro_boundary_error,
+        CASE
+            WHEN left_2020_census_tract <> right_2020_census_tract THEN 'X'
+        END AS segment_locational_status
+    FROM atomicpolygons
+    WHERE left_atomicid <> right_atomicid AND left_borocode = right_borocode
+),
+-- the CTE below accounts for erroneous segments. This is done for 1-1 matching with the segments table
+segments_without_aps AS (
+    SELECT
+        *,
+        NULL AS borough_boundary_indicator,
+        TRUE AS is_ap_boro_boundary_error,
+        NULL AS segment_locational_status
+    FROM atomicpolygons
+    WHERE left_atomicid IS NULL AND right_atomicid IS NULL
+)
+
+SELECT * FROM left_city_boundary_aps
+UNION ALL
+SELECT * FROM right_city_boundary_aps
+UNION ALL
+SELECT * FROM same_ap
+UNION ALL
+SELECT * FROM different_aps_different_boros
+UNION ALL
+SELECT * FROM different_aps_same_boro
+UNION ALL
+SELECT * FROM segments_without_aps
