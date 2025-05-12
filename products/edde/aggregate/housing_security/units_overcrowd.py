f2787a5c554bc4fd0f3252dac9da0b40013cc33e
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2025-05-12 17:53:35 -0400

    REFACTOR: ho_sec.units_overcrowd - generalize

--- products/edde/aggregate/housing_security/units_overcrowd.py
+++ products/edde/aggregate/housing_security/units_overcrowd.py
@@ -1,48 +1,14 @@
-import pandas as pd
-from aggregate.clean_aggregated import (
-    rename_col_housing_security,
-    order_PUMS_QOL_multiple_years,
+from aggregate.load_aggregated import ACSAggregator
+
+units_overcrowd_aggregator = ACSAggregator(
+    name="units_overcrowd",
+    dcp_base_variables=["units_overcrowded", "units_notovercrowded"],
+    dcp_base_variables_conf={
+        "include_race": True,
+        "include_year_in_out_col": True,
+    },
+    internal_review_filename="units_overcrowd.csv",
+    internal_review_category="housing_security",
 )
-from utils.dcp_population_excel_helpers import (
-    race_suffix_mapper,
-    count_suffix_mapper_global,
-)
-from utils.PUMA_helpers import acs_years
-from internal_review.set_internal_review_file import set_internal_review_files
-from aggregate.load_aggregated import load_clean_housing_security_pop_data
-from aggregate.aggregation_helpers import get_geography_pop_data
 
-
-def units_overcrowd(
-    geography: str,
-    start_year=acs_years[0],
-    end_year=acs_years[-1],
-    write_to_internal_review=False,
-) -> pd.DataFrame:
-    """denom units occupied not included because the denom would come from other housing tenure indicator"""
-    name_mapper = {
-        "OcR1p": "units_overcrowded",
-        "OcRU1": "units_notovercrowded",
-    }
-
-    clean_data = load_clean_housing_security_pop_data(name_mapper, start_year, end_year)
-
-    final = get_geography_pop_data(clean_data=clean_data, geography=geography)
-
-    final = rename_col_housing_security(final, name_mapper, race_suffix_mapper, "count")
-
-    col_order = order_PUMS_QOL_multiple_years(
-        categories=["units_overcrowded", "units_notovercrowded"],
-        measures=count_suffix_mapper_global.values(),
-        years=[start_year, end_year],
-    )
-
-    final = final.reindex(columns=col_order)
-
-    if write_to_internal_review:
-        set_internal_review_files(
-            [(final, "units_overcrowd.csv", geography)],
-            "housing_security",
-        )
-
-    return final
+units_overcrowd = units_overcrowd_aggregator.run
