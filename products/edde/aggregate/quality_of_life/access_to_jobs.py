e414b94f320aece37aa34a84b8a38894e0781e92
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2025-05-13 12:18:24 -0400

    REFACTOR: qol.acces_to_jobs
    Source Changed

--- products/edde/aggregate/quality_of_life/access_to_jobs.py
+++ products/edde/aggregate/quality_of_life/access_to_jobs.py
@@ -2,12 +2,14 @@ import pandas as pd
 from internal_review.set_internal_review_file import set_internal_review_files
 from utils.PUMA_helpers import clean_PUMAs, puma_to_borough
 
+from ingest.ingestion_helpers import read_from_excel
+
+SOURCE_DATA_FILE = "resources/quality_of_life/EDDE_2025_Updates_transportation.xlsx"
+INDICATOR_COL_NAME = "access_employment_count"
+
 
 def access_to_jobs(geography, write_to_internal_review=False):
-    indicator_col_name = "access_employment_count"
-    clean_df = load_clean_source_data(indicator_col_name)
-
-    final = clean_df.groupby(geography).sum()[[indicator_col_name]]
+    final = load_clean_source_data().groupby(geography).sum()[[INDICATOR_COL_NAME]]
     if write_to_internal_review:
         set_internal_review_files(
             [(final, "access_employment.csv", geography)],
@@ -16,19 +18,18 @@ def access_to_jobs(geography, write_to_internal_review=False):
     return final
 
 
-def load_clean_source_data(indicator_col_name) -> pd.DataFrame:
-    source_data = pd.read_csv(
-        "resources/quality_of_life/access_to_jobs.csv",
-        usecols=[
-            "PUMA",
-            "Weighted Average Number of Jobs Accessible within 30 mins from Tract Centroid by Transit",
-        ],
+def load_clean_source_data() -> pd.DataFrame:
+    source_data = read_from_excel(
+        file_path=SOURCE_DATA_FILE,
+        category="quality_of_life",
+        sheet_name="Access_to_Jobs",
+    ).rename(
+        columns={
+            "PUMA": "puma",
+            "Weighted Average Number of Jobs Accessible within 30 mins from Tract Centroid by Transit": INDICATOR_COL_NAME,
+        }
     )
-    col_name_mapper = {
-        "PUMA": "puma",
-        "Weighted Average Number of Jobs Accessible within 30 mins from Tract Centroid by Transit": indicator_col_name,
-    }
-    source_data.rename(columns=col_name_mapper, inplace=True)
+
     source_data["puma"] = source_data["puma"].apply(func=clean_PUMAs)
     source_data["borough"] = source_data.apply(axis=1, func=puma_to_borough)
     source_data["citywide"] = "citywide"
