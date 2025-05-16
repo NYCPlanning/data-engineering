783db74a623c1ff6e9f71c19c86463ceb8533b89
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2025-05-16 13:50:04 -0400

    CHANGES:  ho_prod.area_within_hi
    Swap out for 2020 puma boundaries
    
    Also adds utilities to load a geodataframe. This pretty hacky, but works
    for now.
    
    Note: the prefixing of "0" to PUMAS gets cleaned up later in the puma loader

--- products/edde/ingest/ingestion_helpers.py
+++ products/edde/ingest/ingestion_helpers.py
@@ -1,23 +1,32 @@
-"""Miscellaneous ingestion related tasks"""
-
 from dcpy.lifecycle.builds import load
+from dcpy.utils.geospatial import parquet
 import pandas as pd
+import geopandas as gp
 
-
-# import config
 import config
 
 
-def load_data(name: str, version: str = "", cols: list = []) -> pd.DataFrame:
+def load_data(
+    name: str, version: str = "", cols: list = [], is_geospatial: bool = False
+) -> pd.DataFrame | gp.GeoDataFrame:
+    # TODO: is_geospatial flag is hacky. This should be inferred elsewhere
+
     build_metadata = load.get_build_metadata(config.PRODUCT_PATH)
     assert build_metadata.load_result, "You must load data before reading data."
 
-    df = load.get_imported_df(build_metadata.load_result, ds_id=name, version=version)
+    if is_geospatial:
+        df = parquet.read_df(
+            load.get_imported_filepath(build_metadata.load_result, name, version)
+        )
+    else:
+        df = load.get_imported_df(
+            build_metadata.load_result, ds_id=name, version=version
+        )
     return df.filter(items=cols or df.columns.to_list())
 
 
 def read_from_excel(
-    file_path, category: str, sheet_name: str = None, columns: str = None, **kwargs
+    file_path, category: str, sheet_name: str = "", columns: list = [], **kwargs
 ) -> pd.DataFrame:
     read_excel_args = {
         "io": file_path,
