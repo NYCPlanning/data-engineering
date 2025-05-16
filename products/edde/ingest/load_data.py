4f4ec7657c1c92c113ae0d4883dc93f496cb2492
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2025-05-16 16:11:33 -0400

    REFACTOR: delete unused, also kill custom logger

--- products/edde/ingest/load_data.py
+++ products/edde/ingest/load_data.py
@@ -1,18 +1,11 @@
-"""Access to ingestion code"""
-
+from dcpy.utils.logging import logger
 from typing import List
 import pandas as pd
 
 from os.path import exists
 from pathlib import Path
 from ingest.PUMS.PUMS_data import PUMSData
-from ingest.HVS.HVS_ingestion import create_HVS
-from ingest.make_cache_fn import make_HVS_cache_fn
 
-from utils.make_logger import create_logger
-from utils.wd_management import correct_wd
-
-logger = create_logger("load_data_logger", "logs/load_data.log")
 
 allowed_HVS_cache_types = [".csv", ".pkl"]
 
@@ -26,7 +19,6 @@ def load_PUMS(
     return_ingestor=False,
     requery: bool = False,
 ):
-    assert correct_wd(), "Code is not being run from root directory"
     Path("data/").mkdir(exist_ok=True)
 
     cache_path = PUMSData.get_cache_fn(
@@ -55,41 +47,3 @@ def load_PUMS(
         f"PUMS data with {PUMS_data.shape[0]} records loaded, ready for aggregation"
     )
     return rv
-
-
-def load_HVS(
-    year: int,
-    requery: bool = False,
-    human_readable: bool = False,
-    output_type: str = ".csv",
-) -> pd.DataFrame:
-    """
-    To-do:
-    - break out pums download into it's own function that can be called on its own
-    - remove return_ingestor flag and always get ingestor object
-
-    :param limited_PUMA: only query for first PUMA in each borough. For debugging
-    :return: pandas dataframe of PUMS data
-    """
-    assert correct_wd(), "Code is not being run from root directory"
-    Path("data/").mkdir(exist_ok=True)
-
-    if output_type not in allowed_HVS_cache_types:
-        raise Exception(
-            f"{output_type} file type not supported for HVS cache. Allowed file types are {allowed_HVS_cache_types} "
-        )
-    HVS_cache_path = make_HVS_cache_fn(
-        year=year, human_readable=human_readable, output_type=output_type
-    )
-    if requery or not exists(HVS_cache_path):
-        create_HVS(year=year, human_readable=human_readable, output_type=output_type)
-
-    if output_type == ".pkl":
-        HVS_data = pd.read_pickle(HVS_cache_path)
-    elif output_type == ".csv":
-        HVS_data = pd.read_csv(HVS_cache_path)
-    HVS_data
-    logger.info(
-        f"HVS data with {HVS_data.shape[0]} records loaded, ready for aggregation"
-    )
-    return HVS_data
--- products/edde/ingest/make_cache_fn.py
+++
@@ -1,9 +0,0 @@
-""""""
-
-
-def make_HVS_cache_fn(year: 2017, human_readable=True, output_type=".pkl"):
-    rv = f"data/HVS_data_{year}"
-    if human_readable:
-        rv = f"{rv}_human_readable"
-
-    return f"{rv}{output_type}"
