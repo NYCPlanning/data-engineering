1cab3c89b2938f0ae17db5f28fd97108dae99fd9
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2025-05-18 15:04:38 -0400

    REFACTOR: get ACS Demographics working
    This was previously using a separate Excel file for just
    ACS demographics data. This swaps to use the combined file
    
    Also, I'm scattering logger statements through this because I couldn't
    tell what was running

--- products/edde/aggregate/PUMS/pums_demographics.py
+++ products/edde/aggregate/PUMS/pums_demographics.py
@@ -1,20 +1,22 @@
+from dcpy.utils.logging import logger
 import pandas as pd
+
+from aggregate.clean_aggregated import rename_columns_demo
 from aggregate.aggregation_helpers import (
     demographic_indicators_denom,
     order_aggregated_columns,
     get_category,
-    get_geography_pop_data,
 )
-from utils.PUMA_helpers import dcp_pop_races
-from internal_review.set_internal_review_file import set_internal_review_files
-from aggregate.load_aggregated import load_clean_pop_demographics
+from aggregate.load_aggregated import load_acs
 from aggregate.PUMS.pums_2000_demographics import pums_2000_demographics
-from utils.PUMA_helpers import acs_years
+from internal_review.set_internal_review_file import set_internal_review_files
+from utils.PUMA_helpers import dcp_pop_races, acs_years
 
 
 def acs_pums_demographics(
     geography: str, year: str = acs_years[-1], write_to_internal_review=False
 ) -> pd.DataFrame:
+    logger.info(f"Running acs_pums_demographics: {geography}, {year}")
     if year == "2000":
         return pums_2000_demographics(
             geography, write_to_internal_review=write_to_internal_review
@@ -22,7 +24,6 @@ def acs_pums_demographics(
     assert geography in ["citywide", "borough", "puma"]
     assert year in acs_years
 
-    indicators_denom = demographic_indicators_denom
     categories = {
         "LEP": ["lep"],
         "foreign_born": ["fb"],
@@ -32,12 +33,11 @@ def acs_pums_demographics(
         "race": dcp_pop_races,
     }
 
-    clean_data = load_clean_pop_demographics(year)
-    final = get_geography_pop_data(clean_data, geography)
-
+    acs = pd.DataFrame(load_acs(year).loc[geography].rename_axis(geography))
+    renamed = rename_columns_demo(acs, year[2:])
     final = order_aggregated_columns(
-        df=final,
-        indicators_denom=indicators_denom,
+        df=renamed,
+        indicators_denom=demographic_indicators_denom,
         categories=categories,
         household=False,
         exclude_denom=True,
