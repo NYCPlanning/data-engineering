3d960b060a7b0152a94732c017c3b2490fa1efd4
Damon McCullough <dmccullough@planning.nyc.gov>
2025-05-19 12:55:42 -0400

    kpdb tweaks for 2024 build (#1520)
    * more informative corrections log message
    
    * update kpdb source data
    
    * fix extraction of latest EDC shapefile
    
    * fix kpdb column
    
    * fix null-like values
    
    * fix other new shapefiles
    
    * column name for dcp_n_study_future
    
    * drop geometry column from kpdb csv
    
    long geometry values create invalid rows when loaded into excel. no geometry column in a final output csv is a pattern of our data products
    
    * add fgdb export to kpdb
    
    * use version for kpdb upload path instead of datetime
    
    * further corrections
    
    ---------
    
    Co-authored-by: Finn van Krieken <fvankrieken@planning.nyc.gov>

--- products/knownprojects/python/__init__.py
+++ products/knownprojects/python/__init__.py
@@ -11,10 +11,9 @@ _proj_root = _product_path.parent.parent
 # Make `dcpy` available
 sys.path.append(str(_proj_root))
 
-BUILD_DATETIME = datetime.today().strftime("%Y%m%d_%H:%M")
 
 S3_BUCKET = "edm-private"
-S3_SOURCE_HOUSING_TEAM_DIR = "dcp_housing_team/db-knownprojects/20240105"
+S3_SOURCE_HOUSING_TEAM_DIR = "dcp_housing_team/db-knownprojects/20250430"
 S3_OUTPUT_DIR = "db-kpdb"
 
 DATA_PATH = _product_path / "data"
@@ -23,16 +22,16 @@ CORRECTIONS_DATA_PATH = DATA_PATH / "corrections"
 OUTPUT_PATH = _product_path / "output"
 
 DCP_HOUSING_DATA_FILENAMES = {
-    "dcp_knownprojects": "kpdb_20211006_shapefiles.zip",
+    "dcp_knownprojects": "KPDB_20240710.zip",
     "esd_projects": "2021.2.10 State Developments for Housing Pipeline.xlsx",
     "edc_projects": "2022.11.18 EDC inputs for DCP housing projections.xlsx",
-    "edc_dcp_inputs": "edc_shapefile_20221118.zip",
-    "dcp_n_study": "nstudy_rezoning_commitments_shapefile_20221017.zip",
-    "dcp_n_study_future": "future_nstudy_shapefile_20230928.zip",
-    "dcp_n_study_projected": "PastNeighborhoodStudies_20231212.zip",
-    "hpd_rfp": "HPD_RFPs.xlsx",
-    "hpd_pc": "HPD_Pipeline.xlsx",
-    "dcp_planneradded": "dcp_planneradded_2023_03_21.csv",
+    "edc_dcp_inputs": "edc_shapefile_20250225.zip",
+    "dcp_n_study": "nstudy_rezoning_commitments_shapefile_20250430.zip",
+    "dcp_n_study_future": "future_neighborhoodstudies_20250304.zip",
+    "dcp_n_study_projected": "past_neighborhoodstudies_20250304.zip",
+    "hpd_rfp": "HPD_RFPs_20250110.xlsx",
+    "hpd_pc": "HPD_Pipeline_20250225.xlsx",
+    "dcp_planneradded": "dcp_planneradded_2025_04_30.csv",
 }
 DCP_HOUSING_CORRECTIONS_FILENAMES = {
     "corrections_dob": "corrections_dob.csv",
@@ -45,3 +44,4 @@ DCP_HOUSING_CORRECTIONS_FILENAMES = {
 load_dotenv()
 BUILD_ENGINE = os.environ["BUILD_ENGINE"]
 BUILD_ENGINE_SCHEMA = os.environ["BUILD_ENGINE_SCHEMA"]
+VERSION = os.environ["VERSION"]
