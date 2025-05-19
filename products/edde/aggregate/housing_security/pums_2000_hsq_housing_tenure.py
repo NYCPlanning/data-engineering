b925fa3e7fe97b9bca61a179180a68245d3111dc
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2025-05-18 22:57:30 -0400

    REFACTOR: ho_sec.pums_2000_hsq - simplify

--- products/edde/aggregate/housing_security/pums_2000_hsq_housing_tenure.py
+++ products/edde/aggregate/housing_security/pums_2000_hsq_housing_tenure.py
@@ -6,7 +6,7 @@ from utils.dcp_population_excel_helpers import (
     race_suffix_mapper,
     map_stat_suffix,
 )
-from utils.geo_helpers import clean_PUMAs, dcp_pop_races
+from utils.geo_helpers import dcp_pop_races
 from internal_review.set_internal_review_file import set_internal_review_files
 
 _housing_tenure_name_mapper = {
@@ -58,43 +58,23 @@ def _order_pums_2000_hsq(final: pd.DataFrame):
 
 
 def pums_2000_hsq_housing_tenure(geography: str, write_to_internal_review=False):
-    assert geography in ["puma", "borough", "citywide"]
-
-    source_data = load_2000_census()
-
-    """Filter to columns that are only housing tenure related that should be included in the housing
-    Security + quality 2000 output"""
-    df = source_data.filter(regex="Occ|OOcc|ROcc")
-
-    final = _rename_cols(df)
-
-    boroughs = ["BX", "BK", "MN", "QN", "SI"]
-
-    if geography == "citywide":
-        final = (
-            final.loc[["citywide"]].reset_index().rename(columns={"GeoID": "citywide"})
+    df = (
+        pd.DataFrame(
+            load_2000_census()
+            .filter(regex="Occ|OOcc|ROcc")
+            .pipe(_rename_cols)
+            .loc[geography]
         )
-    elif geography == "borough":
-        final = final.loc[boroughs].reset_index().rename(columns={"GeoID": "borough"})
-    else:
-        non_puma_indices = boroughs + ["citywide"]
-        final = (
-            final.loc[~final.index.isin(non_puma_indices)]
-            .reset_index()
-            .rename(columns={"GeoID": "puma"})
-        )
-        final["puma"] = final["puma"].apply(func=clean_PUMAs)
-
-    final.set_index(geography, inplace=True)
+        .rename_axis(geography)
+        .pipe(_order_pums_2000_hsq)
+    )
 
     if write_to_internal_review:
         set_internal_review_files(
             [
-                (final, "security_2000_acs.csv", geography),
+                (df, "security_2000_acs.csv", geography),
             ],
             "housing_security",
         )
 
-    final = _order_pums_2000_hsq(final)
-
-    return final
+    return df
