03b6356fa42796e0dfdddf29d082f1be8ca9e0eb
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2025-05-19 17:26:00 -0400

    CHANGES:  ho_sec.housing_lottery
    The data format for this was... insane. We received data in a different
    format, so I formatted the data in
    excel (I also checked in the raw, for future reference) and rewrote the
    loading.
    
    note: `change_in_units` gets commented out, but is added back later once
    it's rewritten.

--- products/edde/aggregate/all_accessors.py
+++ products/edde/aggregate/all_accessors.py
@@ -2,7 +2,6 @@
 from aggregate.housing_production.area_within_historic_district import (
     fraction_historic,
 )
-from aggregate.housing_production.change_in_units import change_in_units
 from aggregate.housing_production.hpd_housing_ny_affordable_housing import (
     affordable_housing,
 )
@@ -77,7 +76,8 @@ from aggregate.PUMS.pums_2000_economics import pums_2000_economics
 from aggregate.PUMS.pums_demographics import acs_pums_demographics
 from aggregate.PUMS.pums_economics import acs_pums_economics
 
-housing_production_accessors = [fraction_historic, change_in_units, affordable_housing]
+# housing_production_accessors = [fraction_historic, change_in_units, affordable_housing]
+housing_production_accessors = [fraction_historic, affordable_housing]
 
 
 QOL_accessors = [
