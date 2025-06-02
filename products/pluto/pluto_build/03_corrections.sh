90d8905fadd237db0fae739ce30ca829006d5a7d
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-06-02 14:47:36 -0400

    simplify pluto corrections
    no more boilerplate in my house.
    
    would love another pair of careful eyes - it seems
    like all the individual sql files were doing the
    exact same thing but I might have missed a special
    case. lot area had a cast to numeric so I left it
    as a special case

--- products/pluto/pluto_build/03_corrections.sh
+++ products/pluto/pluto_build/03_corrections.sh
@@ -6,13 +6,13 @@ run_sql_file sql/corr_create.sql
 
 echo "Applying corrections to PLUTO"
 run_sql_file sql/corr_lotarea.sql
-run_sql_file sql/corr_yearbuilt_lpc.sql
-run_sql_file sql/corr_ownername_city.sql
+run_sql_file sql/corr_template.sql -v FIELD=yearbuilt
+run_sql_file sql/corr_template.sql -v FIELD=ownername
 run_sql_file sql/corr_ownername_punctuation.sql
-run_sql_file sql/corr_communitydistrict.sql
-run_sql_file sql/corr_numfloors.sql
-run_sql_file sql/corr_units.sql
+run_sql_file sql/corr_template.sql -v FIELD=cd
+run_sql_file sql/corr_template.sql -v FIELD=numfloors
+run_sql_file sql/corr_template.sql -v FIELD=unitsres
+run_sql_file sql/corr_template.sql -v FIELD=unitstotal
 run_sql_file sql/corr_inwoodrezoning.sql
-run_sql_file sql/corr_dropoldrecords.sql
-run_sql_file sql/corr_bct2020.sql
+run_sql_file sql/corr_template.sql -v FIELD=bct2020
 run_sql_file sql/remove_unitlots.sql
