a8b2f081a333a2648ce100075a71730c840a4e9c
Damon McCullough <dmccullough@planning.nyc.gov>
2025-06-09 16:06:31 -0400

    fix minio config in docker images (#1720)
    * fix minio config in docker images
    
    * delete commented-out minio setup

--- bash/docker_container_setup.sh
+++ bash/docker_container_setup.sh
@@ -13,3 +13,6 @@ fi
 python3 -m pip install $option . -c ./admin/run_environment/constraints.txt --no-deps
 
 git config --global --add safe.directory $workspace
+
+# configure minio for use with S3 buckets in Digital Ocean
+mc alias set spaces "$AWS_S3_ENDPOINT" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY" --api S3v4
