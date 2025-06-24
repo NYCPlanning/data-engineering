e5ae1e0da1c7f4767e5eb9f2a814030283c0f8ac
Damon McCullough <damonmcc1391@gmail.com>
2025-06-24 16:11:21 -0400

    add databases to clean build artifacts

--- dcpy/lifecycle/builds/__init__.py
+++ dcpy/lifecycle/builds/__init__.py
@@ -2,12 +2,14 @@ BUILD_REPO = "data-engineering"
 BUILD_DBS = [
     "db-cbbr",
     "db-cdbg",
+    "db-ceqr",
     "db-checkbook",
     "db-colp",
     "db-cpdb",
     "db-devdb",
     "db-facilities",
     "db-green-fast-track",
+    # "db-lion", we need to preserve schemas while this data product is in development
     "db-pluto",
     "db-template",
     "db-ztl",
