c1b05a6126fda40f7ef9e56f62be610b7faa1e81
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2025-07-03 13:22:13 -0400

    Add Coincident Segments Counting

--- products/lion/models/intermediate/2.6/int__centerline_coincident_subway_or_rail.sql
+++ products/lion/models/intermediate/2.6/int__centerline_coincident_subway_or_rail.sql
@@ -1,17 +1,42 @@
 {{ config(
     materialized = 'table',
-    indexes=[
-      {'columns': ['segmentid']},
-    ]
 ) }}
 
-WITH proto AS (
-    SELECT * FROM {{ ref("stg__protosegments") }}
+-- Exact matches catches all but ~50 or so
+WITH exact_matches AS (
+    SELECT
+        r.segmentid AS trains_segment_id,
+        c.segmentid AS all_segments_id,
+        rail_type,
+        'exact' AS match_type
+    FROM {{ ref('stg__underground_rail') }} AS r
+    INNER JOIN {{ ref('stg__centerline') }} AS c ON ST_EQUALS(r.geom, c.geom)
+    -- WHERE a.coincident_seg_count > 1 # TODO: Probably reinstate this check
+),
+inexact_matches AS (
+    SELECT
+        r.segmentid AS trains_segment_id,
+        c.segmentid AS all_segments_id,
+        r.rail_type,
+        'fuzzy' AS match_type
+    FROM {{ ref('stg__underground_rail') }} AS r
+    INNER JOIN {{ ref('stg__centerline') }} AS c
+        ON ST_DWITHIN(r.midpoint, c.midpoint, .1)
+    -- `WHERE NOT...` below shaves off a few seconds from the more conventional:
+    -- WHERE r.segmentid NOT IN (SELECT trains_segment_id FROM exact_matches)
+    WHERE NOT EXISTS (
+        SELECT 1 FROM exact_matches AS em
+        WHERE em.trains_segment_id = r.segmentid
+    )
+    -- AND a.coincident_seg_count > 1 # TODO: Probably reinstate this check
+),
+all_together AS (
+    SELECT * FROM inexact_matches
+    UNION
+    SELECT * FROM exact_matches
 )
-
 SELECT
-    segmentid,
-    count(*) AS subway_or_rail_count
-FROM proto
-WHERE feature_type = '1' -- TODO - there are none found. This seems wrong
-GROUP BY segmentid
+    all_segments_id AS segmentid,
+    COUNT(*) AS subway_or_rail_count
+FROM all_together
+GROUP BY all_segments_id
