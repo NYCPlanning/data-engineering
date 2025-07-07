d69c0956d76a097555c13d15c449dcc6130c0780
sf-dcp <144725249+sf-dcp@users.noreply.github.com>
2025-07-07 10:55:30 -0400

    LION: Address LGC issue (#1776)
    * Uncomment data test that previously could not be run
    
    * Add test for lgc source data:
    * each segment is expected to have unique lgc values
    * severity is set to warningb ecause the source data does not pass the test. It should eventually
    
    * Revise logic for LGC fields:
    * Previous logic de-duplicated LGC values for each segment in `UNION` statement.
    * The docs don't mention to do de-duplication, so I removed it in this commit.

--- products/lion/models/intermediate/2.2.1/int__nonpreferred_local_group_code.sql
+++ products/lion/models/intermediate/2.2.1/int__nonpreferred_local_group_code.sql
@@ -14,7 +14,7 @@ nonpreferred_ranked_by_lgc AS (
     SELECT
         segmentid,
         lgc,
-        RANK() OVER (
+        ROW_NUMBER() OVER (
             PARTITION BY segmentid
             ORDER BY lgc ASC
         ) + 1 AS lgc_rank, -- value of 1 is reserved for preferred lgc
