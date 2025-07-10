15fef995f11b10c67a06efdd62500c3818893f0d
Alex Richey <alexrichey@DCP-APPLE-2180.local>
2025-07-10 15:39:37 -0400

    Bump tolerance to 1 to include more matches

--- products/lion/models/intermediate/2.6/int__centerline_coincident_subway_or_rail.sql
+++ products/lion/models/intermediate/2.6/int__centerline_coincident_subway_or_rail.sql
@@ -9,5 +9,5 @@ SELECT
     all_segments_id AS segmentid,
     COUNT(*) AS subway_or_rail_count
 FROM {{ ref('int__centerline_coincident_subway_or_rail_matches') }}
-WHERE starting_coincident_seg_count > 1 AND distance < .1
+WHERE starting_coincident_seg_count > 1 AND distance < 1
 GROUP BY all_segments_id
