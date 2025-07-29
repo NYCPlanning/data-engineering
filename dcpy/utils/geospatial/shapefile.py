4adbabdc3ac9e750ffeee0ef43a12103c407a725
jackrosacker <jack.rosacker@gmail.com>
2025-07-29 17:09:13 -0400

    post-review: WIP - implement classes

--- dcpy/utils/geospatial/shapefile.py
+++ dcpy/utils/geospatial/shapefile.py
@@ -9,107 +9,6 @@ from zipfile import ZIP_DEFLATED, ZipFile
 # TODO - (possible) write `remove_metadata()` function
 
 
-@dataclass
-class Shapefile:
-    shp_name: str
-
-    def _get_metadata_xml_name(self, file_list: list[str]) -> Optional[str]:
-        """Extracts name of metadata .xml file matching specified .shp file from a
-        list of filenames. Does not require an actual directory/zip location. Only
-        parses a list of names as produced by other functions.
-        Handles cases where metadata ends with either ".xml" or ".shp.xml"
-
-        Args:
-            file_list (list[str]): List of file names to parse
-            shp_name (str): Shapefile name, not path, ending with ".shp"
-
-        Raises:
-            ValueError: Error if more than one .xml file matching the pattern are found.
-
-        Returns:
-            Optional[str]: Either name of metadata file, or None if no file is found
-        """
-        shp_stem = Path(self.shp_name).stem
-        file_names = [Path(f).name for f in file_list]
-        matched_names = [
-            f for f in file_names if f in {f"{shp_stem}.shp.xml", f"{shp_stem}.xml"}
-        ]
-        match len(matched_names):
-            case 0:
-                return None
-            case 1:
-                return matched_names[0]
-            case _:
-                raise ValueError(
-                    f"Expected a single xml with name '{shp_stem}', but found {len(matched_names)}"
-                )
-
-    def _write_file(self, filename, contents):
-        raise NotImplementedError("Not implemented on base class")
-
-    def _remove_file(self, filename):
-        raise NotImplementedError("Not implemented on base class")
-
-    def _list_all_files(self):
-        raise NotImplementedError("Not implemented on base class")
-
-    def _read_file(self, filename) -> str:
-        raise NotImplementedError("Not implemented on base class")
-
-
-@dataclass
-class ShapefileNonZipped(Shapefile):
-    _shp_dir: Path
-
-    def _write_file(self, filename, contents):
-        with open(Path(self._shp_dir) / filename, "w") as xml_file:
-            xml_file.write(contents)
-
-    def _remove_file(self, filename):
-        return
-
-    def _list_all_files(self) -> list[str]:
-        return [str(f.name) for f in self._shp_dir.iterdir()]
-
-    def _read_file(self, filename) -> str:
-        path_to_xml = Path(self._shp_dir) / filename
-        with open(path_to_xml, "r") as f:
-            return f.read()
-
-
-@dataclass
-class ShapefileZipped(Shapefile):
-    _zip_path: Path
-    _subdir: str | None
-
-    def _write_file(self, filename, contents):
-        with ZipFile(self._zip_path, "a", compression=ZIP_DEFLATED) as shp:
-            shp.writestr(filename, contents)
-
-    def _remove_file(self, filename):
-        if self._subdir:
-            filename = Path(self._subdir) / filename
-
-        zip_file = Path(self._zip_path)
-        temp_zip = zip_file.parent / (zip_file.name + ".tmp")
-        with ZipFile(zip_file, "r") as old_zip:
-            with ZipFile(temp_zip, "w", compression=ZIP_DEFLATED) as new_zip:
-                for item in old_zip.infolist():
-                    if item.filename != filename:
-                        data = old_zip.read(item.filename)
-                        new_zip.writestr(item, data)
-        os.replace(temp_zip, zip_file)
-
-    def _list_all_files(self) -> list[str]:
-        with ZipFile(self._zip_path, "r") as archive:
-            return archive.namelist()
-
-    def _read_file(self, filename) -> str:
-        with ZipFile(self._zip_path, "r") as zf:
-            metadata = zf.read(filename).decode(encoding="utf-8")
-            return metadata
-
-
 def _validate_shp_input(shp_string: str | Path) -> None:
     """Ensure the input string conforms to the required format:
     Format if zip file: 'zip://path/to/file.zip!shapefile.shp'
@@ -140,7 +39,7 @@ def _validate_shp_input(shp_string: str | Path) -> None:
         )
 
 
-def _parse_path_to_shp(path_to_shp: str | Path) -> Shapefile:
+def _parse_path_to_shp(path_to_shp: str | Path) -> dict:
     """
     Takes path to shapefile (shp) and returns relevant information, such as:
         - shp name
@@ -165,6 +64,14 @@ def _parse_path_to_shp(path_to_shp: str | Path) -> Shapefile:
 
     _validate_shp_input(path_to_shp)
 
+    output = {
+        "name": str,
+        "is_zipped": bool,
+        "shp_dir": Path | None,
+        "zip_path": Path | None,
+        "subdir": str | None,
+    }
+
     zip_indicator = "zip://"
     end_of_zip_delimiter = ".zip!"
 
@@ -174,6 +81,7 @@ def _parse_path_to_shp(path_to_shp: str | Path) -> Shapefile:
         start_path_idx = len(zip_indicator)
 
         # Get zip path -------------------------
+        # TODO - remove conditional here - redundant when we're running validator func already
         if end_of_zip_delimiter in path_to_shp:
             end_of_zip_idx = path_to_shp.find(end_of_zip_delimiter) + (
                 len(end_of_zip_delimiter) - 1
@@ -186,16 +94,186 @@ def _parse_path_to_shp(path_to_shp: str | Path) -> Shapefile:
         if len(path_in_zip_to_shp.parts) > 1:
             subdir = str(path_in_zip_to_shp.parent)
 
-        return ShapefileZipped(
-            shp_name=path_in_zip_to_shp.name,
-            _zip_path=Path(zip_path),
-            _subdir=subdir,
-        )
+        output["name"] = path_in_zip_to_shp.name
+        output["is_zipped"] = True
+        output["shp_dir"] = None
+        output["zip_path"] = zip_path
+        output["subdir"] = subdir
+
+        # return ShapefileZipped(
+        #     shp_name=path_in_zip_to_shp.name,
+        #     _zip_path=Path(zip_path),
+        #     _subdir=subdir,
+        # )
     else:
-        return ShapefileNonZipped(
-            shp_name=Path(path_to_shp).name,
-            _shp_dir=Path(path_to_shp).parent,
+        output["name"] = Path(path_to_shp).name
+        output["is_zipped"] = False
+        output["shp_dir"] = Path(path_to_shp).parent
+        output["zip_path"] = None
+        output["subdir"] = None
+
+        # return ShapefileNonZipped(
+        #     shp_name=Path(path_to_shp).name,
+        #     _shp_dir=Path(path_to_shp).parent,
+        # )
+    return output
+
+
+class _FileManager:
+    def __init__(self, attributes):
+        self.attributes = attributes
+        self.path = Path(attributes["shp_dir"])
+        self.filename = attributes["name"]
+
+    def write_file(self, filename, contents: str):
+        with open(self.path / filename, "w") as f:
+            f.write(contents)
+
+    def remove_file(self, filename):
+        return
+
+    # def list_all_files(self) -> list[str]:
+    #     return [str(f.name) for f in self.shp_dir.iterdir()]
+
+    def read_file(self, filename):
+        with open(self.path / filename, "r") as f:
+            return f.read()
+
+
+@dataclass
+class _FileManagerZipped:
+    def __init__(self, attributes: dict):
+        self.attributes = attributes
+        self.zip_path = Path(attributes["zip_path"])  # ends in .zip
+        self.zip_subdir = attributes["subdir"]
+
+    def read_file(self, filename):
+        return
+
+    def write_file(self, filename, contents):
+        return
+
+
+class Shapefile:
+    def __init__(self, input_string: str | Path):
+        values: dict = _parse_path_to_shp(input_string)
+
+        self.input_string = input_string
+        self.name: str = values["name"]
+        self.is_zipped = values["is_zipped"]
+        self.shp_dir = values["shp_dir"]
+        self.zip_path = values["zip_path"]
+        self.subdir = values["subdir"]
+        self.file_manager = (
+            _FileManagerZipped(values) if self.is_zipped else _FileManager(values)
         )
+
+    def write_metadata(self, contents: str):
+        self.file_manager.write_file(f"{self.name}.xml", contents)
+
+    def read_metadata(self):
+        return self.file_manager.read_file(f"{self.name}.xml")
+
+
+# @dataclass
+# class Shapefile:
+#     shp_name: str
+
+# def _get_metadata_xml_name(self, file_list: list[str]) -> Optional[str]:
+#     """Extracts name of metadata .xml file matching specified .shp file from a
+#     list of filenames. Does not require an actual directory/zip location. Only
+#     parses a list of names as produced by other functions.
+#     Handles cases where metadata ends with either ".xml" or ".shp.xml"
+
+#     Args:
+#         file_list (list[str]): List of file names to parse
+#         shp_name (str): Shapefile name, not path, ending with ".shp"
+
+#     Raises:
+#         ValueError: Error if more than one .xml file matching the pattern are found.
+
+#     Returns:
+#         Optional[str]: Either name of metadata file, or None if no file is found
+#     """
+#     shp_stem = Path(self.shp_name).stem
+#     file_names = [Path(f).name for f in file_list]
+#     matched_names = [
+#         f for f in file_names if f in {f"{shp_stem}.shp.xml", f"{shp_stem}.xml"}
+#     ]
+#     match len(matched_names):
+#         case 0:
+#             return None
+#         case 1:
+#             return matched_names[0]
+#         case _:
+#             raise ValueError(
+#                 f"Expected a single xml with name '{shp_stem}', but found {len(matched_names)}"
+#             )
+
+#     def _write_file(self, filename, contents):
+#         raise NotImplementedError("Not implemented on base class")
+
+#     def _remove_file(self, filename):
+#         raise NotImplementedError("Not implemented on base class")
+
+#     def _list_all_files(self):
+#         raise NotImplementedError("Not implemented on base class")
+
+#     def _read_file(self, filename) -> str:
+#         raise NotImplementedError("Not implemented on base class")
+
+
+# @dataclass
+# class ShapefileNonZipped(Shapefile):
+#     _shp_dir: Path
+
+#     def _write_file(self, filename, contents):
+#         with open(Path(self._shp_dir) / filename, "w") as xml_file:
+#             xml_file.write(contents)
+
+#     def _remove_file(self, filename):
+#         return
+
+#     def _list_all_files(self) -> list[str]:
+#         return [str(f.name) for f in self._shp_dir.iterdir()]
+
+#     def _read_file(self, filename) -> str:
+#         path_to_xml = Path(self._shp_dir) / filename
+#         with open(path_to_xml, "r") as f:
+#             return f.read()
+
+
+# @dataclass
+# class ShapefileZipped(Shapefile):
+#     _zip_path: Path
+#     _subdir: str | None
+
+#     def _write_file(self, filename, contents):
+#         with ZipFile(self._zip_path, "a", compression=ZIP_DEFLATED) as shp:
+#             shp.writestr(filename, contents)
+
+#     def _remove_file(self, filename):
+#         if self._subdir:
+#             filename = Path(self._subdir) / filename
+
+#         zip_file = Path(self._zip_path)
+#         temp_zip = zip_file.parent / (zip_file.name + ".tmp")
+#         with ZipFile(zip_file, "r") as old_zip:
+#             with ZipFile(temp_zip, "w", compression=ZIP_DEFLATED) as new_zip:
+#                 for item in old_zip.infolist():
+#                     if item.filename != filename:
+#                         data = old_zip.read(item.filename)
+#                         new_zip.writestr(item, data)
+#         os.replace(temp_zip, zip_file)
+
+#     def _list_all_files(self) -> list[str]:
+#         with ZipFile(self._zip_path, "r") as archive:
+#             return archive.namelist()
+
+#     def _read_file(self, filename) -> str:
+#         with ZipFile(self._zip_path, "r") as zf:
+#             metadata = zf.read(filename).decode(encoding="utf-8")
+#             return metadata
 
 
 def write_metadata(
