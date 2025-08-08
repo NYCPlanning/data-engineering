68b83f0d4008902fd052b4169061ac9f800d490e
jackrosacker <jack.rosacker@gmail.com>
2025-08-08 11:32:38 -0400

    post-review: parametrize tests

--- dcpy/test/utils/geospatial/test_shapefile.py
+++ dcpy/test/utils/geospatial/test_shapefile.py
@@ -61,13 +61,56 @@ def temp_xml_string(utils_resources_path):
     return xml_output
 
 
-def test_add_metadata_to_shp_no_existing_metadata(
-    temp_shp_zip_no_md_path, temp_nonzipped_shp_no_md_path, temp_xml_string
-):
-    path = temp_shp_zip_no_md_path
-    shp_name = temp_shp_zip_no_md_path.stem
+def _get_info_from_file_fixture(
+    request: pytest.FixtureRequest, fixture: str, file_type: str
+) -> dict:
+    """Calculate path and shp name for a given shapefile fixture.
+    Calculation differs between zipped and non-zipped fixtures.
 
-    shp = shapefile.from_path(path, shp_name)
+    Args:
+        request (pytest.FixtureRequest):
+        fixture (str): fixture name
+        file_type (str): type of fixture - either "zip" or "nonzip"
+
+    Returns:
+        dict: path and shapefile name for given fixture
+    """
+    if file_type not in ["zip", "nonzip"]:
+        raise Exception(f"Type: {file_type} is an ")
+    elif file_type == "zip":
+        path = request.getfixturevalue(fixture)  # Retrieve fixture by name
+        shp_name = path.stem
+    elif file_type == "nonzip":
+        path_fixture = request.getfixturevalue(fixture)
+        path = path_fixture.parent  # Retrieve fixture by name
+        shp_name = path_fixture.name
+    return {"path": path, "shp_name": shp_name}
+
+
+@pytest.mark.parametrize(
+    "path_fixture, file_type, subdir",
+    [
+        pytest.param(
+            "temp_shp_zip_no_md_path",
+            "zip",
+            None,
+            id="add_md_to_zip_shp_w_no_md",
+        ),
+        pytest.param(
+            "temp_nonzipped_shp_no_md_path",
+            "nonzip",
+            None,
+            id="add_md_to_nonzip_shp_w_no_md",
+        ),
+    ],
+)
+def test_add_metadata_to_shp_no_existing_metadata(
+    request, path_fixture, file_type, subdir, temp_xml_string
+):
+    fixture_info = _get_info_from_file_fixture(
+        request, fixture=path_fixture, file_type=file_type
+    )
+    shp = shapefile.from_path(fixture_info["path"], fixture_info["shp_name"], subdir)
     assert not shp.metadata_exists(), "Expected no metadata, but found some"
     shp.write_metadata(
         metadata=temp_xml_string,
@@ -75,88 +118,116 @@ def test_add_metadata_to_shp_no_existing_metadata(
     assert shp.metadata_exists(), "Expected metadata, but found none"
 
 
+@pytest.mark.parametrize(
+    "path_fixture, file_type, subdir",
+    [
+        pytest.param(
+            "temp_shp_zip_with_md_path",
+            "zip",
+            None,
+            id="overwrite_existing_md_in_zip_shp",
+        ),
+        pytest.param(
+            "temp_nonzipped_shp_with_md_path",
+            "nonzip",
+            None,
+            id="overwrite_existing_md_in_nonzip_shp",
+        ),
+    ],
+)
 def test_overwrite_existing_shp_metadata(
-    temp_shp_zip_with_md_path, temp_nonzipped_shp_with_md_path, temp_xml_string
+    request, path_fixture, file_type, subdir, temp_xml_string
 ):
-    path = temp_shp_zip_with_md_path
-    shp_name = temp_shp_zip_with_md_path.stem
-
-    shp = shapefile.from_path(path, shp_name)
+    fixture_info = _get_info_from_file_fixture(
+        request, fixture=path_fixture, file_type=file_type
+    )
+    shp = shapefile.from_path(fixture_info["path"], fixture_info["shp_name"], subdir)
     assert shp.metadata_exists(), "Expected metadata, but found none"
     shp.write_metadata(metadata=temp_xml_string, overwrite=True)
     assert shp.metadata_exists(), "Expected metadata, but found none"
 
 
+@pytest.mark.parametrize(
+    "path_fixture, file_type, subdir",
+    [
+        pytest.param(
+            "temp_shp_zip_with_md_path",
+            "zip",
+            None,
+            id="dont_overwrite_existing_md_in_zip_shp",
+        ),
+        pytest.param(
+            "temp_nonzipped_shp_with_md_path",
+            "nonzip",
+            None,
+            id="dont_overwrite_existing_md_in_nonzip_shp",
+        ),
+    ],
+)
 def test_dont_overwrite_existing_shp_metadata(
-    temp_shp_zip_with_md_path, temp_nonzipped_shp_with_md_path, temp_xml_string
+    request, path_fixture, file_type, subdir, temp_xml_string
 ):
-    path = temp_shp_zip_with_md_path
-    shp_name = temp_shp_zip_with_md_path.stem
+    fixture_info = _get_info_from_file_fixture(
+        request, fixture=path_fixture, file_type=file_type
+    )
 
     with pytest.raises(
         FileExistsError,
         match="Metadata XML already exists, and overwrite is False. Nothing will be written",
     ):
-        shp = shp = shapefile.from_path(path, shp_name)
+        shp = shapefile.from_path(
+            fixture_info["path"], fixture_info["shp_name"], subdir
+        )
         shp.write_metadata(
             metadata=temp_xml_string,
         )
 
 
-def test_metadata_exists(
-    temp_shp_zip_no_md_path,
-    temp_shp_zip_with_md_path,
-    temp_nonzipped_shp_no_md_path,
-    temp_nonzipped_shp_with_md_path,
-):
-    path = temp_shp_zip_no_md_path
-    shp_name = temp_shp_zip_no_md_path.stem
-    print(f"{temp_shp_zip_no_md_path=}\n{path=}\n{shp_name=}\n")
-    shp = shapefile.from_path(path, shp_name)
+@pytest.mark.parametrize(
+    "path_fixture, file_type, subdir",
+    [
+        pytest.param(
+            "temp_shp_zip_no_md_path",
+            "zip",
+            None,
+            id="md_not_exists_in_zip_shp",
+        ),
+        pytest.param(
+            "temp_nonzipped_shp_no_md_path",
+            "nonzip",
+            None,
+            id="md_not_exists_in_nonzip_shp",
+        ),
+    ],
+)
+def test_metadata_not_exists(request, path_fixture, file_type, subdir):
+    fixture_info = _get_info_from_file_fixture(
+        request, fixture=path_fixture, file_type=file_type
+    )
+    shp = shapefile.from_path(fixture_info["path"], fixture_info["shp_name"], subdir)
     assert not shp.metadata_exists(), "Expected no metadata, but found some"
 
-    path = temp_shp_zip_with_md_path
-    shp_name = temp_shp_zip_with_md_path.stem
-    print(f"{temp_shp_zip_with_md_path=}\n{path=}\n{shp_name=}\n")
-    shp = shapefile.from_path(path, shp_name)
+
+@pytest.mark.parametrize(
+    "path_fixture, file_type, subdir",
+    [
+        pytest.param(
+            "temp_shp_zip_with_md_path",
+            "zip",
+            None,
+            id="md_exists_in_zip_shp",
+        ),
+        pytest.param(
+            "temp_nonzipped_shp_with_md_path",
+            "nonzip",
+            None,
+            id="md_exists_in_nonzip_shp",
+        ),
+    ],
+)
+def test_metadata_exists(request, path_fixture, file_type, subdir):
+    fixture_info = _get_info_from_file_fixture(
+        request, fixture=path_fixture, file_type=file_type
+    )
+    shp = shapefile.from_path(fixture_info["path"], fixture_info["shp_name"], subdir)
     assert shp.metadata_exists(), "Expected metadata, but found none"
-
-    path = temp_nonzipped_shp_no_md_path.parent
-    shp_name = temp_nonzipped_shp_no_md_path.name
-    print(f"{temp_nonzipped_shp_no_md_path=}\n{path=}\n{shp_name=}\n")
-    shp = shapefile.from_path(path, shp_name)
-    assert not shp.metadata_exists(), "Expected no metadata, but found some"
-
-    path = temp_nonzipped_shp_with_md_path.parent
-    shp_name = temp_nonzipped_shp_with_md_path.name
-    print(f"{temp_nonzipped_shp_with_md_path=}\n{path=}\n{shp_name=}\n")
-    shp = shapefile.from_path(path, shp_name)
-    assert shp.metadata_exists(), "Expected metadata, but found none"
-
-    # test_cases = [
-    #     {
-    #         "path": f"zip://{temp_shp_zip_with_md_path}!{temp_shp_zip_with_md_path.stem}",
-    #         "bool": True,
-    #         "msg": "Expected metadata, but found none",
-    #     },
-    #     {
-    #         "path": f"zip://{temp_shp_zip_no_md_path}!{temp_shp_zip_no_md_path.stem}",
-    #         "bool": False,
-    #         "msg": "Expected no metadata, but found some",
-    #     },
-    #     {
-    #         "path": temp_nonzipped_shp_with_md_path,
-    #         "bool": True,
-    #         "msg": "Expected metadata, but found none",
-    #     },
-    #     {
-    #         "path": temp_nonzipped_shp_no_md_path,
-    #         "bool": True,
-    #         "msg": "Expected no metadata, but found some",
-    #     },
-    # ]
-    # for test_case in test_cases:
-    #     assert (
-    #         shapefile.metadata_exists(path_to_shp=test_case["path"])
-    #         is test_case["bool"]
-    #     ), test_case["msg"]
