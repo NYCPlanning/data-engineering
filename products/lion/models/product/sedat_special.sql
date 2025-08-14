40135f6f6e04c102e8ffcdb9ad089330d73eab18
harrrisw <122818174+harrrisw@users.noreply.github.com>
2025-08-14 16:14:13 -0400

    fixed syntax

--- products/lion/models/product/sedat_special.sql
+++ products/lion/models/product/sedat_special.sql
@@ -9,16 +9,16 @@ WITH int_special_sedat AS (
 
 SELECT 
     CONCAT(
-        LPAD(COALESCE(LIONKEY, ''), 10, ' '),     
-        SUBSTR(COALESCE(PARITY, ' '), 1, 1),     
-        RPAD(COALESCE(STREET_NAME, ''), 32, ' '),     
-        SUBSTR(COALESCE(SIDE_OF_STREET, ' '), 1, 1), 
-        LPAD(COALESCE(LOWADDRESS, ''), 7, ' '),             
-        RPAD(COALESCE(LOW_ADDR_SUFFIX, ''), 8, ' '),        
-        LPAD(COALESCE(HIGHADDRESS, ''), 7, ' '),           
-        RPAD(COALESCE(HIGH_ADDR_SUFFIX, ''), 8, ' '),       
-        LPAD(COALESCE(ELECTION_DISTRICT, ''), 3, '0'),       
-        LPAD(COALESCE(ASSEMBLY_DISTRICT, ''), 2, '0'),       
-        SUBSTR(RPAD(COALESCE(B7SC, ''), 8, ' '), 1, 8)                    
+        SUBSTR(LPAD(COALESCE(LIONKEY, ''), 10, ' '), 1, 10),    
+        SUBSTR(COALESCE(PARITY, ' '), 1, 1),                    
+        SUBSTR(RPAD(COALESCE(STREET_NAME, ''), 32, ' '), 1, 32),   
+        SUBSTR(COALESCE(SIDE_OF_STREET, ' '), 1, 1),          
+        SUBSTR(LPAD(COALESCE(LOWADDRESS, ''), 7, ' '), 1, 7),    
+        SUBSTR(RPAD(COALESCE(LOW_ADDR_SUFFIX, ''), 8, ' '), 1, 8),    
+        SUBSTR(LPAD(COALESCE(HIGHADDRESS, ''), 7, ' '), 1, 7),     
+        SUBSTR(RPAD(COALESCE(HIGH_ADDR_SUFFIX, ''), 8, ' '), 1, 8),   
+        SUBSTR(LPAD(COALESCE(ELECTION_DISTRICT, ''), 3, ' '), 1, 3),   
+        SUBSTR(LPAD(COALESCE(ASSEMBLY_DISTRICT, ''), 2, ' '), 1, 2),   
+        SUBSTR(RPAD(COALESCE(B7SC, ''), 8, ' '), 1, 8)                  
     ) as SPECIAL_SEDAT_RECORD
 FROM int_special_sedat
