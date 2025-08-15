b4ea21300a151d6d903a9ce9e404d87aca6edc40
harrrisw <122818174+harrrisw@users.noreply.github.com>
2025-08-15 12:22:57 -0400

    syntax

--- products/lion/models/product/sedat_special.sql
+++ products/lion/models/product/sedat_special.sql
@@ -3,18 +3,18 @@
 ) }}
 
 SELECT
-   CONCAT(
-       format_lion_text(lionkey, 10, ' '),
-       COALESCE(parity, ' '),
-       format_lion_text(street_name, 32, ' ', FALSE, TRUE),
-       COALESCE(side_of_street, ' '),
-       format_lion_text(lowaddress, 7, ' '),
-       format_lion_text(low_addr_suffix, 8, ' ', FALSE, TRUE),
-       format_lion_text(highaddress, 7, ' '),
-       format_lion_text(high_addr_suffix, 8, ' ', FALSE, TRUE),
-       format_lion_text(election_district, 3, '0'),
-       format_lion_text(assembly_district, 2, '0'),
-       format_lion_text(b7sc, 8, ' ', FALSE, TRUE)
-   ) AS special_sedat_record
+    CONCAT(
+        FORMAT_LION_TEXT(lionkey, 10, ' '),
+        COALESCE(parity, ' '),
+        FORMAT_LION_TEXT(street_name, 32, ' ', FALSE, TRUE),
+        COALESCE(side_of_street, ' '),
+        FORMAT_LION_TEXT(lowaddress, 7, ' '),
+        FORMAT_LION_TEXT(low_addr_suffix, 8, ' ', FALSE, TRUE),
+        FORMAT_LION_TEXT(highaddress, 7, ' '),
+        FORMAT_LION_TEXT(high_addr_suffix, 8, ' ', FALSE, TRUE),
+        FORMAT_LION_TEXT(election_district, 3, '0'),
+        FORMAT_LION_TEXT(assembly_district, 2, '0'),
+        FORMAT_LION_TEXT(b7sc, 8, ' ', FALSE, TRUE)
+    ) AS special_sedat_record
 FROM {{ ref("int__special_sedat") }}
 WHERE missing_street_name = FALSE
