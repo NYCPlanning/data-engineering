da0c6858f47f20a1f04d99818fc16328755e0e44
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-09-10 09:33:27 -0400

    sqlfluff

--- products/cbbr/sql/export.sql
+++ products/cbbr/sql/export.sql
@@ -131,7 +131,7 @@ SELECT
     END AS supporters_2,
     agency_acronym,
     agency,
-    substring(agency_category_response from '^[^;]*') AS agency_category_response,
+    substring(agency_category_response FROM '^[^;]*') AS agency_category_response,
     agency_response,
     geo_function,
     geom
