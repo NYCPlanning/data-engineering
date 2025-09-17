042aa303cc74ac717efb625da7280da1491fba4c
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-09-16 21:57:26 -0400

    f

--- products/pluto/pluto_build/sql/create_rpad_geo.sql
+++ products/pluto/pluto_build/sql/create_rpad_geo.sql
@@ -1,7 +1,7 @@
 -- getting distinct BBLs FROM raw data
 ALTER TABLE pluto_input_geocodes RENAME bbl TO geo_bbl;
-ALTER TABLE pluto_input_geocodes ADD COLUMN xcoord integer;
-ALTER TABLE pluto_input_geocodes ADD COLUMN ycoord integer;
+ALTER TABLE pluto_input_geocodes ADD COLUMN xcoord text;
+ALTER TABLE pluto_input_geocodes ADD COLUMN ycoord text;
 
 UPDATE pluto_input_geocodes
 SET
