cec843edfda3c314b5d658419dd744851f0e75f2
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-09-17 17:45:56 -0400

    chk

--- dcpy/lifecycle/ingest/configure.py
+++ dcpy/lifecycle/ingest/configure.py
@@ -1,21 +1,18 @@
-from datetime import datetime
 import jinja2
 from jinja2 import meta
 from pathlib import Path
 import yaml
 
-from dcpy.models.lifecycle.ingest import (
-    ArchivalMetadata,
-    Ingestion,
+from dcpy.models.lifecycle.ingest.definitions import (
     Source,
-    ProcessingStep,
-    Template,
-    Config,
+    DatasetDefinitionSimple,
+    DatasetDefinitionOneToMany,
+    DatasetTransformation,
 )
-from dcpy.utils import metadata
+from dcpy.models.lifecycle.ingest.configuration import ResolvedConfig
 from dcpy.utils.logging import logger
 from dcpy.lifecycle.ingest.connectors import source_connectors
-from dcpy.lifecycle.ingest.validate import find_template_validation_errors
+# from dcpy.lifecycle.ingest.validate import find_template_validation_errors
 
 
 def get_jinja_vars(s: str) -> set[str]:
@@ -25,15 +22,14 @@ def get_jinja_vars(s: str) -> set[str]:
     return meta.find_undeclared_variables(parsed_content)
 
 
-def read_template(
-    dataset_id: str, template_dir: Path, version: str | None = None
-) -> Template:
+def read_definition(
+    filepath: Path, version: str | None = None
+) -> DatasetDefinitionSimple | DatasetDefinitionOneToMany:
     """
     Given _id id, read yml template in template_dir of given dataset
     and insert version as jinja var if provided.
     """
-    file = template_dir / f"{dataset_id}.yml"
-    with open(file, "r") as f:
+    with open(filepath, "r") as f:
         template_string = f.read()
     vars = get_jinja_vars(template_string)
     if vars == {"version"}:
@@ -43,88 +39,68 @@ def read_template(
             f"'version' is only suppored jinja var. Vars in template: {vars}"
         )
     template_yml = yaml.safe_load(template_string)
-    return Template(**template_yml)
+    if "datasets" in template_yml:
+        return DatasetDefinitionOneToMany(**template_yml)
+    else:
+        return DatasetDefinitionSimple(**template_yml)
 
 
-def get_version(source: Source, timestamp: datetime):
+def get_version(source: Source) -> str | None:
     connector = source_connectors[source.type]
-    version = connector.get_latest_version(**source.model_dump())
-    return version or timestamp.strftime("%Y%m%d")
+    if "get_latest_version" in connector.__dir__():
+        return connector.get_latest_version(**source.model_dump())
+    else:
+        return None
 
 
-def determine_processing_steps(
-    steps: list[ProcessingStep],
-    *,
-    target_crs: str | None,
-    mode: str | None,
-) -> list[ProcessingStep]:
-    # TODO default steps like this should probably be configuration
-    step_names = {p.name for p in steps}
-
-    if target_crs and "reproject" not in step_names:
-        reprojection = ProcessingStep(name="reproject", args={"target_crs": target_crs})
-        steps = [reprojection] + steps
-
-    if mode:
-        modes = {s.mode for s in steps}
-        if mode not in modes:
-            raise ValueError(f"mode '{mode}' is not present in template")
-
-    steps = [s for s in steps if s.mode is None or s.mode == mode]
-
-    return steps
-
-
-def get_config(
+def get_resolved_config(
     dataset_id: str,
     version: str | None = None,
     *,
-    mode: str | None = None,
     template_dir: Path,
     local_file_path: Path | None = None,
-) -> Config:
+) -> ResolvedConfig:
     """Generate config object for dataset and optional version"""
-    run_details = metadata.get_run_details()
+    definition_filepath = template_dir / f"{dataset_id}.yml"
 
-    logger.info(f"Reading template from {template_dir / dataset_id}.yml")
-    template = read_template(dataset_id, version=version, template_dir=template_dir)
-    version = version or get_version(template.ingestion.source, run_details.timestamp)
-    template = read_template(dataset_id, version=version, template_dir=template_dir)
+    logger.info(f"Reading template from {definition_filepath}")
+    template = read_definition(definition_filepath, version=version)
+    version = version or get_version(template.source)
+    template = read_definition(definition_filepath, version=version)
 
-    violations = find_template_validation_errors(template)
-    if violations:
-        raise ValueError(f"Template violations found: {violations}")
+    # violations = find_template_validation_errors(template)
+    # if violations:
+    #    raise ValueError(f"Template violations found: {violations}")
 
-    if local_file_path:
-        template.ingestion.source = Source(type="local_file", key=str(local_file_path))
-
-    processing_steps = determine_processing_steps(
-        template.ingestion.processing_steps,
-        target_crs=template.ingestion.target_crs,
-        mode=mode,
+    source = (
+        Source(type="local_file", key=str(local_file_path))
+        if local_file_path
+        else template.source
     )
 
-    ingestion = Ingestion(
-        target_crs=template.ingestion.target_crs,
-        source=template.ingestion.source,
-        file_format=template.ingestion.file_format,
-        processing_mode=mode,
-        processing_steps=processing_steps,
-    )
+    match template:
+        case DatasetDefinitionSimple():
+            args = template.ingestion.model_dump()
+            args.pop("source")
+            transformations = [
+                DatasetTransformation(
+                    id=template.id,
+                    acl=template.acl,
+                    attributes=template.attributes,
+                    **args,
+                )
+            ]
 
-    archival = ArchivalMetadata(
-        archival_timestamp=run_details.timestamp,
-        raw_filename="",  # TODO - this is now set after pulling file
-        acl=template.acl,
-    )
+        case DatasetDefinitionOneToMany():
+            transformations = template.datasets
 
-    return Config(
+        case _:
+            raise Exception("Unknown definition format")
+
+    return ResolvedConfig(
         id=template.id,
-        version=version,
-        crs=ingestion.target_crs,
+        acl=template.acl,
         attributes=template.attributes,
-        archival=archival,
-        ingestion=ingestion,
-        columns=template.columns,
-        run_details=run_details,
+        source=source,
+        transformation=transformations,
     )
