ed03e318789a3e2d920eead2cd0e3d1b22ea2b1f
Damon McCullough <damonmcc1391@gmail.com>
2025-09-29 11:22:06 -0400

    migrate `doe_school_subdistricts` to ingest

--- products/knownprojects/sql/aggregate/preprocessing.sql
+++ products/knownprojects/sql/aggregate/preprocessing.sql
@@ -1,7 +1,7 @@
 -- Create standardization of tables for sca aggregate scripts
 
 ALTER TABLE doe_eszones RENAME COLUMN wkb_geometry TO geometry;
-ALTER TABLE doe_school_subdistricts RENAME COLUMN wkb_geometry TO geometry;
+ALTER TABLE doe_school_subdistricts RENAME COLUMN geom TO geometry;
 ALTER TABLE dcp_school_districts RENAME COLUMN wkb_geometry TO geometry;
 
 -- Create standardization of tables for other aggregate scripts 
