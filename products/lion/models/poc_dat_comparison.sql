df25f812cf6368840c9729bdd3f911a12fe1d42e
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-10-01 13:54:25 -0400

    include row counts in poc comp

--- products/lion/models/poc_dat_comparison.sql
+++ products/lion/models/poc_dat_comparison.sql
@@ -8,10 +8,16 @@ production_lion AS (
     SELECT * FROM {{ source('production_outputs', 'citywide_lion') }}
 )
 SELECT
-{% for col in adapter.get_columns_in_relation(ref('lion_dat_fields')) -%}
+    count(*) FILTER (WHERE dev."Segment ID" IS NOT NULL) AS total_dev_rows,
+    count(*) FILTER (WHERE prod."Segment ID" IS NOT NULL) AS total_prod_rows,
+    count(*) FILTER (WHERE dev."Segment ID" IS NOT NULL AND prod."Segment ID" IS NOT NULL) AS keys_in_both,
+    count(*) FILTER (WHERE prod."Segment ID" IS NULL) AS missing_key_in_production,
+    count(*) FILTER (WHERE dev."Segment ID" IS NULL) AS missing_key_in_dev
+{%- for col in adapter.get_columns_in_relation(ref('lion_dat_fields')) -%}
+    {%- if loop.first -%},{% endif %}
     count(*) FILTER (WHERE dev."{{ col.column }}" <> prod."{{ col.column }}") AS "{{ col.column }}"
     {%- if not loop.last -%},{% endif %}
-{% endfor -%}
+{%- endfor %}
 FROM lion_dat_fields AS dev
 INNER JOIN production_lion AS prod
     ON
