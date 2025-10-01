e08b73546d7d2a78adda30ab9a3fa973f1fdd0c6
Alex Richey <arichey@planning.nyc.gov>
2025-10-01 15:01:39 -0400

    Delete recipe logging functionality

--- apps/qa/src/pages/source_data/helpers.py
+++ apps/qa/src/pages/source_data/helpers.py
@@ -3,7 +3,6 @@ from typing import Dict
 
 from src import ROOT_PATH, APP_PATH
 
-from dcpy.connectors.edm import recipes
 from dcpy.models import library
 from dcpy.lifecycle.builds import plan
 
@@ -14,19 +13,25 @@ def get_source_datasets(product: str) -> list[str]:
     return [dataset.id for dataset in recipe.inputs.datasets]
 
 
+# Commenting all this out until we figure out what we want to do
+# around logging
+
+
 def get_dataset_metadata(product: str) -> Dict[str, list[library.ArchivalMetadata]]:
-    datasets = get_source_datasets(product)
-    metadata_df = recipes.get_logged_metadata(datasets)
-    metadata: Dict[str, list[library.ArchivalMetadata]] = {}
-    for index, row in metadata_df.iterrows():  ## group by
-        m = library.ArchivalMetadata(**row.to_dict())
-        dataset = row["name"]
-        if dataset not in metadata:
-            metadata[dataset] = []
-        metadata[dataset].append(m)
-    return metadata
+    return {}
+    # datasets = get_source_datasets(product)
+    # metadata_df = recipes.get_logged_metadata(datasets)
+    # metadata: Dict[str, list[library.ArchivalMetadata]] = {}
+    # for index, row in metadata_df.iterrows():  ## group by
+    #     m = library.ArchivalMetadata(**row.to_dict())
+    #     dataset = row["name"]
+    #     if dataset not in metadata:
+    #         metadata[dataset] = []
+    #     metadata[dataset].append(m)
+    # return metadata
 
 
 def scrape_metadata(product: str):
-    for dataset in get_source_datasets(product):
-        recipes.scrape_metadata(dataset)
+    pass
+    # for dataset in get_source_datasets(product):
+    #     recipes.scrape_metadata(dataset)
