c2227594d28f947aedfd79a5e2528482b4c107fa
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-10-01 11:45:52 -0400

    RENAME/reorganize product models

--- products/lion/models/product/lion/lion_dat.sql
+++ products/lion/models/product/lion/lion_dat.sql
@@ -1,7 +1,7 @@
 -- create a single column in order to export a text file without a delimeter or header
 -- NOTE the row length may vary by borough until field lengths are tested in lion_dat
 WITH lion AS (
-    SELECT * FROM {{ ref('lion_dat_fields') }}
+    SELECT * FROM {{ ref('lion_dat_by_field') }}
 )
 SELECT rtrim(ltrim(replace(replace(lion::text, ',', ''), '"', ''), '('), ')') AS dat_column
 FROM lion
