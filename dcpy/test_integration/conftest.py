8f7bb5fa5a33b7de7f04bc694f2f341a1de97106
Alex Richey <arichey@planning.nyc.gov>
2025-10-01 15:21:55 -0400

    Move integration conftest up a dir level
    These variables need to be set before anything from dcpy is imported.

--- dcpy/test_integration/conftest.py
+++ dcpy/test_integration/conftest.py
@@ -1,3 +1,20 @@
 from pathlib import Path
+import os
+
 
 RESOURCES_DIR = Path(__file__).parent / "resources"
+
+# Connector buckets
+TEST_EDM_BUCKET = "test-recipes"
+
+os.environ["RECIPES_BUCKET"] = TEST_EDM_BUCKET
+os.environ["PUBLISHING_BUCKET"] = TEST_EDM_BUCKET
+
+# Build Engine setup
+BUILD_ENGINE_SCHEMA = "connectors_edm_tests"
+os.environ["BUILD_ENGINE_SCHEMA"] = BUILD_ENGINE_SCHEMA
+os.environ["BUILD_ENGINE_DB"] = "postgres"
+os.environ["BUILD_ENGINE_SERVER"] = "postgresql://postgis"
+
+os.environ["PGUSER"] = "postgres"
+os.environ["PGPASSWORD"] = "postgres"
