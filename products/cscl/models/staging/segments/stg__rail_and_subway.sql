be5d86088219fe0bc89d131396e6c6300a7ff6aa
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-11-13 14:58:08 -0500

    move feature type code upstream

--- products/cscl/models/staging/segments/stg__rail_and_subway.sql
+++ products/cscl/models/staging/segments/stg__rail_and_subway.sql
@@ -5,14 +5,30 @@
 
 SELECT
     segmentid,
-    row_type,
-    'subway' AS feature_type,
-    row_type NOT IN ('1', '8') AS include_in_geosupport_lion
-FROM {{ source("recipe_sources", "dcp_cscl_subway") }}
-UNION
+    row_type AS right_of_way_type,
+    segment_seqnum,
+    legacy_segmentid,
+    from_level_code,
+    to_level_code,
+    '1' AS feature_type_code,
+    'rail_and_subway' AS feature_type,
+    'rail' AS source_table,
+    row_type NOT IN ('1', '8') AS include_in_geosupport_lion,
+    geom,
+    shape_length
+FROM {{ source("recipe_sources", "dcp_cscl_rail") }}
+UNION ALL
 SELECT
     segmentid,
-    row_type,
-    'rail' AS feature_type,
-    row_type NOT IN ('1', '8') AS include_in_geosupport_lion
-FROM {{ source("recipe_sources", "dcp_cscl_rail") }}
+    row_type AS right_of_way_type,
+    segment_seqnum,
+    legacy_segmentid,
+    from_level_code,
+    to_level_code,
+    '1' AS feature_type_code,
+    'rail_and_subway' AS feature_type,
+    'subway' AS source_table,
+    row_type NOT IN ('1', '8') AS include_in_geosupport_lion,
+    geom,
+    shape_length
+FROM {{ source("recipe_sources", "dcp_cscl_subway") }}
--- products/cscl/models/staging/segments/stg__shoreline.sql
+++ products/cscl/models/staging/segments/stg__shoreline.sql
@@ -4,5 +4,8 @@
 ) }}
 
 SELECT
-    {{ dbt_utils.star(from=source("recipe_sources", "dcp_cscl_shoreline"), except=['geom']) }}
+    *,
+    '2' AS feature_type_code,
+    'shoreline' AS feature_type,
+    'shoreline' AS source_table
 FROM {{ source("recipe_sources", "dcp_cscl_shoreline") }}
