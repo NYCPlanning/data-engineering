337438283d598514fbedc55dc0655d809d6a3e29
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-11-20 13:50:19 -0500

    first pass at lion error logs

---
+++ products/cscl/models/log_files/log__lion_segments_missing_nodes.sql
@@ -0,0 +1,9 @@
+SELECT
+    'segment missing node' AS error,
+    globalid,
+    source_table,
+    'segmentid' AS record_id_type,
+    segmentid AS record_id,
+    '' AS message
+FROM {{ ref('int__lion') }}
+WHERE from_nodeid IS NULL OR to_nodeid IS NULL
