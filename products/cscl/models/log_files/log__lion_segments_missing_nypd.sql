921afac12ed5c5ef8885756fc1f26f8e7c7ea0c2
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-11-28 10:09:03 -0500

    Revert "fix nypdbeat error log"
    This reverts commit ebf58caeaaab46c9d15da17604a079097d500d1b.

--- products/cscl/models/log_files/log__lion_segments_missing_nypd.sql
+++ products/cscl/models/log_files/log__lion_segments_missing_nypd.sql
@@ -1,9 +1,8 @@
 SELECT
     'segment joined to no nypd beat' AS error,
-    segments.globalid,
-    segments.source_table,
+    globalid,
+    source_table,
     'segmentid' AS record_id_type,
-    segments.segmentid AS record_id
-FROM {{ ref('int__segments' ) }} AS segments
-INNER JOIN {{ ref('int__segment_nypdbeat') }} AS beat ON segments.lionkey = beat.lionkey
-WHERE beat.left_nypd_sector IS NULL AND beat.right_nypd_sector IS NULL
+    segmentid AS record_id
+FROM {{ ref('int__lion') }}
+WHERE left_nypd_service_area IS NULL AND right_nypd_service_area IS NULL
