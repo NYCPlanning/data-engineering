d13ab1e805bd24bad6e14d58e46a18485858f3d1
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-12-02 14:28:38 -0500

    create sedat product models with separate fields

--- products/cscl/models/product/sedat/sedat_special.sql
+++ products/cscl/models/product/sedat/sedat_special.sql
@@ -1,20 +1,14 @@
-{{ config(
-   materialized = 'table'
-) }}
-
 SELECT
-    CONCAT(
-        FORMAT_LION_TEXT(lionkey, 10, ' '),
-        COALESCE(parity, ' '),
-        FORMAT_LION_TEXT(street_name, 32, ' ', FALSE, TRUE),
-        COALESCE(side_of_street, ' '),
-        FORMAT_LION_TEXT(lowaddress, 7, ' '),
-        FORMAT_LION_TEXT(low_addr_suffix, 8, ' ', FALSE, TRUE),
-        FORMAT_LION_TEXT(highaddress, 7, ' '),
-        FORMAT_LION_TEXT(high_addr_suffix, 8, ' ', FALSE, TRUE),
-        FORMAT_LION_TEXT(election_district, 3, '0'),
-        FORMAT_LION_TEXT(assembly_district, 2, '0'),
-        FORMAT_LION_TEXT(b7sc, 8, ' ', FALSE, TRUE)
-    ) AS special_sedat_record
+    FORMAT_LION_TEXT(lionkey, 10, ' ') AS lionkey,
+    COALESCE(parity, ' ') AS parity,
+    FORMAT_LION_TEXT(street_name, 32, ' ', FALSE, TRUE) AS street_name,
+    COALESCE(side_of_street, ' ') AS side_of_street,
+    FORMAT_LION_TEXT(lowaddress, 7, ' ') AS lowaddress,
+    FORMAT_LION_TEXT(low_addr_suffix, 8, ' ', FALSE, TRUE) AS low_addr_suffix,
+    FORMAT_LION_TEXT(highaddress, 7, ' ') AS highaddress,
+    FORMAT_LION_TEXT(high_addr_suffix, 8, ' ', FALSE, TRUE) AS high_addr_suffix,
+    FORMAT_LION_TEXT(election_district, 3, '0') AS election_district,
+    FORMAT_LION_TEXT(assembly_district, 2, '0') AS assembly_district,
+    FORMAT_LION_TEXT(b7sc, 8, ' ') AS b7sc
 FROM {{ ref("int__special_sedat") }}
-WHERE missing_street_name = FALSE
+WHERE street_name IS NOT NULL
