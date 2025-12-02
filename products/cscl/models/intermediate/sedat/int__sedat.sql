19aa7766022ae51de29602a0b723e152e26b38a7
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-12-02 14:28:28 -0500

    cleanup int sedat models

--- products/cscl/models/intermediate/sedat/int__sedat.sql
+++ products/cscl/models/intermediate/sedat/int__sedat.sql
@@ -1,54 +1,40 @@
-{{ config(
-    materialized = 'table',
-    indexes=[
-      {'columns': ['segmentid', 'boroughcode']},
-    ]
-) }}
-
 WITH sedat AS (
-    SELECT * FROM {{ source("recipe_sources", "dcp_cscl_sedat") }}
+    SELECT
+        LEFT(lionkey, 1) AS borough_code,
+        *
+    FROM {{ source("recipe_sources", "dcp_cscl_sedat") }}
 ),
-
 preferred_lgc AS (
     SELECT
         segmentid,
         CONCAT(b5sc, lgc) AS b7sc,
-        SUBSTR(b5sc, 1, 1) AS borough_code
+        LEFT(b5sc, 1) AS borough_code
     FROM {{ source("recipe_sources", "dcp_cscl_segment_lgc") }}
     WHERE preferred_lgc_flag = 'Y'
 ),
-
-sedat_with_b7sc AS (
-    SELECT
-        s.*,
-        SUBSTR(s.lionkey, 1, 1) AS boroughcode,
-        pl.b7sc AS preferred_b7sc
-    FROM sedat AS s
-    LEFT JOIN preferred_lgc AS pl
-        ON
-            s.segmentid = pl.segmentid
-            AND SUBSTR(s.lionkey, 1, 1) = pl.borough_code
+streetname AS (
+    SELECT * FROM {{ source("recipe_sources", "dcp_cscl_streetname") }}
+    WHERE principal_flag = 'Y'
 )
-
 SELECT
-    sb.lionkey,
-    sb.parity,
-    sb.segmentid,
-    sb.boroughcode,
-    sn.lookup_key AS street_name,
+    sedat.lionkey,
+    sedat.parity,
+    streetname.lookup_key AS street_name,
     CASE
-        WHEN sb.side = '1' THEN 'L'
-        WHEN sb.side = '2' THEN 'R'
+        WHEN sedat.side = '1' THEN 'L'
+        WHEN sedat.side = '2' THEN 'R'
     END AS side_of_street,
-    sb.lowaddress,
-    sb.low_addr_suffix,
-    sb.highaddress,
-    sb.high_addr_suffix,
-    sb.election_district,
-    sb.assembly_district,
-    sb.preferred_b7sc
-FROM sedat_with_b7sc AS sb
-LEFT JOIN {{ source("recipe_sources", "dcp_cscl_streetname") }} AS sn
+    TRIM(sedat.lowaddress) AS lowaddress,
+    sedat.low_addr_suffix,
+    TRIM(sedat.highaddress) AS highaddress,
+    sedat.high_addr_suffix,
+    sedat.election_district,
+    sedat.assembly_district,
+    preferred_lgc.b7sc
+FROM sedat
+LEFT JOIN preferred_lgc
     ON
-        sb.preferred_b7sc = sn.b7sc
-        AND sn.principal_flag = 'Y'
+        sedat.segmentid = preferred_lgc.segmentid
+        AND sedat.borough_code = preferred_lgc.borough_code
+LEFT JOIN streetname
+    ON preferred_lgc.b7sc = streetname.b7sc
