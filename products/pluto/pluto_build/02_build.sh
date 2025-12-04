418533cc989ad024969bb6351bfa91462830f5ef
Damon McCullough <damonmcc1391@gmail.com>
2025-12-04 16:51:29 -0500

    consolidate bash scripts

--- products/pluto/pluto_build/02_build.sh
+++ products/pluto/pluto_build/02_build.sh
@@ -108,4 +108,44 @@ run_sql_file sql/plutomapid_1.sql
 run_sql_file sql/plutomapid_2.sql
 run_sql_file sql/shorelineclip.sql
 
+run_sql_file sql/corr_create.sql
+
+echo "Applying corrections to PLUTO"
+run_sql_file sql/corr_lotarea.sql
+run_sql_file sql/corr_template.sql -v FIELD=yearbuilt
+run_sql_file sql/corr_template.sql -v FIELD=ownername
+run_sql_file sql/corr_ownername_punctuation.sql
+run_sql_file sql/corr_template.sql -v FIELD=cd
+run_sql_file sql/corr_template.sql -v FIELD=numfloors
+run_sql_file sql/corr_template.sql -v FIELD=numbldgs
+run_sql_file sql/corr_template.sql -v FIELD=unitsres
+run_sql_file sql/corr_template.sql -v FIELD=unitstotal
+run_sql_file sql/corr_inwoodrezoning.sql
+run_sql_file sql/corr_template.sql -v FIELD=bct2020
+run_sql_file sql/corr_template.sql -v FIELD=address
+run_sql_file sql/remove_unitlots.sql
+
+echo "Creating export tables"
+run_sql_file sql/export.sql
+
+run_sql_file \
+  sql/export_mappluto_shp.sql\
+  -v TABLE='mappluto'\
+  -v GEOM='clipped_2263'
+
+run_sql_file \
+  sql/export_mappluto_shp.sql\
+  -v TABLE='mappluto_unclipped'\
+  -v GEOM='geom_2263'
+
+run_sql_file \
+  sql/export_mappluto_gdb.sql\
+  -v TABLE='mappluto_gdb'\
+  -v GEOM='clipped_2263'
+
+run_sql_file \
+  sql/export_mappluto_gdb.sql\
+  -v TABLE='mappluto_unclipped_gdb'\
+  -v GEOM='geom_2263'
+
 echo 'Done'
