a71c486f6442b543e20c15da5330a4b9c860cb1a
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-12-05 11:11:38 -0500

    changed geom column name in priority waterbodies (#2100)

--- products/green_fast_track/models/staging/stg__nysdec_priority_lakes.sql
+++ products/green_fast_track/models/staging/stg__nysdec_priority_lakes.sql
@@ -1,5 +1,5 @@
 WITH clipped_to_nyc AS (
-    {{ clip_to_geom(left=source("recipe_sources", "nysdec_priority_lakes"), left_by="wkb_geometry") }}
+    {{ clip_to_geom(left=source("recipe_sources", "nysdec_priority_lakes")) }}
 )
 
 SELECT
