dcd1ecdcabe91b45d1d30947423f252f545da0b1
Alex Richey <arichey@planning.nyc.gov>
2025-12-11 14:03:12 -0500

    Comment on FAR values

--- products/pluto/pluto_build/sql/far.sql
+++ products/pluto/pluto_build/sql/far.sql
@@ -4,8 +4,18 @@ UPDATE pluto
 SET builtfar = round(bldgarea::numeric / lotarea::numeric, 2)
 WHERE lotarea != '0' AND lotarea IS NOT NULL;
 
--- using dcp_zoning_maxfar maintained by zoning division
--- base on zoning district 1
+-- walks through PLUTO zonedist1, zonedist2, zonedist3 (including split parts like
+-- M1-1A/R6B)
+-- and sets FAR values using dcp_zoning_maxfar maintained by zoning division
+--
+-- Note: the FAR fields in dcp_zoning_maxfar have both nulls and "-".
+-- "-" acts to suppress the FAR value for lesser-ranked zoning districts.
+-- e.g. R8B has commfar of -.
+-- if zonedist1 = R8B/C6-2A, we'd expect a 0 commfar value, as opposed to getting the
+-- nonzero commfar value from C6-2A.
+--
+-- By contrast R10H has null commfar,
+-- so if zonedist1 = R10H/C6-2A, we'd expect to get the commfar value from C6-2A.
 UPDATE pluto a
 SET
     residfar = b.residfar,
@@ -15,7 +25,8 @@ SET
     affresfar = b.affresfar
 FROM dcp_zoning_maxfar AS b
 WHERE a.zonedist1 = b.zonedist;
--- zoning district 1 with / first p art 
+-- zoning district 1 with / first part (Manufacturing)
+-- These are all in the form of a Manufacturing and Residential zone. e.g. M1-1A/R6B
 UPDATE pluto a
 SET
     residfar = (coalesce(a.residfar, b.residfar)),
@@ -26,7 +37,7 @@ SET
 FROM dcp_zoning_maxfar AS b
 WHERE split_part(a.zonedist1, '/', 1) = b.zonedist;
 
--- zoning district 1 with / second part 
+-- zoning district 1 with / second part (Residential)
 UPDATE pluto a
 SET
     residfar = (coalesce(a.residfar, b.residfar)),
@@ -48,7 +59,7 @@ SET
 FROM dcp_zoning_maxfar AS b
 WHERE a.zonedist2 = b.zonedist;
 
--- zoning district 2 with / first part 
+-- zoning district 2 with / first part
 UPDATE pluto a
 SET
     residfar = (coalesce(a.residfar, b.residfar)),
@@ -59,7 +70,7 @@ SET
 FROM dcp_zoning_maxfar AS b
 WHERE split_part(a.zonedist2, '/', 1) = b.zonedist;
 
--- zoning district 2 with / second part 
+-- zoning district 2 with / second part
 UPDATE pluto a
 SET
     residfar = (coalesce(a.residfar, b.residfar)),
@@ -81,7 +92,7 @@ SET
 FROM dcp_zoning_maxfar AS b
 WHERE a.zonedist3 = b.zonedist;
 
--- zoning district 3 with / first part 
+-- zoning district 3 with / first part
 UPDATE pluto a
 SET
     residfar = (coalesce(a.residfar, b.residfar)),
@@ -92,7 +103,7 @@ SET
 FROM dcp_zoning_maxfar AS b
 WHERE split_part(a.zonedist3, '/', 1) = b.zonedist;
 
--- zoning district 3 with / second part 
+-- zoning district 3 with / second part
 UPDATE pluto a
 SET
     residfar = (coalesce(a.residfar, b.residfar)),
