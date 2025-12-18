97c91b789e6fb0fed51e587aac66f14b9b648eb0
Finn van Krieken <fvankrieken@planning.nyc.gov>
2025-12-18 13:15:41 -0500

    saf_i

---
+++ products/cscl/models/intermediate/saf/int__saf_i.sql
@@ -0,0 +1,16 @@
+WITH intersection AS (
+    SELECT * FROM {{ source('recipe_sources','dcp_cscl_namedintersection') }}
+),
+feature_names AS (
+    SELECT * FROM {{ ref('stg__facecode_and_featurename') }}
+)
+SELECT
+    feature_names.saf_place_name AS place_name,
+    intersection.nodeid,
+    CASE WHEN intersection.multiplefield = 'Y' THEN 'M' END AS multiplefield,
+    intersection.b7sc AS b7sc_intersection,
+    intersection.b5sc_cross1,
+    intersection.b5sc_cross2,
+    'I' AS saftype
+FROM intersection
+LEFT JOIN feature_names ON intersection.b7sc = feature_names.b7sc
