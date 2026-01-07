4a5e0813be60c291efbc2e9e7369b65769a79412
Finn van Krieken <fvankrieken@planning.nyc.gov>
2026-01-07 14:19:29 -0500

    deduplicate nysdec permit records

--- products/green_fast_track/models/staging/stg__nysdec_state_facility_permits.sql
+++ products/green_fast_track/models/staging/stg__nysdec_state_facility_permits.sql
@@ -1,15 +1,19 @@
 WITH source AS (
-    SELECT *
-    FROM {{ source('recipe_sources', 'nysdec_state_facility_permits') }}
+    SELECT * FROM {{ source('recipe_sources', 'nysdec_state_facility_permits') }}
 ),
-
+deduplicated AS (
+    SELECT DISTINCT
+        dec_id,
+        facility_name,
+        geom
+    FROM source
+),
 final AS (
     SELECT
         'state_facility_permits' AS variable_type,
         dec_id AS variable_id,
         facility_name,
         ST_TRANSFORM(geom::geometry, 2263) AS permit_geom
-    FROM source
+    FROM deduplicated
 )
-
 SELECT * FROM final
--- products/green_fast_track/models/staging/stg__nysdec_title_v_facility_permits.sql
+++ products/green_fast_track/models/staging/stg__nysdec_title_v_facility_permits.sql
@@ -1,14 +1,20 @@
 WITH source AS (
-    SELECT *
-    FROM {{ source('recipe_sources', 'nysdec_title_v_facility_permits') }}
+    SELECT * FROM {{ source('recipe_sources', 'nysdec_title_v_facility_permits') }}
 ),
-
+deduplicated AS (
+    SELECT DISTINCT
+        dec_id,
+        facility_name,
+        year,
+        siccode,
+        geom
+    FROM source
+),
 final AS (
     SELECT
         'title_v_permits' AS variable_type,
         dec_id AS variable_id,
         st_transform(geom::geometry, 2263) AS permit_geom
-    FROM source
+    FROM deduplicated
 )
-
 SELECT * FROM final
