dd1653eaac80a7114e12b92492d24f176ee6cdd3
Finn van Krieken <fvankrieken@planning.nyc.gov>
2026-01-16 12:01:45 -0500

    linearize nypdbeat polygons

--- products/cscl/models/intermediate/adjacent_polygons/int__segment_nypdbeat.sql
+++ products/cscl/models/intermediate/adjacent_polygons/int__segment_nypdbeat.sql
@@ -16,11 +16,11 @@ SELECT
     left(right_beat.post, 1) AS right_nypd_service_area
 FROM segment_offsets AS co
 -- using a cte around reference can confus the postgres compiler to not use index
-LEFT JOIN {{ source("recipe_sources", "dcp_cscl_nypdbeat") }} AS left_beat
+LEFT JOIN {{ ref("stg__nypdbeat") }} AS left_beat
     ON
         st_within(co.left_offset_point, left_beat.geom)
         AND left_beat.geo_type = 'HP'
-LEFT JOIN {{ source("recipe_sources", "dcp_cscl_nypdbeat") }} AS right_beat
+LEFT JOIN {{ ref("stg__nypdbeat") }} AS right_beat
     ON
         st_within(co.right_offset_point, right_beat.geom)
         AND right_beat.geo_type = 'HP'
