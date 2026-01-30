42e2b03c7a9c15aef54ee397a820f4af450c51e4
Jack Rosacker <18175209+jackrosacker@users.noreply.github.com>
2026-01-29 23:27:01 -0500

    rough out planned fns

--- dcpy/utils/geospatial/geodatabase.py
+++ dcpy/utils/geospatial/geodatabase.py
@@ -1,3 +1,13 @@
 from pathlib import Path
 
 from dcpy.utils.geospatial.metadata import generate_metadata
+from dcpy.models.data.shapefile_metadata import Metadata
+
+
+def read_metadata() -> Metadata:
+    return Metadata()
+
+
+def write_metadata() -> None: ...
+def metadata_exists() -> None: ...
+def remove_metadata() -> None: ...
