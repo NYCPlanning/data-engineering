868e56409448f4431b516b8c9b176f82e966da5f
Finn van Krieken <fvankrieken@planning.nyc.gov>
2026-01-30 16:37:49 -0500

    qpl scraper

--- dcpy/lifecycle/connector_registry.py
+++ dcpy/lifecycle/connector_registry.py
@@ -10,7 +10,7 @@ from dcpy.connectors.edm import builds, drafts, gis, published
 from dcpy.connectors.edm.bytes import BytesConnector
 from dcpy.connectors.edm.open_data_nyc import OpenDataConnector
 from dcpy.connectors.esri.arcgis_feature_service import ArcGISFeatureServiceConnector
-from dcpy.connectors.web_scrapers import uscourts
+from dcpy.connectors.web_scrapers import uscourts, qpl
 from dcpy.connectors.hybrid_pathed_storage import (
     PathedStorageConnector,
     StorageType,
@@ -70,6 +70,7 @@ def _set_default_connectors():
         ArcGISFeatureServiceConnector(),
         web.WebConnector(),
         uscourts.USCourtsConnector(),
+        qpl.QPLConnector(),
         [web.WebConnector(), "api"],
         [filesystem.Connector(), "local_file"],
         [s3.S3Connector(), "s3"],
