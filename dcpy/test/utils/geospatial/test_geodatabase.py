7f864e8e89e187cd65e3028bbc3ccd91a254ffff
Jack Rosacker <18175209+jackrosacker@users.noreply.github.com>
2026-02-02 16:31:08 -0500

    imports plus dummy test

--- dcpy/test/utils/geospatial/test_geodatabase.py
+++ dcpy/test/utils/geospatial/test_geodatabase.py
@@ -1,6 +1,16 @@
 from dcpy.utils.geospatial.metadata import generate_metadata
+from dcpy.utils.geospatial.geodatabase import (
+    read_metadata,
+    write_metadata,
+    metadata_exists,
+    remove_metadata,
+)
 from pytest import fixture
+from pathlib import Path
 
 
-def test_():
+def test_read_metadata():
+    gdb = Path("my_dev_files/data/gdal_cli_test/test_db.gdb")
+    layer = "test"
+    print(f"{read_metadata(gdb, layer)=}")
     assert False
