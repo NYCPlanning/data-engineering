9c7e1f16611356ea8453a5915b620a2acf0954e2
Damon McCullough <damonmcc1391@gmail.com>
2026-02-20 14:19:10 -0500

    refresh distribution notebook

--- notebooks/marimo/lifecycle/distribution/bytes_socrata_versions.py
+++ notebooks/marimo/lifecycle/distribution/bytes_socrata_versions.py
@@ -1,38 +1,49 @@
 import marimo
 
-__generated_with = "0.18.3"
+__generated_with = "0.19.7"
 app = marimo.App(width="full")
+
+with app.setup:
+    import marimo as mo
+
+    from dcpy.lifecycle.scripts import version_compare
 
 
 @app.cell(hide_code=True)
-def _(mo):
+def _():
     mo.md(r"""
     # Socrata <> Bytes version comparison tracker
+    """)
+    return
 
-    FYI, this might take a minute or two to run.
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    ## Helpful links
+    - Github action to distribute from Bytes to Open Data: https://github.com/NYCPlanning/data-engineering/actions/workflows/distribute_socrata_from_bytes.yml
+    - Open Data page to sign in and publish revisions: https://opendata.cityofnewyork.us/
+    - Product Metadata repo: https://github.com/NYCPlanning/product-metadata
     """)
     return
 
 
 @app.cell
-def _():
-    from dcpy.lifecycle.scripts import version_compare
+def _(versions):
+    mo.ui.table(versions.reset_index(), page_size=25, selection=None)
+    return
 
-    versions = version_compare.run()
+
+@app.cell(hide_code=True)
+def _():
+    with mo.status.spinner(title="Fetching versions from Bytes and Open Data ..."):
+        versions = version_compare.run()
     return (versions,)
 
 
 @app.cell
-def _(versions):
-    versions
-    return
-
-
-@app.cell
 def _():
-    import marimo as mo
-
-    return (mo,)
+    return
 
 
 if __name__ == "__main__":
