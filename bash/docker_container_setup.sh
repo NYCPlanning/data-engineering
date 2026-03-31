30d9571518d8b52a66c4cf0372b15c28c7719087
Damon McCullough <damonmcc1391@gmail.com>
2026-03-30 23:02:46 -0400

    fix failing container setup
    error is "detected dubious ownership in repository at '/home/vscode/workspace'" due to use of devcontainer in CI

--- bash/docker_container_setup.sh
+++ bash/docker_container_setup.sh
@@ -2,18 +2,20 @@
 set -e
 
 PARENT_DIR=$(dirname "$(readlink -f "$0")")
+mkdir -p $HOME
 
 if [[ -z "$CI" ]]; then
-    export workspace=/workspace
-    option="-e"
+    # local dev
+    git config --global --add safe.directory /workspace
+    editable_install_flag="--editable"
 else
-    export workspace=/__w/data-engineering/data-engineering
+    # running in github CI
+    git config --global --add safe.directory /__w/data-engineering/data-engineering
+    # in case the devcontainer is being used
+    git config --global --add safe.directory /home/vscode/workspace
 fi 
 
-mkdir -p $HOME
-git config --global --add safe.directory $workspace
-
-python3 -m pip install $option . -c ./admin/run_environment/constraints.txt --no-deps
+python3 -m pip install $editable_install_flag . --constraint ./admin/run_environment/constraints.txt --no-deps
 
 # configure minio for use with S3 buckets in Digital Ocean
 mc alias set spaces "$AWS_S3_ENDPOINT" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY" --api S3v4
