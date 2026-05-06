665f02885ac66437a7f3279d038508881f4c73a5
Alex Richey <arichey@planning.nyc.gov>
2026-05-06 10:39:22 -0400

    Quick tweak of qa__diffs_all

--- products/cscl/models/etl_dev_qa/diffs/qa__diffs_all.sql
+++ products/cscl/models/etl_dev_qa/diffs/qa__diffs_all.sql
@@ -8,22 +8,21 @@
 -- All discrepant records across the entire CSCL project
 -- Source for burndown charts and project-wide diff tracking
 
+WITH unioned_diffs AS (
+    SELECT *
+    FROM {{ ref('qa__diffs_thinlion_summary') }}
+    UNION ALL
+    SELECT *
+    FROM {{ ref('qa__diffs_lion_dat') }}
+    UNION ALL
+    SELECT *
+    FROM {{ ref('qa__diffs_rpl') }}
+    UNION ALL
+    SELECT *
+    FROM {{ ref('qa__diffs_saf_abcegnpx_roadbed') }}
+)
+
 SELECT
     *,
     CURRENT_DATE AS diff_run_date
-FROM {{ ref('qa__diffs_thinlion_summary') }}
-UNION ALL
-SELECT
-    *,
-    CURRENT_DATE AS diff_run_date
-FROM {{ ref('qa__diffs_lion_dat') }}
-UNION ALL
-SELECT
-    *,
-    CURRENT_DATE AS diff_run_date
-FROM {{ ref('qa__diffs_rpl') }}
-UNION ALL
-SELECT
-    *,
-    CURRENT_DATE AS diff_run_date
-FROM {{ ref('qa__diffs_saf_abcegnpx_roadbed') }}
+FROM unioned_diffs
