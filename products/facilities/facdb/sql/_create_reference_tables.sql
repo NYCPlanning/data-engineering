18118e855e4dc10626ffaa8f3b2613fa90c1a688
Damon McCullough <damonmcc1391@gmail.com>
2026-05-11 23:15:05 -0400

    start using lookup tables to assign factype to sources

--- products/facilities/facdb/sql/_create_reference_tables.sql
+++ products/facilities/facdb/sql/_create_reference_tables.sql
@@ -35,3 +35,21 @@ CREATE TABLE manual_corrections (
     new_value TEXT
 );
 \COPY manual_corrections FROM 'facdb/data/manual_corrections.csv' DELIMITER ',' CSV HEADER;
+
+
+DROP TABLE IF EXISTS lookup_factype_source;
+CREATE TABLE lookup_factype_source (
+    source_name TEXT,
+    source_column TEXT,
+    source_value TEXT,
+    factype TEXT
+);
+\COPY lookup_factype_source FROM 'facdb/data/lookup_factype_source.csv' DELIMITER ',' CSV HEADER;
+
+
+DROP TABLE IF EXISTS lookup_factype;
+CREATE TABLE lookup_factype (
+    factype TEXT,
+    facsubgrp TEXT
+);
+\COPY lookup_factype FROM 'facdb/data/lookup_factype.csv' DELIMITER ',' CSV HEADER;
