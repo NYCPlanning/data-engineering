1e7eb1a5342c56f009c483658d089fc3da705869
Alex Richey <arichey@planning.nyc.gov>
2026-06-08 14:59:55 -0400

    delete unused tests and util

--- products/edde/utils/geocode.py
+++
@@ -1,6 +0,0 @@
-# This module previously contained Geosupport geocoding functions.
-# Those have been moved to products/edde/scripts/evictions_ingest/process_eviction_address_for_ingest.py
-# to keep Geosupport dependencies out of the main build pipeline.
-#
-# If you need geocoding functionality, use the geocoded dataset (doi_evictions_geocoded)
-# which is preprocessed with Geosupport before ingestion.
