55b38080f48d88ab08118533ec0e5e7c9fbde9de
Alex Richey <arichey@planning.nyc.gov>
2026-06-10 14:49:38 -0400

    kill unused packaging connector

--- dcpy/connectors/_cli.py
+++ dcpy/connectors/_cli.py
@@ -1,16 +1,12 @@
 import typer
 
-from .edm.packaging import package_app as package_app
 from .esri import arcgis_feature_service
 from .socrata import metadata
 
-edm_app = typer.Typer()
-edm_app.add_typer(package_app, name="packaging")
 
 socrata_app = typer.Typer()
 socrata_app.add_typer(metadata.app, name="metadata")
 
 app = typer.Typer()
-app.add_typer(edm_app, name="edm")
 app.add_typer(socrata_app, name="socrata")
 app.add_typer(arcgis_feature_service.app, name="esri")
