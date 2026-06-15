a5c89f2639ef4ff97fa695ba9ef89d8e9ad22b8b
Damon McCullough <damonmcc1391@gmail.com>
2026-06-15 17:18:30 -0400

    load hpd_hny from ingest parquet instead of library SQL

--- products/developments/devdb_datasync.sh
+++ products/developments/devdb_datasync.sh
@@ -5,10 +5,11 @@ install_minio
 
 function library_archive {
     set_error_traps
-    local latest_version=$(get_version ${2})
+    local ref_dataset=${2}
+    local latest_version=$(python3 -c "from dcpy.lifecycle.connector_registry import connectors; print(connectors.versioned['edm.recipes.datasets'].get_latest_version('$ref_dataset'))")
     local version=${3:-$latest_version}
     echo "version of ${1} is ${version}"
-    
+
     library archive -f templates/$1.yml -s -l -v $version
 }
 
@@ -16,6 +17,12 @@ function import {
     dataset=$1
     version=${2:-latest}
     import_recipe $dataset $version false
+}
+
+function import_ingest {
+    local dataset=$1
+    local version=$(python3 -c "from dcpy.lifecycle.connector_registry import connectors; print(connectors.versioned['edm.recipes.datasets'].get_latest_version('$dataset'))")
+    python3 -m dcpy lifecycle data_loader import -n $dataset -v "$version" -s public -d postgres
 }
 
 function output {
@@ -32,6 +39,6 @@ command="$1"
 shift
 
 case "${command}" in
-    import | output | library_archive) ${command} $@ ;;
+    import | import_ingest | output | library_archive) ${command} $@ ;;
     *) echo "${command} not found" ;;
 esac
