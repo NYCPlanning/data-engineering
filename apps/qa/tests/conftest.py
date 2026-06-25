77ee2cfe90d35416324df38dfbdd1c67387b349d
Damon McCullough <damonmcc1391@gmail.com>
2026-06-24 22:45:29 -0400

    add test importing every qa module

--- apps/qa/tests/conftest.py
+++ apps/qa/tests/conftest.py
@@ -4,3 +4,10 @@ RECIPES_BUCKET = "test-recipes"
 PUBLISHING_BUCKET = "edm-publishing"
 os.environ["RECIPES_BUCKET"] = RECIPES_BUCKET
 os.environ["PUBLISHING_BUCKET"] = PUBLISHING_BUCKET
+
+# Some dcpy modules read these at import time (gdal config, github client); provide harmless
+# defaults (setdefault so real values, when present, win) so importing app modules doesn't crash.
+os.environ.setdefault("AWS_S3_ENDPOINT", "https://example.com")
+os.environ.setdefault("AWS_ACCESS_KEY_ID", "test")
+os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "test")
+os.environ.setdefault("GHP_TOKEN", "test")
