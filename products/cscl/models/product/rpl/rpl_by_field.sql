fa3fd7376fe4fcf3bc901ebc6e8cacabdad8115f
Alex Richey <arichey@planning.nyc.gov>
2026-07-01 18:42:08 -0400

    fix RPL diffs (#2473)

--- products/cscl/models/product/rpl/rpl_by_field.sql
+++ products/cscl/models/product/rpl/rpl_by_field.sql
@@ -1,6 +1,6 @@
 SELECT
     rpl_id,
     {{ apply_text_formatting_from_seed('text_formatting__rpl') }},
-    generic_segmentid || '_' || roadbed_segmentid AS _rpl_key
+    LPAD(generic_segmentid::text, 7, '0') || '_' || LPAD(roadbed_segmentid::text, 7, '0') AS _rpl_key
 FROM {{ ref("int__rpl") }}
 ORDER BY rpl_id ASC
