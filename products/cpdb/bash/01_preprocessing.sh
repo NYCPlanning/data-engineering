89638a0cd299338bc148f5996eecacb7109de475
Damon McCullough <damonmcc1391@gmail.com>
2026-07-12 22:42:10 -0400

    use seed file for type_category_patterns

--- products/cpdb/bash/01_preprocessing.sh
+++ products/cpdb/bash/01_preprocessing.sh
@@ -2,7 +2,7 @@
 source ../../bash/utils.sh
 set_error_traps
 
-run_sql_file "sql/_create.sql"
+run_sql_command "CREATE EXTENSION IF NOT EXISTS tablefunc;"
 
 echo "fixing dot_bridges"
 run_sql_command "ALTER TABLE dot_projects_bridges RENAME COLUMN fmsid TO fms_id;"
