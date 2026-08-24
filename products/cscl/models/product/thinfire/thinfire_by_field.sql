0ea1ddb976806d40f43b623d2d13bc11de1b1f3e
Alex Richey <arichey@planning.nyc.gov>
2026-08-24 17:47:51 -0400

    CSCL: Fix thinfire keys

--- products/cscl/models/product/thinfire/thinfire_by_field.sql
+++ products/cscl/models/product/thinfire/thinfire_by_field.sql
@@ -1,6 +1,11 @@
+WITH combined AS (
+    SELECT
+        globalid,
+        {{ apply_text_formatting_from_seed('text_formatting__thinfire_dat') }}
+    FROM {{ ref('thinfire_by_field_unformatted') }}
+)
 SELECT
-    globalid,
-    {{ apply_text_formatting_from_seed('text_formatting__thinfire_dat') }},
-    globalid AS _thinfire_key
-FROM {{ ref('thinfire_by_field_unformatted') }}
+    *,
+    fire_company_type || fire_company_number AS _thinfire_key
+FROM combined
 ORDER BY fire_company_type, fire_company_number
