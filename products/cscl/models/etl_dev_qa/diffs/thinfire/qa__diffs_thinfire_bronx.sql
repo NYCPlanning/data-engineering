db1799cdaaf399e87594532dddcd2e356352520b
Alex Richey <arichey@planning.nyc.gov>
2026-08-25 11:26:03 -0400

    Exclude globalid from comparison

--- products/cscl/models/etl_dev_qa/diffs/thinfire/qa__diffs_thinfire_bronx.sql
+++ products/cscl/models/etl_dev_qa/diffs/thinfire/qa__diffs_thinfire_bronx.sql
@@ -13,6 +13,7 @@ WITH base_diffs AS (
       old_relation=ref('qa_int__prod_thinfire_bronx'),
       new_relation=ref('thinfire_bronx_by_field'),
       primary_key='_thinfire_key',
+      exclude_columns=['globalid'],
       output_file_id='thinfire_bronx',
       build_table_name='thinfire_bronx_by_field',
       production_table_name='qa_int__prod_thinfire_bronx'
