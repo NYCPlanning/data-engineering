25646da5fb1940345b8140ead988e53e9a4e94c8
Damon McCullough <damonmcc1391@gmail.com>
2026-09-12 22:34:34 -0400

    read geom for the four facdb datasets migrated to ingest

--- products/facilities/facdb/sql/pipelines/sbs_workforce1.sql
+++ products/facilities/facdb/sql/pipelines/sbs_workforce1.sql
@@ -19,7 +19,7 @@ SELECT
     'NYCSBS' AS overabbrev,
     NULL AS capacity,
     NULL AS captype,
-    wkt::geometry AS wkb_geometry,
+    geom AS wkb_geometry,
     geo_1b,
     geo_bl,
     geo_bn
