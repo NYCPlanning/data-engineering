33d6e6e0c4aef3b818474d0475864ba5ebbfae29
Damon McCullough <damonmcc1391@gmail.com>
2026-09-14 15:02:14 -0400

    derive cpp housing growth years from the dcp_housing pin

--- products/knownprojects/tests/assert_cpp_growth_totals_reconcile.sql
+++ products/knownprojects/tests/assert_cpp_growth_totals_reconcile.sql
@@ -1,20 +1,24 @@
 -- NTAs and community districts both tile the city, so their citywide totals must
 -- agree. Catches a join that matched only part of one geography's keys.
-
+{% set latest = var('cpp_latest_complete_year') | int %}
 WITH nta AS (
-    SELECT sum(units_2020) AS u2020, sum(units_2025) AS u2025
+    SELECT
+        sum(units_2020) AS u2020,
+        sum(units_{{ latest }}) AS u_latest
     FROM {{ ref('cpp_housing_growth_nta') }}
 ),
 
 cd AS (
-    SELECT sum(units_2020) AS u2020, sum(units_2025) AS u2025
+    SELECT
+        sum(units_2020) AS u2020,
+        sum(units_{{ latest }}) AS u_latest
     FROM {{ ref('cpp_housing_growth_cd') }}
 )
 
 SELECT
     nta.u2020 AS nta_units_2020,
     cd.u2020 AS cd_units_2020,
-    nta.u2025 AS nta_units_2025,
-    cd.u2025 AS cd_units_2025
+    nta.u_latest AS nta_units_{{ latest }},
+    cd.u_latest AS cd_units_{{ latest }}
 FROM nta, cd
-WHERE nta.u2020 != cd.u2020 OR nta.u2025 != cd.u2025
+WHERE nta.u2020 != cd.u2020 OR nta.u_latest != cd.u_latest
