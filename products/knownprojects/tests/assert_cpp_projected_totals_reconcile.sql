466666ac03daa6fbd19a85d28959445a7cb79a4a
Damon McCullough <damonmcc1391@gmail.com>
2026-09-14 15:02:14 -0400

    derive cpp housing growth years from the dcp_housing pin

--- products/knownprojects/tests/assert_cpp_projected_totals_reconcile.sql
+++ products/knownprojects/tests/assert_cpp_projected_totals_reconcile.sql
@@ -2,15 +2,15 @@
 -- to the same citywide total either way. Units are apportioned by largest
 -- remainder, so the parts sum to the whole and there is no rounding drift
 -- between geographies. Any difference here means something has regressed.
-
+{% set window = (var('cpp_latest_complete_year') | int + 1) ~ '_' ~ (var('cpp_latest_complete_year') | int + 10) %}
 WITH totals AS (
     SELECT
         (
-            SELECT sum(projected_completed_units_2026_2035)
+            SELECT sum(projected_completed_units_{{ window }})
             FROM {{ ref('cpp_housing_growth_nta') }}
         ) AS nta,
         (
-            SELECT sum(projected_completed_units_2026_2035)
+            SELECT sum(projected_completed_units_{{ window }})
             FROM {{ ref('cpp_housing_growth_cd') }}
         ) AS cd
 )
